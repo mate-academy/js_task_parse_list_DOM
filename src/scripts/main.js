0ce1041fe7ed45096eba3ba312bfb917f38ca2f1
Ihor Hranetskyi <graniecki20@gmail.com>
2020-07-05 10:06:03 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+function sortList(list) {
+  function toNumber(element) {
+    return Number(
+      element.dataset.salary.split('$').join('').split(',').join('.')
+    );
+  }
+
+  const sortedList = [...list].sort((a, b) => toNumber(b) - toNumber(a));
+
+  document.querySelector('ul').append(...sortedList);
+}
+
+function getEmployees(list) {
+  const callback = (arr, person) => {
+    return [
+      ...arr,
+      {
+        name: person.textContent.trim(),
+        position: person.dataset.position,
+        salary: person.dataset.salary,
+        age: +person.dataset.age,
+      },
+    ];
+  };
+  const employees = [...list].reduce(callback, []);
+
+  return employees;
+}
+
+const employeesList = [...document.querySelectorAll('li')];
+
+sortList(employeesList);
+getEmployees(employeesList);
