737d3b1ffbb41e72c2402243e5781d302f1e1b87
Liudmyla Ilchenko <liudmyla.ilchenko7@gmail.com>
2020-07-09 18:33:19 +0300

    Added list sorting

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const items = [...list.children];
+
+function convertToNumber(string) {
+  return +string.replace(/[^0-9]/gm, '');
+}
+
+function sortList(listForSorting) {
+  return listForSorting
+    .sort((item1, item2) => {
+      return (
+        convertToNumber(item2.dataset.salary)
+        - convertToNumber(item1.dataset.salary)
+      );
+    })
+    .forEach((item) => list.append(item));
+}
+
+function getEmployees(employeesArr) {
+  return employeesArr.map((item) => {
+    return {
+      name: `${item.innerText}`,
+      position: `${item.dataset.position}`,
+      salary: `${item.dataset.salary}`,
+      age: `${item.dataset.age}`,
+    };
+  });
+}
+
+sortList(items);
+getEmployees(items);
