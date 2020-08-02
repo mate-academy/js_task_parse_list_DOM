3b1d9fcad272a726b8c6e808e24b65d87921e0b0
Victor-Zinchenko-mate <65622349+Victor-Zinchenko-mate@users.noreply.github.com>
2020-08-02 21:36:25 +0300

    SolutionTask2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const listLi = document.querySelectorAll('li');
+
+function sortList(list) {
+  const sortSalary = [...list].sort((a, b) =>
+    b.dataset.salary.replace('$', '').replace(',', '.')
+ - a.dataset.salary.replace('$', '').replace(',', '.'));
+
+  document.querySelector('ul').append(...sortSalary);
+};
+sortList(listLi);
+
+function getEmployees(list) {
+  for (let i = 0; i < list.length; i++) {
+    const resultObject = {};
+    const resultArray = [];
+
+    resultObject.name = list[i].innerText;
+    resultObject.position = list[i].dataset.position;
+    resultObject.salary = list[i].dataset.salary;
+    resultObject.age = list[i].dataset.age;
+    resultArray.push(resultObject);
+  };
+};
+getEmployees(listLi);
