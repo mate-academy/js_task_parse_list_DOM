894bb6e3c68c2b058c6e01302880f624c21789ec
Victor-Zinchenko-mate <65622349+Victor-Zinchenko-mate@users.noreply.github.com>
2020-08-08 11:58:16 +0300

    SolutionReTask2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,29 @@
 'use strict';
 
-const callList = document.querySelectorAll('li');
+const callList = document.querySelector('ul');
 
 function sortList(list) {
-  const sortSalary = [...list].sort((a, b) =>
-    b.dataset.salary.replace('$', '').replace(',', '.')
- - a.dataset.salary.replace('$', '').replace(',', '.'));
+  const sortSalary = [...list.children].sort((a, b) =>
+    b.dataset.salary.replace('$', '').replace(',', '')
+ - a.dataset.salary.replace('$', '').replace(',', ''));
 
-  document.querySelector('ul').append(...sortSalary);
+  list.append(...sortSalary);
 };
 sortList(callList);
 
 function getEmployees(list) {
-  for (let i = 0; i < list.length; i++) {
-    const resultObject = {};
-    const resultArray = [];
+  const resultArray = [];
 
-    resultObject.name = list[i].innerText;
-    resultObject.position = list[i].dataset.position;
-    resultObject.salary = list[i].dataset.salary;
-    resultObject.age = list[i].dataset.age;
+  for (let i = 0; i < list.children.length; i++) {
+    const resultObject = {};
+
+    resultObject.name = list.children[i].innerText;
+    resultObject.position = list.children[i].dataset.position;
+    resultObject.salary = list.children[i].dataset.salary;
+    resultObject.age = list.children[i].dataset.age;
     resultArray.push(resultObject);
   };
+
+  return resultArray;
 };
 getEmployees(callList);
