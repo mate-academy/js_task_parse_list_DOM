cb1a142385a5e7c762f498ea4df297eec90729a9
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-08-11 19:15:06 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const employees = document.querySelectorAll('li');
+
+function sortList(employeesAll) {
+  const employeess = [...employeesAll];
+
+  employeess.sort((a, b) => {
+    return (+b.dataset.salary.split(',').join('').split('$').join(''))
+    - (+a.dataset.salary.split(',').join('').split('$').join(''));
+  });
+
+  const ulList = document.querySelector('ul');
+
+  ulList.append(...employeess);
+}
+
+sortList(employees);
+
+function getEmployees(employees2) {
+  const arrEmployees = [];
+
+  employees2.forEach(employee => {
+    const objEmployee = {};
+
+    objEmployee.name = employee.innerText;
+    objEmployee.position = employee.dataset.position;
+    objEmployee.salary = employee.dataset.salary;
+    objEmployee.age = employee.dataset.age;
+
+    return arrEmployees.push(objEmployee);
+  });
+
+  return arrEmployees;
+}
+
+getEmployees(employees);
