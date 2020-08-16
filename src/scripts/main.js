52e6d9dd0c2a7cb4812ecdc4de0998928a85a622
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-16 15:00:29 +0300

    +

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+[...document.querySelectorAll('[data-salary]')]
+  .sort((a, b) => {
+    return +b.dataset.salary.split('$').join('').split(',').join('')
+    > +a.dataset.salary.split('$').join('').split(',').join('')
+      ? 1 : -1;
+  }).forEach(li => document.querySelector('ul').append(li));
