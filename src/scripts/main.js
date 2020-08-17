bc76f3559ffc7940eb423562949ecc0ef6a00234
Serhii Antonyshyn <serginio4422@gmail.com>
2020-08-17 17:57:43 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+const workers = document.querySelectorAll('li');
+
+function sortList(array) {
+  for (let i = 0; i < array.length; i++) {
+    array[i].dataset.salary = Number(
+      array[i].dataset.salary.replace(/[$,]/g, ''));
+  };
+
+  const newArray = [...array];
+
+  newArray.sort((a, b) => {
+    return b.dataset.salary - a.dataset.salary;
+  });
+
+  return newArray;
+}
+
+const list = document.querySelector('ul');
+
+list.append(...sortList(workers));
