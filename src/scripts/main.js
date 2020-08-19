e5bc2b84ab9c92cb13f06cdf5215d6be8183e114
Artemii Kabanov <artemiikab@gmail.com>
2020-08-19 15:48:48 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('[data-salary]')];
+const employeeList = document.querySelector('ul');
+
+const sortList = function(list) {
+  list.sort((a, b) => {
+    const salary1 = Number(a.dataset.salary.slice(1).replace(/,/g, ''));
+    const salary2 = Number(b.dataset.salary.slice(1).replace(/,/g, ''));
+
+    return salary2 - salary1;
+  });
+
+  employeeList.append(...list);
+};
+
+const getEmployees = function(list) {
+  return list.map(employee => {
+    return Object.assign({}, {
+      name: employee.innerHTML,
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    });
+  });
+};
+
+sortList(employees);
+getEmployees(employees);
