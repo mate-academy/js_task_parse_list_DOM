3fa7190439fcf6552b7549e216d5dc0f0867f8ad
Elena-brv <leena.brv@gmail.com>
2020-08-20 22:12:23 +0300

    Employees

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const employees = document.querySelector('ul');
+
+function sortList(list) {
+  [...list.children]
+    .sort((empl1, empl2) =>
+      toNumber(empl2.dataset.salary) - toNumber(empl1.dataset.salary)
+    )
+    .forEach(employee => list.append(employee));
+
+  return list;
+}
+
+function toNumber(salary) {
+  return +salary.replace(/[^0-9]/gi, '');
+}
+
+function getEmployees(arr) {
+  return [...arr].map(employee => employee.innerText);
+}
+
+getEmployees(sortList(employees));
