4d2fe202562ccbf0b4dcb0180ace035cccf48e97
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-08-20 12:41:30 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,21 @@
 'use strict';
 
 const employees = Array.from(document.querySelectorAll('li'));
+const list = document.querySelector('ul');
 
-function greatePeopeList(people) {
-  return employees.map(person => ({
+function createPeopeList(people) {
+  return people.map(person => ({
     name: person.textContent.trim(),
     ...person.dataset,
   }));
 }
 
 function filterPeopeList(people) {
-  const sortedPeople = employees.sort((a, b) =>
+  people.sort((a, b) =>
     Number(b.dataset.salary.replace(/[^0-9]/gi, ''))
     - Number(a.dataset.salary.replace(/[^0-9]/gi, ''))
   );
-
-  for (let i = 0; i < employees.length; i++) {
-    document.querySelectorAll('li')[i].innerText = sortedPeople[i].innerText;
-  }
-
-  return sortedPeople;
+  list.append(...people);
 }
 
 createPeopeList(employees);
