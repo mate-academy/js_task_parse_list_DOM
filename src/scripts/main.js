7a192cae0ad48dd630f89748d79020fb6dccd40a
Anastasiia Lukina <anlukina24@gmail.com>
2020-08-21 18:00:33 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+
+function sortList(list) {
+  return [...list.children]
+    .sort((a, b) => convert(b.dataset.salary) - convert(a.dataset.salary));
+}
+
+function convert(elem) {
+  return +elem.replace('$', '').replace(',', '.');
+}
+
+function getEmployees(list) {
+  return list.map(elem => ({ ...elem.dataset }));
+}
+
+sortList(ul).map(elem => ul.append(elem));
+getEmployees(sortList(ul));
