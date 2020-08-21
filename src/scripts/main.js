161c9ae2a4bc70cb6fc9d385ed3fbdc5de1b7eeb
Andrii Hrushetskii <andrii.hrushetskii@gmail.com>
2020-08-21 22:44:21 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
+/* eslint-disable no-shadow */
 'use strict';
 
-// write code here
+function getEmployees(list) {
+  const people = [];
+
+  for (let i = 0; i < list.length; i++) {
+    const person = { ...list[i].dataset };
+
+    person.name = list[i].innerText;
+
+    person.salary = +person.salary
+      .slice(1)
+      .split(',')
+      .join('');
+
+    people.push(person);
+  }
+
+  return people;
+}
+
+function sortList(people) {
+  people.sort((a, b) => a.salary - b.salary);
+}
+
+const currentList = document.querySelectorAll('li');
+const people = getEmployees(currentList);
+
+sortList(people);
+
+// console.table(people);
