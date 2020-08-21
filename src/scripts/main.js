10044664139e5ade850dc07d7ad9db49b02d4487
Mikle25 <mikle2593@gmail.com>
2020-08-21 16:13:46 +0300

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const people = document.querySelector('ul');
+
+function sortList(peoples) {
+  [...peoples.children]
+    .sort((person1, person2) => {
+      const people1 = person1.dataset.salary.replace(/\W/gi, '');
+      const people2 = person2.dataset.salary.replace(/\W/gi, '');
+
+      return people2 - people1;
+    })
+    .forEach(person => peoples.append(person));
+}
+
+function getEmployees(peoples) {
+  return [...peoples.children].map(person => ({
+    name: person.textContent.trim(),
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+}
+
+sortList(people);
+getEmployees(people);
