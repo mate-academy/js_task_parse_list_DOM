0a7e6a05ff4239a2dccf2e90021a4963258c43ce
nanccyy <lidiapriymak@gmail.com>
2020-08-23 21:10:51 +0800

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function toNumber(input) {
+  return +input.slice(1).replace(',', '');
+};
+
+function sortList(listFromHTML) {
+  [...listFromHTML.children]
+    .sort((a, b) => toNumber(b.dataset.salary) - toNumber(a.dataset.salary))
+    .forEach(child => list.append(child));
+
+  return list;
+}
+
+function getEmployees(listOfEmployees) {
+  const employees = [];
+
+  [...listOfEmployees.children].forEach(employee =>
+    employees.push({
+      name: employee.innerText,
+      position: employee.dataset.position,
+      age: employee.dataset.age,
+      salary: employee.dataset.salary,
+    }));
+
+  return employees;
+}
+
+sortList(list);
+getEmployees(list);
