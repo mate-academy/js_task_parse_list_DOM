3de5c5c91a384a46454ecc1cc1c926b197001422
martakupka <marta-k2008@ukr.net>
2020-08-23 21:40:07 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+const employees = [...items];
+
+function sortList(list) {
+  function convertToNumber(employeeSalary) {
+    return +employeeSalary
+      .split('')
+      .filter(item => item !== '$' && item !== ',')
+      .join('');
+  }
+
+  list.sort(
+    (a, b) =>
+      convertToNumber(b.dataset.salary) - convertToNumber(a.dataset.salary)
+  );
+
+  document.querySelector('ul').append(...list);
+}
+
+function getEmployees(list) {
+  return list.map(item => {
+    return {
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    };
+  });
+}
+
+sortList(employees);
+getEmployees(employees);
