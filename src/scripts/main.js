6e60d32e4cfeddd9a02db0d88697f55ed3020137
Kostya Sabada <kostyasabada@gmail.com>
2020-08-24 16:33:41 +0300

    ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+function sortList(list) {
+  const users = [...list.children];
+
+  users.sort((userOne, userTwo) =>
+    toNumber(userTwo.dataset.salary)
+    - toNumber(userOne.dataset.salary));
+
+  list.append(...users);
+}
+
+const toNumber = (string) => +string.replace(/\D/g, '');
+
+function getEmployees(list) {
+  const users = [...list.children];
+
+  return users.map(user =>
+    ({
+      name: user.innerText,
+      position: user.dataset.position,
+      salary: user.dataset.salary,
+      age: user.dataset.age,
+    })
+  );
+}
+
+const employees = document.querySelector('ul');
+
+sortList(employees);
+
+getEmployees(employees);
