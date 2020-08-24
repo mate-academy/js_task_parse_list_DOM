1e0976d14e7290774dda41b64e180c3623d1ff43
Anton <a.v.sorokin95@gmail.com>
2020-08-24 21:51:07 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const employee = document.querySelector('ul');
+
+function sortList(employeeList) {
+  const sortListEmployees = [ ...employeeList.children ].sort((a, b) => {
+    const first = +((a.dataset.salary).split(',').join('').slice(1));
+    const second = +((b.dataset.salary).split(',').join('').slice(1));
+
+    return second - first;
+  });
+
+  employeeList.append(...sortListEmployees);
+}
+
+function getEmployees(listEmployee) {
+  const arrObjects = [];
+
+  for (const key of listEmployee.children) {
+    arrObjects.push({
+      name: key.textContent.trim(),
+      ...key.dataset,
+    });
+  }
+
+  return arrObjects;
+}
+
+sortList(employee);
+getEmployees(employee);
