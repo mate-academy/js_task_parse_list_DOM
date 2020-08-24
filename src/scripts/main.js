e3ad79dcd572a7094ba848463ce3ff4c9aeab719
sobchuksasha <sobchuksasha1995@gmail.com>
2020-08-24 18:16:39 +0300

    vers

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const people = document.querySelectorAll('li');
+const peopleList = [...people];
+
+function toNumber(str) {
+  return +str.substring(1).split(',').join('');
+}
+
+function sortList(list) {
+  const sortedList = list.sort((a, b) =>
+    toNumber(b.dataset.salary)
+    - toNumber(a.dataset.salary));
+
+  document.querySelector('ul').append(...sortedList);
+};
+
+function getEmployess(list) {
+  return list.map(item => ({
+    name: item.innerText,
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+}
+
+sortList(peopleList);
+getEmployess(peopleList);
