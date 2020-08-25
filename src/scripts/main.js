ebf81631d549a78af66200f66dfa60fdbfe36c4f
Oleg <mvp.westbrook4k@gmail.com>
2020-08-25 12:32:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write code here
+const employees = [...document.querySelectorAll('li')];
+const listResult = document.querySelector('ul');
+
+const sortList = function(list) {
+  list.sort((a, b) =>
+    (+b.dataset.salary.split(',').join('').replace('$', ''))
+    - (+a.dataset.salary.split(',').join('').replace('$', ''))
+  );
+
+  listResult.append(...list);
+};
+
+const getEmployees = function(list) {
+  return list.map(element => (
+    {
+      name: element.innerHTML.trim(),
+      ...element.dataset,
+    }
+  ));
+};
+
+sortList(employees);
+getEmployees(employees);
