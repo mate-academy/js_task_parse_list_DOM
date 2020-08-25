f24d0969616f0e6b07dd1344c71527486e395c75
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-08-25 21:39:27 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const persons = document.querySelector('ul');
+
+function sortList(list) {
+  return [...list.children].sort((a, b) =>
+    (+a.dataset.salary.replace(/[\s.,$]/g, ''))
+    - (+b.dataset.salary.replace(/[\s.,$]/g, '')))
+    .forEach(person => list.prepend(person));
+};
+
+function getEmployees(list) {
+  return [...list.children].map(person =>
+    ({
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    }));
+};
+
+getEmployees(persons);
+sortList(persons);
