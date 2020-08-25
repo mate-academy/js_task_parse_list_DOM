3108c97964356ce692bb45a389d3b01622f89a3f
tykhan <tykhanvasyl@gmail.com>
2020-08-25 15:38:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const employeesCollection = document.querySelectorAll('li');
+const employeesList = document.querySelector('ul');
+
+function getEmployees(list) {
+  return [...list].map(person => {
+    return {
+      name: person.textContent,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    };
+  });
+}
+
+function sortList(list) {
+  const sortedList = [...list].sort((curr, next) => {
+    const currSalary = Number(curr.dataset.salary.slice(1).replace(/,/g, ''));
+    const nextSalary = Number(next.dataset.salary.slice(1).replace(/,/g, ''));
+
+    return nextSalary - currSalary;
+  });
+
+  employeesList.append(...sortedList);
+}
+
+sortList(employeesCollection);
+getEmployees(employeesCollection);
