a07b3c1f192f3a9d4dd8a7cac185a41265ad1d56
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-08-25 03:52:38 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+function sortList(emploeesList) {
+  return emploeesList.sort((emploee1, emploee2) => {
+    const first = (emploee1.getAttribute('data-salary'))
+      .split('')
+      .filter(item => !isNaN(Number(item)))
+      .join('');
+
+    const second = (emploee2.getAttribute('data-salary'))
+      .split('')
+      .filter(item => !isNaN(Number(item)))
+      .join('');
+
+    return Number(second) - Number(first);
+  });
+}
+
+function getEmploees(emploeesList) {
+  return emploeesList.map(emploee => ({
+    name: emploee.innerText,
+    ...emploee.dataset,
+  }));
+}
+
+const list = [...document.querySelectorAll('li')];
+
+document.querySelector('ul').append(...sortList(list));
+
+getEmploees(list);
