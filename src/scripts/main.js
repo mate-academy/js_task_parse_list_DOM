86ed3e2fc908b80a98d127c5504f1802b18413d6
unknown <pavlo.com.5@gmail.com>
2020-08-26 11:55:08 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,24 @@
 'use strict';
 
-const employees = [...document.querySelectorAll('[data-salary]')];
+const salary = [...document.querySelectorAll('[data-salary]')];
+const employees = [...document.querySelectorAll('li')];
 
-employees
+salary
   .sort((a, b) => b.dataset.salary
     .slice(1).split(',').join('') - a.dataset.salary
     .slice(1).split(',').join(''));
 
-document.querySelector('ul').append(...employees);
+document.querySelector('ul').append(...salary);
+
+const getEmployees = (list) => {
+  return list.map(person => {
+    return {
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    };
+  });
+};
+
+getEmployees(employees);
