b7bdd4c4703430a80111cb7e42158de2307db771
Taras Sidak <tarasidak1990@gmail.com>
2020-09-30 16:32:27 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const employeesArray = [...document.querySelectorAll('li')];
+const list = document.querySelector('ul');
+
+function sortList(employees) {
+  const sortedArr = employees.sort((a, b) => convert(a) > convert(b) ? -1 : 1);
+
+  list.append(...sortedArr);
+}
+
+function convert(employee) {
+  return +employee.dataset.salary.slice(1).replaceAll(',', '');
+}
+
+function getEmployees(persons) {
+  return persons.map(person => ({
+    name: person.textContent.trim(),
+    ...person.dataset,
+  }));
+}
+
+sortList(employeesArray);
+getEmployees(employeesArray);
