19045acd0392adc8d12d0497e87515e1a0eea793
Petro <petdavy@1991gmail.com>
2020-10-06 00:52:52 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const peopleList = document.querySelectorAll('li');
+
+const getPersonSalary = person => {
+  return Number(person.dataset.salary.replace(/[$,]/g, ''));
+};
+
+function sortList(list) {
+  [...list].sort((personA, personB) => {
+    const personASalary = getPersonSalary(personA);
+    const personBSalary = getPersonSalary(personB);
+
+    if (personASalary > personBSalary) {
+      personB.before(personA);
+    }
+
+    return personASalary - personBSalary;
+  });
+}
+
+function getEmployees(list) {
+  const employeesList = [...list].map(person => {
+    return {
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    };
+  });
+
+  return employeesList;
+}
+
+sortList(peopleList);
+getEmployees(peopleList);
