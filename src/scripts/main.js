3ee6f070dac4776dbf544b06390bce15e4865273
Yazheviks <vicsergeyeva@gmail.com>
2020-10-05 15:00:48 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const listElements = document.getElementsByTagName('li');
+
+const sortedList = [...listElements].sort((prev, next) => {
+  let prevSalary = prev.getAttribute('data-salary');
+  let nextSalary = next.getAttribute('data-salary');
+
+  prevSalary = prevSalary.split(',').join('').split('$').join('');
+  nextSalary = nextSalary.split(',').join('').split('$').join('');
+
+  return nextSalary - prevSalary;
+});
+
+const sortedNames = sortedList.map(item => item.innerText);
+
+[...listElements].forEach((item, index) => {
+  item.textContent = sortedNames[index];
+});
