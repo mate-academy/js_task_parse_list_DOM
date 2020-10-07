d0ae9598eee944b81a4efbdb059dada4b32fef27
VoduDay <andriy.maniak@gmail.com>
2020-10-07 16:38:05 +0300

    functions getEmployees and and sortList

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+const employyes = [...document.querySelectorAll('li')];
+const employyesList = document.querySelector('ul');
+
+function sortList(list) {
+  list.sort((prevEmp, curEmp) => {
+    const salaryOfPrevEmp = parseInt(
+      prevEmp.dataset.salary.slice(1).split(',').join('')
+    );
+
+    const salaryOfCurEmp = parseInt(
+      curEmp.dataset.salary.slice(1).split(',').join('')
+    );
+
+    return salaryOfCurEmp - salaryOfPrevEmp;
+  });
+
+  employyesList.append(...employyes);
+}
+
+function getEmployees(list) {
+  const employeeObjects = [];
+
+  for (const employee of list) {
+    const employeeInformation = {
+      name: employee.innerHTML.trim(),
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    };
+
+    employeeObjects.push(employeeInformation);
+  }
+
+  return employeeObjects;
+}
+
+sortList(employyes);
+getEmployees(employyes);
