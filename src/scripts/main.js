44644e2bc3449d032fe7e5fd11ada54ceceb7157
Artem Lavreniuk <artemlavrenyuk@gmail.com>
2020-10-07 14:08:33 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const employees = document.querySelectorAll('li');
+const employeesList = document.querySelector('ul');
+
+const sortEmployees = (list) => {
+  const sortedList = [...list].sort((prev, curr) => {
+    const prevNumber = +prev.dataset.salary.replace(/[$,]/g, '');
+    const currNumber = +curr.dataset.salary.replace(/[$,]/g, '');
+
+    return prevNumber - currNumber;
+  });
+
+  employeesList.append(...sortedList);
+};
+
+const getEmployees = (list) => {
+  const peopleList = [];
+
+  [...list].forEach(person => {
+    peopleList.push({
+      name: person.textContent.trim(),
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    });
+  });
+
+  return peopleList;
+};
+
+sortEmployees(employees);
+getEmployees(employees);
