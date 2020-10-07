dd896ee55c3ead240503ffd7f2db9378ef7ff810
Alina <alina90565@gmail.com>
2020-10-07 16:07:48 +0300

    done parse list DOM task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write code here
+const board = document.querySelector('ul');
+const people = document.querySelectorAll('li');
+
+function sortList(list) {
+  return list.sort(({ salary: prevSalary }, { salary: nextSalary }) => {
+    return convertToNumber(nextSalary) - convertToNumber(prevSalary);
+  });
+}
+
+function getEmployees(list) {
+  return [...list]
+    .map(person => ({
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    }));
+}
+
+function convertToNumber(stringOfNumber) {
+  return +[...stringOfNumber]
+    .slice(1)
+    .join('')
+    .replace(/,/g, '');
+}
+
+const peopleMap = getEmployees(people);
+
+sortList(peopleMap);
+
+board.innerHTML = `
+  ${peopleMap.map(({ name, position, salary, age }) => `
+    <li
+      data-position="${position}"
+      data-salary="${salary}"
+      data-age="${age}"
+    >
+      ${name}
+    </li>
+  `).join('')}
+`;
