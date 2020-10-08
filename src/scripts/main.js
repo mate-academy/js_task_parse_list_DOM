a008986129e19e4f8876dcc235be9157908eedc6
Arthur Bespalyi <53428866+zkhomer@users.noreply.github.com>
2020-10-08 13:48:20 +0300

    Update main.js
    fix format

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,13 @@ const employees = [...document.querySelectorAll('li')];
 const listResult = document.querySelector('ul');
 
 const sortList = (list) => {
-  list.sort((a, b) => (+b.dataset.salary.split(',').join('').replace('$', ''))
-    - (+a.dataset.salary.split(',').join('').replace('$', ''))
+  list.sort(
+    (a, b) => (+b.dataset.salary.split(',')
+      .join('')
+      .replace('$', ''))
+    - (+a.dataset.salary.split(',')
+      .join('')
+      .replace('$', ''))
   );
 
   listResult.append(...list);
