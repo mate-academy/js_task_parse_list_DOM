ade5a895de4837e47165c5dfc2788d90eef02690
Yazheviks <vicsergeyeva@gmail.com>
2020-10-08 13:57:34 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,15 @@ const ul = document.querySelector('ul');
 
 function sortList(list) {
   list.sort(
-    (prev, next) =>  toNumber(next.dataset.salary) - toNumber(prev.dataset.salary);
+    (prev, next) => {
+      return toNumber(next.dataset.salary) - toNumber(prev.dataset.salary);
+    }
   );
 
   ul.append(...listElements);
 }
 
-function convertToNumber(string) {
+function toNumber(string) {
   return Number(string.split(',').join('').split('$').join(''));
 }
 
@@ -19,14 +21,6 @@ function getEmployees(list) {
   const listArray = [];
 
   for (let i = 0; i < listElements.length; i++) {
-    const person = list[i];
-    const personToObject = {
-      name: person.innerText,
-      position: person.dataset.position,
-      salary: person.dataset.salary,
-      age: person.dataset.age,
-    };
-
     listArray.push(
       {
         name: list[i].innerText,
