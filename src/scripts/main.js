9b978cb4d4abd278857ef7b43aed9f85e03dd078
Arthur Bespalyi <53428866+zkhomer@users.noreply.github.com>
2020-10-08 13:16:07 +0300

    Update main.js
    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,7 @@ const employees = [...document.querySelectorAll('li')];
 const listResult = document.querySelector('ul');
 
 const sortList = (list) => {
-  list.sort(
-    (a, b) => (+b.dataset.salary.split(',').join('').replace('$', ''))
+  list.sort((a, b) => (+b.dataset.salary.split(',').join('').replace('$', ''))
     - (+a.dataset.salary.split(',').join('').replace('$', ''))
   );
 
