594dfc57b7481d5b467a326757d7399da3d3cd82
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-10-10 14:04:07 +0300

    solution list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const salary = [...document.querySelectorAll('li')];
+const listUl = document.querySelector('ul');
+
+function sortSalary(list) {
+  list.sort((a, b) => {
+    return toNumber(b.dataset.salary) - toNumber(a.dataset.salary);
+  });
+
+  listUl.append(...list);
+}
+
+function toNumber(num) {
+  return num.match(/[0-9]/g).join('');
+}
+
+function createPerson(person) {
+  const allPerson = [];
+
+  for (const key of person) {
+    const personInfo = {
+      name: key.innerHTML.trim(),
+      position: key.dataset.position,
+      salary: key.dataset.salary,
+      age: key.dataset.age,
+    };
+
+    allPerson.push(personInfo);
+  }
+
+  return allPerson;
+}
+
+sortSalary(salary);
+createPerson(salary);
