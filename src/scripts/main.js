aac1981a1e4bc8025e3d751c44ca411668bc7ce3
dmitry <akulinind@gmail.com>
2020-10-12 17:18:12 +0300

    implement sort and get method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('li')];
+const employList = document.querySelector('ul');
+
+function sortList(list) {
+  list.sort((a, b) => convertSalary(b) - convertSalary(a));
+  employList.append(...list);
+};
+
+function convertSalary(elem) {
+  const result = elem.dataset.salary.replaceAll(',', '').substring(1);
+
+  return result;
+}
+
+function getEmployees(list) {
+  const result = list.map(elem => ({
+    name: elem.textContent,
+    position: elem.dataset.position,
+    salary: elem.dataset.salary,
+    age: elem.dataset.age,
+  })
+  );
+
+  return result;
+}
+
+sortList(employees);
+getEmployees(employees);
