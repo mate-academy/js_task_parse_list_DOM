942b68e66314d51fa73e81f3e5800fe1aa3e4889
goncharukop <goncharukop@gmail.com>
2020-10-12 22:12:55 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('li')];
+const employeesList = [];
+
+function salaryToNumber(salary) {
+  return salary.replace('$', '').split(',').join('');
+}
+
+function sortBySalary(list, array) {
+  list.sort((a, b) =>
+    salaryToNumber(b.dataset.salary) - salaryToNumber(a.dataset.salary));
+
+  array.push(...list);
+}
+
+function newEmployeesList(list) {
+  list.map(person => {
+    return {
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    };
+  });
+}
+
+sortBySalary(employees, employeesList);
+newEmployeesList(employeesList);
