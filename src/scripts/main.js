1addcb8bc0e74224fd04cd98161b145a19c0ed73
Ivan Beletskiy <beletsky02@gmail.com>
2020-10-12 14:06:37 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const employeeList = document.querySelector('ul');
+const employees = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  // eslint-disable-next-line max-len
+  list.sort((a, b) => makeClearSalaryNumber(b.dataset.salary) - makeClearSalaryNumber(a.dataset.salary));
+  employeeList.append(...list);
+}
+
+function makeClearSalaryNumber(num) {
+  return num.split(',').join('').replace('$', '');
+}
+
+function getEmployees(list) {
+  return list.map(employee => {
+    return {
+      name: employee.innerHTML.trim(),
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    }
+  });
+}
+
+sortList(employees);
+getEmployees(employees);
