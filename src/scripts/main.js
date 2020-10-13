4e6cab044807467b49c00ba1316a4137a0cb6fa4
Evgeniy <more.inspirationn@gmail.com>
2020-10-13 14:34:04 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const listDocument = document.querySelectorAll('li');
+
+function sortList(list) {
+  const listPersonWistSalary = [...list].map(
+    elem => ({
+      'name': elem.innerText,
+      'salary': +(elem.getAttribute('data-salary')
+        .replace(/,/g, '').slice(1)),
+    })
+  );
+
+  return listPersonWistSalary.sort((a, b) => b.salary - a.salary);
+}
+
+function getEmployees(list) {
+  return [...list].map(
+    elem => ({
+      'name': elem.innerText,
+      'position': elem.getAttribute('data-position'),
+      'salary': elem.getAttribute('data-salary'),
+      'age': +elem.getAttribute('data-age'),
+    })
+  );
+}
+
+sortList(listDocument);
+getEmployees(listDocument);
