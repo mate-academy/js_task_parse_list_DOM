4dc6732571ea3f6006b92bf7a311e1db29613581
38063 <xeilen1112@gmail.com>
2020-10-14 17:51:23 +0300

    -solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const employeesList = [...document.querySelectorAll('li')];
+
+const listOfPeople = document.querySelector('ul');
+
+sortList(employeesList);
+getEmployees(employeesList);
+
+function sortList(list) {
+  // eslint-disable-next-line max-len
+  list.sort((a, b) => convertToNumber(b.dataset.salary) - convertToNumber(a.dataset.salary));
+  listOfPeople.append(...list);
+}
+
+function getEmployees(list) {
+  return list.map(person => ({
+    name: person.innerText,
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+}
+
+function convertToNumber(string) {
+  return Number(string.replace(/\D+/g, ''));
+}
