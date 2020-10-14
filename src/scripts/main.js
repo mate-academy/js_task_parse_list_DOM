1cd07af58e6062271f2ffe83e559954327274205
KhrystynaYelyseyeva <play1409@gmail.com>
2020-10-14 16:20:54 +0300

    sort list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const liList = document.querySelectorAll('li');
+const convertLiList = [...liList]
+  .map(li => ({
+    salary: +li.dataset.salary.replace(/(\$)|(,)/g, ''),
+    position: li.dataset.position,
+    age: li.dataset.age,
+    name: li.textContent,
+  }))
+  .sort((previousValue, nextValue) => nextValue.salary - previousValue.salary);
+
+for (let i = 0; i < liList.length; i++) {
+  liList[i].dataset.position = convertLiList[i].position;
+  liList[i].dataset.age = convertLiList[i].age;
+  liList[i].dataset.salary = '$' + convertLiList[i].salary.toString();
+  liList[i].textContent = convertLiList[i].name;
+}
