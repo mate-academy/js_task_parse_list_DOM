06d0d046099f549de0b947a8d5fe07c0fea20243
Yuriy Putsan <putsan57@gmail.com>
2020-10-14 13:26:26 +0300

    sorted the list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const listOfEmployees = document.querySelector('ul');
+const parsedListOfEmployees = [ ...listOfEmployees.children ];
+
+function strToNumber(string) {
+  return string.replace(/,|\$/g, '');
+}
+
+function sortList(list) {
+  return list
+    .sort((employeeA, employeeB) => (
+      strToNumber(employeeB.dataset.salary)
+      - strToNumber(employeeA.dataset.salary)
+    ));
+}
+
+function getEmployees(fixedList) {
+  fixedList.forEach(item => item.parentNode.appendChild(item));
+}
+
+const sortedList = sortList(parsedListOfEmployees);
+
+getEmployees(sortedList);
