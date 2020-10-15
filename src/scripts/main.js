17f9594e3b36e4b75f1a6dc0a33d6df3e24ce685
KirillLutsenko <mnrovski68@gmail.com>
2020-10-15 13:58:28 +0300

    Parse List DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('li')];
+const employList = document.querySelector('ul');
+
+function salaryInNumber(salary) {
+  return Number(salary.replace('$', '').split(',').join(''));
+}
+
+function sortList(list) {
+  return [...list].sort((a, b) =>
+    salaryInNumber(b.dataset.salary) - salaryInNumber(a.dataset.salary));
+}
+
+function getEmployees(list) {
+  const employeesArray = [];
+
+  for (const person of list) {
+    const personInfo = {
+      name: person.innerHTML.trim(),
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    };
+
+    employeesArray.push(personInfo);
+  }
+
+  return employeesArray;
+}
+
+employList.append(...sortList(employees));
+getEmployees(employees);
