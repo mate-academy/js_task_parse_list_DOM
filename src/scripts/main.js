9ddbb0da3435088fc0c6758f11a77fc9446195b2
Olena kuzii <okuziy@gmail.com>
2020-10-15 17:52:46 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+// const list = document.querySelector('ul');
+// const listItems = [...document.querySelectorAll('li')];
+
+// function toNumber(string) {
+//   return string.replace('$', '').replace(',', '');
+// }
+
+// function sortList(listSalary) {
+//   listSalary.sort((el1, el2) =>
+//     toNumber(el1.dataset.salary) - toNumber(el2.dataset.salary));
+//   list.append(...listSalary);
+// }
+
+const list = document.querySelector('ul');
+const listItems = [...document.querySelectorAll('li')];
+
+function toNumber(string) {
+  return string.replace('$', '').replace(',', '');
+}
+
+function sortBySalary(listSalary) {
+  listSalary.sort((el1, el2) =>
+    toNumber(el2.dataset.salary) - toNumber(el1.dataset.salary));
+  list.append(...listSalary);
+}
+
+function getEfficiees(property) {
+  return property.map(person => {
+    person.name = person.textContent;
+    person.position = person.dataset.position;
+    person.salary = person.dataset.salary;
+    person.age = person.dataset.age;
+  });
+};
+
+sortBySalary(listItems);
+getEfficiees(listItems);
