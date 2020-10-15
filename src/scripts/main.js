b861db48e89e85e69b196c4b3c92b264e5b239af
milla-romankova <milla-romankova@gmail.com>
2020-10-15 12:17:55 +0300

    add js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const listOfEmployees = document.querySelector('ul');
+const employees = [...document.querySelectorAll('li')];
+
+function convertToNumber(salary) {
+  return Number(salary.replace('$', '').split(',').join(''));
+}
+
+function sortList(list) {
+  list.sort((a, b) =>
+    convertToNumber(b.dataset.salary) - convertToNumber(a.dataset.salary));
+  listOfEmployees.append(...list);
+};
+
+function getEmployees(list) {
+  return list.map(employee => (
+    {
+      name: employee.innerText,
+      ...employee.dataset,
+    }
+  ));
+};
+
+sortList(employees);
+getEmployees(employees);
