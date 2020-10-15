0a77da5f562e4cc5b24404fae49708392b3b7cbb
Lancaelot <Lancaelot@gmail.com>
2020-10-15 21:55:08 +0300

    reworked

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,26 +6,26 @@ const employees = [...employeesList.children];
 function sortBySalary(list) {
   list.sort(
     (a, b) =>
-      +b.dataset.salary.replace(/[^\d]/g, '')
-      - +a.dataset.salary.replace(/[^\d]/g, '')
+      +deleteSymbols(b.dataset.salary)
+      - +deleteSymbols(a.dataset.salary)
   );
   employeesList.append(...list);
 }
 
 function getEmployees(list) {
-  /* eslint-disable */
   return list.map(person => {
-    person = {
+    return {
       name: person.textContent.trim(),
       position: person.dataset.position,
       salary: person.dataset.salary,
       age: person.dataset.age,
     };
-  /* eslint-enable */
-
-    return person;
   });
 };
 
 sortBySalary(employees);
 getEmployees(employees);
+
+function deleteSymbols(string) {
+  return string.replace(/[^\d]/g, '');
+}
