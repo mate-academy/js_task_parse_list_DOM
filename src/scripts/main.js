52566b6515a952713348f1e66f5791a75aa7b1f3
Liza Krasnoshchok <nlight1122@gmail.com>
2020-10-15 18:25:02 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const listOfPeople = document.querySelector('ul');
+
+function getSalary(element) {
+  return +(element.dataset.salary.replace(/\$|,/g, ''));
+}
+
+function sortList(list) {
+  const sortedList = [...list.children]
+    .sort((a, b) => getSalary(b) - getSalary(a));
+
+  list.innerHTML = '';
+  sortedList.forEach(element => list.appendChild(element));
+}
+
+function getEmployees(list) {
+  return [...list.children].map(person => {
+    return {
+      name: person.textContent.trim(),
+      position: person.dataset.position,
+      salary: getSalary(person),
+      age: +person.dataset.age,
+    };
+  });
+}
+
+sortList(listOfPeople);
+getEmployees(listOfPeople);
