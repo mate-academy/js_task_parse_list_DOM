2b387e3052217be4781a65d278e754dbafc037d4
Lancaelot <Lancaelot@gmail.com>
2020-10-15 20:37:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+const employees = [...employeesList.children];
+
+function sortBySalary(list) {
+  list.sort(
+    (a, b) =>
+      +b.dataset.salary.replace(/[^\d]/g, '')
+      - +a.dataset.salary.replace(/[^\d]/g, '')
+  );
+  employeesList.append(...list);
+}
+
+function getEmployees(list) {
+  /* eslint-disable */
+  return list.map(person => {
+    person = {
+      name: person.textContent.trim(),
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    };
+  /* eslint-enable */
+
+    return person;
+  });
+};
+
+sortBySalary(employees);
+getEmployees(employees);
