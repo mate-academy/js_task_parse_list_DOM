07ee9c2af34e0638de328d7efeb34c261332837c
losanov <losanov@ukr.net>
2020-10-16 15:21:06 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const employees = document.querySelector('ul');
+
+function toNumber(text) {
+  return text.replace(/\D/g, '');
+}
+
+function sortList(list) {
+  const sorted = [...list.children].sort((a, b) =>
+    toNumber(b.dataset.salary) - toNumber(a.dataset.salary));
+
+  employees.append(...sorted);
+}
+
+function getEmployees(list) {
+  return [...list.children].map(person => ({
+    name: person.innerText,
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+}
+
+sortList(employees);
+getEmployees(employees);
