efc8faa0705e998b51eb2dc6c128e72448b2af44
Eugene <evolokhin@gmail.com>
2020-10-16 14:09:55 +0300

    fixed linter, add map

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,30 +6,28 @@
 const employees = [...document.querySelectorAll('li')];
 
 function sortlist(list) {
-
   function convertToNumber(str) {
     const cleanStr = str.replace(/,|\$/g, '');
+
     return Number(cleanStr);
   }
 
-  list.sort((a,b) => convertToNumber(b.dataset.salary) - convertToNumber(a.dataset.salary));
+  list.sort((a, b) =>
+    convertToNumber(b.dataset.salary) - convertToNumber(a.dataset.salary)
+  );
 
   document.querySelector('ul').append(...list);
 }
 
 function getEmployees(list) {
-  const employeesArr = [];
-
-  for (let person of list) {
-    const personData = {
+  const employeesArr = list.map(person => {
+    return {
       name: person.innerHTML.trim(),
       position: person.dataset.position,
       salary: person.dataset.salary,
       age: person.dataset.age,
-    }
-
-    employeesArr.push(personData);
-  }
+    };
+  });
 
   return employeesArr;
 }
