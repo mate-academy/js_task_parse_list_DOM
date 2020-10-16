491a1cf92e19092a64c0ecdf96b7dffb794cfb19
TishchenkoSergey <tishchenko.seriy@gmail.com>
2020-10-16 11:38:45 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,18 @@
 const arrEmployees = document.querySelector('ul');
 
 function stringToNumber(string) {
-  return +string.replace(',', '');
+  return +string.replace(',', '').slice(1);
 }
 
 function sortToSalary(list) {
-  return [ ...list.humans ]
+  return [ ...list.children ]
     .sort((a, b) =>
       stringToNumber(b.dataset.salary) - stringToNumber(a.dataset.salary))
     .forEach(person => list.append(person));
 }
 
 function getEmployees(list) {
-  return list.humans.map(
+  return list.children.map(
     person => ({
       name: person.innerText,
       position: person.dataset.position,
