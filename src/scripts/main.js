93818ca323d06b877677d860048c714d3b73bff2
OnlyRaydek <bayramsaid00@gmail.com>
2020-10-16 13:27:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const listBlock = document.querySelector('ul');
+const listOfPeople = document.querySelectorAll('li');
+const listArray = Array.from(listOfPeople);
+
+const personSalary = (el) => (
+  (el.dataset.salary)
+    .slice(1)
+    .split(',')
+    .join('')
+);
+
+const sortList = (list) => {
+  list.sort((a, b) => personSalary(b) - personSalary(a));
+
+  return listBlock.append(...list);
+};
+
+const getEmployees = (list) => {
+  const employeesList = [];
+
+  list.forEach(person => (
+    employeesList.push({
+      name: person.textContent.trim(),
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    })
+  ));
+
+  return employeesList;
+};
+
+sortList(listArray);
+getEmployees(listArray);
