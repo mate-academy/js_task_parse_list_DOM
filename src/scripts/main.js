7737727bbf87782de9c43bca21737da75d7a2d6d
Liza <elizabethhoncharenko@gmail.com>
2020-10-16 12:52:28 +0300

    fixed after student review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,19 +5,18 @@ const employee = document.getElementsByTagName('li');
 const arrayOfEmployee = [...employee];
 
 function sortList(list) {
-  list
-    .sort((prev, cur) => (
-      convert(cur.dataset.salary) - convert(prev.dataset.salary)
-    ))
+  list.sort((prev, cur) => (
+    convert(cur.dataset.salary) - convert(prev.dataset.salary)
+  ))
     .map(person => listOfEmployee.append(person));
 }
 
 function convert(string) {
-  return string.replace(/\$|,/g, '');
+  return +string.replace(/\$|,/g, '');
 }
 
 function getEmployees(list) {
-  list.map(human => {
+  return list.map(human => {
     return {
       name: human.textContent.trim(),
       position: human.dataset.position,
@@ -27,5 +26,5 @@ function getEmployees(list) {
   });
 }
 
-getEmployees(arrayOfEmployee);
 sortList(arrayOfEmployee);
+getEmployees(arrayOfEmployee);
