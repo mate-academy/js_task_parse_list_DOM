01cfb4041e76bdb328c8c132340b9271ba978ae4
TishchenkoSergey <tishchenko.seriy@gmail.com>
2020-10-16 11:23:54 +0300

    Parse list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const arrEmployees = document.querySelector('ul');
+
+function stringToNumber(string) {
+  return +string.replace(',', '');
+}
+
+function sortToSalary(list) {
+  return [ ...list.humans ]
+    .sort((a, b) =>
+      stringToNumber(b.dataset.salary) - stringToNumber(a.dataset.salary))
+    .forEach(person => list.append(person));
+}
+
+function getEmployees(list) {
+  return list.humans.map(
+    person => ({
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: +person.dataset.age,
+    })
+  );
+}
+
+sortToSalary(arrEmployees);
+getEmployees(arrEmployees);
