fc04697e1fa25455b05ba57f30237860ac7d5f38
Taras Shchadylo <tarasdidg@gmail.com>
2020-10-20 20:45:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const listEmployees = document.querySelector('ul');
+const employees = [...listEmployees.children];
+
+function toNumber(salary) {
+  return Number(salary.replace(/,|\$/g, ''));
+}
+
+function sortList(list) {
+  return [...list.children]
+    .sort((a, b) => toNumber(b.dataset.salary) - toNumber(a.dataset.salary))
+    .forEach(node => list.append(node));
+}
+
+function getEmployees(list) {
+  return list.map(elem => (
+    {
+      'name': elem.innerText,
+      ...elem.dataset,
+    })
+  );
+}
+
+sortList(listEmployees);
+getEmployees(employees);
