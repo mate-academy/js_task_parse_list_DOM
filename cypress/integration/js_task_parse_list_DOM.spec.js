e3ffc5fbad87d0e3073420f2679fa4e1f8b65848
Ihor Oliinyk <i.oliinyk@mate.academy>
2020-11-09 18:44:00 +0200

    add tests

---
+++ cypress/integration/js_task_parse_list_DOM.spec.js
@@ -0,0 +1,53 @@
+'use strict';
+
+describe('Actions', () => {
+  beforeEach(() => {
+    cy.visit('index.html');
+  });
+
+  it('should render list', () => {
+    cy.get('ul');
+  });
+
+  it('list should have 11 items', () => {
+    cy.get('ul > li')
+      .then(($ul) => {
+        expect($ul, '11 items').to.have.length(11);
+      });
+  });
+
+  it('1st item should be Vivian Harrell', () => {
+    cy.get('ul > li')
+      .then(($ul) => {
+        expect($ul.eq(0), 'first item').to.contain('Vivian Harrell');
+      });
+  });
+
+  it('2nd item should be Rhona Davidson', () => {
+    cy.get('ul > li')
+      .then(($ul) => {
+        expect($ul.eq(1), 'second item').to.contain('Rhona Davidson');
+      });
+  });
+
+  it('5th item should be Michael Bruce', () => {
+    cy.get('ul > li')
+      .then(($ul) => {
+        expect($ul.eq(4), 'fifth item').to.contain('Michael Bruce');
+      });
+  });
+
+  it('10th item should be Serge Baldwin', () => {
+    cy.get('ul > li')
+      .then(($ul) => {
+        expect($ul.eq(9), 'tenth item').to.contain('Serge Baldwin');
+      });
+  });
+
+  it('11th item should be Thor Walton', () => {
+    cy.get('ul > li')
+      .then(($ul) => {
+        expect($ul.eq(10), 'eleventh item').to.contain('Thor Walton');
+      });
+  });
+});
