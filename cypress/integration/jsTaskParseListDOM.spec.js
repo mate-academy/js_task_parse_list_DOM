2b00914ec926ef7671df51df96588ec59472bd58
Ihor Oliinyk <i.oliinyk@mate.academy>
2020-11-25 11:56:07 +0200

    fix2

--- cypress/integration/jsTaskParseListDOM.spec.js
+++ cypress/integration/jsTaskParseListDOM.spec.js
@@ -17,16 +17,27 @@ describe('List', () => {
   it('should be sorted by salary DSC', () => {
 
     cy.get('ul > li').then(($ul) => {
+     
       const list = [...$ul].map((row) => row.dataset.salary);
-      const sortedlist = [...list].sort((a,b) => b - a);
+      const listCopy = [...list];
       let counter = 0;
+      const arrCopy = [];
+      const sortedList = [];
 
       for (let i = 0; i < list.length; i++) {
-        if (list[i] === sortedlist[i]) {
+        arrCopy.push(list[i].replace('$', '').replace(',', ''))
+        sortedList.push(listCopy[i].replace('$', '').replace(',', ''))
+      }
+
+      sortedList.sort((a,b) => b - a);
+
+      for (let i = 0; i < arrCopy.length; i++) {
+        if (arrCopy[i] === sortedList[i]) {
           counter += 1;
         }
       }
-      expect(counter).to.equal(list.length);
+
+      expect(counter).to.equal(arrCopy.length);
     });
   });
 });
