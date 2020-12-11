9577ab099ec8cdd0ea0ec4c6f0b99d3e114e1163
AndrewKreshchenko <mail2andyk@gmail.com>
2020-12-11 22:43:02 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+function formatSalary(str) {
+  return Number(str.replace(/,|\$/g, ''));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sortBySalary = (elems) => {
+    return [...elems].sort((prev, next) =>
+      formatSalary(next.dataset.salary) - formatSalary(prev.dataset.salary));
+  };
+
+  const getEmployees = (elems) => {
+    const result = [];
+    const arr = [...elems];
+
+    arr.forEach(elem => {
+      result.push({
+        name: elem.innerText,
+        position: elem.innerText,
+        salary: elem.dataset.salary,
+        age: elem.dataset.age,
+      });
+    });
+
+    return result;
+  };
+
+  const list = document.getElementsByTagName('ul')[0];
+
+  const sorted = sortBySalary([...list.children]);
+
+  sorted.forEach((elem) => {
+    list.append(elem);
+  });
+
+  getEmployees(sorted);
+});
