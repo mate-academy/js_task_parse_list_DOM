a9e8ba1692c3d62b2b4ba9b55aef0876d601dd67
Serhii Alieksieievych <kajot1863@gmail.com>
2020-12-31 00:17:08 +0200

    Completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const items = document.querySelectorAll('li');
+
+function sortList(l) {
+  const arr = [...l];
+
+  for (const item of arr) {
+    item.validSalary = item.dataset.salary.replace(/\D/g, '');
+  }
+
+  arr.sort((a, b) => {
+    return b.validSalary - a.validSalary;
+  });
+
+  list.append(...arr);
+}
+
+function getEmployees(l) {
+  return [...l];
+}
+
+sortList(items);
+getEmployees(items);
