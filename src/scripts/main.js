f544f3179a1481af558eec787ddaec45211e2ee5
vovador <vovadorko@gmail.com>
2020-12-30 14:20:59 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write code here
+const people = document.querySelector('ul');
+
+function MoneyStringToNumber(string) {
+  return string.slice(1).split(',').join('');
+}
+
+function sortList(list) {
+  const listChildrenArray = [...list.children];
+
+  return listChildrenArray.sort((prChild, curChild) => {
+    const prChildSalary = MoneyStringToNumber(prChild.dataset.salary);
+    const curChildSalary = MoneyStringToNumber(curChild.dataset.salary);
+
+    return curChildSalary - prChildSalary;
+  });
+}
+
+const sortedPeopleList = sortList(people);
+
+people.append(...sortedPeopleList);
+
+function getEmployees(list) {
+  const listChildrenArray = [...list.children];
+  const peopleObjArray = [];
+
+  for (let i = 0; i < listChildrenArray.length; i++) {
+    peopleObjArray.push(
+      {
+        name: listChildrenArray[i].innerText,
+        position: listChildrenArray[i].dataset.position,
+        salary: listChildrenArray[i].dataset.salary,
+        age: listChildrenArray[i].dataset.age,
+      }
+    );
+  }
+
+  return peopleObjArray;
+}
+
+getEmployees(people);
+sortList(people);
