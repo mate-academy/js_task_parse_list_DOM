4e448e6a40b57808da52d7a06187472376971c65
Your Name <mogolgogol.90@gmail.com>
2020-12-30 12:08:17 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write code here
+const parsed = { ...document.querySelectorAll('li') };
+
+function sortList(list) {
+  const people = [];
+
+  for (const person in list) {
+    people.push({
+      name: list[person].innerText,
+      position: list[person].dataset.position,
+      salary: +(list[person].dataset.salary).slice(1).replace(/,/g, ''),
+      age: list[person].dataset.age,
+    });
+  }
+
+  const sortedBySalary = [ ...people ].sort((a, b) => +b.salary - +a.salary);
+
+  for (const coworker in list) {
+    list[coworker].innerText = sortedBySalary[coworker].name;
+    list[coworker].dataset.position = sortedBySalary[coworker].position;
+    list[coworker].dataset.salary = sortedBySalary[coworker].salary;
+    list[coworker].dataset.age = sortedBySalary[coworker].age;
+  }
+
+  return sortedBySalary;
+}
+sortList(parsed);
+
+function getEmployees(list) {
+  const people = [];
+
+  for (const person in list) {
+    people.push({
+      name: list[person].innerText,
+      position: list[person].dataset.position,
+      salary: list[person].dataset.salary,
+      age: list[person].dataset.age,
+    });
+  }
+
+  return people;
+}
+getEmployees(parsed);
