a033557b0929c3669e1465297451f24e68f5062e
Andriy Alieksieienko <andriy.alieksieienko@gmail.com>
2021-01-03 17:20:49 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write code here
+const employees = document.querySelector('ul');
+
+function sortList(list) {
+  const employeesArr = [...list.children];
+
+  employeesArr.forEach((employee) => {
+    employee.dataset.salary = employee.dataset.salary.slice(1).replace(',', '');
+  });
+
+  employeesArr.sort((a, b) => {
+    return b.dataset.salary - a.dataset.salary;
+  });
+
+  employeesArr.forEach((employee) => {
+    const salaryArr = ('' + employee.dataset.salary).split('');
+
+    for (let i = salaryArr.length - 3; i > 0; i -= 3) {
+      salaryArr.splice(i, 0, ',');
+    }
+
+    employee.dataset.salary = '$' + salaryArr.join('');
+  });
+
+  for (const employee of employeesArr) {
+    list.append(employee);
+  }
+}
+
+function getEmployees(list) {
+  const employeesArr = [...list.children].map(employee => {
+    return {
+      name: employee.innerText,
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    };
+  });
+
+  return employeesArr;
+}
+
+sortList(employees);
+
+getEmployees(employees);
