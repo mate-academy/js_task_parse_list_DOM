48516a6bf6ded642019a329495df0f19ff3fb276
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-01-04 12:09:22 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const persons = document.querySelectorAll('ul li');
+
+const sorted = [...persons].sort((a, b) => {
+  return Number(b.dataset.salary
+    .slice(1)
+    .split(',')
+    .join(''))
+  - Number(a.dataset.salary
+    .slice(1)
+    .split(',')
+    .join(''));
+});
+
+list.innerHTML = '';
+
+for (const li of sorted) {
+  list.appendChild(li);
+};
