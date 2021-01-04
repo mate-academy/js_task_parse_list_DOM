cac15f7dc0bc5f50e5d73685c19c32485c562f14
tamara ostapets <tamara.ostapec@gmail.com>
2021-01-04 17:38:06 +0200

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+function sortList(collection) {
+  const salariesAndNames = [...collection].map(person => {
+    return {
+      name: person.innerHTML,
+      salary: salaryToNumber(person.dataset.salary),
+    };
+  });
+
+  salariesAndNames.sort((one, two) => two.salary - one.salary);
+
+  insertToHtml(salariesAndNames, 'li');
+}
+
+function salaryToNumber(string) {
+  return +string.split(',').join('').replace('$', '');
+}
+
+function insertToHtml(array, tagElement) {
+  const elementArray = [...document.querySelectorAll(tagElement)];
+
+  for (let i = 0; i < elementArray.length; i++) {
+    elementArray[i].innerHTML = array[i].name;
+  }
+}
+
+function getEmployees(collection) {
+  const array = [...collection].map(person => {
+    return {
+      name: person.innerHTML.trim(),
+      position: person.dataset.position,
+      salary: salaryToNumber(person.dataset.salary),
+      age: +person.dataset.age,
+    };
+  });
+
+  return array;
+}
+
+sortList(list);
+getEmployees(list);
