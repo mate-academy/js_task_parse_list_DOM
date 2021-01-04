2549a9145d916884414e16fe026670f7004d9107
Dima Bond <neverpooh@gmail.com>
2021-01-04 17:51:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const container = document.querySelector('ul');
+const content = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  list.sort((a, b) => {
+    return toNumber(b.dataset.salary) - toNumber(a.dataset.salary);
+  });
+
+  container.append(...list);
+}
+
+function toNumber(str) {
+  return Number(str.slice(1).split(',').join(''));
+}
+
+function getEmployees(list) {
+  const employees = [];
+
+  for (const item of document.querySelectorAll('li')) {
+    const person = {
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    };
+
+    employees.push(person);
+  }
+
+  return employees;
+}
+
+sortList(content);
+getEmployees(content);
