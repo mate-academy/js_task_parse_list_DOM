8eebee89b0ca5ead932ac7cd8ae76fa73d0d9200
Nazar Valchyshyn <nvalja@gmail.com>
2021-01-04 23:43:35 +0200

    made task parse-list-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const inputList = document.querySelectorAll('li');
+
+function sortList(list) {
+  const outList = [...list].sort((a, b) => {
+    return convert(b.dataset.salary) - convert(a.dataset.salary);
+  });
+
+  function convert(inputSalary) {
+    return +(inputSalary.slice(1, -1).split(',').join(''));
+  }
+
+  return outList;
+}
+
+const sorted = sortList(inputList);
+const newList = document.querySelector('ul');
+
+newList.append(...sorted);
+
+function getEmployees(list) {
+  const result = [];
+
+  for (const item of list) {
+    result.push({
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    });
+  }
+
+  return result;
+}
+
+getEmployees(inputList);
