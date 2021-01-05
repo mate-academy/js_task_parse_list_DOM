9e4bea79e3f3f97c6a554d3c7725a6d53c925238
Oleksii Spalylo <white.rh42@gmail.com>
2021-01-05 21:27:31 +0200

    added the  functions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,22 @@
 
 // write code here
 const items = document.querySelector('ul').children;
+let arr = [];
 
-const arr = [...items];
+const sortList = function(list) {
+  arr = [...list];
 
-arr.sort((a, b) => {
-  const first = +a.dataset.salary.slice(1).split(',').join('');
-  const second = +b.dataset.salary.slice(1).split(',').join('');
+  arr.sort((a, b) => {
+    const first = +a.dataset.salary.slice(1).split(',').join('');
+    const second = +b.dataset.salary.slice(1).split(',').join('');
 
-  return first <= second ? 1 : -1;
-});
+    return first <= second ? 1 : -1;
+  });
+};
 
-arr.forEach(i => document.querySelector('ul').append(i));
+const getEmployees = function(list) {
+  list.forEach(i => document.querySelector('ul').append(i));
+};
+
+sortList(items);
+getEmployees(arr);
