6cbfa4ed7d67e83200fb2c0250a0737e83e312b2
Pavlo Terletskyi <pavloterleckij2@gmail.com>
2021-01-05 14:57:57 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+const employees = document.querySelectorAll('li');
+
+function sortList(list) {
+  const sortedList = [...list]
+    .sort((a, b) => convToNum(b.dataset.salary) - convToNum(a.dataset.salary));
+
+  employeesList.append(...sortedList);
+}
+
+function convToNum(item) {
+  return +item.replace(/\D/g, '');
+}
+
+function getEmployees(list) {
+  return [...list].map(item => ({
+    name: item.innerText,
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+}
+
+sortList(employees);
+getEmployees(employees);
