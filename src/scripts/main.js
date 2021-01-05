5d1a047693f99aa51956b88abeab0b87b918e599
Oleksii Spalylo <white.rh42@gmail.com>
2021-01-05 21:03:52 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write code here
+const items = document.querySelector('ul').children;
+
+const arr = [...items];
+
+arr.sort((a, b) => {
+  const first = +a.dataset.salary.slice(1).split(',').join('');
+  const second = +b.dataset.salary.slice(1).split(',').join('');
+
+  return first <= second ? 1 : -1;
+});
+
+arr.forEach(i => document.querySelector('ul').append(i));
