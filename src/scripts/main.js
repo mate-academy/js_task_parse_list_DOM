e6bf4a8a2eb57a7e58cea71c73161ce5438cef73
Iryna Cherpak <cherpak.iryna@gmail.com>
2021-01-05 14:03:16 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const employeesCollection = document.querySelector('ul');
+
+const employeesList = document.querySelectorAll('li');
+
+function convertToNumber(string) {
+  return +string.slice(1).split(',').join('');
+}
+
+function sortList(list) {
+  const sorted = [...list].sort((employeeA, employeeB) =>
+    (convertToNumber(employeeB.dataset.salary)
+    - convertToNumber(employeeA.dataset.salary)));
+
+  employeesCollection.append(...sorted);
+}
+
+function getEmployees(list) {
+  return [...list].map(employee => ({
+    name: employee.innerText,
+    position: employee.dataset.position,
+    salary: employee.dataset.salary,
+    age: employee.dataset.age,
+  }));
+}
+
+sortList(employeesList);
+getEmployees(employeesList);
