970b045ca21a2e9659ad59d5697760ac9ca5323d
Your Name <mogolgogol.90@gmail.com>
2021-01-05 14:10:39 +0200

    reduced code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,46 +1,30 @@
 'use strict';
 
-const parsed = [ ...document.querySelectorAll('li') ];
+const parsed = document.querySelector('ul');
 
 function sortList(list) {
-  const people = [];
+  const people = [...list.children]
+    .sort((a, b) => (
+      +(b.dataset.salary).slice(1)
+        .replace(/,/g, '') - +(a.dataset.salary).slice(1).replace(/,/g, ''))
+    );
 
-  for (const person in list) {
-    people.push({
-      name: list[person].innerText,
-      position: list[person].dataset.position,
-      salary: +(list[person].dataset.salary).slice(1).replace(/,/g, ''),
-      age: list[person].dataset.age,
-    });
-  }
+  parsed.append(...people);
 
-  const sortedBySalary = [ ...people ].sort((a, b) => +b.salary - +a.salary);
-
-  return sortedBySalary;
+  return people;
 }
 sortList(parsed);
 
 function getEmployees(list) {
-  const people = [];
-
-  for (const person in list) {
-    people.push({
-      name: list[person].innerText,
-      position: list[person].dataset.position,
-      salary: list[person].dataset.salary,
-      age: list[person].dataset.age,
+  const people = [...list.children].map(person => {
+    return ({
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
     });
-  }
+  });
 
   return people;
 }
 getEmployees(parsed);
-
-const sortedWithFunction = sortList(parsed);
-
-for (const coworker in parsed) { // for (const coworker in list)
-  parsed[coworker].innerText = sortedWithFunction[coworker].name;
-  parsed[coworker].dataset.position = sortedWithFunction[coworker].position;
-  parsed[coworker].dataset.salary = sortedWithFunction[coworker].salary;
-  parsed[coworker].dataset.age = sortedWithFunction[coworker].age;
-}
