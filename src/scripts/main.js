e8f806d1220e6d149bded881b9ba9024f87e1fff
Olesia <olesia.tk@gmail.com>
2021-01-06 09:53:48 +0200

    Changed with map

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,15 +12,13 @@ function sortList(list) {
 }
 
 function getEmployees(list) {
-  const employees = [];
-
-  for (let i = 0; i < list.length; i++) {
-    employees[i] = {};
-    employees[i].name = list[i].innerText;
-    employees[i].position = list[i].dataset.position;
-    employees[i].salary = list[i].dataset.salary;
-    employees[i].age = list[i].dataset.age;
-  }
+  const employees = [...list]
+    .map(employee => ({
+      name: employee.innerText,
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    }));
 
   return employees;
 }
