b5ecf0fa3c4d58891a089134de41969ed65ce881
Viktor Morhun <victor.morgun16@gmail.com>
2021-01-06 17:48:29 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const employees = document.querySelector('ul');
+
+function toNumber(str) {
+  return Number(str.slice(1).split(',').join(''));
+}
+
+function sortList(list) {
+  return [...list.children].sort((first, second) =>
+    toNumber(second.dataset.salary) - toNumber(first.dataset.salary));
+}
+
+employees.append(...sortList(employees));
+
+function getEmployees(list) {
+  return [...list.children].map(obj => {
+    return {
+      name: obj.textContent.trim(),
+      position: obj.dataset.position,
+      salary: obj.dataset.salary,
+      age: obj.dataset.age,
+    };
+  });
+}
+
+sortList(employees);
+getEmployees(employees);
