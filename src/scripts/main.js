b28df87a8e828f8d8c444eb45e5530a76ba40c0d
MaximZhuravlov <maksym.zhuravlov1995@gmail.com>
2021-01-08 16:30:49 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+const employees = document.querySelectorAll('li');
+
+function sortList(list) {
+  const sortedEmployees = [...list]
+    .sort((a, b) => toNumber(b.dataset.salary) - toNumber(a.dataset.salary));
+
+  employeesList.append(...sortedEmployees);
+}
+
+function getEmployees(list) {
+  return [...list].map(employee => ({
+    name: employee.innerText,
+    position: employee.dataset.position,
+    salary: toNumber(employee.dataset.salary),
+    age: employee.dataset.age,
+  }));
+}
+
+/**
+ * Converts the specified string to number.
+ *
+ * @param {string} string string to be converted to number.
+ *
+ * @returns {number} number.
+ */
+function toNumber(string) {
+  return Number(string.match(/\d+/g).join(''));
+}
+
+getEmployees(employees);
+sortList(employees);
