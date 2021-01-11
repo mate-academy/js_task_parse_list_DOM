8c86ebbbe94009cfc2ee40a22ae98bb59edef71f
Nazarii <nazarrishchuk@gmail.com>
2021-01-11 17:08:51 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+const listEmployees = document.querySelector('ul');
+
+function stringToNumber(string) {
+  return +string.replace(/,/g, '').slice(1);
+}
+
+function sortList(list) {
+  const listItems = list.children;
+  const sortedItemsAlphabet = [ ...listItems ].sort((a, b) =>
+    b.textContent.localeCompare(a.textContent));
+
+  sortedItemsAlphabet.sort((a, b) =>
+    stringToNumber(a.dataset.salary) - stringToNumber(b.dataset.salary));
+
+  for (const li of sortedItemsAlphabet) {
+    list.prepend(li);
+  }
+};
+
+sortList(listEmployees);
+
+function getEmployees(list) {
+  const listItems = list.children;
+  const result = [];
+
+  for (const li of listItems) {
+    result.push({
+      name: li.textContent.trim(),
+      position: li.dataset.position,
+      salary: li.dataset.salary,
+      age: li.dataset.age,
+    });
+  }
+
+  return result;
+}
+
+getEmployees(listEmployees);
