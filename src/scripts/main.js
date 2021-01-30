6e81c4ceb4d4580b3b9639c8a5a7e850ea06f9a4
DominoSl <artemenko.mik@gmail.com>
2021-01-30 16:35:03 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const employeesNode = document.querySelector('ul');
+const employees = [...employeesNode.querySelectorAll('li')];
+
+const sortList = (toBeSorted) => {
+  toBeSorted.sort((one, two) => {
+    const first = +one.dataset.salary.replace(/[,$]/g, '');
+    const second = +two.dataset.salary.replace(/[,$]/g, '');
+
+    return second - first;
+  });
+  employeesNode.append(...employees);
+};
+
+const getEmployees = (toBePresented) => {
+  const employeesInfo = [];
+
+  for (const person of toBePresented) {
+    const employee = {
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    };
+
+    employeesInfo.push(employee);
+  }
+
+  return employeesInfo;
+};
+
+sortList(employees);
+getEmployees(employees);
