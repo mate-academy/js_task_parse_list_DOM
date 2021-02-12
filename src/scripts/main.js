9a8d0237ed21b5efab213b261a4d32a237a0a186
Artem Fedoseev <afedoseev359@gmail.com>
2021-02-12 16:50:34 +0200

    Substituted setting innerHTML for append

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,18 +26,7 @@ const list = document.querySelectorAll('li');
 const sortedList = sortList(list);
 const ulElem = document.querySelector('ul');
 
-ulElem.innerHTML = `
-  ${sortedList.map(employee => {
-    return `
-      <li 
-        data-position='${employee.dataset.position}'
-        data-salary='${employee.dataset.salary}'
-        data-age='${employee.dataset.age}'>
-          ${employee.innerText}
-      </li>
-    `;
-  }).join('')}
-`;
+sortedList.forEach(li => ulElem.append(li));
 
 sortList(list);
 getEmployees(list);
