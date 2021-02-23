fb4daa0dd43e9112b24c817ab0f2c58ff4112c3b
Bohdan Dobak <chudolipton@gmail.com>
2021-02-23 16:48:48 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+function getEmployees(element) {
+  [...element].map(person => ({
+    ...person,
+    name: person.innerText,
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+}
+
+function sortList(element) {
+  const sortedList = [...element].sort((next, current) => {
+    const converterCurrent
+    = current.dataset.salary.replace('$', '').replace(',', '');
+    const convereterNext
+      = next.dataset.salary.replace('$', '').replace(',', '');
+
+    return converterCurrent - convereterNext;
+  });
+
+  list.append(...sortedList);
+}
+
+sortList(listItems);
+getEmployees(listItems);
