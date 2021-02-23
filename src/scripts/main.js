6b6c992e348d3513b7eee7800ddd54208fba781c
Misha Lysak <myhaillysak@gmail.com>
2021-02-23 20:36:11 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,32 @@
 'use strict';
 
 const employees = document.querySelectorAll('li');
+const employeesList = document.querySelector('ul');
 
-const people = [...employees].map(person => ({
-  ...person,
-  name: person.innerText,
-  position: person.dataset.position,
-  salary: person.dataset.salary,
-  age: person.dataset.age,
-}));
-
-function sortList(humans) {
-  return humans.sort((currentPerson, nextPerson) => {
-    const currentPersonToNumber
-      = +currentPerson.salary.split('').slice(1).join('').split(',').join('');
-    const nextPersonToNumber
-      = +nextPerson.salary.split('').slice(1).join('').split(',').join('');
-
-    return nextPersonToNumber - currentPersonToNumber;
-  });
-};
-
-const sortedEmployees = sortList(people);
-
-function getEmployees(list, putFrom) {
-  for (let i = 0; i < list.length; i++) {
-    list[i].innerText = putFrom[i].name;
-    list[i].dataset.position = putFrom[i].position;
-    list[i].dataset.salary = putFrom[i].salary;
-    list[i].dataset.age = putFrom[i].age;
-  };
-
-  return list;
+function convertationToNumber(item) {
+  return item.split('').slice(1).join('').split(',').join('');
 }
 
-sortList(people);
-getEmployees(employees, sortedEmployees);
+function sortList(humans) {
+  const sorted = [...humans].sort((currentPerson, nextPerson) => {
+    const currentPersonToNumber
+      = convertationToNumber(currentPerson.dataset.salary);
+    const nextPersonToNumber
+      = convertationToNumber(nextPerson.dataset.salary);
+
+    return +nextPersonToNumber - +currentPersonToNumber;
+  });
+
+  employeesList.append(...sorted);
+};
+
+function getEmployees(emp) {
+  [...emp].map(person => ({
+    ...person,
+    ...person.dataset,
+    name: person.innerText,
+  }));
+}
+
+sortList(employees);
+getEmployees(employees);
