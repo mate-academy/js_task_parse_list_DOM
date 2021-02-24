ee7c1e890e189a7dfd284d43bb8604fc2a75c7ca
antoniakstepan <antoniakstepan1@gmail.com>
2021-02-24 15:39:13 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
 // write code here
+const employesList = document.querySelector('ul');
+
+function covnertToNumber(string) {
+  return Number(string.match(/\d/g).join(''));
+}
+
+function sortList(list) {
+  const employes = list.children;
+  const sortedEmployes = [...employes].sort((curent, next) => covnertToNumber(
+    next.dataset.salary) - covnertToNumber(curent.dataset.salary)
+  );
+
+  list.append(...sortedEmployes);
+
+  return sortedEmployes;
+}
+
+function getEmployees(list) {
+  const employes = list.children;
+
+  return [...employes].map(person => ({
+    name: person.innerText,
+    ...person.dataset,
+  }));
+}
+
+sortList(employesList);
+getEmployees(employesList);
