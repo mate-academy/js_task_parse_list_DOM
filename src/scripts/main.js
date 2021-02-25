a2ff06fad846498e95174914c6b95ccd72c03aea
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-02-25 14:04:30 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const throwSymbols = /[^\d]/g;
+const [...allEmployees] = document.querySelectorAll('li');
+const listOfEmployees = document.querySelector('ul');
+
+const convertSalary = employee => +(employee.dataset.salary)
+  .replace(throwSymbols, '');
+
+function sortList(employees) {
+  const sortedEmployees = employees.sort(
+    (prevEmployee, nextEmployee) => {
+      return convertSalary(nextEmployee) - convertSalary(prevEmployee);
+    });
+
+  listOfEmployees.append(...sortedEmployees);
+
+  return sortedEmployees;
+}
+
+function getEmployees(employees) {
+  return employees.map(employee => ({
+    name: employee.innerText,
+    position: employee.dataset.position,
+    salary: employee.dataset.salary,
+    age: employee.dataset.age,
+  }));
+}
+
+getEmployees(allEmployees);
+sortList(allEmployees);
