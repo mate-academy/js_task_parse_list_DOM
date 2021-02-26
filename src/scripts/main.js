bfc1b25a1cdf22046352cb5e324ab4e24f004127
Taras <seman6745@gmail.com>
2021-02-26 15:13:23 +0200

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,30 +6,25 @@ const stringToNumber = (string) => {
   return +string.slice(1).replace(',', '.');
 };
 
-const createListWithInfo = () => {
-  const persons = [...list.children].map(element => {
+const people = [...list.children];
+
+const createListWithInfo = (peopleList) => {
+  return peopleList.map(element => {
     const person = {};
 
     person.name = element.innerText;
     person.position = element.dataset.position;
     person.salary = element.dataset.salary;
     person.age = element.dataset.age;
-
-    return person;
   });
-
-  return persons;
 };
 
-const listWithInfo = createListWithInfo();
-
-const sortingListOfPeople = () => {
-  listWithInfo.sort((personA, personB) =>
-    stringToNumber(personB.salary) - stringToNumber(personA.salary));
-
-  for (let i = 0; i < listWithInfo.length; i++) {
-    list.children[i].textContent = listWithInfo[i].name;
-  }
+const sortingListOfPeople = (peopleList) => {
+  return peopleList.sort((personA, personB) =>
+    stringToNumber(personB.dataset.salary)
+    - stringToNumber(personA.dataset.salary));
 };
 
-sortingListOfPeople();
+sortingListOfPeople(people);
+createListWithInfo(people);
+list.append(...people);
