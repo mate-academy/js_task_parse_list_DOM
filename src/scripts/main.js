5b16e602db09bf3d3d0d0e83bc4cb73e5c56fae5
Serhii Ivanyk <youdex06@gmail.com>
2021-02-28 17:37:18 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+const colection = document.querySelectorAll('li');
+const colectionUl = document.querySelector('ul');
+
+const arrOfLi = [...colection];
+
+function sortList(list) {
+  list.sort((a, b) => {
+    const first = +(a.dataset.salary.replace('$', '').replace(',', ''));
+    const second = +(b.dataset.salary.replace('$', '').replace(',', ''));
+
+    return second - first;
+  });
+}
+
+function getEmployees(list) {
+  colectionUl.append(...arrOfLi);
+}
+
+sortList(arrOfLi);
+getEmployees(arrOfLi);
