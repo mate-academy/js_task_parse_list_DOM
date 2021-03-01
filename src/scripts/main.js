851da570e48215a94da77ead5b255a9ca7ca75fe
anastasiia <anastasiia.aliinyk@gmail.com>
2021-03-01 18:32:27 +0200

    first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+function convertToNumber(string) {
+  return +(string.replace(/\$|,/g, ''));
+};
+
+function sortList(list) {
+  return [...list].sort((current, next) =>
+    convertToNumber(next.dataset.salary)
+      - convertToNumber(current.dataset.salary));
+}
+
+const employees = document.querySelectorAll('li');
+const sortedEmployees = sortList(employees);
+
+const listWithEmployees = document.querySelector('ul');
+
+sortedEmployees.forEach(function(value) {
+  listWithEmployees.appendChild(value);
+});
