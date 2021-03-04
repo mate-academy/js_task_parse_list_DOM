dd532ce76f557c0dc04bc5e8bfc3ffde6d2d14d0
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-04 23:35:42 +0200

    fix all problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const liElement = document.querySelectorAll('li');
+const ulElement = document.querySelector('ul');
+
+function convertNumber(value) {
+  return +value.slice(1).replace(',', '');
+}
+
+function sortList(list) {
+  return list.sort((firstEmployer, secondEmployer) => {
+    return convertNumber(secondEmployer.dataset.salary) - convertNumber(
+      firstEmployer.dataset.salary);
+  });
+}
+
+const liSorted = sortList([...liElement]);
+
+ulElement.append(...liSorted);
+
+function employeList(incomingArray) {
+  return incomingArray.map((employer) => {
+    return {
+      ...employer.dataset,
+      name: employer.innerTeemployert,
+    };
+  });
+}
+
+employeList(liSorted);
