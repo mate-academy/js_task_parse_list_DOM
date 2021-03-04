9155d7932d1733284db32a075998c56fd0a8042d
Yevhenii Yashnyi <y.yashnyi@gmail.com>
2021-03-04 18:13:07 +0200

    sorted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const employersListElement = document.querySelector('ul');
+const employersItemElement = document.querySelectorAll('li');
+
+function convertToNumber(string) {
+  return +string.slice(1).split(',').join('');
+}
+
+function sortList(list) {
+  const sortedList = [...list].sort((employeeA, employeeB) =>
+    convertToNumber(employeeB.dataset.salary)
+    - convertToNumber(employeeA.dataset.salary));
+
+  employersListElement.append(...sortedList);
+}
+
+sortList(employersItemElement);
