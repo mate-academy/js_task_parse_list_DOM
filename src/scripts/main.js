3c9116dc081babc52ef95bfe0e1dba1083f10e21
Ping <4ivrik@ukr.net>
2021-03-04 20:26:15 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('li')];
+const listOfEmployees = document.querySelector('ul');
+
+function convert(string) {
+  return Number(string.replace(/[^0-9]/g, ''));
+}
+
+function sortList(list) {
+  return list.sort(
+    (previous, next) =>
+      convert(next.dataset.salary) - convert(previous.dataset.salary)
+  );
+}
+
+function getEmployees(list) {
+  return list.map(element => {
+    return {
+      name: element.innerText,
+      position: element.dataset.position,
+      salary: element.dataset.salary,
+      age: element.dataset.age,
+    };
+  });
+}
+
+sortList(employees).forEach(element => {
+  listOfEmployees.append(element);
+});
+
+sortList(employees);
+getEmployees(employees);
