d8210edb804f262d5c818c479da487f669eb71dc
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-04 14:54:15 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const liElement = document.querySelectorAll('li');
+const ulElement = document.querySelector('ul');
+
+function creatingNumbers(value) {
+  return +value.slice(1).replace(',', '');
+}
+
+function sotList(incomingList) {
+  return incomingList.sort((a, b) => {
+    return creatingNumbers(b.dataset.salary) - creatingNumbers(
+      a.dataset.salary);
+  });
+}
+
+const liElementArray = [...liElement];
+
+const liSorted = sotList(liElementArray);
+
+ulElement.append(...liSorted);
+
+function employeList(incomingArray) {
+  return incomingArray.map((x) => {
+    return {
+      ...x.dataset,
+      name: x.innerText,
+    };
+  });
+}
+
+employeList(liSorted);
