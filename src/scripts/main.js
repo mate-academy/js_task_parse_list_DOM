e7b66a6e52e9d30f32ec691bf2641e8c3af62106
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-03-05 01:27:15 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+
+const employees = document.querySelectorAll('li');
+const listOfEmployees = document.querySelector('ul');
+
+function sortList(list) {
+  return [...list].sort((prev, next) => {
+    const sortedA = prev.dataset.salary.slice(1).split(',').join('');
+    const sortedB = next.dataset.salary.slice(1).split(',').join('');
+
+    return +sortedB - +sortedA;
+  });
+}
+
+const sortedEmployees = sortList(employees);
+
+function getEfficiees(list) {
+  listOfEmployees.append(...list);
+}
+
+getEfficiees(sortedEmployees);
