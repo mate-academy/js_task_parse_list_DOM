9a93b9ac099bd34d1940d832e6638276431c1369
ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-03-04 14:43:42 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const listOfEmployees = document.querySelector('ul');
+const employees = document.querySelectorAll('li');
+
+function callBackToSort(a, b) {
+  const firstArgument = +(a.dataset.salary
+    .split('$')
+    .join('')
+    .split(',')
+    .join(''));
+  const secondArgument = +(b.dataset.salary
+    .split('$')
+    .join('')
+    .split(',')
+    .join(''));
+
+  return secondArgument - firstArgument;
+}
+
+function sortList(list) {
+  const sortedList = [...list].sort(callBackToSort);
+
+  listOfEmployees.append(...sortedList);
+}
+
+function getEmployees(listEmployees) {
+  return listEmployees.map(person => ({
+    name: person.innerText,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+}
+sortList(employees);
+getEmployees(employees);
