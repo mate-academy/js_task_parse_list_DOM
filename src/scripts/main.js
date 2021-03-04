02ec7b977e5ee73c77b2f2fec5223aa323af2a94
Vladyslav Semyrekov <semyrekov.vladyslav@gmail.com>
2021-03-04 17:46:58 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('li')];
+const employeesList = document.querySelector('ul');
+
+function toNumber(string) {
+  return +string.replace(/[^\d]/g, '');
+}
+
+function sortList(list) {
+  return list.sort((firstEmployee, secondEmployee) =>
+    toNumber(secondEmployee.dataset.salary)
+      - toNumber(firstEmployee.dataset.salary));
+}
+
+employeesList.append(...sortList(employees));
+
+function getEmployees(list) {
+  return list.map(employee => {
+    const profile = {};
+
+    profile.name = employee.innerText;
+    profile.position = employee.dataset.position;
+    profile.salary = employee.dataset.salary;
+    profile.age = employee.dataset.age;
+  });
+};
+
+getEmployees(employees);
