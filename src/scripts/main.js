54c855be3843a49339875bc5cf8d9a82a33d07a2
Владислав Сандык <nubia@MacBook-Pro-Vladislav.local>
2021-03-05 13:56:46 +0200

    added script

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const employees = list.children;
+
+function sortList(parameters) {
+  return [...parameters].sort(
+    (currentValue, nextValue) => {
+      return +nextValue.dataset.salary.replace(/[$,]/g, '')
+        - +currentValue.dataset.salary.replace(/[$,]/g, '');
+    });
+}
+
+const sortedEmployees = sortList(employees);
+
+function getEmployees(parameters) {
+  return parameters.reduce(
+    (accumulator, parameter) => {
+      accumulator.push({
+        name: parameter.textContent.trim(),
+        ...parameter.dataset,
+      });
+
+      return accumulator;
+    }, []);
+}
+
+getEmployees(sortedEmployees);
+list.append(...sortedEmployees);
