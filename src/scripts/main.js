413bcffde2f2c4dd17503e73884503c70744208e
Olha Briukhanova <oaleksandrovva@gmail.com>
2021-03-05 17:20:37 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const itemsOfEmployees = document.querySelectorAll('li');
+const listOfEmployees = document.querySelector('ul');
+
+const convertToNumber = (item) => {
+  return Number(item.split(',').join('').slice(1));
+};
+
+function sortList(list) {
+  const sortedList = [...list].sort((previousItem, nextItem) => {
+    const convertPreviousItem = convertToNumber(previousItem.dataset.salary);
+    const convertNextItem = convertToNumber(nextItem.dataset.salary);
+
+    return convertNextItem - convertPreviousItem;
+  });
+
+  listOfEmployees.append(...sortedList);
+}
+
+function getEmployees(list) {
+  return [...list].map(item => {
+    return {
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    };
+  });
+}
+
+sortList(itemsOfEmployees);
+getEmployees(itemsOfEmployees);
