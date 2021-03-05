ca3c7ded004c218119507f4aa592522a87c1e60c
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-03-05 15:53:26 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+const employees = [...document.querySelectorAll('li')];
+
+const stringToNumber = (string) =>
+  +string.replace(/[^0-9]/g, '');
+
+function sortedList(list) {
+  return list.sort(
+    (a, b) => stringToNumber(b.dataset.salary)
+    - stringToNumber(a.dataset.salary)
+  );
+}
+
+function getEmployees(list) {
+  return list.map(element => ({
+      name: element.innerText,
+      position: element.dataset.position,
+      salary: element.dataset.salary,
+      age: element.dataset.age,
+    }));
+}
+
+sortedList(employees).forEach(element => {
+  employeesList.append(element);
+});
+
+sortedList(employees);
+getEmployees(employees);
