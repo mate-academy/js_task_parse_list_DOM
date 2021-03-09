9c112a55d849739f88ecc8cac5af61ee686d557c
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-09 11:03:38 +0200

    edited with comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,69 +16,16 @@ function getEmployees() {
   return formattedListOfEmployees;
 }
 
-/* function getEmployees() {
-  const listOfEmployees = [...document.querySelectorAll('li')];
-  const formattedListOfEmployees = [];
-
-  for (let i = 0; i < listOfEmployees.length; i++) {
-    const employee = {};
-
-    employee.name = listOfEmployees[i].innerText;
-    employee.position = listOfEmployees[i].dataset.position;
-    employee.salary = listOfEmployees[i].dataset.salary;
-    employee.age = listOfEmployees[i].dataset.age;
-
-    formattedListOfEmployees.push(employee);
-  }
-
-  return formattedListOfEmployees;
-} */
-
-/* function sortList() {
-  const listOfEmployees = getEmployees().sort((a, b) =>
-    convertToMoney(b.salary) - convertToMoney(a.salary));
-
-  for (let i = 0; i < listOfEmployees.length; i++) {
-    document.querySelectorAll(
-      'li'
-    )[i].innerText = listOfEmployees[i].name;
-
-    document.querySelectorAll(
-      'li'
-    )[i].dataset.age = listOfEmployees[i].age;
-
-    document.querySelectorAll(
-      'li'
-    )[i].dataset.position = listOfEmployees[i].position;
-
-    document.querySelectorAll(
-      'li'
-    )[i].dataset.salary = listOfEmployees[i].salary;
-  }
-
-  return listOfEmployees;
-} */
-
 function sortList() {
   const listOfEmployees = getEmployees().sort((a, b) =>
     convertToMoney(b.salary) - convertToMoney(a.salary));
+  const fieldsForEditing = document.querySelectorAll('li');
 
   return listOfEmployees.forEach((person, i) => {
-    document.querySelectorAll(
-      'li'
-    )[i].innerText = person.name;
-
-    document.querySelectorAll(
-      'li'
-    )[i].dataset.age = person.age;
-
-    document.querySelectorAll(
-      'li'
-    )[i].dataset.position = person.position;
-
-    document.querySelectorAll(
-      'li'
-    )[i].dataset.salary = person.salary;
+    fieldsForEditing[i].innerText = person.name;
+    fieldsForEditing[i].dataset.age = person.age;
+    fieldsForEditing[i].dataset.position = person.position;
+    fieldsForEditing[i].dataset.salary = person.salary;
   });
 }
 
