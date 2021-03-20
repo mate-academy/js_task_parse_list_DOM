047a39ad56312e71a048504b92d3b76f2ab341da
Vitalii Nepomiashchyi <nepomyaschyi1@gmail.com>
2021-03-20 16:53:41 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write code here
+const salaryToNumber = function(salary) {
+  return +salary.substr(1).split(',').join('');
+};
+
+const sortList = function(employees) {
+  const employeesSorted = [...employees.children].sort((personA, personB) => {
+    let personASalary = personA.dataset.salary;
+    let personBSalary = personB.dataset.salary;
+
+    personASalary = salaryToNumber(personASalary);
+    personBSalary = salaryToNumber(personBSalary);
+
+    return personBSalary - personASalary;
+  });
+
+  employees.innerHTML = `${employeesSorted.map(person => `
+    <li
+      data-position=${person.dataset.position}
+      data-salary=${person.dataset.salary}
+      data-age=${person.dataset.age}
+    >
+      ${person.innerText}
+    </li>`).join('')}`;
+};
+
+const getEmployees = function(employees) {
+  const employeesArray = [...employees.children].map(person => {
+    return {
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    };
+  });
+
+  return employeesArray;
+};
+
+const list = document.querySelector('ul');
+
+sortList(list);
+getEmployees(list);
