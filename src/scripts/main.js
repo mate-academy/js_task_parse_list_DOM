006dc1bbe2745a3ac1914373516a977d19e3c51c
Maksym Matuliak <maksymmatuliak@gmail.com>
2021-03-21 21:01:16 +0200

    fixed attributes for employees

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,11 @@ const liElements = [...document.querySelectorAll('li')];
 const employees = getEmployees(liElements);
 const sortedEmployees = sortList(employees);
 
-for (let i = 0; i < document.querySelectorAll('li').length; i++) {
-  document.querySelectorAll('li')[i].innerText = sortedEmployees[i].name;
+for (let i = 0; i < liElements.length; i++) {
+  liElements[i].innerText = sortedEmployees[i].name;
+  liElements[i].setAttribute('data-salary', sortedEmployees[i].salary);
+  liElements[i].setAttribute('data-age', sortedEmployees[i].age);
+  liElements[i].setAttribute('data-postion', sortedEmployees[i].position);
 }
 
 function sortList(listOfEmployees) {
