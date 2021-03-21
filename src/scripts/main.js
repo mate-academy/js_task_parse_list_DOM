9525a9c1d7ab5d1dddfd7ee6bcaf3ffb1a12fb0a
Maksym Matuliak <maksymmatuliak@gmail.com>
2021-03-21 11:29:25 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const liElements = [...document.querySelectorAll('li')];
+const employees = getEmployees(liElements);
+const sortedEmployees = sortList(employees);
+
+for (let i = 0; i < document.querySelectorAll('li').length; i++) {
+  document.querySelectorAll('li')[i].innerText = sortedEmployees[i].name;
+}
+
+function sortList(listOfEmployees) {
+  return listOfEmployees.sort(
+    (a, b) => parseSalary(b.salary) - parseSalary(a.salary)
+  );
+}
+
+function parseSalary(salary) {
+  return parseFloat(salary.replace(',', '').replace('$', ''));
+}
+
+function getEmployees(listOfEmployees) {
+  const result = [];
+
+  for (const employee of listOfEmployees) {
+    result.push({
+      'name': employee.innerText,
+      'position': employee.getAttribute('data-position'),
+      'salary': employee.getAttribute('data-salary'),
+      'age': employee.getAttribute('data-age'),
+    });
+  }
+
+  return result;
+}
