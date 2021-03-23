418dc9ce9aa131cbeeee8652993c16c5491c3301
Max Zadorozhniy <max.zadorozhniy2001@gmail.com>
2021-03-23 21:11:08 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+const employeesItem = document.querySelectorAll('li');
+
+function convertSalary(salary) {
+  return salary.replace(/\D/g, '');
+}
+
+function sortList(list) {
+  const sortedList = [...list].sort((firstElement, secondElement) =>
+    convertSalary(secondElement.dataset.salary)
+      - convertSalary(firstElement.dataset.salary));
+
+  employeesList.append(...sortedList);
+}
+
+function getEmployees(list) {
+  return [...list].map(personInfo => ({
+    name: personInfo.textContent,
+    position: personInfo.dataset.position,
+    salary: personInfo.dataset.salary,
+    age: personInfo.dataset.age,
+  }));
+}
+
+sortList(employeesItem);
+getEmployees(employeesItem);
