92d51bf73d654d2798e24bbfd260bee14e2da6c0
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-03-23 20:42:31 +0200

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write code here
+const peopleList = [...document.querySelectorAll('li')];
+const ulAppendSelector = document.querySelector('ul');
+
+function sortList(list) {
+  const sortSalary = list.sort((salaryA, salaryB) => {
+    return +(salaryB.dataset.salary.slice(1).split(',').join(''))
+    - +(salaryA.dataset.salary.slice(1).split(',').join(''));
+  });
+
+  ulAppendSelector.append(...sortSalary);
+};
+
+function getEmployees(list) {
+  return list.map(person => ({
+    name: person.innerText,
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+};
+
+sortList(peopleList);
+getEmployees(peopleList);
