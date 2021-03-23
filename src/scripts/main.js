1a3409adf8f0afe5b7ecc9039b58f8b688a6be26
Iryna Haiduk <$ git config --global user.name Iryna>
2021-03-23 21:30:57 +0200

    Parsed list with data attributes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write code here
+const employeesList = document.querySelector('ul');
+const employeesItems = [...employeesList.querySelectorAll('li')];
+const sortBySalary = (employee1, employee2) =>
+  salaryToNumber(employee2) - salaryToNumber(employee1);
+const salaryToNumber = (elem) =>
+  parseInt(elem.dataset.salary.slice(1).replace(/,/g, ''));
+const employeesSortedBySalary = employeesItems.sort(sortBySalary);
+
+employeesList.innerHTML = '';
+
+employeesSortedBySalary.forEach(employee =>
+  employeesList.appendChild(employee));
