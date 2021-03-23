a8b57039bb7636ad26aeef3c314037c177e81ba8
Alexander Levchenko <a.levchenko603@gmail.com>
2021-03-23 21:36:26 +0200

    first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const listOfWorkers = document.querySelectorAll('li');
+const workersList = [...listOfWorkers];
+const UlOfWorkers = document.querySelector('ul');
+
+const sortList = (list) => {
+  const sortedList = list.sort((workerA, workerB) => {
+    return +workerB.dataset.salary
+      .slice(1)
+      .split(',')
+      .join('')
+      - +workerA.dataset.salary
+        .slice(1)
+        .split(',')
+        .join('');
+  });
+
+  UlOfWorkers.append(...sortedList);
+};
+
+function getEmployees(list) {
+  return list
+    .map(worker => ({
+      name: worker.innerText,
+      position: worker.dataset.position,
+      salary: worker.dataset.salary,
+      age: worker.dataset.age,
+    }));
+};
+
+sortList(workersList);
+getEmployees(workersList);
