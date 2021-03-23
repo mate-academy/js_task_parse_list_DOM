201f7aa4a57cf64894107afadc6b50f74728f1ee
Vlad Dezhniuk <vladdezhniuk@gmail.com>
2021-03-23 22:01:18 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listItem = document.querySelectorAll('li');
+
+function format(item) {
+  return +item.replace(/\D/g, '');
+}
+
+function sortList(item) {
+  const result = [...item];
+
+  result.sort((a, b) => format(b.dataset.salary) - format(a.dataset.salary));
+  list.append(...result);
+}
+
+function getEmployees(item) {
+  const result = [...item];
+
+  for (let a = 0; a < result.length; a++) {
+    result[a] = { ...result[a].dataset };
+  };
+
+  return result;
+}
+
+sortList(listItem);
+getEmployees(listItem);
