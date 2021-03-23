091b01585583480120446166b36b11aadedeaab0
Serhii-Nikitin <serg.niki@i.ua>
2021-03-23 20:31:44 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const parentList = document.querySelector('ul');
+const people = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  list.sort((personOne, personTwo) => (
+    +(personTwo.dataset.salary.slice(1).replace(',', ''))
+    - +(personOne.dataset.salary.slice(1).replace(',', ''))
+  ));
+
+  parentList.append(...list);
+}
+
+function getEmployees(list) {
+  const employees = [];
+
+  for (let i = 0; i < list.length; i++) {
+    const person = {};
+
+    person.name = list[i].innerText;
+    person.position = list[i].dataset.position;
+    person.salary = list[i].dataset.salary;
+    person.age = list[i].dataset.age;
+
+    employees.push(person);
+  }
+
+  return employees;
+}
+
+sortList(people);
+getEmployees(people);
