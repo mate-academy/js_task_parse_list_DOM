512c2d7ee9cc27354cbf83365eb64ea2e902d006
Taras Chaban <tarekchaban@gmail.com>
2021-03-24 08:55:41 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const itemsList = [...document.querySelectorAll('li')];
+const itemUl = document.querySelector('ul');
+
+const sortList = list => {
+  const sorted = list.sort((personA, personB) => {
+    return +((personB.dataset.salary.slice(1).replace(',', ''))
+    - (personA.dataset.salary.slice(1).replace(',', '')));
+  });
+
+  itemUl.append(...sorted);
+};
+
+const getEmployees = list => {
+  return list.map(person => ({
+    name: person.innerText,
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+};
+
+sortList(itemsList);
+getEmployees(itemsList);
