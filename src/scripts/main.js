29a7beed94b7de246d6ba34ee459cb5cd23a74e2
Ruslan Vasylyshyn <r.y.vasylyshyn@gmail.com>
2021-03-24 02:58:05 +0200

    first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // write code here
+const employeesList = document.querySelector('ul');
+const employeesData = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  list.sort((personOne, personTwo) => (
+    +(personTwo.dataset.salary.slice(1).replace(',', ''))
+    - +(personOne.dataset.salary.slice(1).replace(',', ''))
+  ));
+
+  employeesList.append(...list);
+}
+
+function getEmployees(list) {
+  const employees = [];
+
+  for (let i = 0; i < list.length; i++) {
+    const person = {};
+
+    person.name = list[i].innerText;
+    person.position = list[i].dataset.position;
+    person.salary = list[i].dataset.salary;
+    person.age = list[i].dataset.age;
+
+    employees.push(person);
+  }
+
+  return employees;
+}
+
+sortList(employeesData);
+getEmployees(employeesData);
