35d9f60566716d29a43e81ab47c6bfba022d13d6
Bohdan Prokopov <b.prokopov96@gmail.com>
2021-03-24 13:06:14 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,20 +4,18 @@ const peopleList = [...document.querySelectorAll('li')];
 const ulAppendSelector = document.querySelector('ul');
 
 const sortList = (list) => {
-  const sorted = list.sort((personA, personB) => {
+  return list.sort((personA, personB) => {
     return +(personB.dataset.salary.slice(1).split(',').join(''))
     - +(personA.dataset.salary.slice(1).split(',').join(''));
   });
-
-  ulAppendSelector.append(...sorted);
 };
 
 const getEmployees = (list) => {
+  ulAppendSelector.append(...list);
+
   return list.map(person => ({
     name: person.innerText,
-    position: person.dataset.position,
     salary: person.dataset.salary,
-    age: person.dataset.age,
   }));
 };
 
