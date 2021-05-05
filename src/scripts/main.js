0ff5a52c6771784bb432ca2bff474e61897b7676
Olga Vasylevskaya <olgavasylevskaya@ukr.net>
2021-05-05 13:01:22 +0300

    Solition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const people = document.getElementsByTagName('li');
+const ul = document.querySelector('ul');
+
+function sortList(list) {
+  function helper(str) {
+    return +(str.replace(/\D/g, ''));
+  };
+
+  const sorted = [...list]
+    .sort((a, b) => {
+      return helper(b.dataset.salary)
+      - helper(a.dataset.salary);
+    })
+    .forEach(li => ul.append(li));
+
+  return sorted;
+}
+
+function getEmployees(list) {
+  const employees = [];
+
+  for (const li of [...list]) {
+    const obj = {};
+
+    obj.name = li.textContent.trim();
+    obj.position = li.dataset.position;
+    obj.salary = li.dataset.salary;
+    obj.age = li.dataset.age;
+
+    employees.push(obj);
+  }
+
+  return employees;
+}
+
+getEmployees(sortList(people));
