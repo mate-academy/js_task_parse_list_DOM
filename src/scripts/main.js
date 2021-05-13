1cc845636ec2a840532d233484f53ef2de702cc0
Anton Kostylov <antoniok003@gmail.com>
2021-05-13 16:06:49 +0300

    More simple and clear solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,34 +4,19 @@ const listOfEmployees = document.querySelector('ul');
 
 function sortList(list) {
   function salaryToNumber(salary) {
-    const res = salary.slice(1).split(',').join('.');
-
-    return +res;
+    return +salary.slice(1).replace(',', '.');
   }
 
   const result = [...list.children].sort((a, b) =>
     salaryToNumber(b.dataset.salary) - salaryToNumber(a.dataset.salary));
 
-  list.innerHTML = `
-  <ul>
-    ${result.map(item => item.outerHTML).join('')}
-  </ul>
-  `;
+  list.append(...result);
 }
 
 function getEmployees(list) {
-  const employee = {};
-  const employees = [];
-
-  [...list.children].forEach(item => {
-    employee.name = item.innerText;
-    employee.position = item.dataset.position;
-    employee.salary = item.dataset.salary;
-    employee.age = item.dataset.age;
-    employees.push({ ...employee });
-  });
-
-  return employees;
+  return [...list.children].map(el => ({
+    name: el.innerText, ...el.dataset,
+  }));
 }
 
 sortList(listOfEmployees);
