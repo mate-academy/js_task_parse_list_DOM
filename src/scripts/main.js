622dedf84c3107f8d5d6fc7679b069109fa2ad86
Oleh Senkiv <o.senkiv.v@gmail.com>
2021-05-16 23:27:31 +0300

    Solution-001

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const employees = document.querySelector('ul');
+
+function sortList(list) {
+  function converToNumber(number) {
+    return +number.split(',').join('').slice(1);
+  }
+
+  const result = [...list.children].sort((a, b) =>
+    converToNumber(b.dataset.salary) - converToNumber(a.dataset.salary));
+
+  employees.append(...result);
+}
+
+function getEmployees(list) {
+  [...list.children].map(el => ({
+    name: el.innerText, ...el.dataset,
+  }));
+}
+
+sortList(employees);
+getEmployees(employees);
