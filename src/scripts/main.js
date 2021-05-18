aa65442dc6cdbad70e0c86bcdcb8f00182733780
Inna <inna_temis@ukr.net>
2021-05-18 18:25:14 +0300

    fix after solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write code here
+const itemsLi = document.querySelectorAll('li');
+const itemUl = document.querySelector('ul');
+
+function converted(str) {
+  return Number(str.substr(1).split(',').join(''));
+};
+
+function sortList(list) {
+  const sorted = [...list].sort((a, b) =>
+    converted(b.dataset.salary) - converted(a.dataset.salary));
+
+  itemUl.append(...sorted);
+};
+
+function getEmployees(list) {
+  return [...list].map(value => ({
+    name: value.innerText,
+    position: value.dataset.position,
+    salary: value.dataset.salary,
+    age: value.dataset.age,
+  }));
+}
+
+sortList(itemsLi);
+getEmployees(itemsLi);
