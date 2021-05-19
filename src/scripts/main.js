0bb6ee5f7ae3036d571665ffb3eb2c46df125a19
Kirill Osypov <Kirill.Osypov@gmail.com>
2021-05-19 10:40:43 +0300

    Fixed without function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,27 @@
 'use strict';
 
-const list = document.body.children[1].children;
-const array = [...list].map(item => [
+const list = document.querySelectorAll('li');
+let array = [...list].map(item => [
   item.innerText,
   item.dataset.position,
   item.dataset.salary.replace(/[^0-9]/g, ''),
   item.dataset.age,
 ]);
 
-function sortList(arr) {
-  return arr.sort((a, b) => b[2] - a[2]);
-}
+array = array.sort((a, b) => b[2] - a[2]);
 
-function getEmployees(arr) {
-  return arr.map(item => {
-    return {
-      name: item[0],
-      position: item[1],
-      salary: '$' + item[2],
-      age: item[3],
-    };
-  });
-}
+const employees = [...array].map(item => {
+  return {
+    name: item[0],
+    position: item[1],
+    salary: '$' + item[2],
+    age: item[3],
+  };
+});
 
-sortList(array);
+const newList = document.querySelector('ul');
 
-const employees = getEmployees(array);
-const replace = document.querySelector('ul');
-
-replace.innerHTML = `
+newList.innerHTML = `
   ${employees.map(employee => `
     <li
       data-position="${employee.position}"
