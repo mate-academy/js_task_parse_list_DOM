eedefdf0d2ba351d089585cc81596f2b72345dc3
Andriy Fesych <andriyhimself@gmail.com>
2021-05-24 19:16:59 +0300

    Solution_0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+function salaryMap(salary) {
+  return Number(salary.replace('$', '').replace(',', ''));
+}
+
+function sortList(list) {
+  Array.from(list.getElementsByTagName('LI'))
+    .sort((a, b) => salaryMap(b.dataset.salary)
+    - salaryMap(a.dataset.salary))
+    .forEach(li => {
+      list.appendChild(li);
+    });
+}
+
+function getEmployees(list) {
+  const finalArray = [];
+
+  Array.from(list.getElementsByTagName('LI'))
+    .forEach(person => {
+      const employee = {
+        name: person.innerText,
+        position: person.dataset.position,
+        salary: salaryMap(person.dataset.salary),
+        age: +person.dataset.age,
+      };
+
+      finalArray.push(employee);
+    });
+
+  return finalArray;
+}
+
+sortList(document.querySelector('ul'));
+getEmployees(document.querySelector('ul'));
+
+/*
+  console.log(Array.from(ul.getElementsByTagName("LI"))
+    .map(el => `${salaryMap(el.dataset.salary)} - ${el.innerText}`));
+*/
+
+// .sort((a, b) => a.textContent.localeCompare(b.textContent))
