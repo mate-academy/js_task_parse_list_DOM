194a41c43660c18df921097971bc0895a85d368a
Andriy Fesych <andriyhimself@gmail.com>
2021-05-25 23:18:03 +0300

    Solution_0.3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,34 @@
 'use strict';
 
-function salaryMap(salary) {
+function morphSalary(salary) {
   return Number(salary.replace('$', '').replace(',', ''));
 }
 
+function arrayFromLis(list) {
+  return Array.from(list.querySelectorAll('li'));
+}
+
 function sortList(list) {
-  Array.from(list.getElementsByTagName('LI'))
-    .sort((a, b) => salaryMap(b.dataset.salary)
-    - salaryMap(a.dataset.salary))
-    .forEach(li => {
-      list.appendChild(li);
-    });
+  list.append(...arrayFromLis(list)
+    .sort((a, b) => morphSalary(b.dataset.salary)
+    - morphSalary(a.dataset.salary)));
 }
 
 function getEmployees(list) {
-  const finalArray = [];
-
-  Array.from(list.getElementsByTagName('LI'))
-    .forEach(person => {
+  return arrayFromLis(list)
+    .map(person => {
       const employee = {
         name: person.innerText,
         position: person.dataset.position,
-        salary: salaryMap(person.dataset.salary),
+        salary: morphSalary(person.dataset.salary),
         age: +person.dataset.age,
       };
 
-      finalArray.push(employee);
+      return employee;
     });
-
-  return finalArray;
 }
 
-sortList(document.querySelector('ul'));
-getEmployees(document.querySelector('ul'));
+const ul = document.querySelector('ul');
+
+sortList(ul);
+(getEmployees(ul));
