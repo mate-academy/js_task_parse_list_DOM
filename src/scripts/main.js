29184f15e3f973a5485467b3ebc01678ced1f56b
VasylPerepichai <vperepichay@gmail.com>
2021-06-05 11:00:24 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
 // write code here
+const liList = document.querySelectorAll('li');
+
+function sortList(list) {
+  const liArray = Array.from(list);
+
+  liArray.sort((a, b) => {
+    return formatData(b.dataset.salary) - formatData(a.dataset.salary);
+  });
+
+  const ul = document.querySelector('ul');
+
+  liArray.forEach(elem => ul.append(elem));
+}
+
+function formatData(string) {
+  const recString = string.slice(1);
+  const number = +recString.split(',').join('');
+
+  return number;
+}
+
+function createEmplObj(list) {
+  const empArray = [];
+
+  for (const item of list) {
+    empArray.push({
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    });
+  };
+
+  return empArray;
+}
+
+sortList(liList);
+
+createEmplObj(liList);
