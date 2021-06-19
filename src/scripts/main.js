872b165a53b5548d9878b37ccd726af7dd991886
Tolik <tolikdolinskiy@gmail.com>
2021-06-19 16:46:04 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
 // write code here
+const employees = document.querySelectorAll('ul li');
+
+const strInNumber = (str) => {
+  return str.replace(/[^\d]/g, '') * 1;
+};
+
+const sortList = (empl) => {
+  const sortArr = [...empl].sort(
+    (item, item1) => {
+      return strInNumber(item1.dataset.salary)
+      - strInNumber(item.dataset.salary);
+    }).forEach(function(node) {
+    empl[0].parentNode.appendChild(node);
+  });
+
+  return sortArr;
+};
+
+const getEmployees = (arrEmployees) => {
+  const res = [];
+
+  [...arrEmployees].map((items) => res.push({
+    name: items.textContent.trim(),
+    position: items.dataset.position,
+    salary: items.dataset.salary,
+    age: items.dataset.age,
+  }));
+
+  return res;
+};
+
+sortList(employees);
+getEmployees(employees);
