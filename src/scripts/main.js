a75a1f6ccb822a638cded7f9df78b886a6353e20
korzh <korzh.alexey@yahoo.com>
2021-07-04 20:27:35 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const employees = [ ...document.querySelectorAll('li') ];
+const list = document.querySelector('ul');
+
+employees.sort((previousEmployee, currentEmployee) =>
+  +currentEmployee.dataset.salary.replace(/[$,]/g, '')
+  - +previousEmployee.dataset.salary.replace(/[$,]/g, ''));
+
+list.innerHTML = '';
+
+employees.forEach(employee => list.append(employee));
