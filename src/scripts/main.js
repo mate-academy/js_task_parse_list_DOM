acc748bad584339ac64cd0d85164331cec801656
Artem Dzhereleiko <dgerik2015@gmail.com>
2021-07-05 19:05:15 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listOfPeople = document.querySelectorAll('li');
+
+function sortList(peopleList) {
+  const listToArray = [...peopleList];
+  const salary = (toNumber) => {
+    return +toNumber.dataset.salary.slice(1).split(',').join('');
+  };
+
+  listToArray.sort((current, next) => {
+    const currentSalary = salary(current);
+    const nextSalary = salary(next);
+
+    return nextSalary - currentSalary;
+  });
+
+  list.append(...listToArray);
+};
+
+function getEmployees(peopleList) {
+  const listToArray = [...peopleList];
+
+  return listToArray.map(item => {
+    return {
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    };
+  });
+}
+
+sortList(listOfPeople);
+getEmployees(listOfPeople);
