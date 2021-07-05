89a7d22f0683b3891c77fa4a151ee400398f5686
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-07-05 21:17:59 +0300

    Check me again

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,60 @@
 'use strict';
 
-// write code here
+const list = [...document.querySelectorAll('li')];
+
+sortList(list);
+getEmployees(list);
+
+function sortList(param) {
+  let listCopy = param;
+
+  listCopy = listCopy.sort((a, b) => {
+    const paramA = parseNum(a.getAttribute('data-salary'));
+    const paramB = parseNum(b.getAttribute('data-salary'));
+
+    return paramB - paramA;
+  });
+
+  document.body.children[1].remove();
+
+  const newData = document.createElement('ul');
+
+  for (let i = 0; i < listCopy.length; i++) {
+    newData.append(listCopy[i]);
+    document.body.append(newData);
+  };
+};
+
+function parseNum(par) {
+  let result = '';
+
+  for (const char of par) {
+    if (
+      '1234567890'.includes(char)) {
+      result += char;
+    }
+
+    if (
+      ','.includes(char)) {
+      result += '.';
+    };
+  };
+
+  return +result;
+};
+
+function getEmployees(param) {
+  const result = [];
+
+  for (let i = 0; i < param.length; i++) {
+    const person = {};
+
+    person.name = param[i].innerText;
+    person.position = param[i].dataset.position;
+    person.salary = param[i].dataset.salary;
+    person.age = param[i].dataset.age;
+    result.push(person);
+  };
+
+  return result;
+};
