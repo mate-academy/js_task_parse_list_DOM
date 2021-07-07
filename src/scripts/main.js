a2b7b28a33fca55da8277f22eb39d647836d01d7
Bishko <yurik322412@gmail.com>
2021-07-07 16:20:53 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const peopleList = document.querySelector('ul');
+const peopleItems = [...document.querySelectorAll('li')];
+
+const convertSalary = person => (
+  +(person.dataset.salary.slice(1).split(',').join(''))
+);
+
+const sortList = list => {
+  list.sort((a, b) => convertSalary(b) - convertSalary(a));
+
+  return peopleList.append(...peopleItems);
+};
+
+const getEmployees = list => list.map(
+  person => ({
+    name: person.innerText,
+    ...person.dataset,
+  }));
+
+sortList(peopleItems);
+getEmployees(peopleItems);
