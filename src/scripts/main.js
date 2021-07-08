179b05574e08a81a80be1f4e7e4658626e0d5938
David Dohru <tronsmartmy@gmail.com>
2021-07-08 12:19:57 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const allLiElement = [ ...document.querySelectorAll('li') ];
+const dateList = [];
+const accessToUlTag = document.querySelector('ul');
+
+allLiElement.map(item => (item.dataset.salary = Number(item.dataset.salary
+  .slice(1, item.dataset.salary.length).split(',').join(''))));
+
+allLiElement.map(item => (item.dataset['name'] = item.textContent)
+  && (item.dataset.name = item.dataset.name.trim())
+  && dateList.push(item.dataset));
+
+const sortBySalary = (array) => array.sort((a, b,) => b.salary - a.salary);
+
+accessToUlTag.innerHTML = sortBySalary(dateList).map(x =>
+  `<li>
+    ${x.name}
+  </li>`).join('');
