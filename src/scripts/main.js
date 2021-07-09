57d871bfff895b1259735bac972befeb2161344c
David Dohru <tronsmartmy@gmail.com>
2021-07-09 13:13:31 +0300

    fixed name of the variables

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,20 @@
 'use strict';
 
-const acceesToAllLiElement = [ ...document.querySelectorAll('li') ];
-const accessToList = document.querySelector('ul');
+const employees = [ ...document.querySelectorAll('li') ];
+const employeesList = document.querySelector('ul');
 
-let datA = acceesToAllLiElement.map((x, i) => {
-  x.dataset.name = acceesToAllLiElement[i].textContent.trim();
+const employeesListWithName = employees.map((item, index) => {
+  item.dataset.name = employees[index].textContent.trim();
 
-  return x.dataset;
+  return item.dataset;
 });
 
 const convertSalary = (str) => +(str.slice(1, str.length).split(',').join(''));
 
-datA = (datA.sort((a, b) => convertSalary(b.salary) - convertSalary(a.salary)));
+employeesListWithName.sort((a, b) =>
+  convertSalary(b.salary) - convertSalary(a.salary));
 
-accessToList.innerHTML = (datA).map(x =>
+employeesList.innerHTML = (employeesListWithName).map(item =>
   `<li>
-    ${x.name}
+    ${item.name}
   </li>`).join('');
