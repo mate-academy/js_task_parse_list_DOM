cea3e073603a5af27411ef748fb70bf0f62c83ee
sergei-trofimov <feast.st@gmail.com>
2021-07-14 20:29:01 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const listOfPeople = document.querySelector('ul');
+const listOfEmployees = [...document.querySelectorAll('li')];
+
+const transformSalary = function(person) {
+  return +person.dataset.salary.slice(1).split(',').join('');
+};
+
+const sortList = function(list) {
+  list.sort((person1, person2) => {
+    return transformSalary(person2) - transformSalary(person1);
+  });
+
+  return listOfPeople.append(...listOfEmployees);
+};
+
+const getEmployees = function(list) {
+  return list.map(person => (
+    {
+      name: person.innerText,
+      ...person.dataset,
+    }
+  ));
+};
+
+sortList(listOfEmployees);
+getEmployees(listOfEmployees);
