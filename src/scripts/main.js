42fc30ad2ea070c3a364b470d1763f09f3c35680
Kyrylo Pyniv <pynivkm@gmail.com>
2021-07-25 14:17:29 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
+const empList = [...document.getElementsByTagName('ul')];
+const employeeData = [...document.getElementsByTagName('li')];
+
+function sortList(list) {
+  return list.sort(function(prev, current) {
+    return +current.dataset.salary.replace('$', '').replace(',', '')
+    - +prev.dataset.salary.replace('$', '').replace(',', '');
+  });
+};
+
+function getEmployees(list) {
+  list.innerHTML = '';
+
+  return employeeData.forEach(employee => list.append(employee));
+};
+
+sortList(employeeData);
+getEmployees(empList);
 // write code here
