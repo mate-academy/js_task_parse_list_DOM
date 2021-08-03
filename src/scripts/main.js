6dbf31fdab0807065095064427a71091e920b43e
Tymofii Fesenko <tymofii.fesenko1999@gmail.com>
2021-08-03 10:08:28 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write code here
+const lis = document.querySelectorAll('li');
+
+const liTexts = [...lis].map(el => el.innerText);
+const liTextData = [...lis].map(el => el.dataset);
+
+function getEmployees() {
+  const employArr = [];
+
+  for (let i = 0; i < liTexts.length; i++) {
+    employArr.push({});
+    employArr[i].name = liTexts[i];
+    employArr[i].position = liTextData[i].position;
+    employArr[i].salary = liTextData[i].salary;
+    employArr[i].age = liTextData[i].age;
+    lis[i].innerText = employArr[i].name;
+  };
+
+  return employArr;
+};
+
+const listArr = getEmployees();
+
+function sortList() {
+  const sortedArray = listArr.sort((a, b) => {
+    let aString = a.salary;
+
+    aString = parseFloat(aString.substring(1).replace(',', ''));
+
+    let bString = b.salary;
+
+    bString = parseFloat(bString.substring(1).replace(',', ''));
+
+    return bString - aString;
+  });
+
+  return sortedArray;
+};
+
+const sorted = sortList();
+
+for (let i = 0; i < liTexts.length; i++) {
+  lis[i].innerText = sorted[i].name;
+};
