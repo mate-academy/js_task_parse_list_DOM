8417c844d4f7c2292a25c43560917617f3de9435
Lenur Azizov <lenur29azizov@gmail.com>
2021-08-13 14:25:37 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,32 @@
 'use strict';
 
-function getEmployees(list) {
-  const arrayOfEmployes = [];
-
-  for (let i = 0; i < list.length; i++) {
-    const employee = {};
-
-    employee.name = list[i].textContent.trim();
-    employee.position = list[i].dataset.position;
-    employee.salary = list[i].dataset.salary;
-    employee.age = list[i].dataset.age;
-
-    arrayOfEmployes.push(employee);
-  }
-
-  return arrayOfEmployes;
-}
-
 function sortList(array) {
-  const sortedArray = array
-    .sort((employeeA, employeeB) =>
-      convertToNumber(employeeB.salary) - convertToNumber(employeeA.salary));
+  const sortedArray = array.sort((employeeA, employeeB) =>
+    convertToNumber(employeeB.salary) - convertToNumber(employeeA.salary));
 
   for (let i = 0; i < listOfEmployees.length; i++) {
-    listOfEmployees[i].dataset.textContent = sortedArray[i].name;
+    listOfEmployees[i].innerText = sortedArray[i].name;
     listOfEmployees[i].dataset.position = sortedArray[i].position;
     listOfEmployees[i].dataset.salary = sortedArray[i].salary;
     listOfEmployees[i].dataset.age = sortedArray[i].age;
   }
+}
+
+function getEmployees(list) {
+  const employees = [];
+
+  for (let i = 0; i < list.length; i++) {
+    const employee = {};
+
+    employee.name = list[i].innerText.trim();
+    employee.position = list[i].dataset.position;
+    employee.salary = list[i].dataset.salary;
+    employee.age = list[i].dataset.age;
+
+    employees.push(employee);
+  }
+
+  return employees;
 }
 
 function convertToNumber(item) {
