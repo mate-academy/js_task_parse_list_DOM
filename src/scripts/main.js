b84c875b8bd171bc66667f8bb7f25aad5c5aa9de
Ihor Nahnybida <igor653279@gmail.com>
2021-08-17 01:24:39 +0300

    fixed the sorting function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,27 @@
 'use strict';
 
 function sortBySalary(people) {
-  return people.sort((current, next) => (
-    next.salary.replace(/[$,]/g, '') - current.salary.replace(/[$,]/g, '')
-  ));
+  const sorted = [...people.children].sort(({ dataset: x }, { dataset: y }) =>
+    salaryToNumber(y.salary) - salaryToNumber(x.salary)
+  );
+
+  sorted.forEach(employee => list.append(employee));
 }
 
+const salaryToNumber = (salaryString) => {
+  return +salaryString.replace(/[$,]/g, '');
+};
+
 function getEmployeeObjects(people) {
-  return [...people].map(employee => ({
-    name: employee.innerText,
-    position: employee.dataset.position,
-    salary: employee.dataset.salary,
-    age: employee.dataset.age,
+  return [...people.children].map(({ innerText, dataset }) => ({
+    name: innerText,
+    position: dataset.position,
+    salary: dataset.salary,
+    age: dataset.age,
   }));
 }
 
 const list = document.querySelector('ul');
-const employees = document.querySelectorAll('li');
-const employeeObjects = getEmployeeObjects(employees);
-const sortedBySalary = sortBySalary(employeeObjects);
 
-list.innerHTML = sortedBySalary.map(employee => `
-  <li
-    data-position=${employee.position}
-    data-salary=${employee.salary}
-    data-age=${employee.age}
-  >
-    ${employee.name}
-  </li>
-`).join('');
+getEmployeeObjects(list);
+sortBySalary(list);
