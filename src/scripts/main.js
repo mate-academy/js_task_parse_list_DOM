8d3c44f03873c2b43131e889d6a834da193ba11d
Sasha Lifashkin <alifashkin@outlook.com>
2021-08-17 19:35:41 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const ul = document.querySelector('ul');
+
+function strToNum(str) {
+  return +str.slice(1).split(',').join('');
+}
+
+function getEmployees(listEmployees) {
+  return [...listEmployees].map(
+    item => {
+      return {
+        name: item.innerText,
+        position: item.dataset.position,
+        salary: item.dataset.salary,
+        age: item.dataset.age,
+      };
+    }
+  );
+}
+
+function sortList(listSort) {
+  const listArr = [...list].sort(
+    (a, b) => strToNum(b.dataset.salary) - strToNum(a.dataset.salary));
+
+  return listArr;
+}
+
+ul.append(...sortList(list));
+
+getEmployees(list);
