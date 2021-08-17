eee121ad31e8e2da5e08beb3836180768e4747ba
VeronikaLushchik <loaaary212@gmail.com>
2021-08-17 16:19:37 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write code here
+const listOfEmployees = document.querySelector('ul');
+const sortedList = sortList(listOfEmployees);
+const objList = getEmployees(listOfEmployees);
+
+sortedList.forEach(person => listOfEmployees.append(person));
+
+function sortList(list) {
+  return [...list.children].sort(sort);
+
+  function sort(a, b) {
+    return strToNumber(b.dataset.salary) - strToNumber(a.dataset.salary);
+  }
+};
+
+function strToNumber(string) {
+  return string.replace('$', '').split(',').join('');
+};
+
+function getEmployees(list) {
+  return [...list.children].map(employee => ({
+    name: employee.innerText,
+    position: employee.dataset.position,
+    salary: employee.dataset.salary,
+    age: employee.dataset.age,
+  }));
+}
