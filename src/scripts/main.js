319fde66915cc0ae36eb1859b8a2f54ebb103e1f
SashaRudenko <for_london@ukr.net>
2021-08-17 18:21:30 +0300

    solution edited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
 // write code here
+const personsHTMLCollection = document.querySelectorAll('li');
+
+function formatNumbers(num) {
+  return Number(num.slice(1, num.indexOf(',')) + num.slice(-3));
+}
+
+const dataArray = Array.prototype.slice.call(personsHTMLCollection);
+
+dataArray.sort(function(a, b) {
+  return formatNumbers(b.dataset.salary) - formatNumbers(a.dataset.salary);
+});
+
+function getEmployees(list) {
+  const data = document.querySelector('ul');
+  const listLength = list.length;
+
+  data.innerHTML = '';
+
+  for (let i = 0; i < listLength; i++) {
+    document.querySelectorAll('ul')[0].innerHTML += list[i].outerHTML;
+  }
+}
+
+getEmployees(dataArray);
