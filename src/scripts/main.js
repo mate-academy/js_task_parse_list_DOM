0e497237f129ee6d76439f79ae30b71f97e27c32
Alexandr Somov <aleksandr.somov2011@gmail.com>
2021-08-20 20:35:38 +0300

    'solution'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listItem = list.querySelectorAll('li');
+
+const sortList = () => {
+  const strToNum = (str) => {
+    return str.split('$')[1].split(',').join('');
+  };
+  const sortListItem = [...listItem].sort(
+    (a, b) => strToNum(b.dataset.salary) - strToNum(a.dataset.salary)
+  );
+
+  return (list.innerHTML = `${sortListItem
+    .map((item) => `<li>${item.innerHTML}</li>`)
+    .join('')}`);
+};
+
+const getEmployees = () => {
+  const userArr = [];
+  const obj = {};
+
+  [...listItem].map((item) => {
+    obj.name = item.innerText;
+    obj.position = item.dataset.position;
+    obj.salary = item.dataset.salary;
+    obj.age = item.dataset.age;
+    userArr.push(obj);
+  });
+
+  return userArr;
+};
+
+sortList();
+getEmployees();
