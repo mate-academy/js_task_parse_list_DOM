1db911918881c1ad606d1bd434b2d06688c3b01b
Roman Minchenko <minchenko.poma@gmail.com>
2021-08-24 18:14:59 +0300

    parse_list_DOM-done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const listParent = document.querySelector('ul');
+const list = document.querySelectorAll('li');
+const getNumFromStr = (string) => {
+  return Number(string.replace('$', '').replace(/,/g, ''));
+};
+
+function sortList() {
+  const sortedList = [...list].sort((a, b) => {
+    return getNumFromStr(b.dataset.salary) - getNumFromStr(a.dataset.salary);
+  });
+
+  sortedList.forEach(item => listParent.append(item));
+
+  return sortedList;
+};
+
+function getEmployees() {
+  const array = [...list].map(item => {
+    const obj = {};
+
+    obj.name = item.innerText;
+    obj.position = item.dataset.position;
+    obj.salary = item.dataset.salary;
+    obj.age = item.dataset.age;
+
+    return obj;
+  });
+
+  return array;
+}
+
+getEmployees();
+sortList();
