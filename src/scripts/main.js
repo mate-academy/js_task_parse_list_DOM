2f36ea5e7447c46ccf00d970ae8cb095bd5eb572
Andriy Shevtsiv <andriy.shevtsiv@gmail.com>
2021-08-25 13:34:19 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write code here
+
+const array = [...document.querySelectorAll('li')];
+const element = document.querySelector('ul');
+
+function sortList(list, callback) {
+  list.sort((a, b) => callback(b.dataset.salary) - callback(a.dataset.salary));
+
+  return element.append(...list);
+};
+
+function stringToNumber(str) {
+  return +str.replace(/[^0-9]/g, '');
+}
+
+function getEmployees(list) {
+  return list.map(employee => (
+    {
+      name: employee.innerText,
+      ...employee.dataset,
+    }
+  ));
+}
+
+sortList(array, stringToNumber);
+getEmployees(array);
