51a41dfbe9dcc9f85ee4d6d50d76900b38e38b18
Roman <romakelemen@gmail.com>
2021-09-01 14:23:34 +0300

    solut10n

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
+debugger;
 
-// write code here
+function getEmployees(list = document.getElementsByTagName('li')) {
+  const people = [...list].map(function (item) {
+    const result = {
+      name: item.textContent.trim(),
+      position: item.getAttribute('data-position'),
+      salary: parseFloat(item.getAttribute('data-salary').replace(',', '').replace('$', '')),
+      age: parseInt(item.getAttribute('data-age')),
+    };
+
+    return result;
+  })
+
+  return people;
+}
+
+function sortList(list = getEmployees()) {
+  const people  = list.map((item) => Object.assign({}, item));
+
+  people.sort((a, b) => b.salary - a.salary);
+
+  return people;
+}
+
+sortList();
