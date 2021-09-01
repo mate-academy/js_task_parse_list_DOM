ecd41d6558c4e51281720c4506785d339fe37845
Tykhon Shuliak <tiandzh2013@gmail.com>
2021-09-01 19:16:42 +0300

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
 // write code here
+
+const list = document.querySelectorAll('li');
+
+function sortList(lists) {
+  return [...lists].sort((a, b) => {
+    const textA = a.dataset.salary.split('$').join('').split(',').join('');
+    const textB = b.dataset.salary.split('$').join('').split(',').join('');
+
+    return textB - textA;
+  }).forEach(el => el.parentNode.append(el));
+}
+
+function getEmployees(lists) {
+  const mass = [];
+
+  for (let i = 0; i < lists.length; i++) {
+    const objOrder = {
+      'name': lists[i].textContent.trim(),
+      'position': lists[i].dataset.position,
+      'salary': lists[i].dataset.salary,
+      'age': lists[i].dataset.age,
+    };
+
+    mass.push(objOrder);
+
+  }
+
+  return mass;
+}
+
+// console.log(getEmployees(list));
+
+sortList(list);
+getEmployees(list);
