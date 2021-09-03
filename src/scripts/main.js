e2853c7e2c9c3e64cae0bc115c3b04e98d8e8ca1
Roman <romakelemen@gmail.com>
2021-09-03 15:11:45 +0300

    'Solved;

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,23 @@
 'use strict';
-debugger;
 
 function getEmployees(list = document.getElementsByTagName('li')) {
-  const people = [...list].map(function (item) {
+  const people = [...list].map(function(item) {
     const result = {
       name: item.textContent.trim(),
       position: item.getAttribute('data-position'),
-      salary: parseFloat(item.getAttribute('data-salary').replace(',', '').replace('$', '')),
+      salary: parseFloat(item.getAttribute('data-salary')
+        .replace(',', '').replace('$', '')),
       age: parseInt(item.getAttribute('data-age')),
     };
 
     return result;
-  })
+  });
 
   return people;
 }
 
 function sortList(list = getEmployees()) {
-  const people  = list.map((item) => Object.assign({}, item));
+  const people = list.map((item) => Object.assign({}, item));
 
   people.sort((a, b) => b.salary - a.salary);
 
