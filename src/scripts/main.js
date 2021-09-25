e6b61b0cd2a9f74b31e75b13b5142742cd03e95d
bohdan_kochetov <bodja1208@gmail.com>
2021-09-25 20:34:23 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const people = [];
+
+function sortList() {
+  list
+    .forEach(item => people.push({
+      name: item.innerText,
+      ...item.dataset,
+    }));
+
+  people.sort((a, b) => convert(b.salary) - convert(a.salary));
+
+  function convert(str) {
+    return +str.replace(/\D/g, '');
+  }
+}
+
+function getEmployees() {
+  for (let i = 0; i < people.length; i++) {
+    list[i].innerText = people[i].name;
+    list[i].dataset.position = people[i].position;
+    list[i].dataset.salary = people[i].salary;
+    list[i].dataset.age = people[i].age;
+  }
+}
+sortList();
+getEmployees();
