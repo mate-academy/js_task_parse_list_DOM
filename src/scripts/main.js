f0970f5f674d5ac8b1499b8c80d4f4a072e751f2
Dmytro Baltak <dmitrybaltak@gmail.com>
2021-09-27 23:54:51 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listItems = [...list.children];
+
+listItems.sort((a, b) => {
+  const aSalary = a.dataset.salary;
+  const bSalary = b.dataset.salary;
+
+  return bSalary.localeCompare(aSalary, 'en', { numeric: true });
+});
+
+listItems.map(item => list.appendChild(item));
