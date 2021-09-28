6e1a1593ab6c3a2dbd41cd222e3d76205ebceef3
Dmytro Baltak <dmitrybaltak@gmail.com>
2021-09-28 14:43:08 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function sortList(arr) {
+  const listItems = [...arr.children];
+
+  listItems.sort((a, b) => {
+    const aSalary = a.dataset.salary;
+    const bSalary = b.dataset.salary;
+
+    return bSalary.localeCompare(aSalary, 'en', { numeric: true });
+  });
+
+  listItems.forEach(employee => arr.append(employee));
+}
+
+function getEmployees(arr) {
+  return [...arr.children].map((employee) => {
+    const { dataset: {
+      position,
+      salary,
+      age,
+    } } = employee;
+
+    return {
+      name: employee.innerText,
+      position,
+      salary,
+      age,
+    };
+  });
+}
+
+sortList(list);
+getEmployees(list);
