4764b041adbb55f4466a501341e501a6483fb15c
Aleksandr Maltsev <0po4ta@gmail.com>
2021-10-02 21:38:13 +0400

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const listLi = document.querySelectorAll('li');
+const ulItem = document.querySelector('ul');
+
+const sortList = list => {
+  return [...list].sort((a, b) =>
+    +b.dataset.salary.replace(/[$,]/, '')
+    - +a.dataset.salary.replace(/[$,]/, ''));
+};
+
+const sorted = sortList(listLi);
+
+for (const item of sorted) {
+  ulItem.append(item);
+}
+
+const getEmployees = list => {
+  return [...list].map(person => ({
+    name: person.innerText,
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+};
+
+sortList(listLi);
+getEmployees(listLi);
