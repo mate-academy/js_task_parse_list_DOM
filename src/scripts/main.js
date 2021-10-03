306a6d21acfd059fb4ab1521d577685a58379bd3
Andrii Malyshev <andrey.malyshev.97@gmail.com>
2021-10-03 17:12:02 +0300

    update code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,27 +3,15 @@
 const people = document.querySelector('ul');
 const peopleArray = [...people.children];
 
-const sortList = () => {
-  peopleArray.sort((a, b) =>
+const sortList = () => peopleArray
+  .sort((a, b) =>
     (+b.dataset.salary.replace(/\D/g, ''))
-    - (+a.dataset.salary.replace(/\D/g, '')));
+    - (+a.dataset.salary.replace(/\D/g, '')))
+  .map(person => people.append(person));
 
-  for (let i = 0; i < peopleArray.length; i++) {
-    people.append(peopleArray[i]);
-  }
-};
-
-const getEmployees = () => {
-  const objectArray = [];
-
-  for (let i = 0; i < peopleArray.length; i++) {
-    const object = { name: peopleArray[i].innerText };
-
-    objectArray.push(Object.assign(object, peopleArray[i].dataset));
-  };
-
-  return objectArray;
-};
+const getEmployees = () =>
+  peopleArray.map(object =>
+    Object.assign({ name: object.innerText }, object.dataset));
 
 sortList();
 getEmployees();
