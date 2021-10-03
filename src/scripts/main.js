00ae21525f597e4710f64c9209696df8bbb784bb
Andrii Malyshev <andrey.malyshev.97@gmail.com>
2021-10-03 14:23:53 +0300

    js_task_parse_list_DOM done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const people = document.querySelector('ul');
+const peopleArray = [...people.children];
+
+const sortList = () => {
+  peopleArray.sort((a, b) =>
+    (+b.dataset.salary.replace(/\D/g, ''))
+    - (+a.dataset.salary.replace(/\D/g, '')));
+
+  for (let i = 0; i < peopleArray.length; i++) {
+    people.append(peopleArray[i]);
+  }
+};
+
+const getEmployees = () => {
+  const objectArray = [];
+
+  for (let i = 0; i < peopleArray.length; i++) {
+    const object = { name: peopleArray[i].innerText };
+
+    objectArray.push(Object.assign(object, peopleArray[i].dataset));
+  };
+
+  return objectArray;
+};
+
+sortList();
+getEmployees();
