037e15aebe85e227969abf7582f457667b5288ca
Mariia Kovach <maria_kovach@ukr.net>
2021-10-13 14:47:42 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listOfPeople = [...list.children];
+
+const sortBySalary = () => listOfPeople
+  .sort((a, b) =>
+    (+b.dataset.salary.replace('$', '').split(',').join(''))
+      - (+a.dataset.salary.replace('$', '').split(',').join('')))
+  .forEach(person => list.append(person));
+
+const getListOfEmployees = () =>
+  listOfPeople.map(person =>
+    Object.assign({ name: person.innerText }, person.dataset));
+
+sortBySalary();
+getListOfEmployees();
