324c096a6d4b3658ab212b0e1522bf1fa7f0f56c
Ilona Shulha <ilon.sh.ilo@gmail.com>
2021-10-17 16:06:56 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const u = document.querySelector('ul');
+
+function sortList(list) {
+  const listSort = [...list.children].sort((a, b) => {
+    function GetNumSalary(item) {
+      return Number(item.dataset.salary.slice(1).split(',').join(''));
+    }
+
+    return GetNumSalary(b) - GetNumSalary(a);
+  });
+
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+
+  for (let i = 0; i < listSort.length; i++) {
+    list.append(listSort[i]);
+  }
+}
+sortList(u);
+
+function getEmployees(list) {
+  const allEmployees = [...list.children];
+  const result = [];
+
+  allEmployees.forEach(item => {
+    result.push({
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    });
+  });
+
+  return result;
+}
+getEmployees(u);
