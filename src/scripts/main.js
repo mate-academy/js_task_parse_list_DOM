e931278cb2995c3ab8d91aabc29cf0cc80deb186
Dima Narozhnyi <dmitriy.narozhnyi@gmail.com>
2021-10-25 13:20:39 +0300

    fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const listOfEmployees = document.querySelectorAll('li');
+const list = document.querySelector('ul');
 
 function getNumberFromString(string) {
   return Number(string.slice(1).split(',').join(''));
@@ -11,13 +12,11 @@ function sortList(listItems) {
     .sort((a, b) => getNumberFromString(b.dataset.salary)
       - getNumberFromString(a.dataset.salary));
 
-  const listing = document.querySelector('ul');
-
-  listing.append(...sortedList);
+  list.append(...sortedList);
 }
 
-function getEmployees(list) {
-  const employees = list.map(listItem => Object.assign({
+function getEmployees(table) {
+  const employees = table.map(listItem => Object.assign({
     name: listItem.textContent.trim(),
   }, listItem.dataset));
 
