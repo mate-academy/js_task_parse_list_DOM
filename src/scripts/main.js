2256483b99e50a56d75406359bce09583274a6b9
Dina Samoilova <s13.dina@gmail.com>
2021-10-30 00:04:36 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+function getNumber(string) {
+  return Number(string.replace('$', '').replace(',', ''));
+}
+
+function getEmployees(elementList) {
+  const list = [...liElements].map(el =>
+    ({
+      name: el.innerText,
+      position: el.dataset.position,
+      salary: getNumber(el.dataset.salary),
+      age: el.dataset.age,
+    }));
+
+  return list;
+}
+
+function sortList(elementList) {
+  const list = document.querySelector('ul');
+
+  const sorted = [...liElements].sort((a, b) =>
+    getNumber(b.dataset.salary) - getNumber(a.dataset.salary));
+
+  for (const item of sorted) {
+    list.append(item);
+  }
+
+  return list;
+}
+
+const liElements = document.querySelector('li');
+
+sortList(liElements);
+getEmployees(liElements);
