2db9adbf3675d674ca0ba749988a686df9847aba
Dima Permiakov <dimapermyakov2532@gmail.com>
2021-11-07 21:52:59 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const employ = document.querySelectorAll(`[data-salary]`);
+const list = document.querySelector('ul');
+
+function sortList(a, b) {
+  const result = +b.dataset.salary
+    .split('').splice(1).join('')
+    .split(',').join('')
+    - a.dataset.salary.split('')
+      .splice(1).join('').split(',').join('');
+
+  return result;
+}
+
+const sortAmploy = [...employ].sort(sortList);
+
+sortAmploy.map(elem => {
+  list.append(elem);
+});
