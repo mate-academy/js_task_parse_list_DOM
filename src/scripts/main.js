fd720f6e050865637a306d18dc9ab2087c7680b0
Oleh Khashchevskyi <hashchevskyj@gmail.com>
2021-11-21 22:40:47 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+
+function sortList(list) {
+  const sortedList = [...list.children].map(item => {
+    item.dataset.salary = item.dataset.salary.replace(/[$,]/g, '');
+
+    return item;
+  }).sort((a, b) => b.dataset.salary - a.dataset.salary);
+
+  list.innerHTML = '';
+
+  for (const item of sortedList) {
+    list.append(item);
+  }
+}
+
+function getEmployees(list) {
+  const arr = [...list.children].map(item => ({
+    name: item.innerText,
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+
+  return arr;
+}
+
+sortList(ul);
+getEmployees(ul);
