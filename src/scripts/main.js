3559ee41e7987827cb81988be481be59994f11fc
unknown <skirik0117@gmail.com>
2021-11-22 13:18:42 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const listOfEmployees = document.querySelector('ul');
+
+function getEmployees(list) {
+  const employees = [...list.children];
+
+  return employees;
+}
+
+function sortList(list) {
+  const callBack = (el) => (
+    +el.dataset.salary.replace(/[$,]/g, '')
+  );
+
+  const salary = getEmployees(listOfEmployees).sort((a, b) => (
+    callBack(b) - callBack(a)
+  ));
+
+  list.innerHTML = `
+    ${salary.map(employee => `
+      <li
+        data-position="${employee.dataset.position}"
+        data-salary="${employee.dataset.salary}"
+        data-age="${employee.dataset.age}"
+      >
+        ${employee.innerText}
+      </li>
+    `).join('')}
+  `;
+}
+
+getEmployees(listOfEmployees);
+sortList(listOfEmployees);
