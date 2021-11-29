a6025640e0490de3a35c814385bfb37758089a44
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2021-11-29 15:50:24 +0200

    changed function sortList, added function to convert string to number, put out of function variables

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,51 +9,40 @@ class Employee {
   }
 }
 
+function stringToNumber(string) {
+  const resultNumber = string.split(',').join('').replace('$', '');
+
+  return resultNumber;
+}
+
+const nodeListofLi = document.querySelectorAll('li');
+const nodelistOfUl = document.querySelector('ul');
+
 function getEmployees(list) {
-  const nodeListofLi = document.querySelectorAll(list);
   const arrayOfEmployees = [];
 
-  for (const value of nodeListofLi) {
-    const salaryNumber = value.dataset.salary.split(',').join('')
-      .replace('$', '');
+  for (const value of list) {
+    const salaryNumber = value.dataset.salary;
 
     arrayOfEmployees.push(new Employee(value.innerText, value.dataset.position,
-      salaryNumber, value.dataset.age));
+      stringToNumber(salaryNumber), value.dataset.age));
   }
 
   return arrayOfEmployees;
 }
 
 function sortList(list) {
-  const arrayOfEmployees = getEmployees(list);
+  const arrayOfEmployees = [];
 
-  arrayOfEmployees.sort((a, b) => b.salary - a.salary);
-
-  const ul = document.querySelector('ul');
-
-  ul.remove();
-
-  const h1 = document.querySelector('h1');
-
-  h1.insertAdjacentHTML('afterend', `
-    <ul>
-    </ul>
-  `);
-
-  const ulSelect = document.querySelector('ul');
-
-  for (const employee of arrayOfEmployees) {
-    ulSelect.insertAdjacentHTML('beforeend', `
-    <li
-        data-position=${employee.position}
-        data-salary=${employee.salary}
-        data-age=${employee.age}
-      >
-        ${employee.name}
-      </li>
-    `);
+  for (const value of list) {
+    arrayOfEmployees.push(value);
   }
+
+  arrayOfEmployees.sort((a, b) =>
+    stringToNumber(b.dataset.salary) - stringToNumber(a.dataset.salary));
+
+  arrayOfEmployees.map(value => nodelistOfUl.append(value));
 }
 
-sortList('li');
-getEmployees('li');
+sortList(nodeListofLi);
+getEmployees(nodeListofLi);
