4406ea4d27892e8714b65f8f7eda278dd3d631b7
Pasechnik Vitaly <pasechnik.vitaly.11@gmail.com>
2021-12-02 01:05:23 +0200

    latest saved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+const sorted = function sortList(items) {
+  const arr = [...items];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let y = 1; y < arr.length; y++) {
+      let hub = 0;
+
+      if (arr[i].dataset.salary.replace(/\$|,/gm, '')
+       < arr[y].dataset.salary.replace(/\$|,/gm, '')) {
+        hub = arr[i];
+        arr[i] = arr[y];
+        arr[y] = hub;
+      }
+    }
+  }
+
+  return arr;
+};
+
+function getEmployees(items) {
+  const arr = [];
+
+  for (const key of items) {
+    const obj = {};
+
+    obj.name = key.innerText;
+    obj.position = key.dataset.position;
+    obj.salary = key.dataset.salary;
+    obj.age = key.dataset.age;
+
+    arr.push(obj);
+  };
+
+  return arr;
+};
+
+getEmployees(sorted(list));
