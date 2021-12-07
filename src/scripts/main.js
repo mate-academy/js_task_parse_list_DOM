dc601a71c3b003f3cf9fd758ed5ba65e88cf0126
Vasiliy <voronvasia@gmail.com>
2021-12-07 05:37:56 +0200

    task complite

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const list = [...document.querySelectorAll('li')];
+const ul = document.querySelector('ul');
+
+function sortList(employeeList) {
+  const salary = employeeList.sort((
+    a, b) => +b.dataset.salary.split('$').join('').split(',').join('')
+    - +a.dataset.salary.split('$').join('').split(',').join(''));
+
+  for (const item of salary) {
+    ul.append(item);
+  }
+
+  return ul;
+}
+
+function getEmployees(employeeList) {
+  const arr = [];
+
+  for (const person of employeeList) {
+    arr.push({
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    });
+  }
+
+  return arr;
+}
+
+sortList(list);
+getEmployees(list);
