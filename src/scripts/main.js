1e3ac7ffc5e551d67ade2c171d2ddaddb70f48c3
Vova Alputov <alputov1998@gmail.com>
2021-12-20 19:02:34 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+const sortList = (li) => {
+  const sorted = [...list].sort((a, b) =>
+    +b.dataset.salary.replace(/[,$]/g, '')
+    - +a.dataset.salary.replace(/[,$]/g, ''));
+
+  const ul = document.querySelector('ul');
+
+  for (let i = 0; i < sorted.length; ++i) {
+    ul.appendChild(sorted[i]);
+  }
+
+  return sorted;
+};
+
+const getEmployees = (li) => {
+  const result = [...list].map(x => x.dataset);
+
+  return result;
+};
+
+sortList(list);
+getEmployees(list);
