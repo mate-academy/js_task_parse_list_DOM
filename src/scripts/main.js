42fff1d1207807cce24d52da2891adbf7e63d64b
Pavlo Khashchevskyi <PikiMapo92@gmail.com>
2021-12-23 15:04:54 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+const sortList = () => {
+  const newItems = [...list.children].sort((prev, next) =>
+    +next.dataset.salary.match(/[0-9]/g).join('')
+      - (+prev.dataset.salary.match(/[0-9]/g).join(''))
+  );
+
+  for (const item of newItems) {
+    list.append(item);
+  }
+};
+
+const getEmployees = () => {
+  const listOfEmployees = [...list.children].map(item => ({
+    name: item.textContent,
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+
+  return listOfEmployees;
+};
+
+sortList();
+getEmployees();
