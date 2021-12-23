de9679e64169ed7618f2eaf0e1f58db408095017
Dmytro Brushnivskyi <dimalvivatc@gmail.com>
2021-12-23 16:05:30 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,54 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const listArr = Array.from(list);
+
+function sortList(arr) {
+  const sortedList = arr.sort((a, b) => {
+    const first = a.dataset.salary
+      .slice(1)
+      .split(',')
+      .join('');
+
+    const second = b.dataset.salary
+      .slice(1)
+      .split(',')
+      .join('');
+
+    const result = +second - +first;
+
+    return result;
+  });
+
+  return sortedList;
+}
+
+const newList = sortList(listArr);
+
+const rootElement = document.querySelector('ul');
+
+for (let i = 0; i < newList.length; i++) {
+  rootElement.append(newList[i]);
+}
+
+sortList(listArr);
+
+function getEmployees(employee) {
+  const arrOfEmpoyees = [];
+  const obj = {};
+
+  for (const item of employee) {
+    obj.name = item.innerText;
+    obj.position = item.dataset.position;
+    obj.salary = item.dataset.salary;
+    obj.age = item.dataset.age;
+
+    const result = { ...obj };
+
+    arrOfEmpoyees.push(result);
+  }
+
+  return arrOfEmpoyees;
+}
+
+getEmployees(listArr);
