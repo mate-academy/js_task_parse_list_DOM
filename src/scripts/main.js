9890b2bb1769ee08f1b56163104b569c4d851a62
KHTRE <KHTREE@gmail.com>
2021-12-23 17:16:55 +0200

    works

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('[data-position]');
+const arrayFromList = [...listItems];
+
+arrayFromList.sort((a, b) => {
+  const first = +a.dataset.salary.replace(/[.,#!$?%^&*;:{}=\-_`~()]/g, '');
+  const second = +b.dataset.salary.replace(/[.,#!$?%^&*;:{}=\-_`~()]/g, '');
+
+  return second - first;
+});
+
+const list = document.querySelector('ul');
+
+for (const person of arrayFromList) {
+  list.append(person);
+}
