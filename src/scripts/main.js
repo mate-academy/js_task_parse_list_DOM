7ce08b6a3cbc64aabc50b7b6949acc0b1f7ed1ed
Anna Tovkailo <babylike24@gmail.com>
2021-12-28 17:32:34 +0200

    Solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,58 +1,61 @@
 'use strict';
 
-const queryList = document.querySelector("ul").children;
+const queryList = document.querySelector('ul').children;
 
-const getEmployees = function(listForSort) {
-  const arrayList = [...queryList].map(item =>  {
+const getEmployees = function(list) {
+  const arrayList = [...list].map(item => {
     const itemEmployee = {
       name: item.innerHTML.trim(),
-      position: item.getAttribute("data-position"),
-      salary: item.getAttribute("data-salary"),
-      age: item.getAttribute("data-age"),
-    }
-    
-    return itemEmployee
+      position: item.getAttribute('data-position'),
+      salary: item.getAttribute('data-salary'),
+      age: item.getAttribute('data-age'),
+    };
+
+    return itemEmployee;
   });
-  
+
   return arrayList;
-}
+};
 
-let listForSort = getEmployees(queryList);
+const listForSort = getEmployees(queryList);
 
-const sortList = function(listForSort) {
-  listForSort.sort(function (a, b) {
+const sortList = function(list) {
+  list.sort(function(a, b) {
     let aSort = a.salary.slice(1, 10).split(',');
+
     aSort = Number(aSort.join(''));
+
     let bSort = b.salary.slice(1, 10).split(',');
+
     bSort = Number(bSort.join(''));
-    
+
     if (aSort > bSort) {
       return -1;
     }
+
     if (aSort < bSort) {
       return 1;
     }
+
     return 0;
   });
 
-  return listForSort;
-}
+  return list;
+};
 
 sortList(listForSort);
 
-const thisList = document.querySelector("ul");
-
-for (let i = 0; i < thisList.length; i++) {
-  console.log(thisList[i]);
-}
+const thisList = document.querySelector('ul');
 
 for (let i = 0; i < listForSort.length; i++) {
-  let x = document.querySelector("li");
+  const x = document.querySelector('li');
+
   x.remove();
 }
 
 for (let i = 0; i < listForSort.length; i++) {
-  const item = document.createElement("li");
+  const item = document.createElement('li');
+
   item.innerHTML = listForSort[i].name;
   item.setAttribute('data-position', listForSort[i].position);
   item.setAttribute('data-salary', listForSort[i].salary);
@@ -60,4 +63,3 @@ for (let i = 0; i < listForSort.length; i++) {
 
   thisList.append(item);
 }
-
