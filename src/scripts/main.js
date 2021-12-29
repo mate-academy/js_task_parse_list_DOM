b4133ac39c47030b79ed98745ce3852e7da451e7
E-Sharpilo <evgeniy.sharpilo@gmail.com>
2021-12-29 13:56:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const main = document.querySelector('ul');
+
+function getNumber(str) {
+  return +str.split('$').join('').split(',').join('');
+};
+
+function solrtList(list) {
+  const sortedList = [...list.children].sort((cur, next) => {
+    return getNumber(next.dataset.salary) - getNumber(cur.dataset.salary);
+  });
+
+  main.append(...sortedList);
+}
+
+function getEmployees(list) {
+  const res = [];
+
+  for (const el of [...list.children]) {
+    res.push({
+      name: el.innerText,
+      postision: el.dataset.postision,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    });
+  }
+
+  return res;
+}
+
+solrtList(main);
+getEmployees(main);
