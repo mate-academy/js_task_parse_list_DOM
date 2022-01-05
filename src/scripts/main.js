b58bb3a05fbecf79cd092fa11d5edfed19ffa7f0
Andriy Yurkiv <88943977+andriy-yurkiv@users.noreply.github.com>
2022-01-05 17:12:08 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+const sortList = () => {
+  const sorted = [...list.children].sort((prev, next) =>
+    +next.dataset.salary.match(/[0-9]/g).join('')
+    - (+prev.dataset.salary.match(/[0-9]/g).join(''))
+  );
+
+  for (const el of sorted) {
+    list.append(el);
+  }
+};
+
+const getEmployees = () => {
+  const arrOfObjecs = [...list.children].map(el => ({
+    name: el.textContent,
+    position: el.dataset.position,
+    salary: el.dataset.salary,
+    age: el.dataset.age,
+  }));
+
+  return arrOfObjecs;
+};
+
+sortList();
+getEmployees();
