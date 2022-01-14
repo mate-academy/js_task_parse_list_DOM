ad0064a8780e7a71969c07a8ff7ae00e603ed3be
Victor Hrygorash <viktor12041991@i.ua>
2022-01-14 15:00:50 +0200

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function sortList(data) {
+  const listItems = [...data.children];
+
+  listItems
+    .sort((a, b) => b.dataset.salary.localeCompare(
+      a.dataset.salary, 'en', { numeric: true }));
+
+  listItems.forEach(employee => data.append(employee));
+}
+
+function getEmployees(data) {
+  return [...data.children].map(employee => {
+    const { dataset: { position, salary, age } } = employee;
+
+    return {
+      name: employee.innerText,
+      position,
+      salary,
+      age,
+    };
+  });
+}
+
+sortList(list);
+getEmployees(list);
