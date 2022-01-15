5e46e48cdf6c801e975fbcf1a277f99b7d836f91
Basil-Panasiuk <vasylpanasjuk2001@gmail.com>
2022-01-15 22:41:04 +0200

    bad solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,49 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const itemsOfList = document.querySelectorAll('li');
+
+const convertToNumber = (str) => {
+  return Number(str.slice(1).split(',').join(''));
+};
+
+const sortList = (people) => {
+  const salaries = [];
+
+  for (const person of people) {
+    salaries.push(person.dataset.salary);
+  }
+
+  salaries.sort((salaryA, salaryB) => {
+    const s1 = convertToNumber(salaryA);
+    const s2 = convertToNumber(salaryB);
+
+    return s2 - s1;
+  });
+
+  for (const salary of salaries) {
+    for (const item of itemsOfList) {
+      if (item.dataset.salary === salary) {
+        list.append(item);
+      }
+    }
+  }
+};
+
+const getEmployees = (someList) => {
+  const arr = [];
+
+  for (const item of someList) {
+    arr.push({
+      name: item.textContent,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    });
+  }
+
+  return arr;
+};
+
+sortList(itemsOfList);
+getEmployees(itemsOfList);
