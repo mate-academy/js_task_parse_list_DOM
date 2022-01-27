27d42989ccc26da9d82186d67e2c1475b5dc79b5
Ivan Shulhan <92580512+IvanShulhan@users.noreply.github.com>
2022-01-27 12:54:20 +0200

    my-solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const list = [...document.querySelectorAll('li')];
+const unList = document.querySelector('ul');
+
+function sortList(arr) {
+  const sortAction = val =>
+    +val.dataset.salary.split(',').join('').split('$').join('');
+
+  return arr.sort((a, b) => sortAction(b) - sortAction(a));
+}
+
+const sortedList = sortList(list);
+
+unList.innerHTML = `
+  ${sortedList.map(element =>
+    element.outerHTML
+  ).join('')}
+`;
