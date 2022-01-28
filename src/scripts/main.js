6b47ee16f6e7099606f335e2cf4781be89b58d0f
Osypiuk Dmytro <dimich578ua@gmail.com>
2022-01-28 15:08:15 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listItems = document.querySelectorAll('li');
+
+function sortingListOfPeople(listToSort) {
+  const gettingPersonSellary = (person) => {
+    return person.dataset.salary
+      .split('')
+      .filter((symbol) => !isNaN(Number(symbol)))
+      .join('');
+  };
+
+  const sortedList = [...listToSort].sort(
+    (a, b) => gettingPersonSellary(b) - gettingPersonSellary(a)
+  );
+
+  list.append(...sortedList);
+}
+
+function getEmploees(employeeList) {
+  const emplouyeesInfo = [];
+
+  for (const { innerText, dataset } of employeeList) {
+    emplouyeesInfo.push({
+      name: innerText,
+      age: dataset.age,
+      position: dataset.position,
+      salary: dataset.salary,
+    });
+  }
+
+  return emplouyeesInfo;
+};
+
+sortingListOfPeople(listItems);
+getEmploees(listItems);
