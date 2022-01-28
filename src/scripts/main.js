4fb0f5500d3b207aae0479b9c450829d3b071e07
Oleh Rak <rakole86@gmail.com>
2022-01-28 17:45:56 +0200

    parse_list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const ullist = document.querySelector('ul');
+const list = document.querySelectorAll('li');
+
+function sortList(el) {
+  const arrlist = [...el].sort((a, b) => (
+    +b.dataset.salary.slice(1).split(',').join(''))
+    - (+a.dataset.salary.slice(1).split(',').join('')));
+
+  ullist.append(...arrlist);
+
+  return arrlist;
+}
+
+function getEmployees(el) {
+  const arrlistobj = sortList(el).map(item => {
+    item.dataset.name = item.outerText;
+
+    return item.dataset;
+  });
+
+  return arrlistobj;
+}
+sortList(list);
+getEmployees(list);
