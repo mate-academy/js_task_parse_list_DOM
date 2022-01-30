5d136ced6eea78924a5e851adb92b4b8a05a47d1
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-01-30 17:29:15 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function sortList(value) {
+  const newList = [...value.children];
+  const sorting = item => item.dataset.salary.replace(/[^0-9]/g, '');
+
+  return newList.sort((item1, item2) => sorting(item1) - sorting(item2));
+}
+
+function getEmployers(value) {
+  for (const item of value) {
+    item.remove();
+    list.prepend(item);
+  }
+}
+
+getEmployers(sortList(list));
