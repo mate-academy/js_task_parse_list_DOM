007bc10f597be24139792f6ebc1cee50659c4e0b
Roman Kovalchuk <roma220702@gmail.com>
2022-02-09 14:09:19 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const initialList = document.querySelector('ul');
+const employees = [...initialList.children];
+
+for (const el of employees) {
+  el.dataset.salary.substr(1);
+}
+
+function sortList(list) {
+  list.sort((a, b) => Number(a.dataset.salary) - Number(b.dataset.salary));
+}
+
+function getEmployees(list) {
+  return list;
+}
+
+sortList(employees);
+getEmployees(employees);
+
+// const mass = [];
+// const element = document.body[1];
+// console.log(mass);
+// for (let child of element.children) {
+//   mass.push(child.dataset);
+//   mass[child.dataset].name = child.textContent;
+// }
