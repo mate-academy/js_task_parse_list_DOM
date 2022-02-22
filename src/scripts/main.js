e1fd443fdef957264c2739b9948f1604e3de4e12
Dmytro Semenenko <dmitriy.semenenko@gmail.com>
2022-02-22 17:36:04 +0200

    Solution Parse List DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('li')];
+const ul = document.querySelector('ul');
+
+function sortList(list) {
+  list.sort((a, b) => {
+    const aa = +a.dataset.salary
+      .replace(',', '')
+      .replace('$', '');
+
+    const bb = +b.dataset.salary
+      .replace(',', '')
+      .replace('$', '');
+
+    return bb - aa;
+  });
+  ul.append(...list);
+}
+
+function getEmployee(list) {
+  return list.map(employee => ({
+    name: employee.innerText,
+    ...employee.dataset,
+  }));
+}
+
+sortList(employees);
+getEmployee(employees);
