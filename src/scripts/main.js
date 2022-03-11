0a3db7a1e59abd75f2a011c17da902c5e2a16d82
Mykhailo Ianush <yanushmisha@gmail.com>
2022-03-11 14:45:09 +0200

    sort elements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const allPeople = document.querySelectorAll('li');
+const listElement = document.querySelector('ul');
+
+[...allPeople]
+  .sort(
+    (a, b) =>
+      getNumberFromSalary(b) - getNumberFromSalary(a)
+  )
+  .forEach(elem => listElement.appendChild(elem));
+
+function getNumberFromSalary(elem) {
+  return +elem.dataset.salary.slice(1).split(',').join('');
+}
