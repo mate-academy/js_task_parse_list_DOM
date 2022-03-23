d8c920fa76c84a6b18de1e5e1658dc9e6b63f1cc
Dmytro Bychkivskyi <dbychkivskyi@cccis.com>
2022-03-23 12:03:55 +0200

    Finish hw.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const listItems = [...document.querySelectorAll('li')];
+const domList = document.querySelector('ul');
+
+function parseSalary(salaryString) {
+  return +salaryString.replaceAll('$', '').replaceAll(',', '');
+}
+
+function sortList(list) {
+  list.sort(
+    (a, b) => parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary));
+  list.forEach(listItem => domList.append(listItem));
+}
+
+function getEmployees(list) {
+  return list.map(listItem => ({
+    name: listItem.innerText,
+    position: listItem.dataset.position,
+    salary: listItem.dataset.salary,
+    age: listItem.dataset.age,
+  }));
+}
+
+sortList(listItems);
+getEmployees(listItems);
