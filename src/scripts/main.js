7e2109f5f2c9d66323286c53ab9adff46ecabc4b
Maria Luschik <marialyschik@Marias-MacBook-Air.local>
2022-03-27 12:00:43 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+const employees = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  list.sort((x, y) => {
+    const prev = +x.dataset.salary.replace(',', '').replace('$', '');
+    const next = +y.dataset.salary.replace(',', '').replace('$', '');
+
+    return next - prev;
+  });
+
+  return employeesList.append(...list);
+}
+
+function getEmployees(list) {
+  return [...list].map(employee => ({
+    name: employee.textContent,
+    ...employee.dataset,
+  }));
+}
+
+sortList(employees);
+getEmployees(employees);
