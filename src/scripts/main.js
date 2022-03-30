c20a30f96bf2773306a4b809649c31b370eb7486
Batick3737 <antonandriy@ukr.net>
2022-03-30 22:23:25 +0300

    write code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const listElements = document.querySelectorAll('li');
+const ul = document.querySelector('ul');
+
+const sortList = (list) => {
+  const listSorted = [...list].sort((a, b) => b.dataset.salary.replace(
+    /,/g, '').slice(1) - a.dataset.salary.replace(/,/g, '').slice(1));
+
+  for (const li of listSorted) {
+    ul.append(li);
+  }
+};
+
+const getEmployees = (list) => {
+  return [...list].map((li) => ({
+    name: li.textContent,
+    position: li.dataset.position,
+    salary: li.dataset.salary,
+    age: li.dataset.age,
+  }));
+};
+
+sortList(listElements);
+getEmployees(listElements);
