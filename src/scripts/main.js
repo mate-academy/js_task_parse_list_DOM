cf1db4205b9077aefd078f8638a714e2c5f3a1be
Oleksii Pinchuk <oleksii.pin4uk@gmail.com>
2022-04-06 17:48:31 +0300

    emp

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const listUl = document.querySelector('ul');
+const employees = document.querySelectorAll('li');
+
+const sortList = (list) => {
+  const sortedList = [...list].sort((a, b) => (
+    +b.dataset.salary.slice(1).split(',').join('')
+    - +a.dataset.salary.slice(1).split(',').join('')
+  ));
+
+  listUl.append(...sortedList);
+};
+
+const getEmployees = (list) => {
+  return [...list].map((li) => ({
+    name: li.textContent,
+    ...(li.dataset),
+  }));
+};
+
+sortList(employees);
+getEmployees(employees);
