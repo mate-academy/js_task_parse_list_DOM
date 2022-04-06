81d6434a536a7e3bdee46fbbe4cf3cf09606dc73
Yulia Shabelna <74015631+Yulia2806@users.noreply.github.com>
2022-04-06 15:25:15 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const notitia = document.querySelector('ul');
+const allNotitia = document.querySelectorAll('li');
+
+function sortList(list) {
+  const listSorted = [...list].sort((a, b) =>
+    +b.dataset.salary.slice(1).split(',').join('')
+    - +a.dataset.salary.slice(1).split(',').join('')
+  );
+
+  notitia.append(...listSorted);
+}
+
+function getEmployees(list) {
+  return [...list].map(x => ({
+    name: x.textContent,
+    position: x.dataset.position,
+    salary: x.dataset.salary,
+    age: x.dataset.age,
+  }));
+}
+
+sortList(allNotitia);
+getEmployees(allNotitia);
