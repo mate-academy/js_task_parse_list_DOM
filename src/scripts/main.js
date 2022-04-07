bdfb430c222c8f2336a2a5859d3d3ac65836f5e9
Avdyeyenko Vitalik <dalshe.budet.sobaka@gmail.com>
2022-04-07 14:47:19 +0300

    sort implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const listEl = document.querySelector('ul');
+const employerEls = listEl.children;
+const employers = [...employerEls].map(el => ({
+  name: el.innerText,
+  position: el.dataset.position,
+  salary: parseInt(el.dataset.salary.slice(1).split(',').join('')),
+  age: parseInt(el.dataset.age),
+}));
+
+employers.sort(({ salary: salary1 }, { salary: salary2 }) => salary2 - salary1);
+
+employers.forEach(emp => {
+  const current = [...employerEls]
+    .filter(el => el.innerText === emp.name)[0];
+
+  listEl.insertAdjacentElement('beforeend', current);
+});
