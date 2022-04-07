c0353d4acdff2267152dfaf203cfcd174de474d0
Avdyeyenko Vitalik <dalshe.budet.sobaka@gmail.com>
2022-04-07 15:07:36 +0300

    Everything converted to functions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,33 @@
 'use strict';
 
+function convertSalary(salary) {
+  return parseInt(salary.slice(1).split(',').join(''));
+}
+
+// this function was in tasks list
+function getEmployersObjects(employers) {
+  return [...employers].map(el => ({
+    name: el.innerText,
+    position: el.dataset.position,
+    salary:  convertSalary(el.dataset.salary),
+    age: parseInt(el.dataset.age),
+  }));
+}
+
+function sortBySalary(list) {
+  const employers = [...list.children];
+
+  employers.sort(({ dataset: { salary: s1 } },
+    { dataset: { salary: s2 } }) => {
+    const salary1 = convertSalary(s1);
+    const salary2 = convertSalary(s2);
+
+    return salary2 - salary1;
+  });
+
+  employers.forEach(emp => listEl.insertAdjacentElement('beforeend', emp));
+}
+
 const listEl = document.querySelector('ul');
-const employerEls = listEl.children;
-const employers = [...employerEls].map(el => ({
-  name: el.innerText,
-  position: el.dataset.position,
-  salary: parseInt(el.dataset.salary.slice(1).split(',').join('')),
-  age: parseInt(el.dataset.age),
-}));
 
-employers.sort(({ salary: salary1 }, { salary: salary2 }) => salary2 - salary1);
-
-employers.forEach(emp => {
-  const current = [...employerEls]
-    .filter(el => el.innerText === emp.name)[0];
-
-  listEl.insertAdjacentElement('beforeend', current);
-});
+sortBySalary(listEl);
