7a3b2649a35a6cd8b402d698cbe3e7c71a9ce872
Ostap Tomashivskyi <o.tomash650416@gmail.com>
2022-04-09 01:32:58 +0300

    Added sorted list to DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const employeesCollection = [...document.querySelectorAll('li')];
+const ulElement = document.querySelector('ul');
+
+const convertToNumber = number =>
+  Number(number.split('').filter(num => !isNaN(num)).join(''));
+
+const sortList = list => {
+  const result = list.sort((a, b) =>
+    convertToNumber(b.dataset.salary) - convertToNumber(a.dataset.salary));
+
+  ulElement.append(...result);
+};
+
+const getEmployees = list => sortList(list).map(({ textContent, dataset }) => ({
+  ...dataset,
+  name: textContent,
+}));
+
+sortList(employeesCollection);
+
+getEmployees(employeesCollection);
