d42a3290e9364968deec98437f526aed201fe3e0
Max Shylenko <maxshylenko@gmail.com>
2022-04-13 12:12:21 +0200

    parse list DOM is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,50 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listItems = [...list.querySelectorAll('li')];
+
+const sortList = listOfItems => {
+  listOfItems.forEach(item => {
+    const numFromSalary = +(item.dataset.salary.split('$').pop()
+      .split(',').join('.'));
+
+    item.dataset.salary = numFromSalary;
+  });
+
+  const sortedList = listOfItems.sort((a, b) =>
+    b.dataset.salary - a.dataset.salary);
+
+  sortedList.forEach(element => {
+    const number = +element.dataset.salary;
+    const convertToCorrectFormat = number.toFixed(3).split('');
+
+    convertToCorrectFormat.unshift('$');
+
+    const convertedFormat = convertToCorrectFormat.join('')
+      .split('.').join(',');
+
+    element.dataset.salary = convertedFormat;
+  });
+
+  for (const person of sortedList) {
+    list.append(person);
+  }
+};
+
+const employees = [];
+
+const getEmployees = listOfEmployees => {
+  for (const emloyee of listOfEmployees) {
+    const objectFromEmployee = {
+      name: emloyee.innerText,
+      position: emloyee.dataset.position,
+      salary: emloyee.dataset.salary,
+      age: emloyee.dataset.age,
+    };
+
+    employees.push(objectFromEmployee);
+  }
+};
+
+sortList(listItems);
+getEmployees(listItems);
