bc67017b46a7eebc4fd1085b0887e2d6ddb8ab18
MariaBondar <marybondar777@gmail.com>
2022-04-18 17:26:37 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const employeesList = [...list.children];
+
+function salary(str) {
+  return +str.split('').filter(char => '0123456789'.includes(char)).join('');
+}
+
+function sortList(str) {
+  const sortedBySalary = str.sort((x, y) =>
+    salary(x.dataset.salary) - salary(y.dataset.salary)
+  );
+
+  sortedBySalary.map(element => list.append(element));
+}
+
+function getEmployees(array) {
+  return array.map(element => ({
+    name: element.textContent,
+    position: element.dataset.position,
+    salary: element.dataset.salary,
+    age: element.dataset.age,
+  }));
+}
+
+sortList(employeesList);
+getEmployees(employeesList);
