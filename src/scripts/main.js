7560141de8488dae4dbcb39faf79e8d9f1c95ec4
Alex <asultan4ik@gmail.com>
2022-05-11 17:53:05 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const liArray = document.querySelectorAll('[data-salary]');
+
+function sortList(array) {
+  [...array].sort((a, b) => {
+    const NumbA = a.dataset.salary.slice(1).split(',').join('');
+    const NumbB = b.dataset.salary.slice(1).split(',').join('');
+
+    return NumbB - NumbA;
+  }).forEach(e => document.querySelector('ul').appendChild(e));
+};
+
+sortList(liArray);
+
+function getEmployees(array) {
+  return [...array].sort((a, b) => {
+    const NumbA = a.dataset.salary.slice(1).split(',').join('');
+    const NumbB = b.dataset.salary.slice(1).split(',').join('');
+
+    return NumbB - NumbA;
+  });
+};
+
+const empoyees = getEmployees(liArray);
+
+const ulItem = document.querySelector('ul');
+
+ulItem.innerHTML = `
+${[...empoyees].map(e => `
+    <li
+      data-position=${e.dataset.position}
+      data-salary=${e.dataset.salary}
+      data-age=${e.dataset.age}
+    >
+      ${e.innerHTML}
+    </li>
+  `).join('')}
+`;
