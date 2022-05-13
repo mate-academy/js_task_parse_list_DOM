8b086e5f6a6efcc2c88274748be1ef9377f08667
Alina Yermolenko <alina.yermolenko27@gmail.com>
2022-05-13 21:59:14 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const peopleList = document.querySelector('ul');
+const people = [...peopleList.children];
+
+function sortList(list) {
+  const peopleSalary = salary =>
+    salary.dataset.salary.split('$').join('').split(',').join('');
+
+  list.sort((a, b) => {
+    return peopleSalary(b) - peopleSalary(a);
+  });
+
+  for (const num of list) {
+    peopleList.append(num);
+  }
+}
+
+function getEmployees(list) {
+  const arr = list.map(one => ({
+    'name': one.outerText,
+    'position': one.dataset.position,
+    'salary': one.dataset.salary,
+    'age': one.dataset.age,
+  }));
+
+  return arr;
+};
+
+sortList(people);
+getEmployees(people);
