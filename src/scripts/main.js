03a1e5d5e3bbac200098849d89febc9a27f4740c
Kostiantyn <havryshkostya@gmail.com>
2022-05-13 14:44:57 +0400

    cant test what i wrote

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const listArr = [...list];
+
+function convertSalaryToNumber(salary) {
+  return parseInt(salary.substring(1).split(',').join(''));
+}
+
+const sortList = function(arr) {
+  return arr.sort((x, y) =>
+    convertSalaryToNumber(x.dataset.salary)
+    - convertSalaryToNumber(y.dataset.salary));
+};
+
+const getEmployees = function(array) {
+  return array.map(person => {
+    const personObj = {
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    };
+
+    return personObj;
+  });
+};
+
+sortList(listArr);
+getEmployees(sortList);
