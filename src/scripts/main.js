148f0199ef7ecd31ace274b87088dc908c84df00
Kostiantyn <havryshkostya@gmail.com>
2022-05-16 21:49:51 +0400

    without comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,27 +3,22 @@
 const list = document.querySelectorAll('li');
 const listArr = [...list];
 
-// const listUl = document.getElementsByTagName('ul');
-
 function convertSalaryToNumber(salary) {
   return parseInt(salary.substring(1).split(',').join(''));
 }
 
-const sortList = function(arr) {
-  const sortedArr = arr.sort((x, y) =>
+const sortList = function(arrList) {
+  const sortedArr = arrList.sort((x, y) =>
     convertSalaryToNumber(y.dataset.salary)
     - convertSalaryToNumber(x.dataset.salary));
 
-  //   for (let i = 0; i < sortedArr.length; i++) {
-  //     document.getElementById('list').appendChild(sortedArr[i]);
-  //   }
   return sortedArr.forEach(element => {
     document.querySelector('ul').appendChild(element);
   });
 };
 
-const getEmployees = function(array) {
-  return array.map(person => {
+const getEmployees = function(arrList) {
+  return arrList.map(person => {
     const personObj = {
       name: person.innerText,
       position: person.dataset.position,
@@ -37,6 +32,4 @@ const getEmployees = function(array) {
 
 sortList(listArr);
 
-// Cюди треба передати просто список? чи вже відсортований?
-// Бо я не знаю як дістати вже відсортований список...
 getEmployees(listArr);
