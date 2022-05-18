97283f59969078285b212e2c4aa20b42f22be6d1
Roman Lipatov <96875973+roman-lipatov@users.noreply.github.com>
2022-05-18 19:55:50 +0300

    parse-list-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write code here
+const collection = document.querySelector('ul');
+const people = [...collection.children];
+
+// console.log(collection);
+
+function sortList(list) {
+  const peopleMoney = salary =>
+    salary.dataset.salary.split('$').join('').split(',').join('');
+
+  list.sort((a, b) => {
+    return peopleMoney(b) - peopleMoney(a);
+  });
+
+  collection.append(...list);
+}
+
+function getEmployees(list) {
+  const array = list.map(result => ({
+    'name': result.outerText,
+    'position': result.dataset.position,
+    'salary': result.dataset.salary,
+    'age': result.dataset.age,
+  }));
+
+  return array;
+}
+
+sortList(people);
+getEmployees(people);
