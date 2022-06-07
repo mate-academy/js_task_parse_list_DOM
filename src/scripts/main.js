e95121120812c9718baa4a862a91e26d400b54dc
Dmytro Semeniuk <allesdmytro@gmail.com>
2022-06-07 23:40:27 +0300

    List parsed succesfully

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const peopleCollection = document.querySelector('ul');
+const people = [...peopleCollection.children];
+
+function sortList(list) {
+  const personSalary = salary =>
+    +salary.dataset.salary.split('$').join('').split(',').join('');
+
+  list.sort((a, b) => {
+    return personSalary(b) - personSalary(a);
+  });
+
+  peopleCollection.append(...list);
+}
+
+function getEmployees(list) {
+  const employeesArray = list.map(result => ({
+    name: result.outerText,
+    position: result.dataset.position,
+    salary: result.dataset.salary,
+    age: result.dataset.age,
+  }));
+
+  return employeesArray;
+}
+
+sortList(people);
+getEmployees(people);
