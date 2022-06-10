e06a3a144d015af836ae810a287e0ba5ec85aeee
Volodumur Vitinskiy <vovahunter1989@gmail.com>
2022-06-10 12:05:19 +0300

    first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const peopleList = document.querySelector('ul');
+const peopleArr = [...peopleList.children];
+
+function sortList(list) {
+  const humanSalary = salary =>
+    +salary.dataset.salary.split('$').join('').split(',').join('');
+
+  list.sort((a, b) => {
+    return humanSalary(b) - humanSalary(a);
+  });
+
+  peopleList.append(...list);
+}
+
+function getEmployees(list) {
+  const employeesArray = list.map(result => ({
+    name: result.outerText,
+    position: result.dataset.position,
+    salary: result.dataset.salary,
+    age: result.dataset.age,
+  }));
+
+  return employeesArray;
+}
+
+sortList(peopleArr);
+getEmployees(peopleArr);
