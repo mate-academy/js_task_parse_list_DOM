22062dd9b92df820a409f8e7bf035ca8ca01f561
Mikhail Danylenko <migellyaka@gmail.com>
2022-06-13 11:13:31 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const initList = document.querySelector('ul');
+const employees = [...document.querySelectorAll('li')];
+
+function toNumber(string) {
+  let number = '';
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== ',' && string[i] !== '$') {
+      number += string[i];
+    }
+  }
+
+  return +number;
+}
+
+function sortList(list) {
+  const sorted = list.sort((a, b) =>
+    toNumber(b.dataset.salary) - toNumber(a.dataset.salary));
+
+  initList.append(...sorted);
+}
+
+function getEmployees(list) {
+  return list.map(person => ({
+    name: person.innerText,
+    ...person.dataset,
+  }));
+}
+
+sortList(employees);
+getEmployees(employees);
