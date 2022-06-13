244f688e4d4dd64f33fbc9ee87f7bd2ecc64560a
Olexandr Istomin <gn.53@ukr.net>
2022-06-14 00:02:18 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+const li = document.querySelectorAll('li');
+
+function getNumber(string) {
+  let str = '';
+  const comma = string.indexOf(',');
+
+  str += string.slice(1, comma);
+  str += string.slice(comma + 1);
+
+  return +str;
+}
+
+function sortList(array) {
+  const finalList = [...array].sort(function(a, b) {
+    return getNumber(b.dataset.salary) - getNumber(a.dataset.salary);
+  });
+
+  ul.append(...finalList);
+
+  return finalList;
+}
+
+function getObject(array) {
+  return [...array].map(x => {
+    return {
+      name: x.innerText,
+      ...x.dataset,
+    };
+  });
+}
+
+getObject(sortList(li));
