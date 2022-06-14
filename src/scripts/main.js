989ce70dd38c6bc2a31ecfd2939473e387679a77
Artem Furdela <furdela.artem@gmail.com>
2022-06-15 00:24:10 +0300

    Parse list first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const initialList = [...document.querySelectorAll('li')];
+
+const sortList = (list) => {
+  const sortedList = list.sort((person1, person2) =>
+    toNumber(person2) - toNumber(person1));
+
+  document.querySelector('ul').append(...sortedList);
+};
+
+const toNumber = (numStr) => {
+  return Number(numStr.dataset.salary.replace('$', '').replace(',', ''));
+};
+
+const getEmployees = (list) => {
+  return list.map(person => {
+    const employeeObj = new Employee(person);
+
+    return employeeObj;
+  });
+};
+
+class Employee {
+  constructor(person) {
+    this.name = person.innerText;
+    this.position = person.dataset.position;
+    this.salary = person.dataset.salary;
+    this.age = person.dataset.age;
+  }
+}
+
+sortList(initialList);
+getEmployees(initialList);
