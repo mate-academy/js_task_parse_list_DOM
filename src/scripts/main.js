4759d952d4042cb3da3ede05dcdd9c055af8ca0b
Vitalii-Heiko <papus@ukr.net.com>
2022-06-14 13:55:47 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write code here
+const list = document.querySelector('ul');
+const listItems = [...list.children];
+
+function sortList(items) {
+  function doNumber(str) {
+    return +str.slice(1).split(',').join('');
+  }
+
+  items.sort((a, b) => {
+    return doNumber(b.dataset.salary) - doNumber(a.dataset.salary);
+  });
+  list.append(...items);
+}
+
+function getEmployees(arr) {
+  return arr.forEach(elem => ({
+    name: elem.innerText,
+    ...elem.dataset,
+  }));
+}
+
+sortList(listItems);
+getEmployees(listItems);
