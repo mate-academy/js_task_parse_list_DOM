8262518c6adadb0064b9ed6859c4b52547598e1b
Aleksey Pushkalov <pushkalov86@gmail.com>
2022-06-19 19:14:40 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const peopleList = document.querySelector('ul');
+
+const peopleArr = [...peopleList.children];
+
+function sortList(list) {
+  const convert = salary =>
+    +salary.dataset.salary.slice(1).split(',').join('');
+
+  list.sort((a, b) => {
+    return convert(b) - convert(a);
+  });
+
+  peopleList.append(...list);
+}
+
+function getEmployees(list) {
+  const employeesArr = list.map(elem => ({
+    name: elem.innerText,
+    position: elem.dataset.position,
+    salary: elem.dataset.salary,
+    age: elem.dataset.age,
+  }));
+
+  return employeesArr;
+}
+
+sortList(peopleArr);
+getEmployees(peopleArr);
