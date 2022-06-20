3899623c558dfec5c30b6d18154009005e19708b
Stanislav Topikha <stanislav.topikha@gmail.com>
2022-06-20 18:27:31 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const employees = [ ...list.children ];
+
+function normalizeSalary(salary) {
+  return +salary.replace('$', '').replace(',', '');
+}
+
+function sortList(employeesList) {
+  const sortedList = employeesList
+    .sort((liA, liB) =>
+      normalizeSalary(liB.dataset.salary) - normalizeSalary(liA.dataset.salary)
+    );
+
+  list.append(...sortedList);
+}
+
+function getEmployees(employeesList) {
+  return employeesList.map(li => {
+    return {
+      name: li.textContent.trim(),
+      position: li.dataset.position,
+      salary: normalizeSalary(li.dataset.salary),
+      age: +li.dataset.age,
+    };
+  });
+}
+
+getEmployees(employees);
+sortList(employees);
