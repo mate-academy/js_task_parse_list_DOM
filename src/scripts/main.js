70b39668da935c4e1f05cb11dc53c20bf7b41093
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-06-22 21:12:17 +0300

    firstCommit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write code here
+
+const ul = document.querySelector('ul');
+const list = [...ul.children];
+
+function sortList(x) {
+  const sorted = x.sort((a, b) =>
+    +b.dataset.salary.split('$').join('').split(',').join('')
+    - +a.dataset.salary.split('$').join('').split(',').join(''));
+
+  ul.append(...sorted);
+}
+
+function getEmployees(x) {
+  return x.map(item => ({
+    name: item.innerText,
+    ...item.dataset,
+  }));
+}
+
+sortList(list);
+getEmployees(list);
