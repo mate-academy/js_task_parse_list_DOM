d5f7c082bc15ba2eb05316dec0b4612146e13ec8
Tania Troshchuk <troschuk95@gmail.com>
2022-06-23 18:10:23 +0300

    Solution task: Parse list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listEmployees = [...list.children];
+
+function formatToNumber(str) {
+  return +(str.replace(/,/g, '').slice(1));
+}
+
+function sortList(arr) {
+  const sortedBySalary = arr.sort((a, b) =>
+    formatToNumber(b.dataset.salary) - formatToNumber(a.dataset.salary));
+
+  list.append(...sortedBySalary);
+}
+
+function getEmployees(arr) {
+  return arr.map(person => {
+    return {
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    };
+  });
+}
+
+sortList(listEmployees);
+getEmployees(listEmployees);
