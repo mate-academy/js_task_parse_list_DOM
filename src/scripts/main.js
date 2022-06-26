13188d9588bc282823e796d6babea64392d904dc
mira1zu <mira1zu733@gmail.com>
2022-06-26 13:56:36 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write code here
+function convertSalaryToNumber(salary) {
+  return +salary.split('$')[1].split(',').join('');
+}
+
+function sortList(htmlList) {
+  const employeesList = [...htmlList.children];
+
+  employeesList.sort((first, second) => (
+    convertSalaryToNumber(second.dataset.salary)
+    - convertSalaryToNumber(first.dataset.salary)
+  ));
+
+  htmlList.innerHTML = '';
+
+  for (let i = 0; i < employeesList.length; i++) {
+    htmlList.append(employeesList[i]);
+  }
+}
+
+function getEmployees(htmlList) {
+  const employeesList = [...htmlList.children];
+  const employeesArray = [];
+
+  for (let i = 0; i < employeesList.length; i++) {
+    const currentInEmployeesList = employeesList[i];
+
+    const employeeObj = {
+      name: currentInEmployeesList.innerText,
+      position: currentInEmployeesList.dataset.position,
+      salary: currentInEmployeesList.dataset.salary,
+      age: currentInEmployeesList.dataset.age,
+    };
+
+    employeesArray.push(employeeObj);
+  }
+
+  return employeesArray;
+}
+
+const list = document.querySelector('ul');
+
+sortList(list);
+
+getEmployees(list);
