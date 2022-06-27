9258f37f9f40a7bc0c328040fe75e3df59bebe01
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-27 22:55:10 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
 // write code here
+const unorderedList = document.querySelector(`ul`);
+const list = document.querySelectorAll(`li`);
+
+const sortList = () => {
+  const array = [...list];
+
+  unorderedList.innerHTML = ``;
+
+  const toNumber = (element) => {
+    const string = element.dataset.salary;
+
+    return Number(string.slice(1).replaceAll(',', ''));
+  };
+
+  array
+    .sort((a, b) => toNumber(b) - toNumber(a))
+    .map(element => {
+      unorderedList.appendChild(element);
+    });
+};
+
+const getEmployees = () => {
+  const array = [...list];
+
+  return array.map((element) => (
+    {
+      name: element.innerHTML.trim(),
+      ...element.dataset,
+    }
+  ));
+};
+
+sortList();
+getEmployees();
