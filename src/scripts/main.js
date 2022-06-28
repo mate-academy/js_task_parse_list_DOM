3a292aa9741c4487907d4ddb46f9af007d603f63
Konstantin <ivanchenkomypost@gmail.com>
2022-06-28 11:59:20 +0300

    Task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+
+function formatSalary(str) {
+  return +str.slice(1).split(',').join('');
+}
+
+function sortList(list) {
+  const sortedItems = [...list.children].sort((a, b) => {
+    return formatSalary(b.dataset.salary) - formatSalary(a.dataset.salary);
+  });
+
+  list.innerHtml = '';
+  list.append(...sortedItems);
+}
+
+function getEmployees(list) {
+  const items = [...list.children];
+  const result = [];
+
+  for (const el of items) {
+    result.push({
+      name: el.innerText,
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    });
+  }
+
+  return result;
+}
+
+sortList(employeesList);
+getEmployees(employeesList);
