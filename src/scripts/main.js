bb56c87d6e68d5a884fa9c832c313753c1c94695
Alyona Pisotska <pisotska.o@gmail.com>
2022-07-01 12:17:11 +0300

    Parse list DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listItems = [...list.children];
+
+function sortList(listProperty) {
+  const sortedList = listProperty.sort((person1, person2) =>
+    +(person2.dataset.salary.split('$').join('').split(',').join(''))
+    - +(person1.dataset.salary.split('$').join('').split(',').join(''))
+  );
+
+  list.append(...sortedList);
+}
+
+function getEmployees(listProperty) {
+  return listProperty.map(item => ({
+    name: item.innerText,
+    ...item.dataset,
+  }));
+}
+
+sortList(listItems);
+getEmployees(listItems);
