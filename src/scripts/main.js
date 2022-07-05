2c3721bd9c03ec732992a876f1e5cad44fe3c617
Oleksii Budnik <budnikoleksii@gmail.com>
2022-07-05 19:41:52 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const employeeElems = [...document.querySelectorAll('li')];
+
+const getEmployees = (list) => {
+  return list.map(employee => {
+    return {
+      name: employee.innerText,
+      position: employee.dataset.position,
+      salary: +employee.dataset.salary.replace(/\D/g, ''),
+      age: employee.dataset.age,
+    };
+  });
+};
+
+const sortList = (list) => list.sort((a, b) => b.salary - a.salary);
+
+const employees = getEmployees(employeeElems);
+const sortedEmployees = sortList(employees);
+
+sortedEmployees.forEach((employee, i) => {
+  employeeElems[i].innerText = employee.name;
+  employeeElems[i].dataset.position = employee.position;
+
+  employeeElems[i].dataset.salary = `$${employee.salary
+    .toLocaleString('en-US')}`;
+  employeeElems[i].dataset.age = employee.age;
+});
