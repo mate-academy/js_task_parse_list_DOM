41381658828c80ca6f505c5ad065fbff4275249d
Andrian Melnyk <andrian_m@ukr.net>
2022-07-07 21:44:56 +0300

    First commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const listLi = [...document.querySelectorAll('li')];
+const listUl = document.querySelector('ul');
+
+function sortList(arr) {
+  const sortAction = val =>
+    +val.dataset.salary.split(',').join('').split('$').join('');
+
+  const arr2 = arr.sort((a, b) => sortAction(b) - sortAction(a));
+
+  listUl.append(...arr2);
+}
+
+function getEmployees(list) {
+  const arrOfObjects = [...list].map(x => {
+    const obj = {};
+
+    obj.name = x.innerText;
+    obj.position = x.dataset.position;
+    obj.salary = x.dataset.salary;
+    obj.age = x.dataset.age;
+
+    return obj;
+  });
+
+  return arrOfObjects;
+}
+
+sortList(listLi);
+getEmployees(listLi);
