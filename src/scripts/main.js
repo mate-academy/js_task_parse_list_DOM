4fa95e48ad35c863059e9946772ee794ca3cd922
Oleksandr Kotliarov <de.fr09n@gmail.com>
2022-07-10 12:09:24 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+const employees = [...document.querySelectorAll('li')];
+
+function getEmployees(list) {
+  const infoList = [];
+
+  list.forEach(person => {
+    const { position, salary, age } = { ...person.dataset };
+
+    infoList.push({
+      name: person.textContent.trim(),
+      position,
+      salary: Number(salary.slice(1).split(',').join('')),
+      age,
+    });
+  });
+
+  return infoList;
+}
+
+function toValid(value) {
+  return Number(value.slice(1).split(',').join(''));
+}
+
+function sortList(list) {
+  const sortedList = list.sort(
+    (a, b) => toValid(b.dataset.salary) - toValid(a.dataset.salary));
+
+  return sortedList.forEach(el => {
+    employeesList.append(el);
+  });
+}
+
+sortList(employees);
+getEmployees(employees);
