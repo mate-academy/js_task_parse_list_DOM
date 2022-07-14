b0620d708b979f1bb1f00b0b89fd1c9a1ff10795
Olha Chebotarova <chebotarovaolha@gmail.com>
2022-07-14 16:25:07 -0400

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const root = document.querySelector('ul');
+const list = document.querySelectorAll('li');
+
+function convertToNumber(str) {
+  const number = parseInt(str.slice(1).split(',').join(''));
+
+  return number;
+}
+
+function sortList(arr) {
+  const sortedList = [...arr].sort((a, b) =>
+    convertToNumber(b.dataset.salary) - convertToNumber(a.dataset.salary));
+
+  sortedList.forEach(element => {
+    root.append(element);
+  });
+}
+
+function getEmployees(arr) {
+  // const employees = [...arr];
+  const employees = [...arr].map(person => {
+    const obj = {
+      name: person.textContent.trim(),
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    };
+
+    return obj;
+  });
+
+  return employees;
+}
+
+sortList(list);
+getEmployees(list);
