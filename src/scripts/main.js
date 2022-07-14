fe08aab6c888200c2bb0661add0878b3fe8da21e
Aleksandr Kovalenko <dias.koval@gmail.com>
2022-07-14 19:09:49 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const listUl = document.querySelector('ul');
+const listLi = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  const toNumber = el => +el.dataset.salary.slice(1).split(',').join('');
+
+  list.sort((a, b) => toNumber(b) - toNumber(a));
+
+  listUl.append(...list);
+}
+
+function getEmployees(list) {
+  return list.map(el => ({
+    name: el.innerText,
+    ...el.dataset,
+  }));
+}
+
+sortList(listLi);
+getEmployees(listLi);
