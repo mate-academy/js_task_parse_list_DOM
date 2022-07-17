ce8c8e5b84e05b38b333905f07664a242ff1d2f8
Olha Arama <arama_o_f@ukr.net>
2022-07-17 15:02:28 +0300

    List was sorted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const listOfEmployees = document.querySelector('ul');
+
+function format(element) {
+  return +element.slice(1).split(',').join('');
+}
+
+function sortList(list) {
+  const items = list.querySelectorAll('li');
+
+  const arr = [...items].sort((person1, person2) => {
+    return format(person2.dataset.salary) - format(person1.dataset.salary);
+  });
+
+  arr.forEach(person => list.append(person));
+}
+
+function getEmployees(list) {
+  const items = list.querySelectorAll('li');
+
+  return [...items].map(person => ({
+    name: person.innerText,
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+}
+
+sortList(listOfEmployees);
+getEmployees(listOfEmployees);
