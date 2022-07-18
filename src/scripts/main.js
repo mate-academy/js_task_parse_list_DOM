5db0d3dd0f015766017d50f3bdbb7f9b01909ee2
Denys Dmytruk <dmytrukdenysan@gmail.com>
2022-07-19 00:03:11 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const employeesList = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  list.sort((a, b) => b.salary - a.salary);
+}
+
+function getEmployees(list) {
+  const people = [];
+
+  for (const elem of list) {
+    const person = {};
+
+    person.name = elem.innerText;
+    person.position = elem.dataset.position;
+    person.salary = elem.dataset.salary.replace(/[$,]/g, '');
+    person.age = elem.dataset.age;
+
+    people.push(person);
+  }
+
+  return people;
+}
+
+const employees = getEmployees(employeesList);
+
+sortList(employees);
+
+for (let i = 0; i < employees.length; i++) {
+  employeesList[i].innerText = employees[i].name;
+  employeesList[i].dataset.position = employees[i].position;
+
+  employeesList[i].dataset.salary = '$'
+    + parseInt(employees[i].salary).toLocaleString('en-US');
+
+  employeesList[i].dataset.age = employees[i].age;
+}
