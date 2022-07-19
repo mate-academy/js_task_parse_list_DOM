f1e2c3fc63de4b9381a5667501da584b524676db
Oleksii Zasiedka <alex.zasedka@gmail.com>
2022-07-19 16:27:58 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+function saveEmployersData(nodelist) {
+  const result = [];
+
+  for (const employer of nodelist) {
+    result.push({
+      innerHTML: employer.innerHTML,
+      position: employer.dataset.position,
+      salary: employer.dataset.salary,
+      age: employer.dataset.age,
+    });
+  }
+
+  return result;
+}
+
+const employers = document.querySelectorAll('li');
+
+const sortedEmployers = saveEmployersData(employers).sort((a, b) => (
+  +b.salary.slice(1).split(',').join('')
+  - +a.salary.slice(1).split(',').join('')));
+
+for (let i = 0; i < employers.length; i++) {
+  employers[i].innerHTML = sortedEmployers[i].innerHTML;
+  employers[i].dataset.position = sortedEmployers[i].position;
+  employers[i].dataset.salary = sortedEmployers[i].salary;
+  employers[i].dataset.age = sortedEmployers[i].age;
+}
