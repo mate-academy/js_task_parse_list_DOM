87a5c6744fa1a2fe90a41fa99c1c071c80030d04
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-07-20 18:30:18 +0300

    sorted list was implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const employesList = [...document.querySelectorAll('li')];
+const ul = document.querySelector('ul');
+
+const getFormatString = (stringToFormat) => {
+  return stringToFormat
+    .split('$')
+    .join('')
+    .split(',')
+    .join('');
+};
+
+function sortList(list) {
+  [...list].sort((current, next) => {
+    const formattedSalaryCurrent = getFormatString(current.dataset.salary);
+    const formattedSalaryNext = getFormatString(next.dataset.salary);
+
+    return Number(formattedSalaryNext) - Number(formattedSalaryCurrent);
+  }).forEach(item => ul.append(item));
+}
+
+function getEmployees(list) {
+  return list.map(item => {
+    const employeeData = {};
+
+    employeeData['name'] = item.innerText;
+    employeeData['position'] = item.dataset.position;
+    employeeData['salary'] = item.dataset.salary;
+    employeeData['age'] = item.dataset.age;
+
+    return employeeData;
+  });
+}
+
+sortList(employesList);
+getEmployees(employesList);
