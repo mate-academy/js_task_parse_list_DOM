eaa783a69d8e5c388ca6bf0664f5a128abdba6b8
Natalya Tsybulko <tsybulko.natalya395@gmail.com>
2022-07-21 22:04:07 +0300

    solve the parseListDOM problem

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const salaryList = document.querySelector('ul').children;
+
+function sortList(list) {
+  const salary = [...list]
+    .sort((item1, item2) =>
+      item2.dataset.salary
+        .replace(/[^0-9]/g, '') - item1.dataset.salary
+        .replace(/[^0-9]/g, ''));
+
+  const employeesList = document.querySelector('ul');
+
+  for (const item of salary) {
+    employeesList.append(item);
+  }
+}
+
+function getEmployees(list) {
+  const employees = [];
+
+  for (const item of list) {
+    employees.push({
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: +item.dataset.age,
+    });
+  }
+
+  return employees;
+}
+
+sortList(salaryList);
+getEmployees(salaryList);
