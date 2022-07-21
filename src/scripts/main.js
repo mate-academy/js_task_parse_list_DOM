c6f9a5f09872b61abd37dab6cccb146c5ace8c1d
Oleg Vetrov <oleg94038@gmail.com>
2022-07-21 22:36:33 +0300

    Added sort and set functions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+function sortEmployees(list) {
+  return [...list]
+    .map(item => Object.assign({}, item.dataset, { name: item.innerText }))
+    .sort((prev, curr) => {
+      return Number(curr.salary.slice(1).split(',').join(''))
+        - Number(prev.salary.slice(1).split(',').join(''));
+    });
+}
+
+function setEmployees(elementsList, sortedList) {
+  for (let i = 0; i <= elementsList.length - 1; i++) {
+    elementsList[i].innerText = sortedList[i].name;
+    elementsList[i].dataset.position = sortedList[i].position;
+    elementsList[i].dataset.salary = sortedList[i].salary;
+    elementsList[i].dataset.age = sortedList[i].age;
+  }
+}
+
+const htmlElementsList = document.getElementsByTagName('li');
+const sortedElementsList = sortEmployees(htmlElementsList);
+
+setEmployees(htmlElementsList, sortedElementsList);
