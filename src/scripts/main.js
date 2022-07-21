40432d6623356487752582d25498344f81723d6e
dasha <dashasv76@gmail.com>
2022-07-21 19:51:12 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const HTMLlist = document.querySelectorAll('li');
+
+function sortList(list) {
+  return [...list].sort((a, b) => (
+    +b.dataset.salary
+      .slice(1)
+      .split(',')
+      .join('')
+        - +a.dataset.salary
+          .slice(1)
+          .split(',')
+          .join(''))
+  );
+}
+
+function getEmployees(list) {
+  const employes = [];
+
+  [...list].forEach((item) => employes.push({
+    name: item.textContent.trim(),
+    ...item.dataset,
+  }));
+
+  return employes;
+}
+
+sortList(HTMLlist);
+getEmployees(HTMLlist);
