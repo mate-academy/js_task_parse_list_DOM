8aa06db3c57c72ce7f20689b259ad555f650dbde
Denys Korotkoruchko <denenergii@gmail.com>
2022-07-22 22:00:44 +0300

    ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const listItem = document.querySelector('.list');
+const item = document.querySelectorAll('.item');
+
+function sortList(list) {
+  return [...list].sort((a, b) =>
+    b.dataset.salary.replace(/\D/g, '') - a.dataset.salary.replace(/\D/g, ''));
+}
+
+function getEmployees(list) {
+  [...list].forEach((element, i) => listItem.children[i].before(element));
+}
+
+getEmployees(sortList(item));
