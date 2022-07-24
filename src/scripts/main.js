2f3e29b586ac5eb823cc3787db4b1206298692b1
PavloMarkov <r9markovpn@gmail.com>
2022-07-24 11:09:57 +0300

    list was sorted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const listNode = document.querySelectorAll('li');
+const ulElement = document.querySelector('ul');
+
+const list = [...listNode];
+
+function dataSaleryToNumber(x) {
+  return +x.dataset.salary.slice(1).split(',').join('');
+}
+
+function sortList(arr) {
+  return arr.sort((a, b) => {
+    const first = dataSaleryToNumber(a);
+    const second = dataSaleryToNumber(b);
+
+    return (second - first);
+  });
+}
+
+const answer = sortList(list);
+
+ulElement.innerHTML = '';
+
+answer.forEach(element => {
+  ulElement.innerHTML += `<li
+  data-position=${element.dataset.position}
+  data-salary=${element.dataset.salary}
+  data-age=${element.dataset.age}
+>
+${element.innerHTML}
+</li>`;
+});
