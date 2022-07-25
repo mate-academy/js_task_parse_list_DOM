c28c16bdd2e4e358707cf894b1609994c57a45a8
Vitaliy Bushovskiy <Bushovskiy@gmail.com>
2022-07-25 19:20:57 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listElements = document.querySelectorAll('li');
+
+function sortList(data) {
+  const dataArr = [...data];
+
+  const convertToNumber = (stringValue) => {
+    return +(stringValue.dataset.salary.slice(1).split(',').join(''));
+  };
+
+  dataArr.sort((prev, next) => convertToNumber(next) - convertToNumber(prev));
+
+  list.append(...dataArr);
+}
+
+function getEmployees(data) {
+  return [...data].map(employee => ({
+    name: employee.textContent.trim(),
+    ...employee.dataset,
+  }));
+}
+
+sortList(list);
+getEmployees(listElements);
