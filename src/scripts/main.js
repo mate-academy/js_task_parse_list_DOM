b529faa176aab827089e3fd87f0cecdf9b4d3275
Oksana Shvets <ksenyasvatko1993@gmail.com>
2022-08-09 11:42:48 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // write code here
+
+const allLi = document.querySelectorAll('ul');
+
+function sortList(list) {
+  const listElements = Array.prototype.slice(list.children);
+  const sortedListElements = listElements.sort((a, b) =>
+    Number(b.getAttribute('data-salary').slice(1).replace(/,/g, ''))
+    - Number(a.getAttribute('data-salary').slice(1).replace(/,/g, '')));
+
+  // list.innerHTML = '';
+
+  sortedListElements.forEach(function(el) {
+    list.appendChild(el);
+  });
+}
+
+function getEmployees(list) {
+  const arrLi = Array.prototype.slice(list.children);
+
+  return arrLi.map(person => {
+    return {
+      name: person.innerText,
+      position: person.getAttribute('data-position'),
+      salary: person.getAttribute('data-salary'),
+      age: person.getAttribute('data-age'),
+    };
+  });
+}
+
+sortList(allLi);
+getEmployees(allLi);
