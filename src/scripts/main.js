a84b9b45dd3956eb52aab60460547f785ab24075
Yevhen Veselov <veselovyevhen@gmail.com>
2022-08-12 10:53:50 +0300

    1st commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const startList = document.querySelector('ul');
+const employees = [...document.querySelectorAll('li')];
+
+const convertToNumber = salary => +salary.slice(1).split(',').join('');
+
+function sortList(list) {
+  const sortedList = list.sort((a, b) =>
+    convertToNumber(b.dataset.salary) - convertToNumber(a.dataset.salary)
+  );
+
+  startList.append(...sortedList);
+}
+
+function getEmployees(list) {
+  return list.map(person => ({
+    name: person.innerHTML, ...person.dataset,
+  }));
+}
+
+sortList(employees);
+getEmployees(employees);
