8e6bdc4accfc98400ccbb98a6f304fce3af6bcc8
Nazar Kulchevych <gamebandit38@gmail.com>
2022-08-15 14:25:06 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+function getNumber(str) {
+  return +str.split('').filter(num => !isNaN(num)).join('');
+}
+
+function sortList(list) {
+  return list.sort((a, b) => (
+    getNumber(b.dataset.salary) - getNumber(a.dataset.salary)));
+}
+
+function getEmployees(list) {
+  const employeesInfo = [];
+
+  list.forEach(item => (
+    employeesInfo.push({
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    })
+  ));
+
+  return employeesInfo;
+}
+
+const employees = [ ...document.querySelectorAll('li') ];
+
+sortList(employees);
+getEmployees(employees);
+
+employees.forEach(employee => {
+  document.querySelector('ul').append(employee);
+});
