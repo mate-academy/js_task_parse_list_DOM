fdd982102bca0044f0c9b960ca2cbb5848b48103
Sasha Kolomiiets <kinbean444@gmail.com>
2022-08-15 21:29:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  return list.sort((a, b) => (
+    b.dataset.salary.replace(/\D/g, '') - a.dataset.salary.replace(/\D/g, '')));
+}
+
+sortList(employees);
+
+function getEmployees(list) {
+  const result = [];
+
+  list.forEach(element => {
+    result.push({
+      name: element.innerText,
+      position: element.dataset.position,
+      salary: element.dataset.salary,
+      age: element.dataset.age,
+    });
+  });
+
+  return result;
+}
+
+getEmployees(employees);
+
+employees.forEach(item => {
+  document.querySelector('ul').append(item);
+});
