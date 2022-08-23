22164707c6c241ae090d762e3980f7243d06f6f9
Dima <dimak119988@gmail.com>
2022-08-24 00:02:32 +0300

    First Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+
+function convertToNumber(variable) {
+  [...variable.children].map(item => {
+    item.dataset.salary = item.dataset.salary.slice(1).replace(',', '.');
+  });
+};
+
+function sortList(variable) {
+  const arr
+  = [...ul.children].sort((a, b) => b.dataset.salary - a.dataset.salary);
+
+  variable.append(...arr);
+}
+
+function getEmployees(variable) {
+  const answer = [...variable.children].map(item => {
+    const salary = `$${(item.dataset.salary.replace('.', ','))}`;
+
+    return {
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: salary,
+      age: item.dataset.age,
+    };
+  });
+
+  return answer;
+}
+
+convertToNumber(ul);
+
+sortList(ul);
+
+getEmployees(ul);
