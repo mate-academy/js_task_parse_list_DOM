2994fd7b3d25db3048b75a8c2c122998a5cee78d
Dolhov Andrey <andrej.dolgov.042110@gmail.com>
2022-08-25 16:31:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const employeers = [...document.getElementsByTagName('li')];
+
+function sortBySalary(list) {
+  list.sort((a, b) => (+b.dataset.salary.slice(1).split(',').join(''))
+    - (+a.dataset.salary.slice(1).split(',').join('')));
+
+  list.map(li => li.parentElement.append(li));
+}
+
+function getInfo(list) {
+  return list.map(empl => ({
+    name: empl.innerText,
+    position: empl.dataset.position,
+    salary: empl.dataset.salary,
+    age: empl.dataset.age,
+  }));
+}
+
+sortBySalary(employeers);
+getInfo(employeers);
