a716857252c841a01bc133eb0aa32d1bd9689cbc
p-omogit-e <polinakotyrlo@gmail.com>
2022-09-05 19:31:33 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const liList = document.querySelectorAll('li');
+const ulList = document.querySelector('ul');
+
+function sortList(list) {
+  const sorted = [...list].sort((a, b) =>
+    +(b.dataset.salary.split('$').join('').split(',').join(''))
+    - +(a.dataset.salary.split('$').join('').split(',').join('')));
+
+  ulList.append(...sorted);
+}
+
+function getEmployees(list) {
+  return [...list].map(person => ({
+    name: person.innerText,
+    ...person.dataset,
+  }));
+}
+
+sortList(liList);
+getEmployees(liList);
