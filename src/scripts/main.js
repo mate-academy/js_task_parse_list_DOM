ce0f18e676c07aa47287cfe9c42a1e12586283c2
fenderukr <priststoreua@gmail.com>
2022-09-07 13:42:19 +0300

    parseListWithDataAttributes0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
 // write code here
+const unList = document.querySelectorAll('li');
+
+function sortList(list) {
+  const unpacked = [...list];
+
+  const x = unpacked.sort((a, b) => {
+    return Number(b.dataset.salary.replace(',', '').slice(1))
+      - Number(a.dataset.salary.replace(',', '').slice(1));
+  });
+
+  return x;
+};
+
+const sortedList = sortList(unList);
+
+const resultArr = [];
+
+function getEmployees(list) {
+  list.forEach(item => {
+    const obj = {};
+
+    obj.name = item.innerHTML.trim();
+    obj.position = item.dataset.position.trim();
+    obj.salary = item.dataset.salary.trim();
+    obj.age = item.dataset.age.trim();
+    resultArr.push(obj);
+  });
+
+  return resultArr;
+}
+
+sortList(unList);
+getEmployees(sortedList);
