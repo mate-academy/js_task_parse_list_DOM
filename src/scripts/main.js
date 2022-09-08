ed59d735c766d53a9bce72f10558a35e2ff88e8e
Oleksii <oleksii@it.com>
2022-09-08 14:32:12 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const employeeList = document.querySelector('ul');
+const employeePoint = document.querySelectorAll('li');
+
+function sortList(list) {
+  const sorted = [...list].sort((emp1, emp2) =>
+    +(emp2.dataset.salary.split('$').join('').split(',').join(''))
+    - +(emp1.dataset.salary.split('$').join('').split(',').join('')));
+
+  employeeList.append(...sorted);
+}
+
+function getEmployees(list) {
+  return [...list].map(person => ({
+    name: person.innerText,
+    ...person.dataset,
+  }));
+}
+
+sortList(employeePoint);
+getEmployees(employeePoint);
