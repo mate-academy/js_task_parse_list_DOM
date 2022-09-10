4b06967a0154eba55c9b4322e038f9ffe0a16199
Dima Korol <korovaidima@gmail.com>
2022-09-10 12:36:49 +0200

    2 commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // write code here
+const employees = document.querySelectorAll('li[data-salary]');
+const list = document.querySelector('ul');
+
+function valuesToNumber(string) {
+  return string.replace(',', '.').slice(1);
+}
+
+function sortList(workers) {
+  const sorted
+  = [...workers].sort((a, b) =>
+
+    valuesToNumber(b.dataset.salary) - valuesToNumber(a.dataset.salary));
+
+  list.append(...sorted);
+}
+
+function getEmployees(workers) {
+  // let data = {};
+
+  const arr = [...workers].map(elem => {
+    return {
+      name: elem.innerText,
+      ...elem.dataset,
+    }
+  });
+
+  return arr;
+}
+
+sortList(employees);
+getEmployees(employees);
