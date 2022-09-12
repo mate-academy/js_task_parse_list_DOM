7cc84233351b9e575cf3917deb74ad0bec75e4f9
Oleh Savchuk <sav4uk.oleg@gmail.com>
2022-09-12 23:14:43 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+function convertNumber(n) {
+  const result = n.split('').filter(el => !isNaN(el)).join('');
+
+  return result;
+}
+
+const employeesList = document.querySelectorAll('li');
+
+const employees = [...employeesList].map(function(person) {
+  return {
+    name: person.innerText,
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  };
+});
+
+employees.sort((a, b) => convertNumber(b.salary) - convertNumber(a.salary));
+
+const list = document.querySelector('ul');
+
+list.innerHTML = `
+    ${employees.map(person => `<li>${person.name}</li>`).join('')}
+`;
