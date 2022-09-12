40f08cb42e1922a4f8418998b1cdfb092390d0f6
Inna <dragotaiv@gmail.com>
2022-09-12 20:16:25 +0300

    list_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const employees = document.querySelectorAll('li');
+
+const masList = [...employees];
+
+const getNum = function(str) {
+  return Number(str.replaceAll(',', '').slice(1));
+};
+
+const sortList = function(arr) {
+  return arr.sort((a, b) =>
+    getNum(a.dataset.salary) - getNum(b.dataset.salary));
+};
+
+sortList(masList);
+
+const ul = document.querySelector('ul');
+
+for (const item of masList) {
+  ul.append(item);
+}
+
+const getEmployees = function(list) {
+  return [...list].map(el => ({
+    name: el.innerText,
+    position: el.dataset,
+    salary: el.dataset,
+    age: el.dataset,
+  }));
+};
+
+getEmployees(masList);
