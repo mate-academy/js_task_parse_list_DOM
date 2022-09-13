8a11c87dec18f7ea8b98dd8b97a69ed066ffa773
Anastasiia Mahalias <anastasiabatychenko@gmail.com>
2022-09-13 11:42:12 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const fullList = document.querySelectorAll('li');
+
+function sortList(list) {
+  return [...list].sort((a, b) => {
+    const salaryA = +(a.dataset.salary.slice(1).split('')
+      .filter(item => item !== ',').join(''));
+    const salaryB = +(b.dataset.salary.slice(1).split('')
+      .filter(item => item !== ',').join(''));
+
+    return salaryB - salaryA;
+  });
+}
+
+const sortedList = sortList(fullList);
+
+document.querySelector('ul').innerHTML = sortedList.map(item => {
+  return item.outerHTML;
+}).join('');
+
+function getEmployees(list) {
+  const employees = [];
+
+  [...list].map(element => {
+    const employee = {
+      name: element.innerText.trim(),
+      position: element.dataset.position,
+      salary: element.dataset.salary,
+      age: element.dataset.age,
+    };
+
+    employees.push(employee);
+  });
+
+  return employees;
+}
+
+getEmployees(fullList);
