1b345587b3b8e47ded67a50c95fc3e9bea64dec6
Paul Voronin <voron.paul@gmail.com>
2022-09-15 00:59:43 +0300

    ver.1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const employeeList = document.querySelectorAll('li');
+const ulList = document.querySelector('ul');
+
+function sortList(list) {
+  const arr = [...list];
+  const salaryToNumber = (text) => {
+    return +text.slice(1).replace(/,/g, '');
+  };
+
+  arr.sort((x, y) =>
+    salaryToNumber(y.dataset.salary)
+    - salaryToNumber(x.dataset.salary));
+
+  ulList.append(...arr);
+}
+
+function getEmployees(list) {
+  const employees = [];
+  const arr = [...list];
+
+  for (const person of arr) {
+    employees.push({
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    });
+  }
+
+  return employees;
+}
+
+sortList(employeeList);
+getEmployees(employeeList);
