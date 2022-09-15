54332fb3bc8119e399fc71584bbc69b5cb07bdf1
Vlad Kurachenko <x0674016244@gmail.com>
2022-09-15 17:02:40 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+// const employments = document.querySelector('ul');
+const salaries = [...document.querySelectorAll('[data-salary')];
+
+function sortList(list) {
+  list.sort((a, b) =>
+    +b.dataset.salary.slice(1).split(',').join('')
+    - +a.dataset.salary.slice(1).split(',').join(''));
+
+  list.map(li => li.parentElement.append(li));
+};
+
+const getEmployees = (list) => {
+  return list.map(item => ({
+    name: item.innerText,
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+};
+
+sortList(salaries);
+getEmployees(salaries);
