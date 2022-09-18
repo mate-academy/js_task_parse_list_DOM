b9768baadae114d3b395b67ca10bbb38ec07a1da
Max Bovtun <maxbovtun@gmail.com>
2022-09-18 13:26:42 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
 // write code here
+const ulItem = document.querySelector('ul');
+const liItems = document.querySelectorAll('li');
+
+function sortList(list, listItems) {
+  const itemsSorted = [...listItems].sort((a, b) => (
+    +((b.dataset.salary)
+      .slice(1)
+      .split(',')
+      .join(''))
+    - +((a.dataset.salary)
+      .slice(1)
+      .split(',')
+      .join(''))
+  ));
+
+  list.innerHTML = '';
+
+  for (let i = 0; i < itemsSorted.length; i++) {
+    list.appendChild(itemsSorted[i]);
+  }
+}
+
+function getEmployees(listItems) {
+  const result = [];
+
+  for (const item of listItems) {
+    const employer = {
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    };
+
+    result.push(employer);
+  }
+
+  return result;
+}
+
+sortList(ulItem, liItems);
+
+getEmployees(liItems);
