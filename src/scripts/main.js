7575585eb1f38241bd237044def0937de2d44b99
grafit-off <graf.off.it@gmail.com>
2022-09-20 15:57:34 +0300

    feat: parse list dom task was implemnted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
 // write code here
+const listSelector = document.querySelector('ul');
+
+const formatToNumber = (str) => Number(str.replace(/\$|,/gi, ''));
+
+const sortList = (list) => {
+  const sortedArr = [...list.querySelectorAll('li')];
+
+  sortedArr.sort((prev, curr) => (
+    formatToNumber(curr.dataset.salary) - formatToNumber(prev.dataset.salary)
+  ));
+
+  list.append(...sortedArr);
+};
+
+sortList(listSelector);
+
+const getEmployees = (list) => {
+  const listItems = [...list.querySelectorAll('li')];
+
+  return listItems.map(el => ({
+    name: el.textContent.trim(),
+    position: el.dataset.position,
+    salary: el.dataset.salary,
+    age: el.dataset.age,
+  }));
+};
+
+getEmployees(listSelector);
