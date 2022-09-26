a88b6b71d68c140e2ee2377e9d88359cb8b7bab3
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-09-26 20:35:53 +0300

    list was sorted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const allWorkers = [...document.querySelectorAll('li')];
+const workerData = [];
+
+allWorkers.forEach((worker) => {
+  const workerInfo = {};
+
+  workerInfo.name = worker.innerText;
+  workerInfo.salary = worker.dataset.salary;
+  workerInfo.position = worker.dataset.position;
+  workerInfo.age = worker.dataset.age;
+
+  workerData.push(workerInfo);
+});
+
+const sortedData = sortList(workerData);
+
+allWorkers.forEach((worker, index) => {
+  worker.innerText = sortedData[index].name;
+  worker.dataset.position = sortedData[index].position;
+  worker.dataset.salary = sortedData[index].salary;
+  worker.dataset.age = sortedData[index].age;
+});
+
+function sortList(list) {
+  return list
+    .sort((personA, personB) => (
+      fromDollars(personB.salary) - fromDollars(personA.salary)
+    ));
+}
+
+function fromDollars(salary) {
+  return +salary.replace(/[$,]/g, '');
+}
