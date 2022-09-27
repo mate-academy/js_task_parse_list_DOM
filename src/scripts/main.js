d3eb3ee01d43ce157397ab15f40a86f082e63199
Zykhor Andrii <zuxoran@gmail.com>
2022-09-27 16:35:06 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write code here
+const allList = document.querySelector('ul');
+const itemLi = [...document.querySelectorAll('li')];
+const convertNum = salary => +salary.slice(1).split(',').join('');
+
+function sortList(list) {
+  const result = list.sort((a, b) =>
+    convertNum(b.dataset.salary) - convertNum(a.dataset.salary));
+
+  allList.append(...result);
+};
+
+function getEmployees(list) {
+  return list.map((item) => {
+    return {
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    };
+  });
+}
+
+getEmployees(itemLi);
+
+sortList(itemLi);
