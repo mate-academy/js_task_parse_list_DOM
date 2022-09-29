ec33a18d4552b231daff39d8f4d8000866fccbf8
Volodymyr Shymanskyi <v.shimanskyi@bnb.net.ua>
2022-09-29 16:44:17 +0300

    1-st commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const collection = [...document.querySelectorAll('li')];
+
+function toNumber(string) {
+  return Number(string.slice(1).split(',').join(''));
+}
+
+function sortList(list) {
+  const sortedList = list.sort((a, b) =>
+    toNumber(b.dataset.salary) - toNumber(a.dataset.salary));
+
+  document.querySelector('ul').append(...sortedList);
+}
+
+function getEmployees(list) {
+  const array = list.map(employee => ({
+    name: employee.innerText,
+    position: employee.position,
+    salary: employee.salary,
+    age: employee.age,
+  }));
+
+  return array;
+}
+
+sortList(collection);
+getEmployees(collection);
