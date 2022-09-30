59a84451c2f2688334a41f26c08407adeb70ad8d
Maksym845 <mzuravel845@gmail.com>
2022-09-30 16:48:51 +0300

    solution was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const salaryConvertion = (string) => string.replace(/[^\d]/g, '');
+
+const sortList = (list) => {
+  const sortedList = list.sort((first, second) => {
+    const firstSalary = salaryConvertion(first.dataset.salary);
+    const secondSalary = salaryConvertion(second.dataset.salary);
+
+    return secondSalary - firstSalary;
+  });
+
+  return sortedList;
+};
+
+const getEmployees = (tag) => {
+  const listOfWorkers = [...document.querySelector(tag).children];
+
+  return listOfWorkers;
+};
+
+const formatedList = getEmployees('ul');
+const htmlList = document.querySelector('ul');
+
+getEmployees('ul');
+sortList(formatedList).forEach(item => htmlList.append(item));
