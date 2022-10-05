c633cefecc3106225da4f64dafa82e9c3afc6d35
illia_kots <illyahennadievich@gmail.com>
2022-10-05 20:23:21 +0300

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const startList = document.querySelector('ul');
+
+const sortList = (list) => {
+  const persons = list.querySelectorAll('li');
+
+  const array = [...persons].sort((a, b) => +b.dataset.salary
+    .replace(',', '').slice(1) - +a.dataset.salary.replace(',', '').slice(1));
+
+  list.append(...array);
+};
+
+const getEmployees = (list) => {
+  const obj = list.querySelectorAll('li');
+
+  return [...obj].map(el => ({
+    name: el.innerText,
+    position: el.dataset.position,
+    salary: el.dataset.salary,
+    age: el.dataset.age,
+  }));
+};
+
+sortList(startList);
+getEmployees(startList);
