5d2a7652a51b1e4ceae3b6473723f712240d1191
Hlazman <glazman.b@gmail.com>
2022-10-05 15:11:21 +0300

    made parse list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const items = document.querySelectorAll('li');
+const arr = [...items];
+
+const sortList = (employees) => {
+  const sorted = (item) => item.dataset['salary'].slice(1).replace(',', '.');
+
+  arr.sort((itemOne, itemTwo) => sorted(itemTwo) - sorted(itemOne));
+
+  return arr;
+};
+
+const getEmployees = (place, employees) => {
+  employees.forEach(element => place.append(element));
+};
+
+sortList(items);
+getEmployees(list, arr);
