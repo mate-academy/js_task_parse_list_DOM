ed2cffcfc15825f424aeda2ab50194e1f063f79e
Sergii Iatsenko <sergii.iatsenko@gmail.com>
2022-10-06 15:48:16 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const employeesList = [...document.getElementsByTagName('li')]
+  .map(item => [item.innerText, { ...item.dataset }]);
+
+function sortList(list) {
+  list.sort((a, b) => (+b[1].salary.slice(1)
+    .split(',').join('')) - (+a[1].salary.slice(1).split(',').join('')));
+
+  return list;
+};
+
+function getEmployees(list) {
+  const objectArray = [];
+
+  for (const item of list) {
+    objectArray.push(item[1]);
+    objectArray[objectArray.length - 1].name = item[0];
+  };
+
+  return objectArray;
+}
+
+sortList(employeesList);
+getEmployees(employeesList);
