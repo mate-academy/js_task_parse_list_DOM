e18a3f94db6d4df18b7890aadbf36c493e144c0a
Dima <ddikey1@gmail.com>
2022-10-10 14:28:56 +0300

    Solution of "Parse list DOM" task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+const list = document.querySelector('ul');
+const people = [...document.querySelectorAll('[data-position]')]
+  .map(person => (
+    {
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    }
+  ))
+  .sort((personA, personB) => (
+    Number(personB.salary.slice(1).split(',').join(''))
+    - Number(personA.salary.slice(1).split(',').join(''))
+  ));
+
+list.innerHTML = people
+  .map(person => (`
+    <li>
+        ${person.name}
+    </li>
+  `))
+  .join('');
