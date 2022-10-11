f0911557eda548ee6277dd0c2c9e185a5d183f41
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-10-11 14:35:06 +0300

    cod for task parse list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+function employee(info) {
+  const personInfo = {};
+
+  personInfo.name = info.innerText;
+  personInfo.position = info.dataset.position;
+  personInfo.salary = info.dataset.salary;
+  personInfo.age = info.dataset.age;
+
+  return personInfo;
+}
+
+const arrayEmploees = [];
+
+for (const each of list) {
+  arrayEmploees.push(employee(each));
+}
+
+arrayEmploees.sort((a, b) =>
+  (b.salary.replace(/[^+\d]/g, '')) - (a.salary).replace(/[^+\d]/g, ''));
+
+let i = 0;
+
+for (const item of list) {
+  item.innerText = arrayEmploees[i].name;
+  item.dataset.position = arrayEmploees[i].position;
+  item.dataset.salary = arrayEmploees[i].salary;
+  item.dataset.age = arrayEmploees[i].age;
+
+  i++;
+}
