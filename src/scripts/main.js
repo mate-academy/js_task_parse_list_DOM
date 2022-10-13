06a235cb12a9371296a3d04a6fc0449548b2675f
Yuri Gatskovskyi <yurimoreau@gmail.com>
2022-10-13 22:45:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const employeeList = document.querySelectorAll('li');
+const employeesSorted = sortList(employeeList);
+const ul = document.querySelector('ul');
+
+updateList(ul, employeesSorted);
+
+function sortList(list) {
+  return [...list].sort((a, b) => salaryToNum(b) - salaryToNum(a));
+};
+
+function salaryToNum(salaryStr) {
+  return +salaryStr.dataset.salary.slice(1).split(',').join('.');
+};
+
+function updateList(list, employees) {
+  employees.forEach(employee => list.append(employee));
+}
