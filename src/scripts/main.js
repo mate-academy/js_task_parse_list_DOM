f8f977afb7fe3fdd99764590ffb04613b76d6059
Dima <ddikey1@gmail.com>
2022-10-15 11:39:46 +0300

    Fixed code style mistakes and created helper variables for sort method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,18 @@
 // write code here
 const list = document.querySelector('ul');
 const people = [...document.querySelectorAll('[data-position]')]
-  .map(person => (
-    {
-      name: person.innerText,
-      position: person.dataset.position,
-      salary: person.dataset.salary,
-      age: person.dataset.age,
-    }
-  ))
-  .sort((personA, personB) => (
-    Number(personB.salary.slice(1).split(',').join(''))
-    - Number(personA.salary.slice(1).split(',').join(''))
-  ));
+  .map(person => ({
+    name: person.innerText,
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }))
+  .sort((personA, personB) => {
+    const preparedPersonA = Number(personA.salary.slice(1).split(',').join(''));
+    const preparedPersonB = Number(personB.salary.slice(1).split(',').join(''));
+
+    return preparedPersonB - preparedPersonA;
+  });
 
 list.innerHTML = people
   .map(person => (`
