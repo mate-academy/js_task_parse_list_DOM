97bd9e0d74d6cd7fe8af7620bac1bae27482b659
John Rust <johnrust12@gmail.com>
2022-10-16 09:07:46 +0300

    Solution1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
+/* eslint-disable no-console */
 'use strict';
 
-// write code here
+const workersList = [...document.querySelectorAll('[data-salary]')];
+
+const salaryToNum = (item) =>
+  Number(item.dataset.salary.replace(/[$,]/g, ''));
+
+const sortList = (list) =>
+  document.querySelector('ul')
+    .append(...list.sort((a, b) => salaryToNum(b) - salaryToNum(a)));
+
+const getEmployees = (list) =>
+  list.map(e =>
+    ({
+      name: e.innerText,
+      position: e.getAttribute('data-position'),
+      salary: e.getAttribute('data-salary'),
+      age: e.getAttribute('data-age'),
+    }));
+
+sortList(workersList);
+getEmployees(workersList);
