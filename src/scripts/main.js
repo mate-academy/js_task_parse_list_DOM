f7cce052245d1a7206e7706a558712e7a7f60139
Illia Kyselov <illya.kis99@gmail.com>
2022-10-18 18:53:32 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+const arr = [...items];
+
+function sortList(list) {
+  list.sort((a, b) => {
+    a.dataset.salary = +a.dataset.salary.split('').filter(
+      el => el !== '$' && el !== ',').join('');
+
+    b.dataset.salary = +b.dataset.salary.split('').filter(
+      el => el !== '$' && el !== ',').join('');
+
+    return b.dataset.salary - a.dataset.salary;
+  });
+
+  items[0].replaceWith(...arr);
+}
+
+function getEmployees(list) {
+  const employeeList = document.querySelector('ul');
+
+  employeeList.append(...arr);
+
+  return employeeList;
+}
+
+sortList(arr);
+getEmployees(arr);
