5f6912b858b4e2394380af7aee1fb644b8c0ed63
Yana <yanamelnychuk10@gmail.com>
2022-10-20 15:56:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const listArr = [...list];
+const listAfterSort = document.querySelector('ul');
+
+const getNumber = (str) => Number(str.split(',').join('').slice(1));
+
+const sortList = (arr) => arr.sort((prev, current) =>
+  getNumber(current.dataset.salary) - getNumber(prev.dataset.salary));
+
+sortList(listArr);
+
+for (const item of listArr) {
+  listAfterSort.append(item);
+};
+
+const getEmployees = (listItem) => [...listItem];
+
+getEmployees(list);
