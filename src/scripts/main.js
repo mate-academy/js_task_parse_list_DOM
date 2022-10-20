e8030c6383a8da38a589446f48ca51cf7abf24cc
Kolya Movchan <kmovchan261@gmail.com>
2022-10-20 03:47:16 +0300

    js_task_parse_list_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+function sortList(listProperties) {
+  return [...listProperties].sort((salary1, salary2) =>
+    Number(replaceSigns(salary2.dataset.salary))
+    - Number(replaceSigns(salary1.dataset.salary))
+  );
+};
+
+function replaceSigns(string) {
+  return string.replace(',', '').replace('$', '');
+};
+
+const sortedList = sortList(listItems);
+
+list.innerHTML
+= sortedList.map(employee => `
+  <li
+  data-position = ${employee.dataset.position}
+  data-salary = ${employee.dataset.salary}
+  data-age = ${employee.dataset.age}
+  >
+  ${employee.innerText}
+  </li>`).join('');
+
+function getEmployees(listProperties) {
+  return [...listProperties].map(employee => {
+    return {
+      ...employee,
+      name: employee.innerText.replace('\n', '').trim(),
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    };
+  });
+};
+
+getEmployees(sortedList);
