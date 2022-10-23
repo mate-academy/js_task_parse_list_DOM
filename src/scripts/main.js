51c5662e6bef99aa49c8f955ee70631bc85fd059
Margarita Chorniak <chorniak.rita@gmail.com>
2022-10-23 14:19:45 +0300

    parse_list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const employList = document.querySelector('ul');
+
+function convertToNumber(str) {
+  return +str.slice(1).split(',').join('');
+}
+
+function sortList(list) {
+  const sortedList = [...list.children].sort((a, b) => {
+    return convertToNumber(b.dataset.salary)
+    - convertToNumber(a.dataset.salary);
+  });
+
+  list.innerHtml = '';
+  list.append(...sortedList);
+}
+
+function getEmployees(list) {
+  const employArray = [...list.children];
+  const res = [];
+
+  for (const el of employArray) {
+    res.push({
+      name: el.innerHtml,
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    });
+  }
+
+  return res;
+}
+
+sortList(employList);
+getEmployees(employList);
