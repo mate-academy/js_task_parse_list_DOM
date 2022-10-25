acd0c02352d0e36289e53c44a71b10f5744371c1
Anastasiya145 <asiva@ukr.net>
2022-10-25 18:32:39 +0200

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+function sortList(listProporties) {
+  return [...listItems].sort((a, b) =>
+    Number(changingSignes(b.dataset.salary)) 
+    - Number(changingSignes(a.dataset.salary))
+  );
+};
+
+const sortedList = sortList(listItems);
+
+function changingSignes(string) {
+  return string.replace(',', '').replace('$', '');
+};
+
+list.innerHTML
+= sortedList.map(employee => `
+  <li
+  data-position = ${employee.dataset.position}
+  data-salary = ${employee.dataset.salary}
+  data-age = ${employee.dataset.age}
+  >
+  ${employee.innerText}
+  </li>`).join('');
+
+function getEmployees(listProperties) {
+  return [...listProperties].map(employee => {
+    return {
+      ...employee,
+      name: employee.innerText.replace('\n', '').trim(),
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    };
+  });
+};
+
+getEmployees(sortedList);
