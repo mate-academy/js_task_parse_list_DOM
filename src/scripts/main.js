acbbff3bf39ea9dcc281f5134cdfb4600d3c72cc
Valentyn Ivanenko <Ivanenko1402@gmail.com>
2022-10-25 17:24:59 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const arrList = [...document.querySelectorAll('li')];
+const ul = document.querySelector('ul');
+
+function sortForSalary(arr) {
+  return arr.sort((a, b) =>
+    +b.dataset.salary.split(',').join('').slice(1)
+    - +a.dataset.salary.split(',').join('').slice(1));
+}
+
+function getObj(arr) {
+  const arrPeople = [];
+
+  for (const item of arr) {
+    arrPeople.push({
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    });
+  }
+
+  return arrPeople;
+}
+
+for (const item of sortForSalary(arrList)) {
+  ul.append(item);
+}
+
+getObj(arrList);
