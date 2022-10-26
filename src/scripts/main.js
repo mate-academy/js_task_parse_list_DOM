10db168378524935522fcbbfdc47f57c3b06c4a9
Kate Riazantseva <katyarezvova96@gmail.com>
2022-10-26 16:56:25 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
+const list = document.querySelectorAll('li');
 
-// write code here
+let listOfEmployees = [...list].map(person => ({
+    name: person.innerText,
+    position: person.dataset.position,
+    salary: Number(person.dataset.salary.replace(/[\,\$]/g, '')),
+    age: Number(person.dataset.age),
+}));
+
+const sortList = (list) => {
+    return list.sort((a, b) => b.salary - a.salary);
+
+};
+
+let sortListOfEmployees = sortList(listOfEmployees);
+
+console.log(sortListOfEmployees);
+
+const getEmployees = (sortListOfEmployees) => {
+  for (let i = 0; i < sortListOfEmployees.length; i++) {
+    list[i].innerText = sortListOfEmployees[i].name;
+    list[i].dataset.position = sortListOfEmployees[i].position;
+    list[i].dataset.salary = `$${sortListOfEmployees[i].salary.toLocaleString('en-US')}`;
+    list[i].dataset.age = sortListOfEmployees[i].age;
+  };
+};
+
+getEmployees(sortListOfEmployees);
+
+console.log(list[0])
