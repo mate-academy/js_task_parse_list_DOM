4524e2d493619cbda771c13bc09c4349391c3589
Kate Riazantseva <katyarezvova96@gmail.com>
2022-10-27 17:04:43 +0300

    update solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,29 @@
 'use strict';
+
 const list = document.querySelectorAll('li');
 
-let listOfEmployees = [...list].map(person => ({
-    name: person.innerText,
-    position: person.dataset.position,
-    salary: Number(person.dataset.salary.replace(/[\,\$]/g, '')),
-    age: Number(person.dataset.age),
+const listOfEmployees = [...list].map(person => ({
+  name: person.innerText,
+  position: person.dataset.position,
+  salary: Number(person.dataset.salary.replace(/[, $]/g, '')),
+  age: Number(person.dataset.age),
 }));
 
-const sortList = (list) => {
-    return list.sort((a, b) => b.salary - a.salary);
-
+const sortList = (listToSort) => {
+  return listToSort.sort((a, b) => b.salary - a.salary);
 };
 
-let sortListOfEmployees = sortList(listOfEmployees);
+const sortListOfEmployees = sortList(listOfEmployees);
 
-const getEmployees = (sortListOfEmployees) => {
-  for (let i = 0; i < sortListOfEmployees.length; i++) {
-    list[i].innerText = sortListOfEmployees[i].name;
-    list[i].dataset.position = sortListOfEmployees[i].position;
+const getEmployees = (listToFollow) => {
+  for (let i = 0; i < listToFollow.length; i++) {
+    list[i].innerText = listToFollow[i].name;
+    list[i].dataset.position = listToFollow[i].position;
+
     list[i].dataset.salary
-      = `$${sortListOfEmployees[i].salary.toLocaleString('en-US')}`;
+      = `$${listToFollow[i].salary.toLocaleString('en-US')}`;
     list[i].dataset.age = sortListOfEmployees[i].age;
   };
 };
 
 getEmployees(sortListOfEmployees);
-
