c07146560f8d8d6ce90577c036177a41f76ea9b3
Dima Romanenko <demigod177712@gmail.com>
2022-10-29 17:18:15 +0300

    list sorted by salary

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+const employees = document.querySelectorAll('li');
+
+function sortList(list) {
+  const sorted = [...list].sort((emp1, emp2) =>
+    +(emp2.dataset.salary.split('$').join('').split(',').join(''))
+    - +(emp1.dataset.salary.split('$').join('').split(',').join('')));
+
+  employeesList.append(...sorted);
+}
+
+function getEmployees(list) {
+  return [...list].map(person => ({
+    name: person.innerText,
+    ...person.dataset,
+  }));
+}
+
+sortList(employees);
+getEmployees(employees);
