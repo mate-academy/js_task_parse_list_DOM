3a49eb6e20df4b0fc371194e426e25c2eb32c732
Dima Romanenko <demigod177712@gmail.com>
2022-11-03 00:24:01 +0200

    duplicated code moved to separate function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,17 @@
 const employeesList = document.querySelector('ul');
 const employees = document.querySelectorAll('li');
 
+function fixSalaryString(string) {
+  return string.split('$')
+    .join('')
+    .split(',')
+    .join('');
+}
+
 function sortList(list) {
   const sorted = [...list].sort((emp1, emp2) =>
-    +(emp2.dataset.salary
-      .split('$')
-      .join('')
-      .split(',')
-      .join(''))
-    - +(emp1.dataset.salary
-      .split('$')
-      .join('')
-      .split(',')
-      .join('')));
+    Number(fixSalaryString(emp2.dataset.salary))
+    - Number(fixSalaryString(emp1.dataset.salary)));
 
   employeesList.append(...sorted);
 }
