60c7b2264959b779a378a813b88b524be946d81a
ovsiannikov <ovsiannikov@ucu.edu.ua>
2022-11-02 15:50:20 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+function toNumber(salary) {
+  return parseFloat(salary.substring(1).replaceAll(',', ''));
+}
+
+function sortList(list) {
+  const lst = document.querySelector(list);
+
+  [...lst.children]
+    .sort((a, b) => toNumber(b.dataset.salary) - toNumber(a.dataset.salary))
+    .forEach(node => lst.appendChild(node));
+}
+
+function getEmployees(list) {
+  const lst = document.querySelector('ul');
+  const employees = [...lst.children].map(item => ({
+    name: item.innerHTML.trim(),
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+
+  return employees;
+}
+
+getEmployees('ul');
+sortList('ul');
