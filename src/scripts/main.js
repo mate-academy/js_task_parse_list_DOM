052263f9c4079ed2c696e2d7c7cb22fdca632288
Antonenko-AA <alexandr.antonenko.fr@gmail.com>
2022-11-04 03:43:11 +0200

    Parse list has been created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const normalize = str => Number(str.replace(/[$,]/g, ''));
+const userList = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  userList[0].parentNode.append(...userList.sort((a, b) => {
+    return normalize(b.dataset.salary) - normalize(a.dataset.salary);
+  }));
+}
+
+function getEmployees(list) {
+  return userList.map(user => {
+    return {
+      name: user.textContent.trim(),
+      ...user.dataset,
+    };
+  });
+}
+
+sortList(userList);
+getEmployees(userList);
