e5a39da96a00b87822e72e59f10bbd7ddb2fb531
Elena Ver <hellena84@i.ua>
2022-11-04 14:41:29 +0100

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write code here
+const people = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  const listWithSalary = list.sort((a, b) =>
+    Number(b.getAttribute('data-salary').replace(/[^0-9]/gim, ''))
+  - Number(a.getAttribute('data-salary').replace(/[^0-9]/gim, '')));
+
+  document.querySelector('ul').append(...listWithSalary);
+}
+
+function getEmployees(list) {
+  return list.map(person => ({
+    name: person.innerText,
+    position: person.dataSet.position,
+    salary: person.dataSet.salary,
+    age: person.dataSet.age,
+  }));
+};
+
+sortList(people);
+getEmployees(people);
