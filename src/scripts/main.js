a6d9db59da52d9ca145eba75da0d72cb60b97a4c
Oleg Gord <kvb3.ol@gmail.com>
2022-11-08 20:53:06 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+function sortList(people) {
+  const ulSort = document.querySelector('ul');
+  const sortPeople = [...people].sort((a, b) =>
+    +(b.dataset.salary.substring(1).split(',').join(''))
+    - +(a.dataset.salary.substring(1).split(',').join(''))
+  );
+
+  for (const item of sortPeople) {
+    ulSort.append(item);
+  }
+}
+
+function getEmployees(listOfEmployees) {
+  const employ = [...listOfEmployees];
+  const arrayOfEmployees = [];
+
+  for (let i = 0; i < employ.length; i++) {
+    const man = {};
+
+    man.name = employ[i].innerText;
+    man.position = employ[i].dataset.position;
+    man.salary = employ[i].dataset.salary;
+    man.age = employ[i].dataset.age;
+
+    arrayOfEmployees.push(man);
+  }
+
+  return arrayOfEmployees;
+}
+sortList(list);
+getEmployees(list);
