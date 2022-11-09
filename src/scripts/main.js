5555f1f862d77b2322f8cca7d3498fc18471058c
Maryna Kucher <marina.kucher8@gmail.com>
2022-11-09 16:48:18 +0200

    Solution 18 is fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,26 @@
 'use strict';
 
 function num(string) {
-  return string.replace('$', '').replace(',', '');
-}
-
-function sortList(list) {
-  return list.sort((a, b) => num(b.dataset.salary) - num(a.dataset.salary));
-}
-
-function getEmployees(list) {
-  return list.map(employee => ({
-    name: employee.innerText,
-    position: employee.dataset.position,
-    salary: employee.dataset.salary,
-    age: employee.dataset.age,
-  }));
+  return string.replace(/[,$]/g, '');
 }
 
 const listEmployees = Array.from(document.querySelectorAll('ul > li'));
 
 sortList(listEmployees);
+
+function sortList(list) {
+  list.sort((a, b) =>
+    num(b.dataset.salary) - num(a.dataset.salary)).forEach(employee => {
+    document.querySelector('ul').append(employee);
+  });
+}
+
+function getEmployees(list) {
+  return list.map(employee => {
+    return {
+      name: employee.innerText, ...employee.dataset,
+    };
+  });
+}
+
 getEmployees(listEmployees);
