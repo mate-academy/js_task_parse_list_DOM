3de21b10c4816f5ed15f6896936e1766f5c38b3c
Bohdan Rudenko <RudBeee@gmail.com>
2022-11-12 19:05:16 +0200

    vol_1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write code here
+const startList = document.querySelectorAll('li');
+const startListSorted = sortList(startList);
+const ul = document.querySelector('ul');
+
+function correctNum(string) {
+  return +string.dataset.salary.slice(1).split(',').join('');
+}
+
+function sortList(list) {
+  return [...list].sort((a, b) => correctNum(b) - correctNum(a));
+};
+
+function getEmployees(where, afterElements) {
+  afterElements.forEach(element => where.append(element));
+};
+
+getEmployees(ul, startListSorted);
