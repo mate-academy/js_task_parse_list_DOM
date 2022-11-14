3fc55d9dbe664d713252140db61201ec6eb32390
Olena Shevchenko <shevochkas@gmail.com>
2022-11-14 19:16:52 +0200

    js task parse list DOM was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+const itemOfList = document.querySelectorAll('li');
+
+function convertingSalary(salary) {
+  return +salary.slice(1).split(',').join('');
+};
+
+function sortList(list) {
+  return [...list]
+    .sort((x, y) =>
+      convertingSalary(y.dataset.salary) - convertingSalary(x.dataset.salary)
+    );
+}
+
+ul.append(...sortList(itemOfList));
+
+function getEmployees(list) {
+  return [...itemOfList].map(person => ({
+    name: person.innerText,
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+}
+
+getEmployees(itemOfList);
