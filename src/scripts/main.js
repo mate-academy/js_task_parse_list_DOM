256c0d2d6dc285f4c3943edca7333a68abfaf023
Sergey Vorobey <sergey88nik@gmail.com>
2022-11-16 18:12:11 +0200

    Parse list is solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const employsList = [...document.querySelectorAll('li')];
+const listParent = document.querySelector('ul');
+
+function sortList(list) {
+  list.sort((a, b) => {
+    return normalize(b.dataset.salary) - normalize(a.dataset.salary);
+  });
+
+  list.forEach(element => listParent.append(element));
+}
+
+function normalize(item) {
+  return Number(item.replace(/[$,]/g, ''));
+}
+
+sortList(employsList);
+
+function getEmployees(list) {
+  return list.map(item => {
+    return {
+      name: item.innerText,
+      ...item.dataset,
+    };
+  });
+}
+
+getEmployees(employsList);
