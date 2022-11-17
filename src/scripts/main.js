5629ed75a188cd701c3566aef463e270cdad0176
Sergey Vorobey <sergey88nik@gmail.com>
2022-11-17 12:00:24 +0200

    fixed all comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,29 @@
 'use strict';
 
-const employsList = [...document.querySelectorAll('li')];
+const employeesList = [...document.querySelectorAll('li')];
 const listParent = document.querySelector('ul');
 
 function sortList(list) {
-  list.sort((a, b) => {
+  const copyList = [...list];
+
+  copyList.sort((a, b) => {
     return normalize(b.dataset.salary) - normalize(a.dataset.salary);
   });
 
-  list.forEach(element => listParent.append(element));
+  copyList.forEach(element => listParent.append(element));
 }
 
 function normalize(item) {
   return Number(item.replace(/[$,]/g, ''));
 }
 
-sortList(employsList);
+sortList(employeesList);
 
 function getEmployees(list) {
-  return list.map(item => {
-    return {
-      name: item.innerText,
-      ...item.dataset,
-    };
-  });
+  return list.map(item => ({
+    name: item.innerText,
+    ...item.dataset,
+  }));
 }
 
-getEmployees(employsList);
+getEmployees(employeesList);
