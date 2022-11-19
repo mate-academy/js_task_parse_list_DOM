1877ca77e327847642e7c737e3e068274de0be7f
Andrii Medintsev <medintsev.andrew@gmail.com>
2022-11-19 23:26:15 +0200

    Employees list sorting feature was implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+
+function convertSalary(salaryString) {
+  return salaryString.slice(1).replace(',', '.') * 1000;
+}
+
+function sortList(list) {
+  return [...list.children]
+    .sort((a, b) =>
+      convertSalary(b.dataset.salary) - convertSalary(a.dataset.salary))
+    .forEach(elem => list.append(elem));
+}
+
+function getEmployees(list) {
+  return [...list.children].map(employee => ({
+    name: employee.textContent.trim(),
+    position: employee.dataset.position,
+    salary: employee.dataset.salary,
+    age: employee.dataset.age,
+  }));
+}
+
+sortList(employeesList);
+getEmployees(employeesList);
