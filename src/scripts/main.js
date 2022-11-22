b01d5daad60be973657bfa7e37c412200a1ed9f6
Maks <comanderius@gmail.com>
2022-11-22 12:46:33 +0100

    Parse list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const names = document.querySelectorAll('li');
+
+function sortList(personList) {
+  return personList.sort((a, b) => b.salary - a.salary);
+}
+
+function getEmployees(people) {
+  return [...people].map(person => {
+    return {
+      name: person.textContent.replace(/\n/g, '').trim(),
+      position: person.dataset.position,
+      salary: +person.dataset.salary.slice(1).split(',').join(''),
+      age: +person.dataset.age,
+    };
+  });
+}
+
+sortList(getEmployees(names));
+
+sortList(getEmployees(names)).forEach((element, index) => {
+  names[index].textContent = element.name;
+  names[index].dataset.salary = element.salary;
+});
