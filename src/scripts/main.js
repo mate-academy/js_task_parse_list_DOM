d1c584986df026e2aab4f33a2e3ef27d63fa5ec1
divineretribution1 <danya00776@gmail.com>
2022-11-26 15:28:50 +0200

    changes 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,28 @@
 'use strict';
 
-const employeesNodeList = document.querySelectorAll('body > ul > li')
+const employeesNodeList = document.querySelectorAll('body > ul > li');
 const convertToObjects = (nodeList) => Array.from(nodeList).map(el => {
-    return {
-        name: el.textContent.trim(),
-        salary: el.getAttribute('data-salary'),
-        age: el.getAttribute('data-age'),
-        position: el.getAttribute('data-position'),
-    }
-})
+  return {
+    name: el.textContent.trim(),
+    salary: el.getAttribute('data-salary'),
+    age: el.getAttribute('data-age'),
+    position: el.getAttribute('data-position'),
+  };
+});
 const sortArrBySalary = (arr) => {
-    const convertStrToNumber = (str) => Number(str.slice(1).replaceAll(',', ''));
-    return arr.sort((a, b) => convertStrToNumber(b.salary) - convertStrToNumber(a.salary))
-}
-const convertedArr = convertToObjects(employeesNodeList)
-const sortedArr = sortArrBySalary(convertedArr)
+  const StrToNumber = (str) => Number(str.slice(1).replaceAll(',', ''));
+
+  return arr.sort((a, b) => StrToNumber(b.salary) - StrToNumber(a.salary));
+};
+const convertedArr = convertToObjects(employeesNodeList);
+const sortedArr = sortArrBySalary(convertedArr);
 const changeLiToSorted = (arr, nodeList) => {
-    Array.from(nodeList).forEach((item, i) => {
-        item.textContent = arr[i].name
-        item.setAttribute('data-age', arr[i].age)
-        item.setAttribute('data-salary', arr[i].salary)
-        item.setAttribute('data-position', arr[i].position)
-    })
-}
-changeLiToSorted(sortedArr, employeesNodeList)
+  Array.from(nodeList).forEach((item, i) => {
+    item.textContent = arr[i].name;
+    item.setAttribute('data-age', arr[i].age);
+    item.setAttribute('data-salary', arr[i].salary);
+    item.setAttribute('data-position', arr[i].position);
+  });
+};
+
+changeLiToSorted(sortedArr, employeesNodeList);
