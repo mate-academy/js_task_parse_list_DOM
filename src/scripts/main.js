b8a87fbd5318c9b27283eba7d12e586d86507889
Dmitriy-Kovalenko <dmitrijkovalenko@gmail.com>
2022-11-30 02:05:18 +0200

    sort Ok

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const list = document.getElementsByTagName('li');
+
+function getEmployees(listProperty) {
+  const employees = [];
+
+  for (const employee of list) {
+    employees.push({
+      name: employee.innerText,
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    });
+  }
+
+  return employees;
+}
+
+function sortList(listProperty) {
+  const sort = getEmployees(listProperty).sort((a, b) =>
+    convertToNum(b.salary) - convertToNum(a.salary)
+  );
+
+  for (let i = 0; i < listProperty.length; i++) {
+    listProperty[i].dataset.position = sort[i].position;
+    listProperty[i].dataset.salary = sort[i].salary;
+    listProperty[i].dataset.age = sort[i].age;
+    listProperty[i].innerText = sort[i].name;
+  }
+}
+
+sortList(list);
+
+function convertToNum(string) {
+  return +(string.slice(1).replace(',', '.'));
+}
