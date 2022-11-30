0205556801e0edb5335ebab0937b464301b36089
Anthony Buhaienko <dubsterok@gmail.com>
2022-11-30 17:38:04 +0200

    added sort function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+function decreaseSortOrder() {
+  const list = document.querySelector('ul');
+  const items = list.childNodes;
+  const itemsArr = [];
+
+  for (const i in items) {
+    if (items[i].nodeType === 1) {
+      itemsArr.push(items[i]);
+    }
+  }
+
+  itemsArr.sort((a, b) => {
+    return (
+      +b.dataset.salary
+        .split('')
+        .filter(item => !isNaN(item)).join('')
+    ) - (
+      +a.dataset.salary
+        .split('')
+        .filter(item => !isNaN(item)).join('')
+    );
+  });
+
+  for (let i = 0; i < itemsArr.length; i++) {
+    list.appendChild(itemsArr[i]);
+  }
+
+  return itemsArr;
+}
+
+decreaseSortOrder();
