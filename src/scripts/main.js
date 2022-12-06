3db9021c6908b3cc2995ef1e9026af5d5dda7f55
Serhii Vovchenko <seriivigas@gmail.com>
2022-12-06 20:08:28 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const employees = document.querySelectorAll('li');
+const employeesList = document.querySelector('ul');
+
+function converting(text) {
+  return Number(text.replace(/\D/g, ''));
+}
+
+function sort(list) {
+  const copy = [...list];
+  const sortElement = copy.sort((a, b) =>
+    converting(b.dataset.salary) - converting(a.dataset.salary));
+
+  employeesList.append(...sortElement);
+};
+
+function getEmployeesList(list) {
+  return [...list].map(element => {
+    return {
+      name: element.innerText,
+      position: element.dataset.position,
+      salary: element.dataset.salary,
+      age: element.dataset.age,
+    };
+  });
+};
+
+sort(employees);
+getEmployeesList(employees);
