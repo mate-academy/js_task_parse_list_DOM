86c5eb908c5e2282d98613a1382a0117fadb3248
ZUBOV ILLIA <zubovillia@gmail.com>
2022-12-08 14:43:24 +0300

    lgtm

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+const sortedBySalaryEmployees = sortList(ul.children, 'salary');
+const btn = document.querySelector('button');
+
+function sortList(arr, sortingBy) {
+  return [...arr].sort((a, b) => {
+    const aa = a.dataset[sortingBy].slice(1).replace(/,/g, '');
+    const bb = b.dataset[sortingBy].slice(1).replace(/,/g, '');
+
+    return bb - aa;
+  });
+}
+
+ul.innerHTML = `
+  ${sortedBySalaryEmployees.map(el => (
+    `<li
+        data-position="${el.dataset['position']}"
+        data-salary="${el.dataset['salary']}"
+        data-age="${el.dataset['age']}"
+      >
+        ${el.innerText}
+        <span id="hided" class="money hided">${el.dataset['salary']}</span>
+      </li>`
+  )).join(' ')}
+`;
+
+const hided = document.querySelectorAll('.money');
+
+btn.addEventListener('click', () => {
+  hided.forEach((el) => {
+    el.classList.toggle('hided');
+  });
+});
