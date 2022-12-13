7f7457e7ef94e82b665a0b736fc4a24d912e5076
Roman Malysh <romanmalysh@romans-macbook-air.home>
2022-12-13 19:07:47 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write code here
+
+const [...list] = document.querySelectorAll('li');
+
+function sortList(list) {
+    list.sort((a, b) => a.salary - b.salary);
+
+    return list;
+}
+
+function getEmployees(people) {
+    return people.map((elem) => {
+        return {
+            name: elem.textContent,
+            position: elem.dataset.position,
+            salary: +elem.dataset.salary.slice(1).split(',').join(''),
+            age: +elem.dataset.age,
+        };
+    });
+}
+
+const sortedList = sortList(getEmployees(list));
+
+sortedList.forEach((element, index) => {
+  list[index].textContent = element.name;
+  list[index].dataset.salary = element.salary;
+});
