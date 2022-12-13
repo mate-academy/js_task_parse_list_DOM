bdfdf628ab34594692e37d2000f6a6d36af7a474
Dmytro Kravchenko <dmytro318@gmail.com>
2022-12-13 22:28:18 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+const strToNo = (str) => {
+  return Number(str.slice(1).split(',').join(''));
+};
+
+const sortBySalary = (employees) => {
+  return [...employees].sort((a, b) =>
+    strToNo(b.dataset.salary) - strToNo(a.dataset.salary));
+};
+
+const getEmployees = (employees) => {
+  return [...employees].map(person => (
+    {
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    }));
+};
+
+document.querySelector('ul').append(...sortBySalary(list));
+getEmployees(list);
