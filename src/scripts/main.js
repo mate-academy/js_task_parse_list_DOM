0b038325a4d57b85b4e539bc54202e751a4bab54
Sergey Kovyazin <mr.roland1989@gmail.com>
2022-12-13 02:35:23 +0200

    Parse list task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+let list = document.body.querySelectorAll('li');
+
+function sortList(item) {
+  const result = [...item].sort((a, b) => {
+    return salaryToNumber(b.dataset.salary) - salaryToNumber(a.dataset.salary);
+  });
+
+  return result;
+};
+
+list = sortList(list);
+
+function getEmployees(item) {
+  const resultArr = [];
+
+  for (const person of item) {
+    const obj = {};
+
+    obj.name = person.innerText;
+    obj.position = person.dataset.position;
+    obj.salary = person.dataset.salary;
+    obj.age = person.dataset.age;
+
+    resultArr.push(obj);
+  }
+
+  return resultArr;
+};
+
+sortList(list);
+getEmployees(list);
+
+function salaryToNumber(money) {
+  return +money.slice(1).split(',').join('');
+}
