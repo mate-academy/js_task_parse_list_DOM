2b14e8756fc58706c0aa333a9b0eb62fb7dc6a2c
Oselskyi Oleksandr <oselskyi3@gmail.com>
2022-12-13 22:10:53 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write code here
+const list = document.querySelector('ul');
+
+const listElements = [...list.children];
+
+const sortedElements = listElements.sort((a, b) => {
+  return (salaryInNumber(b)) - (salaryInNumber(a));
+});
+
+function salaryInNumber(value) {
+  return +(value.dataset.salary.split(',').join('').slice(1));
+}
+
+list.innerHTML = '';
+
+sortedElements.forEach(el => list.appendChild(el));
