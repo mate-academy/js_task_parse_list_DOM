c231118d2a716138869c5810dacea2168fd7537f
Konstantin Burnazaki <kostas.burnazaki@gmail.com>
2022-12-15 21:40:40 +0200

    initial

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
-// write code here
+function sortList(ul) {
+  const newUl = ul.cloneNode(false);
+
+  const lis = parseEmployees(ul);
+
+  lis.sort(function(a, b) {
+    return convertSalary(b) - convertSalary(a);
+  });
+
+  for (let i = 0; i < lis.length; i++) {
+    newUl.append(lis[i]);
+  }
+
+  ul.replaceWith(newUl);
+}
+
+function convertSalary(x) {
+  return parseFloat(x.dataset.salary.replace(/\$|,/g, ''));
+}
+
+function parseEmployees(list) {
+  const res = [];
+
+  for (let i = list.childNodes.length; i >= 0; i--) {
+    if (list.childNodes[i] && list.childNodes[i].nodeType === 1) {
+      res.push(list.childNodes[i]);
+    }
+  }
+
+  return res;
+}
+
+function getEmployees(ul) {
+  const employeesNodes = parseEmployees(ul);
+  const res = [];
+
+  for (let i = 0; i < employeesNodes.length; i++) {
+    const employee = {
+      name: employeesNodes[i].innerText,
+      position: employeesNodes[i].dataset.position,
+      salary: convertSalary(employeesNodes[i]),
+      age: parseFloat(employeesNodes[i].dataset.age),
+    };
+
+    res.push(employee);
+  }
+
+  return res;
+}
+
+sortList(document.querySelector('ul'));
+
+getEmployees(document.querySelector('ul'));
