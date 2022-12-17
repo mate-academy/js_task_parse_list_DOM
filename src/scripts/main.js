388486c95d3682e535ae9977ec8b770aeb4c54c7
Nick_Radchuk <mukola.radchuk@gmail.com>
2022-12-17 21:11:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+const list = [...ul.children];
+
+function sortList(params) {
+  const sorted = params.sort((a, b) =>
+    Number(b.dataset.salary.split('$').join('').split(',').join('')
+    - Number(a.dataset.salary.split('$').join('').split(',').join('')))
+  );
+
+  ul.append(...sorted);
+}
+
+function getEmployee(params) {
+  return params.map(item => ({
+    name: item.innerText,
+    ...item.dataset,
+  }));
+}
+
+sortList(list);
+getEmployee(list);
