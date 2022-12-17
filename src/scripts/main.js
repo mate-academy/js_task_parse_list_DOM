241be8c8def7273a83e6a60e01db2dbe6c0e8313
Oleksii Pishchuhin <aliliolek@gmail.com>
2022-12-17 23:28:15 +0100

    solution 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write code here
+const items = document.querySelectorAll('li');
+
+// sorts an array
+function sortList(list) {
+  const arrEmpl = [...list].map(item => ({
+    ...item.dataset,
+    'name': item.innerText,
+  }));
+
+  arrEmpl.forEach(item => {
+    item.salary = item.salary.replace(/[^a-zа-яё0-9\s]/gi, '');
+    item.salary = Number(item.salary);
+  });
+
+  return arrEmpl.sort((a, b) => b.salary - a.salary);
+}
+
+function getEmployees(sortedArray, list) {
+  for (let i = 0; i < list.length; i++) {
+    list[i].dataset.position = sortedArray[i].position;
+    list[i].dataset.salary = sortedArray[i].salary;
+    list[i].dataset.age = sortedArray[i].age;
+    list[i].innerText = sortedArray[i].name;
+  }
+}
+getEmployees(sortList(items), items);
