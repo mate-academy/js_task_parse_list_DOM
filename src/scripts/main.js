8c0c8fda13039964a7d2aec2cf76e779f2d2dceb
Denis Bokov <denisbokov1703@gmail.com>
2022-12-20 20:56:50 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const employees = document.querySelector('ul');
+
+const listEmployees = [...employees.children];
+
+const sorted = listEmployees.sort((a, b) => {
+  return sallary(a) - sallary(b);
+});
+
+function sallary(value) {
+  const noDollar = value.dataset.salary.split('$').join('');
+  const salary = noDollar.split(',').join('');
+
+  return (salary);
+}
+
+employees.innerHTML = '';
+
+for (const sort in sorted) {
+  employees.prepend(sorted[sort]);
+}
+
+function getEmployee(list) {
+  const result = [];
+  const arrayObj = [...list].map((person) => ({
+    name: person.textContent.trim(),
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+
+  result.push(arrayObj);
+
+}
+
+getEmployee(listEmployees);
