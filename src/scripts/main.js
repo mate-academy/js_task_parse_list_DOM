33cdd0b4ae97b7ae0daf9c68fb25d953ff279b53
Oleksii Mishchenko <alex.mishch@gmail.com>
2022-12-24 05:15:18 +0200

    Sorted list of employees

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const salaryNum = str => +str.slice(1).split(',').join('');
+
+function sortList(parseList) {
+  return [...parseList].sort((a, b) =>
+    salaryNum(b.dataset.salary) - salaryNum(a.dataset.salary)
+  );
+};
+
+function getEmployees(parseList) {
+  return [...parseList].map(employee => {
+    return {
+      name: (employee.innerText).trim(),
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    };
+  });
+};
+
+const listOfEmployees = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+const sortedEmployees = sortList(listOfEmployees);
+
+list.innerHTML = `
+  ${(sortedEmployees.map(employee => `
+    <li
+      data-position=${employee.dataset.position}
+      data-salary=${employee.dataset.salary}
+      data-age=${employee.dataset.age}
+    >
+      ${employee.innerText}
+    </li>
+  `).join(''))}
+`;
+
+getEmployees(listOfEmployees);
