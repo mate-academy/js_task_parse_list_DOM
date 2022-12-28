c0dc5eb0a2423102709f9710afe7d38095602529
Mykola Huryn <mukola197022@gmail.com>
2022-12-28 17:56:04 +0100

    js_task_parse_list_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const element = document.body.children[1];
+const list = element.children;
+
+const sortList = function(value) {
+  return [...list].sort((a, b) =>
+    Number(b.getAttribute('data-salary').slice(1).split(',').join(''))
+      - Number(a.getAttribute('data-salary').slice(1).split(',').join('')));
+};
+
+element.append(...sortList(list));
+
+const getEmployees = function(value) {
+  const arrayList = [...list];
+
+  return arrayList.map(x =>
+    (
+      {
+        name: x.innerText,
+        position: x.getAttribute('data-position'),
+        salary: x.getAttribute('data-salary'),
+        age: x.getAttribute('data-age'),
+      }
+    )
+  );
+};
+
+getEmployees(list);
