08d3653075885f596a62a7feffdcf6dc4117d9de
yana-kotsulym <yanak7170@gmail.com>
2023-01-05 22:56:06 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+const sortList = (listOfValue) => {
+  let sort = [...listOfValue].sort((a, b) =>
+    b.dataset.salary.split(',').join('').slice(1)
+    - a.dataset.salary.split(',').join('').slice(1));
+
+  sort = sort.forEach(item => list.append(item));
+
+  return sort;
+};
+
+function getEmployees(listOfValue) {
+  const result = [...listOfValue].map(item => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+
+  return result;
+}
+
+sortList(list.children);
+getEmployees(list.children);
