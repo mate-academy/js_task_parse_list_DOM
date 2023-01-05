db918370cd3c0009731b26758bdbdcd7164433f0
Kate Matviiv <katyaya56@gmail.com>
2023-01-05 22:42:57 +0200

    Splotion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write code here
+
+const peopleList = [...document.querySelectorAll('li')];
+
+function toNumber(str) {
+  return +str.replace(/[^\d]/g, '');
+}
+
+function sortList(list) {
+  list.sort((a, b) => toNumber(b.dataset.salary)
+  - toNumber(a.dataset.salary));
+
+  document.querySelector('ul').append(...list);
+}
+
+function getEmployee(list) {
+  return [...list].map(person => ({
+    name: person.textContent.trim(),
+    ...person.dataset,
+  }));
+}
+
+sortList(peopleList);
+getEmployee(peopleList);
