134cda3988f925336bfe61b450efbaeb08b10aaa
Karadzha Mykola <nicholaskaradzha@gmail.com>
2023-01-09 09:29:45 +0000

    task parse list DOM comleted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const listItems = [...document.querySelectorAll('li')];
+const list = document.getElementsByTagName('ul')[0];
+
+const getNumber = (salary) => {
+  return Number(salary
+    .replace(',', '')
+    .replace('$', ''));
+};
+
+const sortList = (employees) => {
+  return employees.sort((li1, li2) => (
+    getNumber(li2.dataset.salary) - getNumber(li1.dataset.salary)));
+};
+
+const getEmployees = (employees) => {
+  for (const item of employees) {
+    list.append(item);
+  }
+};
+
+sortList(listItems);
+getEmployees(listItems);
