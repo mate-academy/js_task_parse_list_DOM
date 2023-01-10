ca05e96682ae5444863e0f6b4d5d78631312ec14
Anna Inozemets <inoznetta@gmail.com>
2023-01-10 20:40:43 +0200

    List of employees was sorted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+const employees = [...document.querySelectorAll('li')];
+
+function correctSalaryNum(salary) {
+  return Number(salary.split(',').join('').slice(1));
+}
+
+function sortList(list) {
+  const sortedList = list.sort((a, b) => {
+    return (correctSalaryNum(b.dataset.salary)
+      - correctSalaryNum(a.dataset.salary));
+  });
+
+  employeesList.append(...sortedList);
+}
+
+function getEmployees(list) {
+  return list.map(employee => {
+    return {
+      name: employee.innerText,
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: +employee.dataset.age,
+    };
+  });
+}
+
+sortList(employees);
+getEmployees(employees);
