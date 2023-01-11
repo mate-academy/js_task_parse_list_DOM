f6fae684339d4bc599e7e3c53cdbf6112615a8b7
abobyr <artem.bobyr@dataart.com>
2023-01-11 12:57:27 +0200

    task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+function sortList(list) {
+  const employeesCopy = [...list.children];
+
+  employeesCopy
+    .sort((a, b) => {
+      const firstSalary = +a.dataset.salary.replaceAll(/[$,]/g, '');
+      const secondSalary = +b.dataset.salary.replaceAll(/[$,]/g, '');
+
+      return secondSalary - firstSalary;
+    });
+
+  employeesCopy.forEach((emp) => employeesList.append(emp));
+}
+
+function getEmployees(list) {
+  return [...list.children].map((emp) => ({
+    name: emp.innerText,
+    position: emp.dataset.position,
+    salary: emp.dataset.salary,
+    age: emp.dataset.age,
+  }));
+}
+
+const employeesList = document.querySelector('ul');
+
+sortList(employeesList);
+getEmployees(employeesList);
