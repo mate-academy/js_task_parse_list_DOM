dba45e27f99dd682d8e5802516b6e9d7a5754873
unknown <zhodko32@gmail.com>
2023-01-14 15:17:08 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+
+const sortListItems = (list) => {
+  const listChildren = [...list.children];
+
+  return listChildren.sort((a, b) => {
+    const innerA = +a.attributes.getNamedItem('data-salary')
+      .value.slice(1).split(',').join('');
+    const innerB = +b.attributes.getNamedItem('data-salary')
+      .value.slice(1).split(',').join('');
+
+    if (innerA < innerB) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+};
+
+const sortList = (list) => {
+  const listItems = sortListItems(list);
+
+  for (const item of listItems) {
+    list.append(item);
+  }
+};
+
+sortList(employeesList);
