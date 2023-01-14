a4211fe5b3c373339fc7813479fee88d30bf1d58
Sofiia Triasko <sofia13tryasko@gmail.com>
2023-01-14 23:42:00 +0200

    commit 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+const listOfEmloyees = document.querySelector('ul');
+const employeesItems = document.querySelectorAll('li');
+
+function convertSalaryStringToNumber(string) {
+  const result = string.split(',').join('').substring(1);
+
+  return result;
+}
+
+function sortingBySalary(a, b) {
+  return convertSalaryStringToNumber(b.dataset.salary)
+- convertSalaryStringToNumber(a.dataset.salary);
+}
+
+function sortList(listItems, list) {
+  const sorted = [...listItems].sort(sortingBySalary);
+
+  for (const element of sorted) {
+    list.append(element);
+  }
+}
+
+function getEmployees(listItems) {
+  const employeesArray = [...listItems].map(item => (
+    {
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    }
+  )
+  );
+
+  return employeesArray;
+}
+
+sortList(employeesItems, listOfEmloyees);
+getEmployees(employeesItems);
