8b109a03956196c90c2b0b0d4c5f87b5d7b2d27c
Yuliia Andrieieva <andrieieva.y@gmail.com>
2023-01-15 19:54:55 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const peopleOfList = document.querySelectorAll('li');
+
+const listString = function(string) {
+  return +string.slice(1).split(',').join('');
+};
+
+const sortPeople = function(list) {
+  return [...list].sort((a, b) =>
+    listString(b.dataset.salary) - listString(a.dataset.salary));
+};
+
+document.querySelector('ul').append(...sortPeople(peopleOfList));
+
+const getEmployees = function(list) {
+  const allList = [...list];
+
+  return allList.map(people => (
+    {
+      name: people.innerText,
+      position: people.dataset.position,
+      salary: people.dataset.salary,
+      age: people.dataset.age,
+    }));
+};
+
+getEmployees(peopleOfList);
