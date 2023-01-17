249c8bbcf87f3209a09bb2286ee39447965a95fe
Vadym Prydatok <vadymprydatok@gmail.com>
2023-01-17 18:19:09 +0200

    list DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelectorAll('li');
+const sumString = string => +string.slice(1).split(',').join('');
+
+const sortList = function(list) {
+  return [...list].sort((a, b) =>
+    sumString(b.dataset.salary) - sumString(a.dataset.salary));
+};
+
+document.querySelector('ul').append(...sortList(employeesList));
+
+const getEmployees = function(list) {
+  return [...list].map(el => {
+    return ({
+      name: el.innerText,
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    });
+  });
+};
+
+getEmployees(employeesList);
