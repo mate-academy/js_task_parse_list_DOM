caafbf89219c87b4c806e00aaf86c00ee0245efe
Anastasia Radchenko <radchenckooo@gmail.com>
2023-01-19 16:59:32 +0200

    Salary

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
 // write code here
+const ul = document.querySelector('ul');
+const list = [...document.querySelectorAll('li')];
+
+function sortList(items) {
+  const sortItems = items.sort(
+    (a, b) =>
+      (+b.dataset.salary.slice(1).split(',').join(''))
+      - (+a.dataset.salary.slice(1).split(',').join(''))
+  );
+
+  ul.append(...sortItems);
+}
+
+function getEmployees(items) {
+  return items.map(item => ({
+    name: item.textContent,
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+}
+
+sortList(list);
+getEmployees(list);
