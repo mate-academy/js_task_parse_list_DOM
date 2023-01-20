f259cc76124db394fea90e56b4b595b2c280cc8e
Anastasia Radchenko <radchenckooo@gmail.com>
2023-01-20 11:37:23 +0200

    Salary

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,26 +2,25 @@
 
 // write code here
 const ul = document.querySelector('ul');
-const list = [...document.querySelectorAll('li')];
+const list = document.querySelectorAll('li');
+const sumString = string => +string.slice(1).split(',').join('');
 
-function sortList(items) {
-  const sortItems = items.sort(
-    (a, b) =>
-      (+b.dataset.salary.slice(1).split(',').join(''))
-      - (+a.dataset.salary.slice(1).split(',').join(''))
-  );
+const sortList = function(items) {
+  return [...items].sort((a, b) =>
+    sumString(b.dataset.salary) - sumString(a.dataset.salary));
+};
 
-  ul.append(...sortItems);
-}
+ul.append(...sortList(list));
 
-function getEmployees(items) {
-  return items.map(item => ({
-    name: item.textContent,
-    position: item.dataset.position,
-    salary: item.dataset.salary,
-    age: item.dataset.age,
-  }));
-}
+const getEmployees = function(items) {
+  return [...items].map(item => {
+    return ({
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    });
+  });
+};
 
-sortList(list);
 getEmployees(list);
