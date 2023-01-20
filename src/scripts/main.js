576f58857b2673d72983a4c100399daa40e5b325
valntyn <romanenko.valentyn1@gmail.com>
2023-01-20 15:29:44 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const people = document.querySelector('ul');
+const persons = [...people.querySelectorAll('li')];
+
+function toNumber(salary) {
+  return +salary.replace(/[^\d]/g, '');
+}
+
+function sortList(list) {
+  list
+    .sort((a, b) => toNumber(b.dataset.salary)
+    - toNumber(a.dataset.salary));
+
+  people.append(...list);
+}
+
+function getEmployees(list) {
+  return [...list].map(person => {
+    return {
+      name: person.textContent.trim(),
+      ...person.dataset,
+    };
+  });
+}
+
+sortList(persons);
+getEmployees(persons);
