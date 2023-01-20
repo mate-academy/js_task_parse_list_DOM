83d80c3c8f95d2e1fbd0a2ccad87b65030a512b3
valntyn <romanenko.valentyn1@gmail.com>
2023-01-20 20:14:46 +0200

    mistakes were fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,20 +3,20 @@
 const people = document.querySelector('ul');
 const persons = [...people.querySelectorAll('li')];
 
-function toNumber(salary) {
+function getNumber(salary) {
   return +salary.replace(/[^\d]/g, '');
 }
 
 function sortList(list) {
   list
-    .sort((a, b) => toNumber(b.dataset.salary)
-    - toNumber(a.dataset.salary));
+    .sort((a, b) => getNumber(b.dataset.salary)
+    - getNumber(a.dataset.salary));
 
   people.append(...list);
 }
 
 function getEmployees(list) {
-  return [...list].map(person => {
+  return list.map(person => {
     return {
       name: person.textContent.trim(),
       ...person.dataset,
