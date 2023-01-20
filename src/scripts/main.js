faece18884940631f220c83b4ba2d96fe73445f9
Roman Shevchenko <r.shevchenko87@gmail.com>
2023-01-20 21:22:55 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write code here
+const list = document.querySelectorAll('li');
+const employees = [];
+
+for (const li of list) {
+  li.dataset.salary = +(li.dataset.salary.slice(1).split(',').join(''));
+}
+
+const sortList = [...list].sort((a, b) => b.dataset.salary - a.dataset.salary);
+
+for (const li of sortList) {
+  const employe = {};
+
+  employe.name = li.innerText;
+  employe.salary = li.dataset.salary;
+
+  employees.push(employe);
+}
+
+for (let i = 0; i < sortList.length; i++) {
+  list[i].innerText = employees[i].name;
+}
