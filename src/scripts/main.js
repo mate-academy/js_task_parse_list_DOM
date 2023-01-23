048824b68e1f131003de83a38e5e526dd8ebd6a6
Roman Kvas <naruto.yzymaki4@gmail.com>
2023-01-23 12:32:29 +0100

    Solution/list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const listOfSalary = document.querySelectorAll('li');
+const ul = document.querySelector('ul');
+const arrOfList = [...listOfSalary];
+
+function getSalary(str) {
+  return +str.replace(/\D/g, '');
+}
+
+function sortList(list) {
+  const createList = list.sort((a, b) =>
+    getSalary(b.dataset.salary) - getSalary(a.dataset.salary));
+
+  ul.append(...createList);
+}
+
+function getEmployees(list) {
+  return list.map(item => ({
+    ...item.dataset,
+    'name': item.textContent,
+  }));
+}
+
+sortList(arrOfList);
+getEmployees(arrOfList);
