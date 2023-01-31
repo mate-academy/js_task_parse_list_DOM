6152642999434baec411633b5dc54eb56723c775
Samir Mamedov <samirmamedoviphone@gmail.com>
2023-01-31 14:13:10 +0200

    Parse list task was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+
+const employees = [...document.querySelectorAll('li')];
+
+function correctSalary(salary) {
+  return Number(salary.split(',').join('').slice(1));
+}
+
+function sortList(list) {
+  const sortedList = list.sort((a, b) => {
+    return (correctSalary(b.dataset.salary)
+      - correctSalary(a.dataset.salary));
+  });
+
+  employeesList.append(...sortedList);
+}
+
+function getEmployees(list) {
+  return list.map(employee => {
+    return {
+      name: employee.innerText,
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: +employee.dataset.age,
+    };
+  });
+}
+
+sortList(employees);
+getEmployees(employees);
