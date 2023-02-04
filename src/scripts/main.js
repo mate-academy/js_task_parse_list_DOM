992e7f5f365a22240c06ccff8ce1bde5a54e2390
Max Zelenskyi <rio1391@gmail.com>
2023-02-04 14:39:28 +0100

    Rio Solutions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const employyes = [...list.children];
+
+function removeChars(str) {
+  return +str.replaceAll(',', '').replaceAll('$', '');
+}
+
+const getEmployees = (employeesList) => {
+  return employeesList.map((employee) => ({
+    name: employee.innerText,
+    position: employee.dataset.position,
+    salary: employee.dataset.salary,
+    age: +employee.dataset.age,
+  }));
+};
+
+getEmployees(employyes);
+
+const sortList = (employeesList) => {
+  employeesList.sort((empA, empB) => {
+    const salaryA = removeChars(empA.dataset.salary);
+    const salaryB = removeChars(empB.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  for (const person of employeesList) {
+    list.append(person);
+  }
+};
+
+sortList(employyes);
