3aa56161194a3d3d85e891aec8557f84a684db2b
Kate <kate@Kates-MacBook-Pro-16.local>
2023-02-05 14:43:57 +0100

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const data = [...document.getElementsByTagName('li')].map(tag => {
+  return {
+    position: tag.getAttribute('data-position'),
+    salary: +tag.getAttribute('data-salary').slice(1).replaceAll(',', ''),
+    age: tag.getAttribute('data-age'),
+    name: tag.innerText,
+  };
+});
+
+const sortList = (arr) => {
+  arr.sort((a, b) => b.salary - a.salary);
+};
+
+const getEmployees = (model) => {
+  return model.map(entry => entry.name);
+};
+
+sortList(data);
+getEmployees(data);
+
+const root = document.getElementsByTagName('ul')[0];
+
+root.innerHTML = data.map((el, idx) => {
+  return `<li
+  data-position=${el.position}
+  data-salary=$${el.salary.toLocaleString()}
+  data-age=${el.age}>
+  ${el.name}
+</li>`;
+}).join('');
