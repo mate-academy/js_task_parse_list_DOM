d4574fce75aaaa076fa46fa788bc7b4230984d14
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-02-07 21:40:54 +0200

    some changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,30 @@
 'use strict';
 
-function sortList(people) {
-  const array = [...people];
-
-  array.sort((person1, person2) => {
-    const salary1 = +person1.dataset.salary.replace(/\W/g, '');
-    const salary2 = +person2.dataset.salary.replace(/\W/g, '');
-
-    return salary2 - salary1;
-  });
-
-  return array;
-};
-
-function getEmployees(people) {
-  return [...people].map(person => {
-    return `
-    <li
-      data-position="${person.dataset.position}"
-      data-salary="${person.dataset.salary}"
-      data-age="${person.dataset.age}"
-    >
-      ${person.innerText}
-    </li>
-    `;
-  });
-}
-
 const items = document.querySelectorAll('li');
 const list = document.querySelector('ul');
 
-list.innerHTML = getEmployees(sortList(items)).join(' ');
+function sortList(people) {
+  const array = [...people];
+
+  const number = person => +person.dataset.salary.replace(/\W/g, '');
+
+  array.sort((person1, person2) => number(person2) - number(person1));
+
+  list.append(...array);
+};
+
+function getEmployees(people) {
+  const array = [];
+
+  people.forEach(person => {
+    array.push({
+      name: person.innerText,
+      ...person.dataset,
+    });
+  });
+
+  return array;
+}
+
+sortList(items);
+getEmployees(items);
