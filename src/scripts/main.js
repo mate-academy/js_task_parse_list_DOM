1bf97cfe0197c3f16b8a64dac8eb5e4c10410afb
Sergei Nikolaienko <serjexdeath@gmail.com>
2023-02-07 16:39:55 +0200

    Task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
+const list = document.querySelector('ul');
+const employees = [...document.querySelectorAll('li')];
+
+function sortEmployees(array) {
+  const salaryNum = (salary) => +salary.slice(1).split(',').join('');
+  
+  const sorted = array.sort((a, b) => {
+    return salaryNum(b.dataset.salary) - salaryNum(a.dataset.salary);
+  });
+
+  list.append(...sorted);
+};
+
+function getEmployees(array) {
+  return array.map((item) => {
+    return {
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    };
+  });
+}
+
+sortEmployees(employees);
+getEmployees(employees);
+
+
 // write code here
