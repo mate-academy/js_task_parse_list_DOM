733ad7de2efcb1f3023ee95f00387fb955ce2662
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-02-07 18:59:28 +0200

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const nodes = document.querySelectorAll('li')
+const list = document.querySelector("ul")
+
+const frag = document.createDocumentFragment()
+
+
+const sortedList = Array.from(nodes).sort((a,b) => parseInt(a.dataset.salary.replace(/\D/g, '')) - parseInt(b.dataset.salary.replace(/\D/g, '')));
+
+const sortList = function (list) {
+  for (let item of sortedList) {
+    frag.prepend(item);
+  }
+  list.appendChild(frag)
+}
+
+sortList(list)
+
+const getEmployees = function () {
+  const massive = [];
+
+  for(let item of sortedList) {
+    const object = {
+      name: item.innerText,
+      salary: item.dataset.salary,
+      position: item.dataset.position,
+      age: item.dataset.age
+    };
+    massive.push(object)
+  }
+  return massive;
+}
+
+getEmployees()
+
+
+
+
+
+
