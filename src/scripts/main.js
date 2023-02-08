91733b1bea08cb3e05cfb9def00bfdd23f32578a
Jaxxxon78 <jaxxxon78@ukr.net>
2023-02-08 20:00:12 +0200

    problems with npm

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+const list = [...document.querySelectorAll('li')];
+
+function sortList(arr) {
+  const sorted = arr.sort((a, b) =>
+    converter(b) - converter(a));
+
+  ul.append(...sorted);
+
+  return sorted;
+}
+
+function converter(item) {
+  return item.dataset.salary.slice(1).split(',').join('');
+}
+
+function getEmployees(arr) {
+  const listOfEmployees = arr.map(el => ({
+    name: el.textContent,
+    position: el.dataset.position,
+    salary: el.dataset.salary,
+    age: el.dataset.age,
+  }));
+
+  return listOfEmployees;
+}
+
+sortList(list);
+getEmployees(list);
