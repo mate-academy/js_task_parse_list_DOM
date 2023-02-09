1a522c24b8ce903fce99cd21a05c549aee1065fe
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-02-09 10:27:01 +0200

    task was solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write code here
+const li = [...document.querySelectorAll('li')];
+const ul = document.querySelector('ul');
+
+function toNumber(string) {
+  return Number(string.replace('$', '').replaceAll(',', ''));
+}
+
+function sortList(list) {
+  list.sort((a, b) => {
+    const salaryA = toNumber(a.dataset.salary);
+    const salaryB = toNumber(b.dataset.salary);
+
+    return salaryA - salaryB;
+  });
+
+  while (ul.lastElementChild) {
+    ul.removeChild(ul.lastElementChild);
+  }
+
+  list.map(element => {
+    const person = document.createElement('li');
+
+    person.innerText = element.innerText.trim();
+    ul.append(person);
+  });
+}
+
+function getEmployees(list) {
+  return list.reduce((acc, person) => {
+    acc.push({
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    });
+
+    return acc;
+  }, []);
+}
+
+getEmployees(li);
+sortList(li);
