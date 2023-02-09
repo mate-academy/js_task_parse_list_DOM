94ea910f0148b764e47470e28be32c8f09b235e4
Vasyl Pavlenko <genlus@ukr.net>
2023-02-09 15:09:33 +0200

    Some changes were made

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,24 +2,23 @@
 
 const list = document.querySelector('ul');
 const employees = [...document.querySelectorAll('li')];
+const sal = (item) => +item.slice(1).split(',').join('');
 
 function sortEmployees(arr) {
-  const salary = (item) => +item.slice(1).split(',').join('');
-
   const sorted = arr.sort((a, b) => {
-    return salary(b.dataset.salary) - salary(a.dataset.salary);
+    return sal(b.dataset.salary) - sal(a.dataset.salary);
   });
 
   list.append(...sorted);
 };
 
 function getEmployees(arr) {
-  return arr.map((item) => {
+  return arr.map(({ innerText, dataset: { position, salary, age } }) => {
     return {
-      name: item.innerText,
-      position: item.dataset.position,
-      salary: item.dataset.salary,
-      age: item.dataset.age,
+      name,
+      position,
+      salary,
+      age,
     };
   });
 }
