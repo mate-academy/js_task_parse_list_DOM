06552b5fa78a7cef2844402add8c7fa75f3b1f23
Oleksandr <98667890az@gmail.com>
2023-02-12 15:38:08 +0200

    parse_list_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+function convertToNum(num) {
+  return num.split(',').join('').split('$').join('');
+};
+
+function sortBySalary(list) {
+  const sorted = [...list].sort(function(a, b) {
+    const FirstSalary = convertToNum(a.dataset.salary);
+
+    const SecondSalary = convertToNum(b.dataset.salary);
+
+    return +SecondSalary - +FirstSalary;
+  });
+
+  document.querySelector('ul').append(...sorted);
+}
+
+function employeesObjects(list) {
+  const employees = [];
+
+  list.forEach(element => {
+    const nameObj = element.innerHTML.trim();
+    const position = element.dataset.position;
+    const salary = element.dataset.salary;
+    const age = element.dataset.age;
+
+    employees.push(
+      {
+        name: nameObj,
+        position: position,
+        salary: salary,
+        age: age,
+      }
+    );
+  });
+
+  return employees;
+}
+
+employeesObjects(document.querySelectorAll('li'));
+sortBySalary(document.querySelectorAll('li'));
