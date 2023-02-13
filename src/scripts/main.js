0a798223f2d4530d48aa86f1e5d0a4dc8d09dcbc
Roshan Jha <kartohajha@gmail.com>
2023-02-13 10:00:36 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+const employees = [...document.querySelectorAll('li')];
+
+function salaryNum(salary) {
+  return Number(salary.split(',').join('').slice(1));
+}
+
+function sortList(list) {
+  const sorted = list.sort((a, b) => {
+    return (salaryNum(b.dataset.salary) - salaryNum(a.dataset.salary));
+  });
+
+  employeesList.append(...sorted);
+}
+
+function getEmployees(list) {
+  return list.map(employee => {
+    return {
+      name: employee.innerText,
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    };
+  });
+}
+
+sortList(employees);
+getEmployees(employees);
