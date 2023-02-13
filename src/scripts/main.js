29fe4ea2aac86a7cae8683f007558501274564fc
Nick <tortickk8475@gmail.com>
2023-02-13 11:10:15 +0200

    add js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+function camparator(a, b) {
+  if (a.dataset.salary > b.dataset.salary) {
+    return -1;
+  } else if (a.dataset.salary < b.dataset.salary) {
+    return 1;
+  }
+
+  return 0;
+}
+
+const salary = document.querySelectorAll(['[data-salary]']);
+const salaryArrey = [...salary];
+
+const sorted = salaryArrey.sort(camparator);
+
+sorted.forEach(item => document.querySelector('ul').appendChild(item));
+
+const list = document.querySelector('ul');
+const firstLi = document.querySelector('li');
+
+list.append(firstLi);
