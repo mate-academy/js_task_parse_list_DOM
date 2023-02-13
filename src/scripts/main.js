545de0226cb741d56e8d5edde4be0cd4b29e3c48
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-02-13 13:22:55 +0200

    Solution parse_list_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write code here
+
+const listOfEmployees = document.querySelector('ul');
+
+function sortList(list) {
+  const sortedEmployees = [];
+
+  for (let i = 0; i < list.children.length; i++) {
+    list.children[i].dataset.salary = +list.children[i].dataset.salary
+      .replace('$', '')
+      .replace(',', '');
+
+    sortedEmployees.push(list.children[i]);
+  }
+
+  sortedEmployees.sort((emloyeeA, employeeB) =>
+    employeeB.dataset.salary - emloyeeA.dataset.salary);
+
+  for (let i = 0; i < sortedEmployees.length; ++i) {
+    list.appendChild(sortedEmployees[i]);
+  }
+};
+
+sortList(listOfEmployees);
