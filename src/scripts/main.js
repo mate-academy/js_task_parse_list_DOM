6e1dee8fada3ea38014b36e018beb7c4fe28dffc
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-02-14 15:37:11 +0200

    Code was refacktored

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,36 +3,26 @@
 const listOfEmployees = document.querySelector('ul');
 
 function sortList(list) {
-  const sortedEmployees = [];
+  [...list.children].map(
+    employee =>
+      (employee.dataset.salary = +employee.dataset.salary
+        .replace('$', '')
+        .replace(',', ''))
+  );
 
-  function toNumber(employee) {
-    employee.dataset.salary = employee.dataset.salary
-      .replace('$', '')
-      .replace(',', '');
-
-    sortedEmployees.push(employee);
-  }
-
-  [...list.children].map(toNumber);
-
-  sortedEmployees.sort((emloyeeA, employeeB) =>
+  const sortedEmployees = [...list.children].sort((emloyeeA, employeeB) =>
     employeeB.dataset.salary - emloyeeA.dataset.salary);
 
   list.append(...sortedEmployees);
 };
 
 function getEmployees(list) {
-  const employees = [];
-
-  [...list.children].map(employee => employees.push({
+  return [...list.children].map(employee => ({
     name: employee.textContent.trim(),
     position: employee.dataset.position,
     salary: employee.dataset.salary,
     age: employee.dataset.age,
-  })
-  );
-
-  return employees;
+  }));
 }
 
 sortList(listOfEmployees);
