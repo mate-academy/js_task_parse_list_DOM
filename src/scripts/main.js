8e80f4d44e2d206dbc5099322cbb6100a925dc2e
Dmytro Hubych <dh.hubych@gmail.com>
2023-02-17 22:16:54 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+function sortList(list) {
+  const items = [...list.children];
+
+  items.sort((a, b) =>
+    (+b.dataset.salary.replace(/\D/g, ''))
+    - (+a.dataset.salary.replace(/\D/g, ''))
+  );
+
+  items.map(item => list.append(item));
+
+  return items;
+}
+
+function getEmployees(list) {
+  const items = [...list.children];
+  const employees = [];
+
+  items.forEach(item => {
+    const employee = {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    };
+
+    employees.push(employee);
+  });
+
+  return employees;
+}
+
+sortList(document.querySelector('ul'));
+getEmployees(document.querySelector('ul'));
