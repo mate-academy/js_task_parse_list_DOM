1cf1f3c40c0ac4f740fb12a392ebd1dec21b1aea
tarasnakonechnyi <nakonechnyi.taras@gmail.com>
2023-02-20 20:35:11 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const workerColection = Array.from(
+  document.getElementsByTagName('li')
+);
+const listWorker = document.querySelector('ul');
+const solaryArray = [];
+
+workerColection.forEach(element => {
+  solaryArray.push(formatItem(element));
+});
+
+function formatItem(item) {
+  return Number(item.dataset.salary.match(/\d+/g).join(''));
+}
+
+solaryArray.sort((a, b) => b - a).map(salary => {
+  workerColection.forEach(element => {
+    if (formatItem(element) === salary) {
+      listWorker.append(element);
+    };
+  });
+});
