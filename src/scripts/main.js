5ba67996ab8e3833a3ac9a6eaafa71ac555f66ed
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-02-21 21:04:40 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write code here
+const ulList = [...document.querySelector('ul').children];
+const newList = document.querySelector('ul');
+
+function formatSalary(employee) {
+  const salary = employee.dataset.salary;
+  const formattedSalary = +salary.replaceAll(',', '').slice(1);
+
+  return formattedSalary;
+}
+
+function sortList(list) {
+  list.sort(
+    (firstEmployee, secondEmployee) =>
+      formatSalary(secondEmployee) - formatSalary(firstEmployee)
+  );
+
+  newList.innerHTML = `
+  ${list.map((element) => `<li>${element.textContent}</li>`).join('')}`;
+}
+
+function getEmployees(list) {
+  const arrayOfEmployees = [];
+
+  const employeeData = list.map((element) => element.dataset);
+
+  for (let i = 0; i < employeeData.length; i++) {
+    const employeeObject = {};
+    const trimmedEmployeeName = list[i].innerText.trim();
+
+    employeeObject.name = trimmedEmployeeName;
+
+    for (const key in employeeData[i]) {
+      employeeObject[key] = employeeData[i][key];
+    }
+
+    arrayOfEmployees.push(employeeObject);
+  }
+
+  return arrayOfEmployees;
+}
+
+sortList(ulList);
+getEmployees(ulList);
