694b660c962a487c9150a736051e28f72ead0d11
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-02-22 23:06:41 +0200

    Made more cleaner

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,49 +1,42 @@
 'use strict';
 
-const ulList = [...document.querySelector('ul').children];
+const ulListRef = [...document.querySelector('ul').children];
 const newList = document.querySelector('ul');
 
-function formatSalary(employee) {
-  const salary = employee.dataset.salary;
-  const formattedSalary = +salary.replaceAll(',', '').slice(1);
-
-  return formattedSalary;
+function formatSalary(salary) {
+  return +salary.replaceAll(',', '').slice(1);
 }
 
 function sortList(list) {
-  const sortedList = [...list]
+  const sortedList = [...list];
 
-  sortedList.sort(
-    (firstEmployee, secondEmployee) =>
-      formatSalary(secondEmployee) - formatSalary(firstEmployee)
-  );
+  sortedList.sort((firstEmployee, secondEmployee) => {
+    const firstEmployeeSalary = formatSalary(firstEmployee.dataset.salary);
+    const secondEmployeeSalary = formatSalary(secondEmployee.dataset.salary);
+
+    return secondEmployeeSalary - firstEmployeeSalary;
+  });
 
   newList.innerHTML = `
-  ${list.map((element) => `<li>${element.textContent}</li>`).join('')}`;
+  ${sortedList.map((element) => `<li>${element.textContent}</li>`).join('')}`;
 
   return sortedList;
 }
 
 function getEmployees(list) {
-  const arrayOfEmployees = [];
+  const arrayOfEmployees = list.map((employee) => {
+    const employeeData = employee.dataset;
 
-  const employeeData = list.map((element) => element.dataset);
-
-  for (let i = 0; i < employeeData.length; i++) {
-    const employeeObject = {};
-    const trimmedEmployeeName = list[i].innerText.trim();
-
-    employeeObject.name = trimmedEmployeeName;
-
-    for (const key in employeeData[i]) {
-      employeeObject[key] = employeeData[i][key];
-    }
-
-    arrayOfEmployees.push(employeeObject);
-  }
+    return {
+      fullname: employee.innerText.trim(),
+      position: employeeData.position,
+      salary: employeeData.salary,
+      age: +employeeData.age,
+    };
+  });
 
   return arrayOfEmployees;
 }
 
-sortList(ulList);
-getEmployees(ulList);
+sortList(ulListRef);
+getEmployees(ulListRef);
