c24c5da5ccc1d10f66efee48ac17916056776fd8
Ruslan Liapin <joy.esenin@gmail.com>
2023-02-24 17:46:03 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+const convertNum = (numStr) => {
+  const cleanedNumStr = numStr.split('$').join('').split(',').join('');
+
+  return parseInt(cleanedNumStr);
+};
+
+const sortList = (htmlList) => {
+  const sortedList = Array.from(htmlList).sort((a, b) => {
+    const salaryA = convertNum(a.dataset.salary);
+    const salaryB = convertNum(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  const parentElem = htmlList[0].parentNode;
+
+  sortedList.forEach(li => parentElem.appendChild(li));
+};
+
+const getEmployees = (htmlList) => {
+  const result = [];
+
+  for (const li of htmlList) {
+    const employee = {
+      name: li.textContent,
+      position: li.dataset.position,
+      salary: convertNum(li.dataset.salary),
+      age: parseInt(li.dataset.age),
+    };
+
+    result.push(employee);
+  }
+
+  return result;
+};
+
+sortList(list);
+
+getEmployees(list);
