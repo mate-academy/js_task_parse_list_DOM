06d0844f5a6eedb1bbeadb4be873915c010b76b2
Yelisieiev Kim <rbv271@outlook.com>
2023-02-26 17:16:20 +0200

    function sortBySalary and getEmpoyees added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write code here
+const list = document.querySelector('ul');
+const employees = [...document.querySelectorAll('li')];
+
+function getSalary(worker) {
+  return worker.dataset.salary.replace(/[$,]/g, '');
+}
+
+function sortList(people) {
+  const sorted = people.sort((personA, personB) => (
+    getSalary(personB) - getSalary(personA)
+  ));
+
+  list.append(...sorted);
+};
+
+function getEmployees(workers) {
+  return workers.map(worker => ({
+    name: worker.innerText,
+    position: worker.dataset.position,
+    salary: worker.dataset.salary,
+    age: worker.dataset.age,
+  }));
+}
+
+sortList(employees);
+getEmployees(employees);
