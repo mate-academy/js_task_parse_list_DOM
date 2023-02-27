b6ee257db7d9b4e86eb5056a3650723c71c77ac9
Andrew Litkovets <andriylao@gmail.com>
2023-02-27 21:33:43 +0200

    Task donr

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const employees = [...document.querySelectorAll('li')];
+
+const normalisedSalary = (str) => +str.slice(1).split(',').join('');
+
+function sortList(array) {
+  array.sort((a, b) => normalisedSalary(b.dataset.salary)
+    - normalisedSalary(a.dataset.salary));
+
+  list.append(...array);
+}
+
+function getEmployees(array) {
+  return array.map(el => {
+    return {
+      name: el.innerText,
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    };
+  });
+}
+
+sortList(employees);
+getEmployees(employees);
