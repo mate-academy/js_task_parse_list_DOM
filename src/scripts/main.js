f8ff5fd40f244bf5077a905153e00fbca2db3eae
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2023-03-01 21:38:29 +0200

    Solurion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const ulItem = document.querySelector('ul');
+
+function sortList(list) {
+  const employees = [...list.children];
+
+  employees.sort((a, b) => {
+    return convertToNumber(b.dataset.salary)
+      - convertToNumber(a.dataset.salary);
+  })
+    .map(person => ulItem.append(person));
+
+  return employees;
+};
+
+function convertToNumber(salary) {
+  return Number(salary.split('$').join('').replaceAll(',', ''));
+};
+
+function getEmployees(list) {
+  const employees = [...list.children];
+
+  return employees.map(person => ({
+    name: person.textContent.trim(),
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+};
+
+sortList(ulItem);
+
+getEmployees(ulItem);
