6c138201793bf863434c8d6cf4dcba14ac3d5f66
Yelisieiev Kim <rbv271@outlook.com>
2023-03-01 12:49:33 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,28 +2,28 @@
 
 // write code here
 const list = document.querySelector('ul');
-const employees = [...document.querySelectorAll('li')];
 
-function getSalary(worker) {
-  return worker.dataset.salary.replace(/[$,]/g, '');
+function normalizeSalary(str) {
+  return +str.replace(/[$,]/g, '');
 }
 
-function sortBySalary(people) {
-  const sorted = people.sort((personA, personB) => (
-    getSalary(personB) - getSalary(personA)
+function sortList(listToSort) {
+  const sorted = [...listToSort.children].sort((personA, personB) => (
+    normalizeSalary(personB.dataset.salary)
+    - normalizeSalary(personA.dataset.salary)
   ));
 
   list.append(...sorted);
-};
+}
 
-function getEmployees(workers) {
-  return workers.map(worker => ({
-    name: worker.innerText,
-    position: worker.dataset.position,
-    salary: worker.dataset.salary,
-    age: worker.dataset.age,
+function getEmployees(employees) {
+  return [...employees.children].map(employee => ({
+    name: employee.innerText,
+    position: employee.dataset.position,
+    salary: employee.dataset.salary,
+    age: employee.dataset.age,
   }));
 }
 
-sortBySalary(employees);
-getEmployees(employees);
+sortList(list);
+getEmployees(list);
