e1b8d75d2b9ac536e1b29077469ff46e0bec49d5
Ilona Romaniuk <ilona09041983@gmail.com>
2023-03-01 16:51:47 +0200

    Parse list with data attributes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+function convertToNumber(string) {
+  const number = Number(string.replace(/[$,]/g, ''));
+
+  return number;
+}
+
+const employees = document.querySelectorAll('li');
+const arrayOfEmployees = [...employees];
+
+function sortList(list) {
+  const ul = document.querySelector('ul');
+  const sortedList = arrayOfEmployees.sort((a, b) =>
+    convertToNumber(b.dataset.salary) - convertToNumber(a.dataset.salary)
+  );
+
+  for (const element of sortedList) {
+    ul.append(element);
+  }
+}
+
+function getEmployees(list) {
+  return list.map(item => ({
+    name: item,
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+}
+
+getEmployees(arrayOfEmployees);
+sortList(arrayOfEmployees);
