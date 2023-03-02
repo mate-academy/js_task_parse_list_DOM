bdc1fe8b95fb09188d0c6d9313a149f1eb68fa5d
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-03-03 00:41:53 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const ullist = document.querySelector('ul');
+// получаем все элементы списка
+// const items = list.querySelectorAll('li');
+
+function sortList(list) {
+  const items = Array.from(list.children);
+  const sortable = items.map((item) => ({
+    element: item,
+    salary: convertToNumber(item.dataset.salary),
+  }));
+
+  // сортируем элементы по зарплате в порядке убывания
+  sortable.sort((a, b) => b.salary - a.salary);
+
+  // перемещаем элементы в список в порядке отсортированного массива
+  sortable.forEach(({ element }) => ullist.appendChild(element));
+}
+
+function convertToNumber(salaryString) {
+  return parseFloat(salaryString.replace(/\$/g, ''));
+}
+
+function getEmployees(list) {
+  const employees = Array.from(list.children).map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: convertToNumber(item.dataset.salary),
+    age: parseInt(item.dataset.age),
+  }));
+
+  return employees;
+}
+
+sortList(ullist);
+getEmployees(ullist);
