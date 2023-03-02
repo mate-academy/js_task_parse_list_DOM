b54c99e18928536e5ae3bb0c60d1be42c2ca82d7
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-03-02 15:12:48 +0200

    Task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+
+function sortList(list) {
+  const people = [...ul.children];
+
+  people.sort((person1, person2) => converter(person2.dataset.salary)
+    - converter(person1.dataset.salary));
+
+  people.forEach(person => ul.append(person));
+}
+
+function converter(item) {
+  return +item.slice(1).split(',').join('');
+}
+
+function getEmployees(list) {
+  let array = [];
+  const people = [...ul.children];
+
+  array = people.map(person => ({
+    name: person.innerText,
+    position: person.dataset.position,
+    salary: converter(person.dataset.salary),
+    age: +person.dataset.age,
+  }));
+
+  return array;
+}
+
+sortList(ul);
+getEmployees(ul);
