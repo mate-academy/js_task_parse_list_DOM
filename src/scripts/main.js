0e3786121571a593572affd24d71c798a6ec8c48
Tolik Petras <toliketras4@gmail.com>
2023-03-02 21:55:33 +0200

    solution fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,21 +2,23 @@
 
 let list = document.querySelectorAll('li');
 
+function toNumber(str) {
+  return +str.dataset.salary.slice(1).split(',').join('');
+}
+
 function sortList(someList) {
   list = [...someList].sort((person1, person2) =>
-    +person2.dataset.salary.slice(1).split(',').join('')
-      - +person1.dataset.salary.slice(1).split(',').join(''));
+    toNumber(person2)
+      - toNumber(person1));
 
   document.querySelector('ul').append(...list);
 }
 
 function getEmployees(someList) {
-  return [...someList].map(person => {
-    return {
-      name: person.innerText,
-      ...person.dataset,
-    };
-  });
+  return [...someList].map(person => ({
+    name: person.innerText,
+    ...person.dataset,
+  }));
 }
 
 sortList(list);
