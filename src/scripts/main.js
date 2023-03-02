8d79ba50fa4415bcba9a0c4605e638618f194eeb
Tolik Petras <toliketras4@gmail.com>
2023-03-02 20:36:21 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+let list = document.querySelectorAll('li');
+
+function sortList(someList) {
+  list = [...someList].sort((person1, person2) =>
+    +person2.dataset.salary.slice(1).split(',').join('')
+      - +person1.dataset.salary.slice(1).split(',').join(''));
+
+  document.querySelector('ul').append(...list);
+}
+
+function getEmployees(someList) {
+  return [...someList].map(person => {
+    return {
+      name: person.innerText,
+      ...person.dataset,
+    };
+  });
+}
+
+sortList(list);
+
+getEmployees(list);
