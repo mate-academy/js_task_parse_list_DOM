87dd04830f032e25b430f9c3a2e574ea3435997c
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2023-03-03 17:24:12 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const list = [...document.querySelectorAll('li')];
+
+const sortList = (argList) => {
+  argList.sort(
+    (e1, e2) => toNumber(e2.dataset.salary) - toNumber(e1.dataset.salary));
+};
+
+const getEmployees = (argList) => {
+  return argList.map(e => ({
+    name: e.innerText,
+    position: e.dataset.position,
+    salary: e.dataset.salary,
+    age: e.dataset.age,
+  }));
+};
+
+const toNumber = (e) => {
+  return e.slice(1).replaceAll(',', '');
+};
+
+sortList(list);
+
+getEmployees(list);
