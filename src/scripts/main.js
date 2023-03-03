8788b5fc28f35797bf6d93e986e724614fdd4790
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-03-03 14:52:28 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,9 @@ function sortList(list) {
     element: item,
     salary: convertToNumber(item.dataset.salary),
   }));
+
   sortable.sort((a, b) => b.salary - a.salary);
+
   sortable.forEach(({ element }) => ullist.appendChild(element));
 }
 
@@ -16,16 +18,4 @@ function convertToNumber(salaryString) {
   return parseFloat(salaryString.replace(/\$/g, ''));
 }
 
-function getEmployees(list) {
-  const employees = Array.from(list.children).map((item) => ({
-    name: item.textContent.trim(),
-    position: item.dataset.position,
-    salary: convertToNumber(item.dataset.salary),
-    age: parseInt(item.dataset.age),
-  }));
-
-  return employees;
-}
-
 sortList(ullist);
-getEmployees(ullist);
