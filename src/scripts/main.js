40739f1f89b52f2bb9bc3c9715a40423a1653041
Dmitriy Rybak <ryba@MacBook-Air-Dmitriy.local>
2023-03-11 13:34:58 -0600

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
+/* eslint-disable max-len */
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const ulElement = document.querySelector('ul');
+
+const sorted = [...list].sort((a, b) => {
+  const salaryOne = parseInt(a.dataset.salary.replace(/[^\w\s]/gi, ''));
+  const salaryTwo = parseInt(b.dataset.salary.replace(/[^\w\s]/gi, ''));
+
+  return salaryTwo - salaryOne;
+});
+
+list.forEach((li) => li.remove());
+
+for (let i = 0; i < sorted.length; i++) {
+  ulElement.append(sorted[i]);
+}
