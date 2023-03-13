7661d87a2ee98740ce1a67cb893c6e7b36505796
Andrii Khomiak <andr.hamster@gmail.com>
2023-03-13 22:43:01 +0200

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  list.sort((a, b) => {
+    return getNum(b.dataset.salary) - getNum(a.dataset.salary);
+  });
+  document.querySelector('ul').append(...list);
+}
+
+sortList(employees);
+
+function getNum(str) {
+  let result = '';
+
+  for (const char of str) {
+    if (!isNaN(char)) {
+      result += char;
+    }
+  }
+
+  return result;
+}
+
+function getEmployees(list) {
+  list.map(item => {
+    return {
+      name: item.innerText,
+      positon: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    };
+  });
+}
+
+getEmployees(employees);
