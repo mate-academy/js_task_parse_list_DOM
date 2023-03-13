e8dc2769457217c324298cb62e671678940bc228
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-13 16:02:29 +0100

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const lists = [...document.querySelectorAll('li')];
+const unList = document.querySelector('ul');
+
+function sortList(list) {
+  list.sort((a, b) => toNum(b.dataset.salary) - toNum(a.dataset.salary));
+  unList.append(...list);
+}
+
+function toNum(money) {
+  return money.replace(/[^0-9.-]+/g, '');
+}
+
+function getEmployees(list) {
+  list.map(el => {
+    return {
+      name: el.innerText,
+      positon: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    };
+  });
+}
+
+sortList(lists);
+getEmployees(lists);
