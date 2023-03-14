93e9d68e70b02576089f679b902a67228477a9a4
Kocherha Roman <f.your.d.2017@gmail.com>
2023-03-14 21:48:17 +0200

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const listPosition = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  const sortSalary = [...list].sort(
+    (a, b) =>
+      transformToNum(b.dataset.salary) - transformToNum(a.dataset.salary)
+  );
+
+  return sortSalary;
+}
+
+function transformToNum(str) {
+  return +str.split(',').join('').slice(1);
+}
+
+function getEmployees(list) {
+  const employees = list.map((el) => ({
+    name: el.innerText,
+    position: el.dataset.position,
+    salary: el.dataset.salary,
+    age: el.dataset.age,
+  }));
+
+  return employees;
+}
+
+document.querySelector('ul').append(...sortList(listPosition));
+// sortList(listPosition);
+getEmployees(listPosition);
