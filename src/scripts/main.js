648c2c930dd27279ae510c95e0c1bad5a017e04e
Serhii Melnychenko <8melya8@gmail.com>
2023-03-15 18:40:31 +0200

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,23 @@
 'use strict';
 
-const listEmployees = document.querySelector('ul')
+const listEmployees = document.querySelector('ul');
 const employees = document.querySelectorAll('li');
 
 const toNumber = item => item.slice(1).split(',').join('');
 
 function sortList(list) {
-  const result = [...list].sort((a, b) => toNumber(b.dataset.salary) - toNumber(a.dataset.salary));
+  const result = [...list]
+    .sort((a, b) => toNumber(b.dataset.salary) - toNumber(a.dataset.salary));
 
   listEmployees.append(...result);
 };
 
 function getEmployees(list) {
-  return [...list].map(item => ({
-    name: item.innerText,
-    position: item.dataset.position,
-    salary: item.dataset.salary,
-    age: item.dataset.age,
+  return [...list].map(({ innerText, dataset: { position, salary, age } }) => ({
+    name: innerText,
+    position,
+    salary,
+    age,
   }));
 };
 
