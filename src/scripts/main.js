73588a9fcd2130a99c803692c2d1038c2640ef17
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-03-17 16:39:10 +0100

    create list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+function sortList(someList) {
+  return [...someList].sort((item1, item2) => {
+    const salary1 = +item1.dataset.salary
+      .split(',').join('')
+      .split('$').join('');
+  console.log(item2.dataset.salary);
+    const salary2 = +item2.dataset.salary
+      .split(',').join('')
+      .split('$').join('');
+
+    return salary2 - salary1;
+  });
+}
+
+
+
+function getEmployees(someList) {
+  return [...someList].map(item => (
+    {
+      name: item.textContent,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    },
+  ));
+}
+
+sortList(list);
+getEmployees(list);
