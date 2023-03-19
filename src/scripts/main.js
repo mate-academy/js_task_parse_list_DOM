b64c07bd530cae82191618f87b2c6ba4648a4b88
Sergey Lipikhin <football3@ukr.net>
2023-03-19 21:45:01 +0200

    Complete solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,52 @@
 'use strict';
 
-// write code here
+function convertSalaryToNumber(salary) {
+  return Number(
+    salary
+      .slice(1)
+      .replaceAll(',', '')
+  );
+}
+
+function convertSalaryToString(salary) {
+  return `$${salary.toLocaleString('en')}`;
+}
+
+function createEmployeeElement(employee) {
+  return `
+    <li
+      data-position="${employee.position}"
+      data-salary="${convertSalaryToString(employee.salary)}"
+      data-age="${employee.age}"
+    >
+    ${employee.name}
+    </li>`;
+}
+
+function getEmployees(list) {
+  return [...list.children].map(child => (
+    {
+      name: child.innerText,
+      position: child.dataset.position,
+      salary: convertSalaryToNumber(child.dataset.salary),
+      age: Number(child.dataset.age),
+    }
+  ));
+}
+
+function sortList(list) {
+  const sortedEmployees = getEmployees(list)
+    .sort((employeePrevious, employeeCurrent) => (
+      employeeCurrent.salary - employeePrevious.salary
+    ));
+
+  const employeesSortedElementsList = sortedEmployees
+    .map(employee => createEmployeeElement(employee));
+
+  return employeesSortedElementsList;
+}
+
+const employeesElementsList = document.querySelector('ul');
+const employeesElementsSortedList = sortList(employeesElementsList);
+
+employeesElementsList.innerHTML = employeesElementsSortedList.join('');
