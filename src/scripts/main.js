c93614f22e2abfc8fefd98b5770f8df12f714da5
Oleksandr Marchenko <sasha@mate.academy>
2023-03-20 18:33:35 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const employee = [...document.querySelectorAll('li')];
+const toNumber = str => {
+  return +(str.split(',').join('').slice(1));
+};
+
+function sortList(list) {
+  const sortedList = list.sort((a, b) => toNumber(b.dataset.salary)
+    - toNumber(a.dataset.salary));
+
+  sortedList.forEach(element => {
+    document.querySelector('ul').append(element);
+  });
+}
+
+function getEmployees(list) {
+  return list.map(element => ({
+    name: element.innerText,
+    position: element.dataset.position,
+    salary: element.dataset.salary,
+    age: element.dataset.age,
+  }));
+}
+
+sortList(employee);
+getEmployees(employee);
