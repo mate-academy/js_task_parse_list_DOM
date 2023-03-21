35a5ab7634d8c187c4ed0b4e1856f71de0d5ec9f
Nazar Kharkevych <nkharkevych@gmail.com>
2023-03-21 19:38:13 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const listNode = document.querySelector('ul');
+const [...employees] = listNode.children;
+
+sortList(employees);
+getEmployees(employees);
+
+function sortList(list) {
+  list.sort((a, b) => {
+    const aSalary = convertToNumber(a.getAttribute('data-salary'));
+    const bSalary = convertToNumber(b.getAttribute('data-salary'));
+
+    return bSalary - aSalary;
+  });
+
+  listNode.append(...employees);
+};
+
+function convertToNumber(string) {
+  const glued = string.replaceAll(',', '');
+
+  return Number(glued.slice(1));
+};
+
+function getEmployees(list) {
+  return list.map((person) => {
+    return {
+      name: person.innerText,
+      position: person.getAttribute('data-position'),
+      salary: person.getAttribute('data-salary'),
+      age: person.getAttribute('data-age'),
+    };
+  });
+};
