3efdd877714d8ddc19e02e2789ffaeb8489fc152
Maksym Avramenko <nonste@outlook.com>
2023-03-21 10:31:51 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+
+const sortedList = [...items].sort(
+  (a, b) =>
+    +b.dataset.salary.replace(/[^+\d]/g, '')
+    - +a.dataset.salary.replace(/[^+\d]/g, '')
+);
+
+document.querySelector('ul').remove();
+
+const list = document.createElement('ul');
+
+for (const item of sortedList) {
+  list.append(item);
+}
+
+document.querySelector('body').append(list);
