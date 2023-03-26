dbfd6eee60da2600150d5f71d5ed3cc167a82136
Leonid <leonidbondarchuk6@gmail.com>
2023-03-26 11:41:43 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const list = document.getElementsByTagName('li');
+const people = [];
+
+for (const item of list) {
+  const person = {
+    name: item.innerHTML.trim(),
+  };
+
+  Object.assign(person, item.dataset);
+
+  people.push(person);
+
+  people.sort((a, b) =>
+    (conversionToNumber(b.salary) - conversionToNumber(a.salary)));
+}
+
+for (let i = 0; i < people.length; i++) {
+  const { salary, age, position } = people[i];
+
+  list[i].dataset.position = position;
+  list[i].dataset.salary = salary;
+  list[i].dataset.age = age;
+  list[i].innerHTML = people[i].name;
+}
+
+function conversionToNumber(string) {
+  let result = '';
+
+  for (let i = 0; i < string.length; i++) {
+    if (+string[i] || +string[i] === 0) {
+      result += string[i];
+    }
+  }
+
+  return +result;
+}
