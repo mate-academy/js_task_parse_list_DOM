76d2707b9d0d90edcf41a101edbb2eb3173d358b
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-03-26 14:25:59 +0300

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const listing = document.querySelectorAll('li');
+const ull = document.querySelector('ul');
+
+function toNumber(salary) {
+  return Number(salary.split('$').join('').split(',').join(''));
+}
+
+function sortList(list) {
+  const people = [...list];
+
+  people.sort((a, b) =>
+    toNumber(b.dataset.salary) - toNumber(a.dataset.salary));
+
+  for (const person of people) {
+    ull.append(person);
+  }
+}
+
+function getEmployees(list) {
+  for (const element of list) {
+    const elementObj = element.dataset;
+
+    return elementObj;
+  }
+}
+
+getEmployees(listing);
+sortList(listing);
