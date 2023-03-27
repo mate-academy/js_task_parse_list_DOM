818ebcdc6ac40d0c3929b27efa8838855000769c
Nikoramo <kanada387@gmail.com>
2023-03-27 19:55:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const li = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  const sortedList = list.sort((a, b) => {
+    return parseFloat(b.dataset.salary.slice(1))
+    - parseFloat(a.dataset.salary.slice(1));
+  });
+
+  document.querySelector('ul').append(...sortedList);
+}
+
+function getEmployees(list) {
+  const employees = list.map(employee => {
+    return {
+      name: employee.innerText,
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    };
+  });
+
+  return employees;
+}
+
+sortList(li);
+getEmployees(li);
