ddb488e893fae79e87a715f40e9ef03eaae82f12
DmytroHorshunov <cnimipignak@icloud.com>
2023-03-29 18:34:03 +0300

    sort person

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+function getNum(string) {
+  return +string.replace(/[$,]/g, '');
+}
+
+function sortList(li) {
+  const sortPerson = [...li].sort((a, b) =>
+    getNum(b.dataset.salary) - getNum(a.dataset.salary));
+
+  document.querySelector('ul').append(...sortPerson);
+}
+
+function getEmployees(li) {
+  const employeesArr = [...li].map(person => {
+    return {
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    };
+  });
+
+  return employeesArr;
+}
+
+sortList(list);
+getEmployees(list);
