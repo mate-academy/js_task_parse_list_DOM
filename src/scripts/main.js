741af35bdff10513cb796684c78c545f8386ce56
Mikhieiev Ivan <navigator79@gmail.com>
2023-03-29 12:03:28 +0300

    mainJS parse list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
 // write code here
+const liElements = document.querySelectorAll('li');
+
+function getSalaryNum(str) {
+  return +str.replace(/[$,]/g, '');
+}
+
+function sortList(list) {
+  const sorted = [...list].sort((a, b) =>
+    getSalaryNum(b.dataset.salary) - getSalaryNum(a.dataset.salary));
+
+  document.querySelector('ul').append(...sorted);
+}
+
+function getEmployees(list) {
+  const employeesArr = [...list].map(elem => {
+    return {
+      name: elem.innerText.trim(),
+      position: elem.dataset.position,
+      salary: elem.dataset.salary,
+      age: elem.dataset.age,
+    };
+  });
+
+  return employeesArr;
+}
+
+sortList(liElements);
+getEmployees(liElements);
