1825e5d808f1722d2d712c331c931c6dabcea72f
metja <mikolapenzanik@gmail.com>
2023-03-31 21:26:48 +0300

    Comleted task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const elements = document.querySelectorAll('li');
+
+function getSalaryValue(str) {
+  return +str.replace('$', '').replace(',', '');
+};
+
+function sortList(list) {
+  const sortedList = [...elements].sort((a, b) =>
+    getSalaryValue(b.dataset.salary) - getSalaryValue(a.dataset.salary)
+  );
+
+  document.querySelector('ul').append(...sortedList);
+};
+
+function getEmployees(list) {
+  const employees = [...list].map(employe => {
+    return {
+      name: employe.innerText,
+      position: employe.dataset.position,
+      salary: employe.dataset.salary,
+      age: employe.dataset.age,
+    };
+  });
+
+  return employees;
+};
+
+getEmployees(elements);
+sortList(elements);
