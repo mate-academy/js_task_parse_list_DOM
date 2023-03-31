b2a84dfad47d7d26d7bf4ddfc036a8fdf4febbd3
Daryna Hnidash <melissa998877@gmail.com>
2023-03-31 13:47:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+function toNumber(str) {
+  return +str.slice(1).replace(',', '');
+};
+
+function sortList(items) {
+  const itemsArray = [...items].sort((a, b) =>
+    toNumber(b.dataset.salary) - toNumber(a.dataset.salary));
+
+  document.querySelector('ul').append(...itemsArray);
+}
+
+function getEmployees(items) {
+  return [...items].map(person => {
+    return {
+      name: person.innerText,
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    };
+  });
+}
+
+sortList(list);
+getEmployees(list);
