0cde72d35a720ec2dce51484834ae3f3cd440db2
Sasha Green <aleksa146311@gmail.com>
2023-04-02 14:59:16 +0300

    add task parse list DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const list = [...document.querySelectorAll('li')];
+
+sortList(list);
+
+getEmployees(list);
+
+function toNumber(str) {
+  return str.split('').filter(item => '0123456789'.includes(item)).join('');
+}
+
+function sortList(items) {
+  items.sort((a, b) => toNumber(b.dataset.salary) - toNumber(a.dataset.salary));
+
+  document.querySelector('ul').append(...items);
+}
+
+function getEmployees(items) {
+  return items.map(item => ({
+    name: item.innerText,
+    ...item.dataset,
+  }));
+}
