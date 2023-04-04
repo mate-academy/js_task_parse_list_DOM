b48226faef8d6f355e0f586429bccb6601a8f6ce
Anna Grechana <grechanaanna@gmial.com>
2023-04-04 21:51:42 +0300

    1st version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const element = document.body.children[1].children;
+const sortlist = [...element].sort((x, y) =>
+  (+(((y.dataset['salary']).replace(/,/g, '')).replace('$', '')))
+  - (+(((x.dataset['salary']).replace(/,/g, '')).replace('$', ''))));
+const sortListNames = [];
+
+for (const person of sortlist) {
+  sortListNames.push(person.innerText);
+}
+
+for (let i = 0; i <= element.length; i++) {
+  element[i].innerText = sortListNames[i];
+}
