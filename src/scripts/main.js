97d97df51b01d4e6e8463c2f0fa47b816564ad8d
Yurii Hlushenko <yauglushenko@gmail.com>
2023-04-05 13:24:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const sortedList = [...list].sort((a, b) =>
+  +b.attributes[1].value.replace(/[^\d]/g, '')
+  - +a.attributes[1].value.replace(/[^\d]/g, ''));
+
+const ul = document.querySelector('ul');
+
+ul.innerHTML = sortedList.map(li => `
+  <li>
+    ${li.innerText}
+  </li>
+`).join('');
