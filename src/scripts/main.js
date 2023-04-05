3cfeee1803f46146a96cb0d562c0109644dfb82f
Oksana Tytanych <oksanatytanych@gmail.com>
2023-04-05 13:21:25 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+
+function getNumber(el) {
+  return +el.dataset.salary.slice(1, el.length).split(',').join('.');
+}
+
+function sortList([...array], callback) {
+  return array.sort((a, b) => callback(b) - callback(a));
+}
+
+const sortedList = sortList(items, getNumber);
+
+const newList = document.getElementsByTagName('ul')[0];
+
+newList.innerHTML = sortedList.map((item, i) =>
+  `<li
+    data-position=${sortedList[i].dataset.position}
+    data-salary=${sortedList[i].dataset.salary}
+    data-age=${sortedList[i].dataset.age}
+  >
+    ${sortedList[i].innerText}
+  </li>`
+).join('');
