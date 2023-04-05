070f99bde4ae21b77fb5c8766963d34d5b85df19
Oksana Tytanych <oksanatytanych@gmail.com>
2023-04-05 14:27:34 +0300

    code was changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const items = document.querySelectorAll('li');
+const list = document.querySelectorAll('li');
 
 function getNumber(el) {
   return +el.dataset.salary.slice(1, el.length).split(',').join('.');
@@ -10,16 +10,6 @@ function sortList([...array], callback) {
   return array.sort((a, b) => callback(b) - callback(a));
 }
 
-const sortedList = sortList(items, getNumber);
+const sortedList = sortList(list, getNumber);
 
-const newList = document.getElementsByTagName('ul')[0];
-
-newList.innerHTML = sortedList.map((item, i) =>
-  `<li
-    data-position=${sortedList[i].dataset.position}
-    data-salary=${sortedList[i].dataset.salary}
-    data-age=${sortedList[i].dataset.age}
-  >
-    ${sortedList[i].innerText}
-  </li>`
-).join('');
+document.querySelector('ul').append(...sortedList);
