179e73e914b5cdf686a6cbe45ed8b1ba21bfd7af
Yurii Hlushenko <yauglushenko@gmail.com>
2023-04-05 15:15:32 +0300

    comments implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,27 @@
 'use strict';
 
 const List = document.querySelectorAll('li');
-let sortedList;
 
 function sortList(list) {
-  sortedList = [...list].sort((a, b) =>
+  const employees = [];
+  const sortedList = [...list].sort((a, b) =>
     +b.attributes[1].value.replace(/[^\d]/g, '')
     - +a.attributes[1].value.replace(/[^\d]/g, ''));
 
-  const ul = document.querySelector('ul');
+  sortedList.map(li => {
+    document.querySelector('ul').append(li);
 
-  ul.innerHTML = sortedList.map(li => `
-    <li>
-      ${li.innerText}
-    </li>
-  `).join('');
-}
-
-function getEmployees(list) {
-  const employees = [];
-
-  sortedList.map((li, index) => employees.push(
-    {
-      name: li.innerText.trim(),
-      position: index + 1,
-      salary: li.attributes[1].value,
-      age: li.attributes[2].value,
-    }
-  ));
+    employees.push(
+      {
+        name: li.innerText.trim(),
+        position: li.attributes[0].value,
+        salary: li.attributes[1].value,
+        age: li.attributes[2].value,
+      }
+    );
+  });
 
   return employees;
 }
 
 sortList(List);
-getEmployees(List);
