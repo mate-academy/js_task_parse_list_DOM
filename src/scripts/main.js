70084cae9c61ceadd99a1d3600ece0a740abef9f
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-07 13:49:55 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+
+const list = document.querySelector('ul');
+const people = document.querySelectorAll('li');
+
+
+function sortList(arr) {
+  function transNum(num) {
+    return Number(num.split('$').join('').split(',').join(''));
+  }
+
+  return arr.map(item => {
+    const dataInfo = item.dataset;
+    const innerText = `${item.innerText}`;
+
+    return {name : innerText, ...dataInfo}
+  }).sort((a, b) => transNum(b.salary) - transNum(a.salary));
+}
+const qwe = sortList([...people]);
+list.innerHTML = ''
+function getEmployees(obj) {
+  
+
+  for (let i = 0; i < obj.length; i++) {
+    const keysData = Object.keys(obj[i]);
+    let itemList = document.createElement('li');
+      
+    for (let j = 1; j < keysData.length; j++) {
+      itemList.dataset[keysData[j]] = obj[i][keysData[j]];
+    }
+
+    itemList.innerText = obj[i].name;
+    list.appendChild(itemList)
+  }
+}
+
+getEmployees(qwe);
