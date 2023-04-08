07a9350be034da81ece9c9d908786ea7968e4b74
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-04-08 17:23:47 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+function convertToNumber(str) {
+  return parseInt(str.replace(/\D/g, ''));
+}
+
+const sortedList = [...list].sort((a, b) => {
+  const salaryA = convertToNumber(a.dataset.salary);
+  const salaryB = convertToNumber(b.dataset.salary);
+
+  return salaryB - salaryA;
+});
+
+document.body.innerHTML = `
+  <h1>List of employees</h1>
+  <ul>
+    ${sortedList.map(item => `
+    <li 
+      data-name = '${item.textContent}'
+      data-position='${item.dataset.position}' 
+      data-salary='${item.dataset.salary}'
+      data-age= '${item.dataset.age}'>
+    ${item.textContent}
+    </li>`).join('')}
+  </ul>
+`;
