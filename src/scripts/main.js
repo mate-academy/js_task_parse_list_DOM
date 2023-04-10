a186b45a3da95732152545978195bf0dbbdb7de8
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-04-10 10:29:27 +0300

    created functions and changed output

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,59 @@
 'use strict';
 
 const list = document.querySelectorAll('li');
+const ul = document.querySelector('ul');
 
-function convertToNumber(str) {
-  return parseInt(str.replace(/\D/g, ''));
+// сортування за з.п
+// eslint-disable-next-line no-shadow
+function sortBySalary(list) {
+  function convertToNumber(str) {
+    return parseInt(str.replace(/\D/g, ''));
+  }
+
+  // eslint-disable-next-line no-shadow
+  const sortedList = [...list].sort((a, b) => {
+    const salaryA = convertToNumber(a.dataset.salary);
+    const salaryB = convertToNumber(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  return sortedList;
 }
 
-const sortedList = [...list].sort((a, b) => {
-  const salaryA = convertToNumber(a.dataset.salary);
-  const salaryB = convertToNumber(b.dataset.salary);
+// масив об'єктів
+// eslint-disable-next-line no-shadow
+function getEmployees(list) {
+  // eslint-disable-next-line no-shadow
+  const employees = [];
 
-  return salaryB - salaryA;
+  list.forEach(item => {
+    employees.push({
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    });
+  });
+
+  return employees;
+}
+
+// запуск обох функцій для списку li-шок
+const sortedList = sortBySalary(list);
+const employees = getEmployees(sortedList);
+
+// очистити вміст ul
+ul.innerHTML = '';
+
+// створення нових li-елементів з масиву відсортованих об'єктів
+employees.forEach(item => {
+  const li = document.createElement('li');
+
+  li.textContent = item.name;
+  li.setAttribute('data-name', item.name);
+  li.setAttribute('data-position', item.position);
+  li.setAttribute('data-salary', item.salary);
+  li.setAttribute('data-age', item.age);
+  ul.appendChild(li);
 });
-
-document.body.innerHTML = `
-  <h1>List of employees</h1>
-  <ul>
-    ${sortedList.map(item => `
-    <li 
-      data-name = '${item.textContent}'
-      data-position='${item.dataset.position}' 
-      data-salary='${item.dataset.salary}'
-      data-age= '${item.dataset.age}'>
-    ${item.textContent}
-    </li>`).join('')}
-  </ul>
-`;
