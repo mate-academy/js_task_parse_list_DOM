f4e69eb4b625c3f9b7f6d2757512772a3ba2954c
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-04-14 12:31:53 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+const employees = document.querySelector('.employess');
+const employeesArray = [...employees.children];
+
+employeesArray.forEach(item => {
+  const num = Number(item.getAttribute('data-salary').replace(/[$,]/g, ''));
+
+  item.setAttribute('data-salary', num);
+});
+
+employeesArray.sort((a, b) =>
+  b.getAttribute('data-salary')
+  - a.getAttribute('data-salary')
+);
+
+employees.innerHTML = '';
+
+employeesArray.forEach(employee => {
+  const liElement = document.createElement('li');
+
+  liElement.setAttribute(
+    'data-position',
+    employee.getAttribute('data-position')
+  );
+
+  liElement.setAttribute(
+    'data-salary',
+    employee.getAttribute('data-salary')
+  );
+
+  liElement.setAttribute(
+    'data-age',
+    employee.getAttribute('data-age')
+  );
+
+  liElement.innerText = employee.innerText.trim();
+
+  employees.appendChild(liElement);
+});
