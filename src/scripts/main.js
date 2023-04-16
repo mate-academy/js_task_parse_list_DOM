213eff7607adeee836da79f42a75f9a2f9b31168
Zibi95 <sylwester.sob95@gmail.com>
2023-04-16 15:01:51 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const nodesList = document.querySelectorAll('[data-salary]');
+
+function prepareNumber(string) {
+  return +string
+    .slice(1)
+    .split(',')
+    .join('');
+}
+
+function sortList(list) {
+  const listArray = Array.from(list);
+
+  listArray.sort((a, b) => (
+    prepareNumber(b.dataset.salary) - prepareNumber(a.dataset.salary)
+  ));
+
+  for (const node of listArray) {
+    document.querySelector('ul').append(node);
+  }
+}
+
+function getEmployees(list) {
+  const listArray = Array.from(list);
+
+  return listArray.reduce((accumulator, current) => {
+    const fullName = current.innerText;
+    const { position, salary, age } = current.dataset;
+
+    return [...accumulator, {
+      fullName,
+      position,
+      salary,
+      age,
+    }];
+  }, []);
+}
+
+sortList(nodesList);
+getEmployees(nodesList);
