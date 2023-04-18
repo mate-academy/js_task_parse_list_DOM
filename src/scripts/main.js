9b843ef5117e336983025e96f7fd5792d7544797
Mikołaj <ostermi@wp.pl>
2023-04-18 10:28:58 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,56 @@
 'use strict';
 
-// write code here
+const formatSalaryToNumber = (salaryText) => (
+  Number(salaryText.slice(1).split(',').join('')));
+
+const formatSalaryToText = (salaryNumber) => (
+  `$${salaryNumber.toLocaleString('en-US')}`
+);
+
+const employeesElement = document.querySelectorAll('li');
+const employees = getEmployeesList();
+
+function getEmployeesList() {
+  const list = [];
+
+  employeesElement.forEach(employeeElement => {
+    const employeeProperties = employeeElement.dataset;
+    const employeeName = employeeElement.innerText;
+
+    const { position, salary, age } = employeeProperties;
+
+    list.push({
+      employeeName,
+      position,
+      salary: formatSalaryToNumber(salary),
+      age,
+    });
+  });
+
+  return list;
+}
+
+function sortEmployeesList() {
+  // descending order
+  employees.sort((firstEmployee, secondEmployee) => (
+    secondEmployee.salary - firstEmployee.salary));
+}
+
+function setEmployeesElement() {
+  let currentEmployee = 0;
+
+  employeesElement.forEach(employeeElement => {
+    const { position, salary, age, employeeName } = employees[currentEmployee];
+
+    employeeElement.dataset.age = age;
+
+    employeeElement.dataset.salary = formatSalaryToText(salary);
+
+    employeeElement.dataset.position = position;
+    employeeElement.innerText = employeeName;
+    currentEmployee++;
+  });
+}
+
+sortEmployeesList();
+setEmployeesElement();
