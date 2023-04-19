c19c3bfd20f6ae9b7cf5e48a89d47d554f18ed40
Alla Gutsul <allagutsul1990@gmail.com>
2023-04-19 23:32:20 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const listTotal = document.querySelector('ul');
+const listOfEmployees = document.querySelectorAll('ul > li');
+
+function sortList(list) {
+  return ([...list].sort((a, b) => formatSalary(
+    b.dataset.salary) - formatSalary(a.dataset.salary)));
+}
+
+const sortedList = sortList(listOfEmployees);
+
+listTotal.append(...sortedList);
+
+function getEmployees(list) {
+  const arrOfEmployees = [];
+
+  list.forEach(item => {
+    const objOfEmployee = {};
+
+    objOfEmployee['name'] = item.innerText;
+    objOfEmployee['position'] = item.dataset.position;
+    objOfEmployee['salary'] = item.dataset.salary;
+    objOfEmployee['age'] = item.dataset.age;
+    arrOfEmployees.push(objOfEmployee);
+  });
+
+  return arrOfEmployees;
+};
+
+getEmployees(sortedList);
+
+function formatSalary(salary) {
+  return +salary.replace('$', '').replace(',', '');
+}
