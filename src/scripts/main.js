ac54981f07921aae236c3130f1d35a6f80780f1c
Nadiein Oleh <olegnadein1994@gmail.com>
2023-04-20 14:24:04 +0300

    refactoring the code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,15 @@ const listOfEmployees = document.querySelector('ul');
 const employees = document.querySelectorAll('li');
 
 function sortPeople(listPeople, list) {
-  const peopleSortSalary = [...listPeople].sort((str1, str2) => {
-    const num2 = str2.dataset.salary
-      .replace(/,/g, '').replace('$', '');
-    const num1 = parseFloat(str1.dataset.salary
-      .replace(/,/g, '').replace('$', ''));
+  const replaceString = (str) => {
+    return str.replace(/,/g, '').replace('$', '');
+  };
 
-    return num2 - num1;
+  const peopleSortSalary = [...listPeople].sort((str1, str2) => {
+    const num2 = str2.dataset.salary;
+    const num1 = str1.dataset.salary;
+
+    return replaceString(num2) - replaceString(num1);
   });
 
   peopleSortSalary.forEach(people => list.append(people));
@@ -20,16 +22,15 @@ sortPeople(employees, listOfEmployees);
 
 function createObjFromEmployess(listPeople) {
   for (let i = 0; i < listPeople.length; i++) {
-    const elementContent = listPeople[i].textContent.trim();
-    const position = listPeople[i].dataset.position;
-    const age = listPeople[i].dataset.age;
-    const salary = listPeople[i].dataset.salary;
+    const infoPeople = listPeople[i];
+    const { position, age, salary } = infoPeople.dataset;
+    const elementContent = infoPeople.textContent.trim();
 
     const obj = {
       name: elementContent,
-      position: position,
-      age: age,
-      salary: salary,
+      position,
+      age,
+      salary,
     };
 
     return obj;
