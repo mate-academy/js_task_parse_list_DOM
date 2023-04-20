83ef4c83ab81dc6f4eca43b03a622908c9f27c52
Nadiein Oleh <olegnadein1994@gmail.com>
2023-04-20 12:34:21 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const listOfEmployees = document.querySelector('ul');
+const employees = document.querySelectorAll('li');
+
+function sortPeople(listPeople, list) {
+  const peopleSortSalary = [...listPeople].sort((str1, str2) => {
+    const num2 = str2.dataset.salary
+      .replace(/,/g, '').replace('$', '');
+    const num1 = parseFloat(str1.dataset.salary
+      .replace(/,/g, '').replace('$', ''));
+
+    return num2 - num1;
+  });
+
+  peopleSortSalary.forEach(people => list.append(people));
+}
+
+sortPeople(employees, listOfEmployees);
+
+function createObjFromEmployess(listPeople) {
+  for (let i = 0; i < listPeople.length; i++) {
+    const elementContent = listPeople[i].textContent.trim();
+    const position = listPeople[i].dataset.position;
+    const age = listPeople[i].dataset.age;
+    const salary = listPeople[i].dataset.salary;
+
+    const obj = {
+      name: elementContent,
+      position: position,
+      age: age,
+      salary: salary,
+    };
+
+    return obj;
+  }
+}
+
+createObjFromEmployess(employees);
