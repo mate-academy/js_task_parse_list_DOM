abc5dfb6b91f07c397c4fa27869a1af9babfa64a
Victor Buldenko <gookidoo@gmail.com>
2023-04-20 13:59:53 +0300

    done, but i don`t know what for getEmployees need

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+function getNumberFromSalary(item) {
+  const result = +[...item.dataset.salary].map((char) => {
+    if (isNaN(char)) {
+      return;
+    }
+
+    return char;
+  }).join('');
+
+  return result;
+}
+
+function getEmployees(arr) {
+  const result = [];
+
+  arr.forEach((el) => {
+    result.push({
+      name: el.innerText,
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    });
+  });
+
+  return result;
+}
+
+const list = document.querySelector('UL');
+const employees = [...document.getElementsByTagName('li')];
+
+employees.sort((a, b) => {
+  if (getNumberFromSalary(a) - getNumberFromSalary(b) > 0) {
+    return -1;
+  }
+});
+
+employees.forEach((el) => list.append(el));
+
+// eslint-disable-next-line no-unused-vars
+const employeesObjectsList = getEmployees(employees);
