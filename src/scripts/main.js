2533737779225ba1c0b03b8a42055d766c0ac221
danulyk05 <ivan.danulyk2016@gmail.com>
2023-04-21 18:22:47 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const ulMain = document.querySelector('ul');
+const listUl = ulMain.children;
+
+function sortList(list) {
+  const sortL = [...listUl].sort((a, b) => {
+    return creatNumber(b.dataset.salary) - creatNumber(a.dataset.salary);
+  });
+
+  for (const lis of sortL) {
+    ulMain.insertAdjacentElement('beforeend', lis);
+  }
+}
+
+function getEmployees(list) {
+  const objEl = [...list].map(elem => {
+    return {
+      name: elem.innerText,
+      position: elem.dataset.position,
+      salary: elem.dataset.salary,
+      age: elem.dataset.age,
+    };
+  });
+
+  return objEl;
+}
+
+function creatNumber(str) {
+  const corectNumb = str.slice(1).split(',').join('');
+
+  return corectNumb;
+};
+
+sortList(listUl);
+getEmployees(listUl);
