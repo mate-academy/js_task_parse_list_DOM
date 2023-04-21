8d63da9e121830de2615389272dd7a2eaa061eba
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-04-21 22:51:45 +0200

    Parse List was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,52 @@
 'use strict';
 
-// write code here
+const list = document.body.querySelector('ul');
+
+class Employee {
+  constructor(boss, position, salary, age) {
+    this.name = boss;
+    this.position = position;
+    this.salary = salary;
+    this.age = age;
+  }
+}
+
+function sortList() {
+  const employeesArr = [ ...list.children ];
+  let resultString = '';
+
+  employeesArr.sort((a, b) => {
+    return Number(b.dataset.salary.substring(1).replaceAll(',', ''))
+    - Number(a.dataset.salary.substring(1).replaceAll(',', ''));
+  });
+
+  for (const li of employeesArr) {
+    resultString += `
+    <li
+      data-position=${li.dataset.position}
+      data-salary=${li.dataset.salary.toString()}
+      data-age=${li.dataset.age}
+    >
+      ${li.innerText}
+    </li>
+    `;
+  }
+
+  list.innerHTML = resultString;
+}
+
+function getEmployees() {
+  const employeesArray = [];
+
+  for (const li of [ ...list.children ]) {
+    const employee = new Employee(li.innerText, li.dataset.position,
+      li.dataset.salary, li.dataset.age);
+
+    employeesArray.push(employee);
+  }
+
+  return employeesArray;
+}
+
+sortList();
+getEmployees();
