82a8333338427b6b15091fb9f44b7fcfe18cf174
Antonio S <soi4.toxa@ukr.net>
2023-04-27 14:29:25 +0300

    Parse-list-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function sortListSalary(ul) {
+  const items = [...list.querySelectorAll('li')];
+
+  items.sort((itemA, itemB) => {
+    const saleryA = +itemA.dataset.salary.slice(1).split(',').join('');
+    const saleryB = +itemB.dataset.salary.slice(1).split(',').join('');
+
+    return saleryB - saleryA;
+  });
+
+  for (let i = 0; i < items.length; i++) {
+    list.children[i].replaceWith(items[i].cloneNode(true));
+  }
+
+  return list;
+}
+
+sortListSalary(list);
