dc403d564ae47eb9c8eb6d0f09a0bb9bf971620a
Anastasiia Tarasova <tarasova0721@gmail.com>
2023-05-02 22:51:59 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+function sortList(list) {
+  const sortedList = list.sort((a, b) => {
+    return (Number(b.dataset.salary) - Number(a.dataset.salary));
+  });
+
+  document.querySelector('ul').append(...sortedList);
+}
+
+function getEmployees(list) {
+  const employees = list.map(el => {
+    return {
+      name: el.textContent,
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    };
+  });
+
+  return employees;
+}
+
+sortList(li);
+getEmployees(li);
