f551c243beb49c4f71fb2d771ec2c481c928e7fd
Veronika Pastushkova <d9169393507@gmai.com>
2023-05-05 11:21:43 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const listEmployees = document.querySelector('ul');
+const employees = document.querySelectorAll('li'); // nodelist
+const employeesList = [ ...employees ]; // array
+
+function salary(person) {
+  return +(person.dataset.salary).slice(1).replace(',', '');
+}
+
+function sortList(list) { // list is array of li
+  const sortEmloyees = list.sort((a, b) => salary(b) - salary(a));
+
+  listEmployees.append(...sortEmloyees);
+}
+
+function getEmployees(list) {
+  list.map(el => ({
+    name: el.textContent,
+    position: el.dataset.position,
+    salary: el.dataset.salary,
+    age: el.append.age,
+  })
+  );
+}
+
+sortList(employeesList);
+getEmployees(employeesList);
