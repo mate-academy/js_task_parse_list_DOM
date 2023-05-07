628a89d818f3be3d00e60b1565f82803770c209c
Oleksii Lavka <lavka.alexey37gmail.com>
2023-05-07 13:04:13 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+// we get a list of employees
+const listNode = document.querySelector('ul');
+
+const sortList = list => {
+  // we get all the elements of the list
+  const listItems = [...list.children];
+
+  // we sorting employees by decreasing salary
+  listItems.sort((empl1, empl2) => {
+    const salary1 = parseSalary(empl1.dataset.salary);
+    const salary2 = parseSalary(empl2.dataset.salary);
+
+    return salary2 - salary1;
+  });
+
+  // we move list items to new locations
+  list.append(...listItems);
+};
+
+const getEmployees = list => {
+  const listItems = [...list.children].map(employee => {
+    const fullName = employee.innerText;
+    const { position, salary, age } = employee.dataset;
+
+    return {
+      name: fullName,
+      position,
+      salary: parseSalary(salary),
+      age: +age,
+    };
+  });
+
+  return listItems;
+};
+
+// we remove commas and dollar signs from wages and convert to a number
+const parseSalary = salary => +salary.replace(/[$,]/g, '');
+
+sortList(listNode);
+getEmployees(listNode);
