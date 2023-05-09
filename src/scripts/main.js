fa970f7131f43d43e0398dcfd993507f2ea17c61
DefaultGitUser <usingforgit@gmail.com>
2023-05-09 21:01:10 +0300

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const employeesElArray = [...document.querySelectorAll('li')];
+const listEl = document.querySelector('ul');
+
+const sortList = (list) => {
+  const sorted = list.sort((a, b) => {
+    const aSalary = +a.dataset.salary.replace(/[^0-9]/g, '');
+    const bSalary = +b.dataset.salary.replace(/[^0-9]/g, '');
+
+    return bSalary - aSalary;
+  });
+
+  listEl.append(...sorted);
+};
+
+const getEmployees = (list) => {
+  return list.map(person => ({
+    name: person.innerText,
+    position: person.dataset.positon,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+};
+
+sortList(employeesElArray);
+getEmployees(employeesElArray);
