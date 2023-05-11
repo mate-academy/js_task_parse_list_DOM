aa460843c507ff40597eb96529861d77ece3a18b
Absolution26 <zet2571428@gmail.com>
2023-05-11 21:52:32 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const listUl = document.querySelector('ul');
+const collectionOfListEmployees = document.querySelectorAll('li');
+
+function sortList(list) {
+  const sortCollection = [...list].sort((a, b) =>
+    +(b.dataset.salary.replace('$', '').replace(',', ''))
+    - +(a.dataset.salary.replace('$', '').replace(',', '')));
+
+  for (const li of sortCollection) {
+    listUl.append(li);
+  }
+}
+
+function getEmployees(list) {
+  return [...list].map(employee => {
+    employee.dataset.name = employee.innerText;
+  });
+}
+
+sortList(collectionOfListEmployees);
+getEmployees(collectionOfListEmployees);
