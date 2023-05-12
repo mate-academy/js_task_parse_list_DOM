203a3eb79c2d31719e33e6c91146d581b712000c
Denys Didenko <ddidenko441@gmail.com>
2023-05-12 18:50:42 +0300

    add parse_list_DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const listOfEmployees = document.querySelector('ul');
+const employees = [...listOfEmployees.children];
+
+function sortList(list) {
+  function employeeSalary(employee) {
+    return employee.dataset.salary
+      .slice(1)
+      .replace(/,/gi, '');
+  };
+
+  const sortedList = list
+    .sort((a, b) => +employeeSalary(b) - +employeeSalary(a));
+
+  listOfEmployees.append(...sortedList);
+};
+
+function getEmployees(list) {
+  return list.map(employee => ({
+    name: employee.innerText,
+    position: employee.dataset.position,
+    salary: employee.dataset.salary,
+    age: employee.dataset.age,
+  }));
+};
+
+sortList(employees);
+getEmployees(employees);
