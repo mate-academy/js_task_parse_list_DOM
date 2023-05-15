465cff9f9536d816090e63addb52b310ac0df9de
Izzet Yusufov <17izzet@gmail.com12345Hello>
2023-05-15 23:39:58 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+function salaryToNum(str) {
+  return +str.dataset.salary.slice(1).replace(',', '');
+}
+
+const list = document.querySelector('ul');
+const listItem = document.querySelectorAll('li');
+
+const sortedList = [...listItem]
+  .sort((a, b) => salaryToNum(b) - salaryToNum(a));
+
+for (let i = 0; i < sortedList.length; i++) {
+  list.append(sortedList[i]);
+}
