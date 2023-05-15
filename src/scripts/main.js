fc32b78150e8765501b808d5cd47ed85567f0c63
Liliia Danylenko <liliya.danylenko96@gmail.com>
2023-05-15 17:33:47 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul').children;
+
+function sortList(params) {
+  const sort = [...params].sort((itemA, itemB) => {
+    const numA = toNumber(itemA);
+    const numB = toNumber(itemB);
+
+    return numB - numA;
+  });
+
+  for (let i = 0; i < sort.length; i++) {
+    list[i].before(sort[i]);
+  }
+
+  return list;
+}
+
+function toNumber(number) {
+  return number.dataset.salary.slice(1).split(',').join('');
+}
+
+function getEmployees(params) {
+  return [...params].map((item, index) => ({
+    name: item.innerText,
+    position: index,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+}
+
+sortList(list);
+getEmployees(list);
