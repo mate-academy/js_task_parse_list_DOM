168b2b9d3394b149f3f687beab66f3bdb31a12f5
Anton <crossnaders4949@gmail.com>
2023-05-17 20:42:19 +0200

    simplified solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,29 +3,15 @@
 const listMain = document.querySelector('ul');
 
 function convNumber(text) {
-  const transformTextPhase1 = text.split('');
-
-  transformTextPhase1.shift();
-
-  const transformTextPhase2 = transformTextPhase1.join('');
-  const transformTextPhase3 = transformTextPhase2.split(',').join('');
-  const result = Number(transformTextPhase3);
-
-  return result;
+  return Number(text.replace(/[^0-9.-]+/g, ''));
 }
 
 function sortList(list) {
   const items = list.querySelectorAll('li');
   const itemsArray = [ ...items ];
 
-  for (const elem of itemsArray) {
-    const first = elem.dataset.salary;
-
-    elem.dataset.salary = convNumber(first);
-  }
-
   const result = itemsArray.sort((a, b) =>
-    Number(b.dataset.salary) - Number(a.dataset.salary));
+    convNumber(b.dataset.salary) - convNumber(a.dataset.salary));
 
   for (const elem of result) {
     list.append(elem);
@@ -38,13 +24,12 @@ function getEmployees(list) {
   const arr = [];
 
   for (const elem of itemsArray) {
-    const obj = {};
-
-    obj.name = elem.innerText;
-    obj.position = elem.dataset.position;
-    obj.salary = elem.dataset.salary;
-    obj.age = elem.dataset.age;
-    arr.push(obj);
+    arr.push({
+      name: elem.innerText,
+      position: elem.dataset.position,
+      salary: elem.dataset.salary,
+      age: elem.dataset.age,
+    });
   }
 
   return arr;
