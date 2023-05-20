e600187748e6bf3230dc5d509f1b3862313a98c6
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-05-20 13:43:17 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+function convertNumber(str) {
+  const newStr = str.slice(1);
+
+  return +newStr.split(',').join('');
+}
+
+function sortList(arr) {
+  const sortArr = Array.from(arr);
+
+  sortArr.sort((a, b) => {
+    const first = convertNumber(a.getAttribute('data-salary'));
+    const second = convertNumber(b.getAttribute('data-salary'));
+
+    return second > first ? 1 : second < first ? -1 : 0;
+  });
+
+  for (const item of sortArr) {
+    list.appendChild(item);
+  }
+
+  return sortArr;
+}
+
+function getEmployees(arr) {
+  const arrObjects = [];
+
+  for (const item of arr) {
+    const attrs = item.getAttributeNames().reduce((acc, atname) => {
+      return {
+        ...acc, [atname]: item.getAttribute(atname),
+      };
+    }, {});
+
+    arrObjects.push(attrs);
+  }
+
+  return arrObjects;
+}
+
+sortList(items);
+getEmployees(items);
