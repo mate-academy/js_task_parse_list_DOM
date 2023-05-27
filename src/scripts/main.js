0545d320656b26dd8c076e6360c446854763caa9
Roma Tsvindin <tsvindin1995@gmail.com>
2023-05-28 01:27:35 +0300

    js_task_parse_list_DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+
+function salaryToNumber(salary) {
+  return parseInt(salary.replace(/\$|,/g, ''));
+}
+
+function getEmployees(list) {
+  const employees = [];
+
+  for (const person of list.children) {
+    const salary = person.getAttribute('data-salary');
+    const position = person.getAttribute('data-position');
+    const age = person.getAttribute('data-age');
+    const nameOfPerson = person.innerText;
+    const personObject = {
+      name: nameOfPerson,
+      position,
+      salary,
+      age,
+    };
+
+    employees.push(personObject);
+  }
+
+  return employees;
+}
+
+function sortList(list) {
+  const listItems = Array.from(list.children);
+
+  listItems.sort((a, b) => {
+    const aSalary = a.getAttribute('data-salary');
+    const bSalary = b.getAttribute('data-salary');
+
+    return salaryToNumber(bSalary) - salaryToNumber(aSalary);
+  });
+
+  listItems.forEach(item => {
+    list.append(item);
+  });
+}
+
+sortList(ul);
+getEmployees(ul);
