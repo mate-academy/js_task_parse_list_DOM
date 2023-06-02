e62411cf1b3953f00b66c010ea6100d22ae34db7
Adrian Yevchuk <adrianyevchuk@MacBook-Air-Adrian.local>
2023-06-02 11:15:55 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,50 @@
 'use strict';
 
-// write code here
+const listOfEmployees = document.querySelectorAll('li');
+
+function toInt(string) {
+  const result = string.replace('$', '').split(',').join('');
+
+  return Number.parseInt(result);
+}
+
+function sortList(listEmp) {
+  const sortedList = [...listEmp].sort((a, b) => {
+    const aSalary = toInt(a.dataset.salary);
+    const bSalary = toInt(b.dataset.salary);
+
+    return bSalary - aSalary;
+  });
+
+  const list = document.querySelector('ul');
+
+  list.innerHTML = `
+    ${sortedList.map((el, i) => `
+        <li
+            data-position="${el.dataset.position}"
+            data-salary="${el.dataset.salary}"
+            data-age="${el.dataset.age}"
+        >
+            ${el.innerText}
+        </li>
+    `).join('')}
+  `;
+}
+
+function getEmployees(listEmp) {
+  const arr = [];
+
+  [...listEmp].forEach(el =>
+    arr.push({
+      name: el.innerText,
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    })
+  );
+
+  return arr;
+}
+
+sortList(listOfEmployees);
+getEmployees(listOfEmployees);
