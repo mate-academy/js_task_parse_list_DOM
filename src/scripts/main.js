56edc0b50e213b5e2944bdda4e2d74113a976081
domkab <dominykas.bobinas@gmail.com>
2023-06-03 13:47:25 +0300

    1.0.0 initial

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const employees = document.querySelectorAll('li[data-salary]');
+
+const employeeArray = [...employees].map(employee => {
+  const salary = employee
+    .getAttribute('data-salary')
+    .replace(/[^\d.-]/g, '');
+
+  return {
+    element: employee, salary: parseInt(salary),
+  };
+});
+
+employeeArray.sort((a, b) => b.salary - a.salary);
+
+const ul = document.querySelector('ul');
+
+ul.innerHTML = '';
+
+employeeArray.forEach(employee => {
+  ul.appendChild(employee.element);
+});
