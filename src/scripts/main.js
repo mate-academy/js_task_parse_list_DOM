a84a6ac826daccfaba627237cfcf1b9971d8b55e
Mykola Plekan <zikzar@gmail.com>
2023-06-06 22:27:15 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const items = document.querySelector('ul');
+const list = document.querySelectorAll('li');
+
+function sortList(item) {
+  const sortItems = [...item].sort((a, b) =>
+    convert(b) - convert(a));
+
+  items.append(...sortItems);
+};
+
+function convert(str) {
+  return +str.dataset.salary.slice(1).split(',').join('');
+}
+
+function getEmployees(property) {
+  const arr = [];
+
+  [...property].forEach(employee => {
+    const obj = {};
+
+    obj.name = employee.innerText;
+    obj.position = employee.dataset.position;
+    obj.salary = employee.dataset.salary;
+    obj.age = employee.dataset.age;
+
+    arr.push(obj);
+  });
+
+  return arr;
+}
+
+getEmployees(list);
+sortList(list);
