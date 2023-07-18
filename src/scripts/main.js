e73d466a3ee32a9431bee100bb20d3052ece666f
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-07-18 15:05:06 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+function sortList(element) {
+  const elementArray = Array.from(element);
+
+  elementArray.sort((a, b) => {
+    let x = b.getAttribute('data-salary').substring(1);
+
+    x = Number(x.replace(',', ''));
+
+    let y = a.getAttribute('data-salary').substring(1);
+
+    y = Number(y.replace(',', ''));
+
+    return x - y;
+  });
+
+  const ul = document.querySelector('ul');
+
+  elementArray.forEach(x => ul.appendChild(x));
+}
+
+sortList(list);
+
+function getEmployees(element) {
+  const employeesList = [];
+
+  for (let i = 0; i < element.length; i++) {
+    employeesList.push({
+      name: element[i].innerText,
+      position: element[i].getAttribute('data-position'),
+      salary: element[i].getAttribute('data-salary'),
+      age: element[i].getAttribute('data-age'),
+    });
+  }
+
+  return employeesList;
+}
+
+getEmployees(list);
