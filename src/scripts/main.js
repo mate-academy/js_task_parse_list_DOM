76a43767c183f6471d1e1ba1f0c5e252ef7cf7ad
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-07-24 10:53:56 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,40 +2,70 @@
 
 const list = document.querySelectorAll('li');
 
+const salaryToNumber = (string) => string.match(/\d+/g).join('');
+
 function sortList(elements) {
-  const elementArray = Array.from(elements);
+  // const elementArray = Array.from(elements);
 
-  elementArray.sort((a, b) => {
-    const x = salaryToNumber(b.getAttribute('data-salary'));
-    const y = salaryToNumber(a.getAttribute('data-salary'));
+  // elementArray.sort((a, b) => {
+  //   const x = salaryToNumber(b.getAttribute('data-salary'));
+  //   const y = salaryToNumber(a.getAttribute('data-salary'));
 
-    return x - y;
-  });
+  //   return x - y;
+  // });
 
+  // const ul = document.querySelector('ul');
+
+  // elementArray.forEach(x => ul.appendChild(x));
   const ul = document.querySelector('ul');
 
-  elementArray.forEach(x => ul.appendChild(x));
+  const sortedEmployees = [...elements].sort(
+    (a, b) =>
+      salaryToNumber(b.getAttribute('data-salary'))
+      - salaryToNumber(a.getAttribute('data-salary'))
+  );
+
+  sortedEmployees.forEach((x) => ul.appendChild(x));
+
+  return sortedEmployees;
 }
 
 sortList(list);
 
-function getEmployees(elements) {
-  const employeesList = [];
+const getEmployees = (elements) =>
+// function getEmployees(elements) {
+  // const employeesList = [];
 
-  for (let i = 0; i < elements.length; i++) {
-    employeesList.push({
-      name: elements[i].innerText,
-      position: elements[i].getAttribute('data-position'),
-      salary: elements[i].getAttribute('data-salary'),
-      age: elements[i].getAttribute('data-age'),
-    });
-  }
+  // for (let i = 0; i < elements.length; i++) {
+  //   employeesList.push({
+  //     name: elements[i].innerText,
+  //     position: elements[i].getAttribute('data-position'),
+  //     salary: elements[i].getAttribute('data-salary'),
+  //     age: elements[i].getAttribute('data-age'),
+  //   });
+   [...elements].map(element => ({
+    name: element.innerText,
+    position: element.getAttribute('data-position'),
+    salary: element.getAttribute('data-salary'),
+    age: element.getAttribute('data-age'),
+  }));
 
-  return employeesList;
+
+  // return employeesList;
+// }
+
+// function salaryToNumber(string) {
+//   return string.slice(1).replaceAll(',', '');
+// }
+
+// getEmployees(list);
+
+function curry(firstCallback, secondCallback) {
+  return function (...args) {
+    return secondCallback(firstCallback(...args));
+  };
 }
 
-function salaryToNumber(string) {
-  return string.slice(1).replaceAll(',', '');
-}
+const sortAndGetEmployees = curry(sortList, getEmployees);
 
-getEmployees(list);
+sortAndGetEmployees(list);
