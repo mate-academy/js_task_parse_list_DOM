f73d4a2060ccbf005f4459988752f8725a1afe95
Karolina <karolinabednarek888@gmail.com>
2023-07-26 16:40:05 +0200

    Fixing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function convertToNumber(salary) {
-  const cleanedString = salary.replace(/[$,]/g, '');
-  const number = Number(cleanedString);
+  const number = Number(salary.replace(/[$,]/g, ''));
 
   return number;
 }
@@ -11,28 +10,25 @@ const ul = document.querySelector('ul');
 const liArr = [...ul.querySelectorAll('li')];
 
 function sortList(propName) {
+  ul.innerHTML = '';
+
   liArr.sort((a, b) => {
     const salaryB = convertToNumber(b.dataset[propName]);
     const salaryA = convertToNumber(a.dataset[propName]);
 
     return salaryB - salaryA;
-  });
-
-  ul.innerHTML = '';
-  liArr.forEach(li => ul.append(li));
+  }).forEach(li => ul.append(li));
 }
 
 sortList('salary');
 
 function getEmployees() {
-  const result = liArr.map(item => {
-    return {
-      name: item.textContent.trim(),
-      position: item.dataset.position,
-      salary: item.dataset.salary,
-      age: item.dataset.age,
-    };
-  });
+  const result = liArr.map(item => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
 
   return result;
 }
