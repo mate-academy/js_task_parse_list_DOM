e7cd1e19452d36249bebb9188886b288199d3606
Volodymyr Biletskyi <volodymyrbiletskyi92@gmail.com>
2023-07-30 22:09:15 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+function convertToNumber(salaryString) {
+  return Number(salaryString.replace(/[^0-9.-]+/g, ''));
+}
+
+function sortList(listProperty) {
+  const list = document.querySelectorAll(listProperty);
+  const items = Array.from(list);
+
+  items.sort((a, b) => {
+    const salaryA = convertToNumber(a.dataset.salary);
+    const salaryB = convertToNumber(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  document.querySelector('ul').innerHTML = '';
+  document.querySelector('ul').append(...items);
+}
+
+function getEmployees(listProperty) {
+  const list = document.querySelectorAll(listProperty);
+
+  const employees = Array.from(list).map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: convertToNumber(item.dataset.salary),
+      age: Number(item.dataset.age),
+    };
+  });
+
+  return employees;
+}
+
+sortList('li');
+
+getEmployees('li');
