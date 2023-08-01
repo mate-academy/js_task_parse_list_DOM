badd7c80ca0401c403e3190bbe0162db11a66837
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-08-01 16:19:34 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const listElement = document.querySelector('ul');
+
+const parseSalary = (salary) => {
+  const parsedSalary = salary.replace('$', '').replace(',', '');
+
+  try {
+    return Number(parsedSalary);
+  } catch (error) {
+    return 0;
+  }
+};
+
+const sortList = (list) => {
+  const listElements = Array.from(list.children);
+
+  listElements.sort((a, b) =>
+    parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary));
+
+  list.innerHTML = '';
+
+  listElements.forEach((element) => list.appendChild(element));
+};
+
+const getEmployees = (list) => {
+  return [...list.children].map((employee) => ({
+    name: employee.innerText,
+    position: employee.dataset.position,
+    salary: employee.dataset.salary,
+    age: employee.dataset.age,
+  }));
+};
+
+sortList(listElement);
+getEmployees(listElement);
