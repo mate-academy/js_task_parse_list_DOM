bc5d5deddab922d1a39e9234678607b1cbc09a7e
Edgar Kocuba <edgarkocuba@gmail.com>
2023-08-14 17:58:42 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write code here
+const listElement = document.querySelectorAll('li');
+const employees = [...listElement];
+
+function makeNumber(string) {
+  return Number(string.attributes[1].value.replace(/\$|,/g, ''));
+}
+
+function getEmployees(list) {
+  return list.sort((a, b) => makeNumber(b) - makeNumber(a));
+}
+
+getEmployees(employees);
