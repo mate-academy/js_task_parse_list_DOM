5cd333d5e3c79f90115cb72c0bfc01121e0b8f67
Edgar Kocuba <edgarkocuba@gmail.com>
2023-08-15 09:23:02 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,36 @@
 'use strict';
 
-// write code here
 const listElement = document.querySelectorAll('li');
-const employees = [...listElement];
+const items = [...listElement];
 
-function makeNumber(string) {
-  return Number(string.attributes[1].value.replace(/\$|,/g, ''));
+function makeNumber(item) {
+  return Number(item.dataset.salary.replace(/\$|,/g, ''));
 }
 
-function getEmployees(list) {
-  return list.sort((a, b) => makeNumber(b) - makeNumber(a));
+function sortList(list) {
+  list.sort((a, b) => makeNumber(b) - makeNumber(a));
+
+  const ulElement = list[0].parentNode;
+
+  ulElement.innerHTML = '';
+  ulElement.append(...list);
 }
 
-getEmployees(employees);
+function getEmployees(listProperty) {
+  const list = document.querySelectorAll(listProperty);
+
+  const employees = [...list].map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: makeNumber(item),
+      age: Number(item.dataset.age),
+    };
+  });
+
+  return employees;
+}
+
+sortList(items);
+
+getEmployees('li');
