13f49fc78e92c5173f8470bfd25cee3821eb733f
Eugene Kulish <genekulish13@gmail.com>
2023-09-12 16:42:49 +0300

    solve the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function sortList(listOfElements) {
+  const itemElements = listOfElements.querySelectorAll('li');
+
+  const sortedElements = [...itemElements].sort(
+    (element1, element2) => +element2.dataset.salary.replace(/\$|,/g, '')
+      - +element1.dataset.salary.replace(/\$|,/g, '')
+  );
+
+  listOfElements.append(...sortedElements);
+}
+
+function getEmployees(listOfElements) {
+  const itemElements = listOfElements.querySelectorAll('li');
+
+  return [...itemElements].map(element => {
+    const { position, salary, age } = element.dataset;
+
+    return {
+      name: element.textContent.trim(),
+      position,
+      salary,
+      age,
+    };
+  });
+}
+
+sortList(list);
+getEmployees(list);
