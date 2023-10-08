eb2b3fa7d6e4dff4bc79f5063143f750dc437054
Emil Owczarek <owczarekemil@gmail.com>
2023-10-08 22:46:05 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+const listFromDocument = document.querySelectorAll('li');
+
+function sortList(list) {
+  const arrList = Array.from(list);
+
+  arrList.sort(function(a, b) {
+    const salaryA = Number(a.dataset.salary.replace(/[$,]/g, ''));
+    const salaryB = Number(b.dataset.salary.replace(/[$,]/g, ''));
+
+    return salaryB - salaryA;
+  });
+
+  return arrList;
+}
+
+const sortedList = sortList(listFromDocument);
+
+const ulElement = document.querySelector('ul');
+
+ulElement.append(...sortedList);
