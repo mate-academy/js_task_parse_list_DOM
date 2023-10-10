e448c1d0e70fc178ab90ef645d9a3bc594c30735
david.shamiiev <david.shamiiev@rad4m.com>
2023-10-10 20:27:43 +0300

    minor fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,18 @@
 
 const allEmployers = [...document.querySelectorAll('li')];
 
+const getSalary = (el, attr) => {
+  return el.getAttribute(attr)
+    .slice(1)
+    .split(',')
+    .join('');
+};
+
+const attributeToGet = 'data-salary';
+
 const sortBySalaries = allEmployers.sort((a, b) => {
-  const salaryA = a.getAttribute('data-salary').slice(1).split(',').join('');
-  const salaryB = b.getAttribute('data-salary').slice(1).split(',').join('');
+  const salaryA = getSalary(a, attributeToGet);
+  const salaryB = getSalary(b, attributeToGet);
 
   return Number(salaryB) - Number(salaryA);
 });
@@ -15,15 +24,12 @@ list.innerHTML = '';
 
 sortBySalaries.forEach(worker => list.appendChild(worker));
 
-const getEmployees = () => {
-  return allEmployers.map(emp => {
-    return {
-      name: emp.innerText,
-      position: emp.getAttribute('data-position'),
-      salary: emp.getAttribute('data-salary'),
-      age: emp.getAttribute('data-age'),
-    };
-  });
-};
+const getEmployees = () =>
+  allEmployers.map(emp => ({
+    name: emp.innerText,
+    position: emp.getAttribute('data-position'),
+    salary: emp.getAttribute('data-salary'),
+    age: emp.getAttribute('data-age'),
+  }));
 
 getEmployees();
