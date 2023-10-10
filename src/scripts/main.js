2e23c69c087d13c2fcae83235ca14687d0a79945
david.shamiiev <david.shamiiev@rad4m.com>
2023-10-10 19:38:09 +0300

    solution fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const allEmployers = [...document.querySelectorAll('li')];
+
+const sortBySalaries = allEmployers.sort((a, b) => {
+  const salaryA = a.getAttribute('data-salary').slice(1).split(',').join('');
+  const salaryB = b.getAttribute('data-salary').slice(1).split(',').join('');
+
+  return Number(salaryB) - Number(salaryA);
+});
+
+const list = document.querySelector('ul');
+
+list.innerHTML = '';
+
+sortBySalaries.forEach(worker => list.appendChild(worker));
+
+const getEmployees = () => {
+  return allEmployers.map(emp => {
+    return {
+      name: emp.innerText,
+      position: emp.getAttribute('data-position'),
+      salary: emp.getAttribute('data-salary'),
+      age: emp.getAttribute('data-age'),
+    };
+  });
+};
+
+getEmployees();
