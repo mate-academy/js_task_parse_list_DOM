58fbc0a384592c380a48cbb05457cca0c2c8c616
david.shamiiev <david.shamiiev@rad4m.com>
2023-10-11 09:36:09 +0300

    changed variables names

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const allEmployers = [...document.querySelectorAll('li')];
+const allEmployees = [...document.querySelectorAll('li')];
 
 const getSalary = (el, attr) => {
   return el.getAttribute(attr)
@@ -11,25 +11,25 @@ const getSalary = (el, attr) => {
 
 const attributeToGet = 'data-salary';
 
-const sortBySalaries = allEmployers.sort((a, b) => {
-  const salaryA = getSalary(a, attributeToGet);
-  const salaryB = getSalary(b, attributeToGet);
+const sortedEmployeesBySalary = allEmployees.sort((emploee1, emploee2) => {
+  const salary1 = getSalary(emploee1, attributeToGet);
+  const salary2 = getSalary(emploee2, attributeToGet);
 
-  return Number(salaryB) - Number(salaryA);
+  return Number(salary2) - Number(salary1);
 });
 
 const list = document.querySelector('ul');
 
 list.innerHTML = '';
 
-sortBySalaries.forEach(worker => list.appendChild(worker));
+sortedEmployeesBySalary.forEach(worker => list.appendChild(worker));
 
-const getEmployees = () =>
-  allEmployers.map(emp => ({
+const getEmployee = () =>
+  allEmployees.map(emp => ({
     name: emp.innerText,
     position: emp.getAttribute('data-position'),
     salary: emp.getAttribute('data-salary'),
     age: emp.getAttribute('data-age'),
   }));
 
-getEmployees();
+getEmployee();
