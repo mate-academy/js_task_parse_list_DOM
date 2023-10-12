ad55bb69a5467036f8c6d2b55f99d1e994758b63
AndriiYelieva <andrew.yelieva@gmail.com>
2023-10-12 16:37:22 +0300

    completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const listElement = document.querySelector('ul');
+const listItems = document.querySelectorAll('li');
+
+const listArray = [...listItems];
+
+const sortList = (list) => {
+  list.sort((a, b) => {
+    const first = parseInt(a.dataset.salary.replace(/\D/g, ''));
+    const second = parseInt(b.dataset.salary.replace(/\D/g, ''));
+
+    return second - first;
+  });
+};
+
+sortList(listArray);
+
+const addList = (list) => {
+  list.map(element => {
+    listElement.append(element);
+  });
+};
+
+addList(listArray);
