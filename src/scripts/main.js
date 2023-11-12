098ce34a5e55a66ae68a6ebd55daefa8de49a315
Marek Maciejewski <marekmaciejewski1982@gmail.com>
2023-11-12 19:48:12 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+
+const sortList = (list) => {
+  [...list.querySelectorAll('li')].sort((a, b) =>
+    Number(b.dataset.salary.slice(1).split(',').join(''))
+  - Number(a.dataset.salary.slice(1).split(',').join('')))
+    .map(e => list.append(e));
+};
+
+const getEmployees = (list) => {
+  const employees = [];
+
+  [...list.querySelectorAll('li')].map(e => employees.push({
+    name: e.innerText,
+    position: e.dataset.position,
+    salary: e.dataset.salary,
+    age: e.dataset.age,
+  }));
+
+  return employees;
+};
+
+getEmployees(employeesList);
+sortList(employeesList);
