e86e6ce66ba379193b034d8f89183c4bf4d6e4bc
Liliya Kalinichenko <fedotova.liliya90@gmail.com>
2023-11-13 16:28:05 +0200

    task solution was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const employees = document.querySelectorAll('li');
+
+function sortList() {
+  [...employees]
+    .sort((empA, empB) =>
+      toNumber(empA.dataset.salary) - toNumber(empB.dataset.salary))
+    .reverse()
+    .map(emp => list.append(emp));
+}
+
+function getEmployees() {
+  return [...employees].map(person => ({
+    name: person.textContent.trim(),
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+}
+
+sortList();
+getEmployees();
+
+function toNumber(str) {
+  return parseFloat(str.slice(1).replaceAll(',', ''));
+}
