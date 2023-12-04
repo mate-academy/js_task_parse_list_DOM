20fdf55387c6c66e2025e78e181e1fe4adf5bc45
Andrey Bondar <andrey_bondar@outlook.com>
2023-12-04 19:33:22 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const lists = document.querySelectorAll('li');
+const elemUl = document.querySelector('ul');
+
+function sortList(nodes) {
+  elemUl.innerHTML = '';
+
+  const sortedList = [...nodes].sort((li1, li2) => {
+    const salary1 = convertSalary(li1.dataset.salary);
+    const salary2 = convertSalary(li2.dataset.salary);
+
+    return salary2 - salary1;
+  });
+
+  sortedList.forEach(node => {
+    elemUl.appendChild(node);
+  });
+}
+
+function getEmployees(nodes) {
+  return [...nodes].map(li => {
+    const example = li.dataset;
+
+    return {
+      ...example,
+      name: li.innerText,
+    };
+  });
+}
+
+function convertSalary(salary) {
+  return +salary.slice(1).replace(',', '');
+}
+
+getEmployees(lists);
+sortList(lists);
