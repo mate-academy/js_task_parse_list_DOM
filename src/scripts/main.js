39d7aaf2ddf1e279b756d274d59ce8631ab004d1
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-12-08 18:21:27 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+function convertSalaryToNumber(salaryString) {
+  return Number(salaryString.replace(/[$,]/g, ''));
+}
+
+function sortList(list) {
+  const items = Array.from(list.children);
+
+  items.sort((a, b) => {
+    const salaryA = convertSalaryToNumber(a.dataset.salary);
+    const salaryB = convertSalaryToNumber(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  const items = Array.from(list.children);
+
+  return items.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: convertSalaryToNumber(item.dataset.salary),
+    age: Number(item.dataset.age),
+  }));
+}
+
+const employeeList = document.querySelector('ul');
+
+sortList(employeeList);
+getEmployees(employeeList);
