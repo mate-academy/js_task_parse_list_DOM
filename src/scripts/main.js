1d84181b272e189bedf1ca65a31f6e71ad383238
Alina Kabanets <kabanets.dolls@gmail.com>
2023-12-15 17:52:22 +0000

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const employees = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+const employeesList = [...employees];
+
+function getSalary(person) {
+  return person.dataset.salary.slice(1).replaceAll(',', '');
+}
+
+function sortList() {
+  return employeesList.sort((a, b) => getSalary(b) - getSalary(a))
+    .forEach(person => list.append(person));
+}
+
+function getEmployees() {
+  return employeesList.map(person => ({
+    name: person.innerText,
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+}
+
+sortList();
+getEmployees();
