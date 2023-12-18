d0ac7a68b4454396fedd8c18e5c7ed11ed95960b
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-18 20:56:41 +0200

    Revert "parse list solution"
    This reverts commit 1f6e3914b0156849a3fd370903fa53382cb06228.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,3 @@
 'use strict';
 
-const listOfEmployees = document.querySelector('ul');
-const employees = [...document.querySelectorAll('li')];
-
-function sortList(list) {
-  const listToNumber = salary => +salary.slice(1).split(',').join('');
-
-  const sorted = list.sort((a, b) =>
-    listToNumber(b.dataset.salary) - listToNumber(a.dataset.salary));
-
-  listOfEmployees.append(...sorted);
-}
-
-function getEmployees(list) {
-  return list.map(employee => {
-    return {
-      name: employee.innerText,
-      position: employee.dataset.position,
-      salary: employee.dataset.salary,
-      age: employee.dataset.age,
-    };
-  });
-}
-
-sortList(employees);
-getEmployees(employees);
+// write code here
