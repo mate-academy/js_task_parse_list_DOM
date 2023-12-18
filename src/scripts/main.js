68b3905f8f0f86add599355617f1deeb7efc3a39
Rodionsav <rodionsav@mate.academy>
2023-12-18 15:39:45 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const items = document.querySelectorAll('li');
+
+function sortList(array) {
+  return [...array].sort((a, b) => {
+    const valueA = parseFloat(a.dataset.salary.replace(/[^\d.]/g, ''));
+    const valueB = parseFloat(b.dataset.salary.replace(/[^\d.]/g, ''));
+
+    return valueB - valueA;
+  },
+  );
+}
+
+function getEmployees(array) {
+  return [...array].map(item => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: +item.dataset.age,
+  }));
+}
+
+getEmployees(items);
+
+const newList = sortList(items);
+
+list.innerHTML = `
+  <ul>
+    ${newList.map(item => `
+      <li
+        data-position=${item.dataset.position}
+        data-salary=${item.dataset.salary}
+        data-age=${item.dataset.age}
+      >
+        ${item.textContent}
+      </li>
+    `).join('')}
+  </ul>
+`;
