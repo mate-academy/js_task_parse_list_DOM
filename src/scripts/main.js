8e425c082934ff4804ad05adda0dcc2748f3b6bc
UlianaChorna <110450537+UlianaChorna@users.noreply.github.com>
2024-01-02 14:51:50 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const mainList = document.querySelector('ul');
+const listOfEmployees = document.querySelectorAll('li');
+
+const toNumber = (str) => {
+  return +str.dataset.salary.slice(1).split(',').join('');
+};
+
+const sortList = (item) => {
+  const sort = [...item].sort((a, b) =>
+    toNumber(b) - toNumber(a));
+
+  mainList.append(...sort);
+};
+
+const getEmployees = (property) => {
+  const arr = [];
+
+  [...property].forEach(employee => {
+    const obj = {};
+
+    obj.name = employee.dataset.innerText;
+    obj.position = employee.dataset.position;
+    obj.salary = employee.dataset.salary;
+    obj.age = employee.dataset.age;
+
+    arr.push(obj);
+  });
+};
+
+getEmployees(listOfEmployees);
+sortList(listOfEmployees);
