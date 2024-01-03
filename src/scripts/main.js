2e287c14f5cbdbaba4494cee29a9ce08b4dbf909
Kseniia Chepur <kseniia.chepur@gmail.com>
2024-01-03 15:29:26 +0200

    implement solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const listEl = document.querySelector('ul');
+
+const sortList = (list) => {
+  [...list.querySelectorAll('li')]
+    .sort((a, b) => b.dataset.salary.slice(1).replace(',', '')
+    - a.dataset.salary.slice(1).replace(',', ''))
+    .forEach(li => list.appendChild(li));
+};
+
+const getEmployees = (list) => {
+  const employees = [];
+
+  [...list.querySelectorAll('li')]
+    .forEach(li => employees.push({
+      name: li.innerText,
+      position: li.dataset.position,
+      salary: li.dataset.salary,
+      age: li.dataset.age,
+    }));
+
+  return employees;
+};
+
+sortList(listEl);
+getEmployees(listEl);
