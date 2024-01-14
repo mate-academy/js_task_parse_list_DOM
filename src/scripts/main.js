0889f145b504951e80d9043e83a2927a9732aa93
Robert Szałapak <robsza1212@gmail.com>
2024-01-14 15:45:18 +0100

    good order of list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,62 @@
 'use strict';
 
-// write code here
+const fullList = document.querySelector('ul');
+const elementOfFullList = document.querySelectorAll('li');
+
+const gettingEmployees
+ = (list) => [...list].map((item) => ({
+   name: item.textContent,
+   postion: item.getAttribute('data-position'),
+   salary: item.getAttribute('data-salary'),
+   age: item.getAttribute('data-age'),
+ }));
+
+const gettingSalaries = (salary) => {
+  return +salary.getAttribute('data-salary').replace(/[$,]/g, '');
+};
+
+const sortingEmployees = (list) => {
+  const sortedList = [...list].sort((x, y) => {
+    return gettingSalaries(y) - gettingSalaries(x);
+  });
+
+  sortedList.forEach((item) => {
+    fullList.append(item);
+  });
+};
+
+while (fullList.firstChild) {
+  fullList.removeChild(fullList.firstChild);
+};
+
+gettingEmployees(elementOfFullList);
+sortingEmployees(elementOfFullList);
+
+// 'use strict';
+
+// const getEmployees = (list) => [...list].map(item => ({
+//   name: item.textContent.trim(),
+//   position: item.getAttribute('data-position'),
+//   salary: item.getAttribute('data-salary'),
+//   age: item.getAttribute('data-age'),
+// }));
+
+// const getSalary = (item) => {
+//   return +item.getAttribute('data-salary').replace(/[$,]/g, '');
+// };
+
+// const sortList = (list) => [...list].sort(
+//   (x, y) => getSalary(y) - getSalary(x));
+
+// const sortedListItems = sortList(document.querySelectorAll('li'));
+// const ulElement = document.querySelector('ul');
+
+// while (ulElement.firstChild) {
+//   ulElement.removeChild(ulElement.firstChild);
+// }
+
+// sortedListItems.forEach((item) => {
+//   ulElement.append(item);
+// });
+
+// getEmployees(document.querySelectorAll('li'));
