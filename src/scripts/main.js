b4708b6c8cab785414b87b8ccab3ba9ebe1c1814
Siarhei Tsishkavets <kenny.sergey@gmail.com>
2024-01-17 19:51:13 +0100

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const myList = document.querySelector('ul');
+
+const sortList = function(list) {
+  const listElements = Array.prototype.slice.call(list.children);
+
+  const sortListElements = listElements.sort((a, b) => {
+    const solaryA = +a.dataset.salary.slice(1).replaceAll(',', '');
+    const solaryB = +b.dataset.salary.slice(1).replaceAll(',', '');
+
+    if (solaryA > solaryB) {
+      return -1;
+    }
+
+    if (solaryA < solaryB) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  list.innerHTML = '';
+
+  sortListElements.forEach(element => {
+    list.appendChild(element);
+  });
+};
+
+sortList(myList);
