03af45fbe6ac0bce56f2581f6054d7de1e82e9a7
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-01-18 19:33:49 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+const ul = document.querySelector('ul');
+
+function convertToNumber(text) {
+  return +(text.replace('$', '').replace(',', ''));
+}
+
+const sortList = (input) => {
+  return [...input].sort((item1, item2) => {
+    if (convertToNumber(item1.dataset.salary)
+      === convertToNumber(item2.dataset.salary)) {
+      return item1.innerText.localeCompare(item2.innerText);
+    }
+
+    return convertToNumber(item1.dataset.salary)
+      < convertToNumber(item2.dataset.salary) ? 1 : -1;
+  });
+};
+
+const sortedList = sortList(list);
+
+ul.innerHTML = `
+  ${sortedList.map(item => `
+    <li
+      data-position={item.dataset.position}
+      data-salary={item.dataset.salary}
+      data-age={item.dataset.age}
+    >
+      ${item.innerText}
+    </li>
+  `).join('')}
+`;
+
+const getEmployees = (input) => {
+  return [...input].map(item => (
+    {
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    }
+  ));
+};
+
+getEmployees(list);
