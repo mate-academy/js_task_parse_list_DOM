fd3854fcde89078b04ded756579d6d37f30840bd
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-01-28 19:31:18 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,60 @@
+/* eslint-disable max-len */
 'use strict';
 
-// write code here
+addEventListener('DOMContentLoaded', () => {
+  const sortEmployeesInit = () => {
+    const listBlock = document.querySelector('ul');
+
+    if (listBlock) {
+      const items = listBlock.querySelectorAll('li');
+
+      // get salary
+      const getSalary = (salary) => +salary.replace('$', '').replace(',', '');
+
+      // get employees
+      const getEmployees = (employees) => {
+        const arr = [];
+
+        employees.forEach((employee) => {
+          const employeePosition = employee.dataset.position;
+          const employeeSalary = employee.dataset.salary;
+          const employeeAge = employee.dataset.age;
+          const employeeName = employee.textContent.trim();
+
+          arr.push({
+            position: employeePosition,
+            salary: employeeSalary,
+            age: employeeAge,
+            fullName: employeeName,
+          });
+        });
+
+        return arr;
+      };
+
+      // sort list
+      const sortList = (list) => {
+        const employees = getEmployees(list);
+        const sortedEmployees = employees.sort((a, b) => {
+          const aSalary = getSalary(a.salary);
+          const bSalary = getSalary(b.salary);
+
+          return bSalary - aSalary;
+        });
+
+        listBlock.innerHTML = '';
+
+        sortedEmployees.forEach(({ position, salary, age, fullName }) => {
+          const li = `<li data-position="${position}" data-age="${salary}" data-age="${age}">${fullName}</li>`;
+
+          listBlock.innerHTML += li;
+        });
+      };
+
+      // run func
+      sortList(items);
+    }
+  };
+
+  sortEmployeesInit();
+});
