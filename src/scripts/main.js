69a26b39b9f5bcb448dfac78ef3d245ac0f5f380
Misha Siv <0Sivcov0@gmail.com>
2024-02-01 18:32:40 +0200

    add list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const items = document.querySelectorAll('li');
+
+function sortList() {
+  return [...items].sort((a, b) => (
+    +b.dataset.salary.replace(/[^\d]/g, '')
+    - +a.dataset.salary.replace(/[^\d]/g, '')
+  ));
+}
+
+function getEmployees() {
+  const sortedEmployees = sortList(items);
+
+  return (
+    list.innerHTML = sortedEmployees.map(item => (`
+    <li
+      data-position="${item.dataset.position}"
+      data-salary="${item.dataset.salary}"
+      data-age="${item.dataset.age}"
+    >
+      ${item.textContent}
+    </li>
+  `)).join('')
+  );
+}
+
+getEmployees(list);
