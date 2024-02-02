17d9bfd41dc07a358df2775d30c937ad0f4a0817
Arkadiusz <stefanski.a00@gmail.com>
2024-02-02 05:50:22 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+function convertToNumber(string) {
+  return parseFloat(string.replace(/[^0-9]/g, ''));
+}
+
+const list = document.querySelector('ul');
+const listItems = Array.from(document.querySelectorAll('ul li'));
+
+function sortList() {
+  listItems.sort((a, b) => {
+    const salaryA = convertToNumber(a.dataset.salary);
+    const salaryB = convertToNumber(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  list.innerHTML = '';
+
+  listItems.forEach(item => list.appendChild(item));
+}
+
+sortList();
