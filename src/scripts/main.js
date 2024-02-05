ab2ff8e14359bc90aecf8ec71362a519558e09ca
Maryna Kyrychynska <m_kirichok@ukr.net>
2024-02-05 14:09:49 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+function convertToNumber(salary) {
+  const number = parseFloat(salary.replace('$', '').replaceAll(',', ''));
+
+  return number;
+}
+
+const mainList = document.querySelector('ul');
+
+function sortList(list) {
+  [...list.querySelectorAll('li')]
+    .sort((a, b) => {
+      const convertA = convertToNumber(a.dataset.salary);
+      const convertB = convertToNumber(b.dataset.salary);
+
+      return convertB - convertA;
+    })
+    .forEach(li => list.appendChild(li));
+}
+
+function getEmploees(list) {
+  [...list.querySelectorAll('li')]
+    .map(item => ({
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    }));
+}
+
+sortList(mainList);
+getEmploees(mainList);
