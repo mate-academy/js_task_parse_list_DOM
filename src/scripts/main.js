fa36a62fce5da5d9e5c675b7125a2ddfe9742dea
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-02-15 11:22:36 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const liList = document.querySelectorAll('li');
+
+function parseSalary(str) {
+  return parseFloat(str.replace(/[^\d.-]/g, ''));
+}
+
+function sortList(list) {
+  const sortedList = [...list].sort((li1, li2) => {
+    const salary1 = parseSalary(li1.dataset.salary);
+    const salary2 = parseSalary(li2.dataset.salary);
+
+    return salary2 - salary1;
+  });
+
+  const dad = list[0].parentNode;
+
+  dad.innerHTML = '';
+  sortedList.forEach(item => dad.appendChild(item));
+}
+
+function getEmployees(list) {
+  return [...list].map(person => {
+    return {
+      name: person.textContent.trim(),
+      position: person.dataset.position,
+      salary: person.dataset.salary,
+      age: person.dataset.age,
+    };
+  });
+}
+
+sortList(liList);
+getEmployees(liList);
