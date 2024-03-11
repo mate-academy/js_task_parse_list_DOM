e3055cdfbf8635b5a6e0041ab8ef86e4ab8897c7
Justin <thedarkvibe1@gmail.com.com>
2024-03-11 17:49:56 +0100

    task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+function sortList(elementList) {
+  return elementList.sort((a, b) => {
+    function getNum(listItem) {
+      return +listItem.dataset['salary'].replace('$', '').replace(',', '');
+    }
+
+    return getNum(b) - getNum(a);
+  });
+}
+
+function setNewList(elementList) {
+  const list = document.querySelector('ul');
+
+  elementList.forEach((element) => {
+    list.appendChild(element);
+  });
+}
+
+setNewList(sortList([...document.querySelectorAll('li')]));
