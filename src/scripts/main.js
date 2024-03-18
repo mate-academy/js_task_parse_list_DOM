5e88601745df7cc0fc051ed914e1bd6c446ab0ec
Vladyslav Filonenko <oladik05@gmail.com>
2024-03-18 22:12:01 +0200

    add parse list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const nodeList = document.querySelectorAll('li');
+const listEmployees = [...nodeList].map((employee) => ({
+  name: employee.innerText,
+  position: employee.dataset.position,
+  salary: employee.dataset.salary.slice(1).split(',').join(''),
+  age: employee.dataset.age,
+}));
+
+listEmployees.sort((a, b) => b.salary - a.salary);
+
+for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].innerText = listEmployees[i].name;
+  nodeList[i].dataset.position = listEmployees[i].position;
+  nodeList[i].dataset.salary = listEmployees[i].salary;
+  nodeList[i].dataset.age = listEmployees[i].age;
+}
