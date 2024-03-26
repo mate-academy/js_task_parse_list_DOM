b98c69b66f8eda19a216c3152464f0f3face29a3
Valeria Oleynik <oleynik.valeria27@gmail.com>
2024-03-26 16:23:15 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const listElements = document.querySelectorAll('li');
+const listEmployees = [];
+
+listElements.forEach((elem) => {
+  const person = {
+    nameEmployee: elem.innerText,
+    position: elem.dataset.position,
+    salary: elem.dataset.salary,
+    age: elem.dataset.age,
+  };
+
+  listEmployees.push(person);
+});
+
+const getNumber = (value) => {
+  return Number(value.slice(1).split(',').join(''));
+};
+
+function sortList() {
+  listEmployees.sort(
+    (first, second) => getNumber(second.salary) - getNumber(first.salary),
+  );
+}
+
+function getEmployees() {
+  listElements.forEach((elem, index) => {
+    const { position, salary, age, nameEmployee } = listEmployees[index];
+
+    elem.dataset.position = position;
+    elem.dataset.salary = salary;
+    elem.dataset.age = age;
+    elem.innerText = nameEmployee;
+  });
+}
+
+sortList();
+getEmployees();
