01c62f46c3c55d318c5ef6c9677f491e7e1d8665
Yehor <eee.houmich@gmail.com>
2024-03-29 11:32:02 +0200

    Complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const employees = document.querySelectorAll('li');
+
+// Функція для перетворення рядка на число
+const toNumber = (str) => parseFloat(str.slice(1).replaceAll(',', ''));
+
+const sortList = () => {
+  [...employees]
+    .sort((a, b) => toNumber(b.dataset.salary) - toNumber(a.dataset.salary))
+    .forEach((employee) => list.append(employee));
+};
+
+const getEmployees = () =>
+  [...employees].map((person) => ({
+    name: person.textContent.trim(),
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+
+sortList();
+getEmployees();
