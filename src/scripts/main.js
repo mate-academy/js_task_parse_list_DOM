1fe5811f0f42b72b1e29bdd5223d2fbfe88a8489
Emil Sukiennik <ewilemil@gmail.com>
2024-04-02 23:13:14 +0200

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listItems = document.querySelectorAll('li');
+
+const sortList = () => {
+  [...listItems]
+    .sort((a, b) => toNumber(b.dataset.salary) - toNumber(a.dataset.salary))
+    .map((employee) => list.append(employee));
+};
+
+const toNumber = (salary) => +salary.replace(/[$,]/g, '');
+
+const getEmployees = () => {
+  [...listItems].map((employee) => ({
+    name: employee.textContent,
+    position: employee.dataset.position,
+    salary: employee.dataset.salary,
+    age: employee.dataset.age,
+  }));
+};
+
+sortList();
+getEmployees();
