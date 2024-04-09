00fcc1aa89fc96bb5fb59e88ba87465326cf2223
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-04-09 21:25:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const items = [...list.querySelectorAll('li')];
+
+function convertToNumber(salary) {
+  return +salary.replace(/[$,]/g, '');
+}
+
+function sortList(property) {
+  items.sort((a, b) => {
+    const salaryA = convertToNumber(a.dataset[property]);
+    const salaryB = convertToNumber(b.dataset[property]);
+
+    return salaryB - salaryA;
+  });
+
+  list.innerHTML = '';
+  items.forEach((li) => list.insertBefore(li, null));
+}
+
+function getEmployees() {
+  const employees = items.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    };
+  });
+
+  return employees;
+}
+
+sortList('salary');
+getEmployees();
