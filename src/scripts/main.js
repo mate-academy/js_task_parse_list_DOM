f706b3cb662e2a88f496e75543b7b7305d687400
Bohdan Liutyi <liutyibohdan13@gmail.com>
2024-04-16 22:39:25 +0300

    sorted list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const fullList = document.querySelector('ul');
+const employees = document.querySelectorAll('li');
+
+function convertToNumber(number) {
+  return +number.slice(1).replaceAll(',', '');
+}
+
+function sortList(list) {
+  return [...list].sort((list1, list2) => {
+    return (
+      convertToNumber(list2.dataset.salary) -
+      convertToNumber(list1.dataset.salary)
+    );
+  });
+}
+
+const sortedList = sortList(employees);
+
+fullList.textContent = '';
+
+sortedList.map((item) => fullList.append(item));
