ae44bb30e8defe5151c4ba215d322587387dfcf5
Nataliia Miroshnyk <natamrshn@gmail.com>
2024-04-22 10:43:48 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write code here
+const listOfEmployees = document.querySelector('ul');
+const employees = document.querySelectorAll('li');
+const newListOfEmployees = [...employees];
+
+function getSalary(salaryString) {
+  return Number(salaryString.slice(1).replaceAll(',', ''));
+}
+
+function sortList() {
+  return newListOfEmployees
+    .sort((a, b) => getSalary(b.dataset.salary) - getSalary(a.dataset.salary))
+    .forEach((employee) => listOfEmployees.append(employee));
+}
+
+function getEmployees() {
+  return newListOfEmployees.map((employee) => ({
+    name: employee.innerText,
+    position: employee.dataset.position,
+    salary: employee.dataset.salary,
+    age: employee.dataset.age,
+  }));
+}
+
+sortList();
+getEmployees();
