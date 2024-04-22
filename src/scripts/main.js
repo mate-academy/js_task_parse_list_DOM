d042e7dad60ecf9ac3b3ae4510e5727882bd0c26
Nazar Pastuschak <kns19pna@gmail.com>
2024-04-22 22:50:17 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+let li = document.querySelectorAll('li');
+
+function sortList(list) {
+  const sortedList = Array.from(list).sort((a, b) => {
+    const salaryA = parseFloat(a.getAttribute('data-salary'));
+    const salaryB = parseFloat(b.getAttribute('data-salary'));
+
+    return salaryB - salaryA;
+  });
+
+  return sortedList;
+}
+
+function getEmployees(list) {
+  const employees = [];
+
+  list.forEach((item) => {
+    const employee = {
+      name: item.textContent.trim().split(' ')[0],
+      position: item.getAttribute('data-position'),
+      salary: item.getAttribute('data-salary'),
+      age: item.getAttribute('data-age'),
+    };
+
+    employees.push(employee);
+  });
+
+  return employees;
+}
+
+getEmployees(li);
+li = sortList(li);
