ee866fd1b39ac2a7f45a36d5e6869e4eedc6ff8e
VolodymyrKolisnichenko <90705859+vk-workshop@users.noreply.github.com>
2024-05-02 19:05:35 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
+/* eslint-disable function-paren-newline */
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('li')];
+
+const employeesObj = [];
+
+function prepareNumber(param) {
+  return param.replace('$', '').replace(',', '');
+}
+
+employees.forEach((e) =>
+  employeesObj.push({
+    name: e.textContent.trim(),
+    position: e.getAttribute('data-position'),
+    salary: prepareNumber(e.getAttribute('data-salary')),
+    age: e.getAttribute('data-age'),
+  }),
+);
+
+function sortList(list) {
+  list.sort((a, b) => b.salary - a.salary);
+
+  for (let i = 0; i < list.length; i++) {
+    employees[i].textContent = list[i].name;
+  }
+}
+
+function getEmployees() {
+  return employeesObj;
+}
+
+sortList(employeesObj);
+getEmployees();
