595218879c16806e8e8f717db859433282049a22
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-05-06 22:26:21 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const listOfEmployees = document.querySelectorAll('li');
+
+const getSalary = (employee) => {
+  return parseInt(employee.dataset.salary.replace(/[$,]/g, ''), 10);
+};
+
+const sortList = (list) => {
+  const newList = [...list];
+
+  return newList.sort((employee1, employee2) => {
+    return getSalary(employee2) - getSalary(employee1);
+  });
+};
+
+const getEmployees = (list) => {
+  const newList = [...list];
+
+  return newList.map((employee) => {
+    return {
+      name: employee.outerText,
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    };
+  });
+};
+
+sortList(listOfEmployees);
+getEmployees(listOfEmployees);
+
+document.querySelector('ul').prepend(...sortList(listOfEmployees));
