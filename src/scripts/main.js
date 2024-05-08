82b0467364ca6cfb9e695bdbffbfdff6f81be5ec
Dmytro_Krylov <dmutrokrulov@gmail.com>
2024-05-08 12:32:42 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const ulEl = document.querySelector('ul');
+const liEl = [...document.querySelectorAll('li')];
+
+const sortList = (list) => {
+  list.sort((a, b) => {
+    const salaryA = +a.dataset.salary.replaceAll('$', '').replaceAll(',', '');
+    const salaryB = +b.dataset.salary.replaceAll('$', '').replaceAll(',', '');
+
+    return salaryB - salaryA;
+  });
+
+  list.forEach((li) => ulEl.append(li));
+};
+
+const getEmployees = (list) => {
+  return list.map((e) => ({
+    name: e.textContent,
+    position: e.dataset.position,
+    salary: e.dataset.salary,
+    age: e.dataset.age,
+  }));
+};
+
+sortList(liEl);
+getEmployees(liEl);
