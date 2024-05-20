61f1e0c9a5ec033763fcf8c5bfff48cdd15137ba
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-05-20 12:00:21 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const fullList = document.querySelector('ul');
+const employees = document.querySelectorAll('li');
+
+function convertToNumber(element) {
+  return Number(element.slice(1).replaceAll(',', ''));
+}
+
+function sortList(elements) {
+  return Array.from(elements).sort(
+    (el1, el2) =>
+      convertToNumber(el2.dataset.salary) - convertToNumber(el1.dataset.salary),
+  );
+}
+
+const sortedList = sortList(employees);
+
+fullList.textContent = '';
+
+sortedList.map((item) => fullList.append(item));
