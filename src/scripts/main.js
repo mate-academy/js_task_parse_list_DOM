1a481d13cf0dfcc509e743c7ef2fa831f3c83fc3
zozulia.kirill <kirillzozulya95@gmail.com>
2024-05-28 13:52:28 +0300

    implement task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const listElements = document.querySelectorAll('ul li');
+const listArray = Array.from(listElements);
+
+function formatStringToNumber(string) {
+  return +string.replace(/[^0-9.-]+/g, '');
+}
+
+function sortList(list) {
+  let sortedList = [...list];
+
+  sortedList.sort((a, b) => {
+    const firstElement = formatStringToNumber(a.dataset.salary);
+    const secondElement = formatStringToNumber(b.dataset.salary);
+
+    return secondElement - firstElement;
+  });
+
+  const ul = document.querySelector('ul');
+  ul.innerHTML = '';
+
+  sortedList.forEach((item) => ul.appendChild(item));
+}
+
+function getEmployees(list) {
+  return list.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    };
+  });
+}
+
+sortList(listArray);
+console.log(getEmployees(listArray));
