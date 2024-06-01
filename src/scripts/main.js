636dfa12258b30c0c2bbd71abdb5b36377e7b44d
Roma <roma150269@gmail.com>
2024-06-01 14:25:52 +0300

    Develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const sortList = document.querySelector('ul');
+
+const getSalary = (salary) => {
+  return +salary.slice(1).replace(',', '');
+};
+
+const getEmployees = (value) => {
+  const arryList = Array.from(value.children);
+
+  arryList.sort(
+    (a, b) =>
+      getSalary(b.getAttribute('data-salary')) -
+      getSalary(a.getAttribute('data-salary')),
+  );
+
+  arryList.forEach((item) => value.appendChild(item));
+};
+
+getEmployees(sortList);
