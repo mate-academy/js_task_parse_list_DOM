4a435942b7f51757bf920aa3cc2c5fb894a2bcaa
Oksana Pokhvalenko <oksanapokhvalenko@gmail.com>
2024-06-05 18:57:46 +0200

    add soluution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const toNumber = (value) => {
+  return +value.dataset.salary.replace(/[$,]/g, '');
+};
+
+const sortList = (list) => {
+  const items = [...list.children].sort((a, b) => {
+    return toNumber(b) - toNumber(a);
+  });
+
+  items.forEach((el) => list.append(el));
+};
+
+const getEmployees = (list) => {
+  return [...list.children].map((el) => ({
+    name: el.innerHTML,
+    position: el.dataset.position,
+    salary: el.dataset.salary,
+    age: el.dataset.age,
+  }));
+};
+
+const element = document.querySelector('ul');
+
+sortList(element);
+getEmployees(element);
