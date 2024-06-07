b5dfcfdd2385fb4d2f8d865f1e3ce7e49612d34c
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-06-07 16:14:04 +0200

    js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+// getting an array of list items
+const list = [...document.getElementsByTagName('li')];
+
+// function for sorting values by a descending order (according to a salary)
+function sortList() {
+  // sorting by a descending order
+  list.sort((a, b) => parseInt(b.dataset.salary) - parseInt(a.dataset.salary));
+
+  const ul = document.getElementsByTagName('ul')[0];
+
+  // deleting all list items that were inside ul before
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+
+  // adding sorted list items inside ul
+  list.forEach((li) => {
+    ul.appendChild(li);
+  });
+}
+sortList(list);
+
+// function to get information about employees
+function getEmployees() {
+  list.map((person) => {
+    const initials = person.textContent; // 'initials' bc 'name' is a global val
+    const position = person.dataset.position;
+    const salary = person.dataset.salary;
+    const age = person.dataset.age;
+
+    return {
+      name: initials,
+      position,
+      salary,
+      age,
+    };
+  });
+}
+getEmployees(list);
