94e1e9805c8cd0ac827315d265355e269fba74ca
Viktor Talashko <viktortalashko@gmail.com>
2024-06-08 08:29:52 +0300

    my solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+const employeeList = document.querySelector('ul');
+
+if (employeeList) {
+  sortList(employeeList);
+  getEmployees(employeeList);
+}
+
+function sortList(list) {
+  const listItems = Array.from(list.querySelectorAll('li'));
+
+  listItems.sort((a, b) => {
+    const itemA = helper(a.getAttribute('data-salary'));
+    const itemB = helper(b.getAttribute('data-salary'));
+
+    return itemB - itemA;
+  });
+
+  const sortedHTML = listItems.map((item) => item.outerHTML).join('');
+
+  list.innerHTML = sortedHTML;
+}
+
+function helper(string) {
+  return Number(string.replace(/[^0-9.-]+/g, ''));
+}
+
+function getEmployees(list) {
+  const listItems = Array.from(list.querySelectorAll('li'));
+
+  listItems.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.getAttribute('data-position'),
+      salary: helper(item.getAttribute('data-salary')),
+      age: helper(item.getAttribute('data-age')),
+    };
+  });
+}
