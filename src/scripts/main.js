97ee3d8de87bb63b0860c66785724eda958228d1
Hanna Svirina <26anna07@ukr.net>
2024-06-09 20:22:30 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+function getCorrectSalary(salary) {
+  return +salary.replace('$', '').replace(',', '');
+}
+
+function sortListBySalary(list) {
+  const listOfElements = [...document.querySelectorAll('li')];
+
+  listOfElements.sort(
+    (a, b) =>
+      getCorrectSalary(b.dataset.salary) - getCorrectSalary(a.dataset.salary),
+  );
+
+  listOfElements.forEach((item) => list.appendChild(item));
+}
+
+function getArrayOfEmployees(list) {
+  const result = [];
+
+  list.querySelectorAll('li').forEach((element) => {
+    const person = {
+      name: element.innerText,
+      position: element.dataset.position,
+      salary: getCorrectSalary(element.dataset.salary),
+      age: element.dataset.age,
+    };
+
+    result.push(person);
+  });
+
+  return result;
+}
+
+sortListBySalary(document.querySelector('ul'));
+getArrayOfEmployees(document.querySelector('ul'));
