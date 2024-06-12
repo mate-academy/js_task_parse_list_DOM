81cf181e85caf0932bac828db24f25376bbc034c
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-06-12 12:37:26 +0300

    task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+
+const employeesItems = document.querySelectorAll('li');
+
+function stringToNumber(el) {
+  return parseInt(el.split(',').join('').replace(/\D/g, ''));
+}
+
+function sortList(list) {
+  return list.sort(
+    (employee1, employee2) =>
+      stringToNumber(employee2.dataset.salary) -
+      stringToNumber(employee1.dataset.salary),
+  );
+}
+
+function getEmployees(list) {
+  return list.map((employee) => {
+    return {
+      name: employee.innerText,
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    };
+  });
+}
+
+getEmployees(sortList([...employeesItems]));
+
+while (employeesList.firstChild) {
+  employeesList.removeChild(employeesList.firstChild);
+}
+
+sortList([...employeesItems]).forEach((item) => {
+  return employeesList.appendChild(item);
+});
