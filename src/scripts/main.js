c58d1a597ccaad37f8f36f2380287d0bb44cb121
Anastasiia <vasilisandreeva00@gmail.com>
2024-06-13 15:54:07 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const liElements = [...document.querySelectorAll('li')];
+
+const employes = liElements.map((li) => ({
+  name: li.innerText,
+  position: li.getAttribute('data-position'),
+  salary: convertsToNumber(li.getAttribute('data-salary')),
+  age: li.getAttribute('data-age'),
+}));
+
+function convertsToNumber(value) {
+  const number = parseFloat(value.slice(1));
+
+  return number;
+}
+
+function sortList(people) {
+  return people.sort((a, b) => b.salary - a.salary);
+}
+
+function getEmployees(people) {
+  return people.map((person) => ({
+    ...person,
+    salary: '$' + person.salary.toFixed(2),
+  }));
+}
+
+sortList(employes);
+getEmployees(employes);
