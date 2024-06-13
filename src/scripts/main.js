7d8f68c5ee0645d2a43c63bddb5f4a0a4e1ca65d
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2024-06-13 18:29:03 +0200

    created sortList and getEmpoloyees functions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write code here
+function convertToNumber(string) {
+  return parseInt(string.replace(/\$|,/g, ''), 10);
+}
+
+function sortList() {
+  const ul = document.querySelector('ul');
+  const list = Array.from(ul.children);
+
+  list.sort((a, b) => {
+    const aSalary = convertToNumber(a.dataset.salary);
+    const bSalary = convertToNumber(b.dataset.salary);
+
+    return bSalary - aSalary;
+  });
+
+  list.forEach((li) => ul.appendChild(li));
+}
+
+function getEmployees() {
+  const employees = [];
+
+  const list = Array.from(document.querySelectorAll('ul').children);
+
+  list.array.forEach((li) => {
+    const employee = {
+      name: li.textContent,
+      position: li.dataset.position,
+      salary: convertToNumber(li.dataset.salary),
+      age: li.dataset.age,
+    };
+
+    employees.push(employee);
+  });
+
+  return employees;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  sortList();
+});
+
+getEmployees();
