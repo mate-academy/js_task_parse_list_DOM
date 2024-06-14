969534455d5223372cf548755f5778e2013e5341
Misha Vynnyk <vinnikmisha94@gmail.com>
2024-06-14 17:57:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const listFromDocument = document.querySelectorAll('li');
+
+const parseSalary = (salaryString) => {
+  return Number(salaryString.replace(/[^0-9.-]+/g, ''));
+};
+
+const sortList = (list) => {
+  const listArray = Array.from(list);
+
+  listArray.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryA - salaryB;
+  });
+
+  const parentElement = list[0].parentNode;
+
+  parentElement.innerHTML = '';
+  listArray.forEach((item) => parentElement.appendChild(item));
+
+  return listArray;
+};
+
+const getEmployees = (list) => {
+  return Array.from(list).map((item) => ({
+    name: item.innerText,
+    position: item.dataset.position,
+    salary: parseSalary(item.dataset.salary),
+    age: Number(item.dataset.age),
+  }));
+};
+
+sortList(listFromDocument);
+
+const employees = getEmployees(sortList(listFromDocument));
+
+// eslint-disable-next-line no-console
+console.log(employees);
