b115f057a161f74a290d9e8323dbf0f78bc63cd1
Anastasiia <vasilisandreeva00@gmail.com>
2024-06-14 11:13:16 +0300

    fix sort function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,26 +5,31 @@ const liElements = [...document.querySelectorAll('li')];
 const employes = liElements.map((li) => ({
   name: li.innerText,
   position: li.getAttribute('data-position'),
-  salary: convertsToNumber(li.getAttribute('data-salary')),
+  salary: li.getAttribute('data-salary'),
   age: li.getAttribute('data-age'),
 }));
 
 function convertsToNumber(value) {
-  const number = parseFloat(value.slice(1));
-
-  return number;
+  return parseFloat(value.replace(/[^0-9.-]+/g, ''));
 }
 
 function sortList(people) {
-  return people.sort((a, b) => b.salary - a.salary);
+  return people.sort((a, b) => {
+    const person1Salary = convertsToNumber(a.salary);
+    const person2Salary = convertsToNumber(b.salary);
+
+    return person2Salary - person1Salary;
+  });
 }
 
 function getEmployees(people) {
   return people.map((person) => ({
     ...person,
-    salary: '$' + person.salary.toFixed(2),
+    // salary: '$' + person.salary,
   }));
 }
 
 sortList(employes);
 getEmployees(employes);
+
+// console.log(employes)
