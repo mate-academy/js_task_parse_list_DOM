8587bb442f43eac153141f26675c2a5c24993fd1
Nazarii <Kyzenko.nr@gmail.com>
2024-06-17 16:10:26 +0300

    developTaskParseListCommitOne

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 // write code here
+const listOfEmployees = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  const ul = document.querySelector('ul');
+
+  list.sort((el1, el2) => {
+    return (
+      +el2.dataset.salary.slice(1).replace(',', '') -
+      +el1.dataset.salary.slice(1).replace(',', '')
+    );
+  });
+
+  list.forEach((li) => ul.append(li));
+}
+
+function getEmployees(list) {
+  return list.map((employee) => {
+    const { position, salary, age } = employee.dataset;
+
+    return {
+      name: employee.textContent.trim(),
+      position,
+      salary,
+      age,
+    };
+  });
+}
+
+sortList(listOfEmployees);
+getEmployees(listOfEmployees);
