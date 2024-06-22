94fb9b5786c804b22cd28d176eafd3e88945c8e4
luter <newgglutt@gmail.com>
2024-06-22 19:17:24 +0300

    add new

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('#employeeList > li');
+
+const getSalary = (item) => {
+  const salaryString = item.getAttribute('data-salary').replace(/[$,]/g, '');
+
+  return parseFloat(salaryString);
+};
+
+const sortList = (list) => {
+  return Array.from(list).sort((a, b) => getSalary(b) - getSalary(a));
+};
+
+const sortedList = sortList(listItems);
+
+const employeeList = document.getElementById('employeeList');
+
+employeeList.innerHTML = '';
+sortedList.forEach((item) => employeeList.appendChild(item));
+
+// const employees = getEmployees(sortedList);
+// console.log(employees);
