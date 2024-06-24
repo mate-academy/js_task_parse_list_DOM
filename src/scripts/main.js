fa91c36fd16ec5f8a843ce8a6ad50f4fe4c9912b
beliyandr <beliyandr@gmail.com>
2024-06-24 20:01:56 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
 // write code here
+'use strict';
+
+const elemsList = [...document.querySelectorAll('li')];
+const ulist = document.querySelector('ul');
+
+function getEmployees(list) {
+  return list.map((element) => {
+    return {
+      name: element.innerText,
+      position: element.dataset.position,
+      salary: element.dataset.salary,
+      age: element.dataset.age,
+    };
+  });
+}
+
+function sortList(list, ul) {
+  function converToNumber(str) {
+    return +str.replace(',', '').replace('$', '');
+  }
+
+  const sortLiForSalary = list.sort((a, b) => {
+    return converToNumber(a.dataset.salary) - converToNumber(b.dataset.salary);
+  });
+
+  sortLiForSalary.forEach((elem) => {
+    ul.insertAdjacentElement('afterbegin', elem);
+  });
+}
+
+getEmployees(elemsList);
+sortList(elemsList, ulist);
