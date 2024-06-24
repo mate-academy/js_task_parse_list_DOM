cb2380a8128843cd2228d034d7d15512a5160c00
Iryna Popovych <popovicirina40@gmail.com>
2024-06-24 14:56:18 +0100

    add sulution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+const employees = [];
+
+const mylist = document.getElementsByTagName('li');
+
+function getEmployees(list) {
+  for (const el of list) {
+    employees.push({
+      name: el.innerText,
+      position: el.dataset.position,
+      salary: Number(el.dataset.salary.replace(/[^0-9.-]+/g, '')),
+      age: Number(el.dataset.age),
+    });
+  }
+}
+
+function sortList(list) {
+  return list.sort((a, b) => b.salary - a.salary);
+}
+
+function renderResult() {
+  getEmployees(mylist);
+
+  const sortedEmployees = sortList(employees);
+
+  const container = document.querySelector('ul');
+
+  container.innerHTML = '';
+
+  for (const emp of sortedEmployees) {
+    const li = document.createElement('li');
+
+    li.dataset.position = emp.position;
+    li.dataset.salary = `$${emp.salary.toLocaleString()}`;
+    li.dataset.age = emp.age;
+    li.innerText = emp.name;
+    container.appendChild(li);
+  }
+}
+
+renderResult();
