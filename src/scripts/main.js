fb0541367f08fda1a3531f8252c86f2ff3fa4571
Anastasia Granovskaya <iamgranovskaya@gmail.com>
2024-06-25 15:33:38 +0300

    parse list js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const listTag = document.querySelector('ul');
+const data = [...document.querySelectorAll('li')];
+
+function getSalary(employee) {
+  return Number(employee.getAttribute('data-salary').replace(/[^\w\s]|_/g, ''));
+}
+
+function getEmployees() {
+  const arrayOfEmployees = [];
+
+  data.forEach((employee) => {
+    employee.remove();
+
+    arrayOfEmployees.push({
+      name: employee.innerHTML.trim(),
+      position: employee.getAttribute('data-position').trim(),
+      salary: getSalary(employee),
+      age: Number(employee.getAttribute('data-age')),
+    });
+  });
+
+  return arrayOfEmployees;
+}
+
+function sortList(list) {
+  return [...list].sort((a, b) => b.salary - a.salary);
+}
+
+const employees = sortList(getEmployees());
+
+employees.forEach((employee) => {
+  const li = document.createElement('li');
+
+  li.setAttribute('data-position', employee.position);
+  li.setAttribute('data-salary', `$${employee.salary.toLocaleString('en-US')}`);
+  li.setAttribute('data-age', employee.age.toString());
+  li.innerText = employee.name;
+  listTag.append(li);
+});
