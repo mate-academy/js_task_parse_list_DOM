a186ab4b1528c18fe3b8367a61955b52d993f3e0
Mykola Bilenkiy <nikolosknv@gmail.com>
2024-06-28 20:09:10 +0300

    make task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write code here
+const salaryElements = document.querySelectorAll('li[data-salary]');
+const salaryArray = Array.from(salaryElements);
+
+salaryArray.sort((a, b) => {
+  const salaryA = parseInt(a.getAttribute('data-salary').replace(/[$,]/g, ''));
+  const salaryB = parseInt(b.getAttribute('data-salary').replace(/[$,]/g, ''));
+
+  return salaryB - salaryA;
+});
+
+const ul = document.querySelector('ul');
+
+ul.innerHTML = '';
+
+salaryArray.forEach((element) => {
+  ul.appendChild(element);
+});
