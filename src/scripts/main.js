2e6d57ff9619bb02e0439f6334ff033f99b99f69
4xmplme <andrii_kondratenko@icloud.com>
2024-06-29 17:25:16 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+function getEmployees(list) {
+  return list.map((employee) => ({
+    name: employee.innerText,
+    position: employee.dataset.position,
+    salary: employee.dataset.salary,
+    age: employee.dataset.age,
+  }));
+}
+
+function sortList(list) {
+  return [...list].sort((a, b) => getNumber(b.salary) - getNumber(a.salary));
+}
+
+function getNumber(value) {
+  return value.replace(/\D/g, '');
+}
+
+const employeesList = document.querySelector('ul');
+const employees = getEmployees([...employeesList.children]);
+const sortedEmployees = sortList(employees);
+
+sortedEmployees.forEach((employee, id) => {
+  employeesList.children[id].innerText = employee.name;
+  employeesList.children[id].dataset.position = employee.position;
+  employeesList.children[id].dataset.salary = employee.salary;
+  employeesList.children[id].dataset.age = employee.age;
+});
