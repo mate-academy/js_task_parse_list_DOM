2fa30a256584f906958cefac2b5873dd32f99daa
smachyloooo <smachyloooo@gmail.com>
2024-07-01 18:27:48 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // write code here
+const li = [...document.querySelectorAll('li')];
+const sortedElements = li.sort((a, b) => {
+  const salaryA = Number(a.dataset.salary.replace('$', '').replace(',', '.'));
+  const salaryB = Number(b.dataset.salary.replace('$', '').replace(',', '.'));
+
+  return salaryA - salaryB;
+});
+
+sortedElements.forEach((el) => document.querySelector('ul').appendChild(el));
