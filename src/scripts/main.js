89d790c3f567019beabd98ebbe312dd6fe07a929
valentyn19939 <valik.hai1993@gmail.com>
2024-07-01 17:52:59 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function norm(salary) {
+  return +salary.split(',').join('').slice(1);
+}
+
+function sort(l) {
+  const childrenCopy = [...l.children];
+
+  childrenCopy.sort((a, b) => {
+    return norm(b.dataset.salary) - norm(a.dataset.salary);
+  });
+
+  childrenCopy.forEach((emp) => l.append(emp));
+}
+
+function getEmps(l) {
+  return [...l.children].reduce((arr, child) => {
+    arr.push({
+      name: child.innerText,
+      position: child.dataset.position,
+      age: child.dataset.age,
+      salary: child.dataset.salary,
+    });
+
+    return arr;
+  }, []);
+}
+
+sort(list);
+getEmps(list);
