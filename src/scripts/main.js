2c9359331686d24bb0585b8e999afd74ab2f1dd5
Evhen Teneta <tenetaevgen@gmail.com>
2024-07-02 14:54:20 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function parseIntegers(num) {
+  return parseInt(num.slice(1).replace(',', ''), 10);
+}
+
+function sortList(arr) {
+  const items = Array.from(arr.children);
+
+  items.sort((a, b) => {
+    const salaryA = parseIntegers(a.dataset.salary);
+    const salaryB = parseIntegers(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  list.innerHTML = '';
+
+  items.forEach((item) => {
+    list.appendChild(item);
+  });
+
+  return list;
+}
+
+const getEmployees = (arr) => {
+  const items = Array.from(arr.children);
+
+  return items.map((item) => ({
+    name: item.textContent.trim(),
+    salary: parseIntegers(item.dataset.salary),
+    age: parseInt(item.dataset.age),
+  }));
+};
+
+sortList(list);
+
+getEmployees(list);
