5e7ed6cca9bffc3c481f10bd5018de1cad3df10b
Kirill <ovoo1339@gmail.com>
2024-07-04 11:22:12 +0300

    work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 // write code here
+const amployeeElements = document.querySelectorAll('ul > li');
+const employeeArray = Array.from(amployeeElements);
+
+const parseSalary = (salaryString) => {
+  return parseFloat(salaryString.replaceAll('$', '').replaceAll(',', ''));
+}
+
+const sortList = () => {
+  employeeArray.sort((a, b) => {
+    const salaryA = parseSalary(a.getAttribute('data-salary'));
+    const salaryB = parseSalary(b.getAttribute('data-salary'));
+    return salaryB - salaryA;
+  })
+}
+
+const getEmployees = () => {
+  const ul = document.querySelector('ul');
+  ul.innerHTML = '';
+
+  employeeArray.forEach(employee => {
+    ul.appendChild(employee);
+  })
+}
+
+sortList();
+getEmployees();
+
