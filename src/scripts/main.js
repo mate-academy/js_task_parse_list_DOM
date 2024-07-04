245ccb45618ea9c4315019ccc115ae2b059b339a
Lidiia Shvets <lepoki051003@gmail.com>
2024-07-04 10:04:24 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function transformInNumbers(str) {
+  return parseInt(str.split(',').join('').slice(1));
+}
+
+function sortList(arr) {
+  const items = [...arr.children].sort(
+    (a, b) =>
+      transformInNumbers(b.dataset.salary) -
+      transformInNumbers(a.dataset.salary),
+  );
+
+  list.innerHTML = '';
+  items.forEach((item) => arr.appendChild(item));
+}
+
+const getEmployees = (arr) =>
+  [...arr.children].map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: Number(item.dataset.salary),
+    age: Number(item.dataset.age),
+  }));
+
+sortList(list);
+
+getEmployees(list);
