29af42df995e193879127ea48b2519b53e24a978
BoGoBooM <MyPikcha@gmail.com>
2024-07-04 22:16:02 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const employeeEl = document.querySelectorAll('ul > li');
+const employeeArr = Array.from(employeeEl);
+
+const salary = (salaryString) => {
+  return parseFloat(salaryString.replace('$', '').replace(',', ''));
+};
+
+const sortList = () => {
+  employeeArr.sort((a, b) => {
+    const salaryA = salary(a.getAttribute('data-salary'));
+    const salaryB = salary(b.getAttribute('data-salary'));
+
+    return salaryB - salaryA;
+  });
+};
+
+const getEmployees = () => {
+  const ul = document.querySelector('ul');
+
+  ul.innerHTML = '';
+
+  employeeArr.forEach((employee) => {
+    ul.appendChild(employee);
+  });
+};
+
+sortList();
+getEmployees();
