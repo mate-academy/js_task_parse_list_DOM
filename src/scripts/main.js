2ca56c21e2ac9e1e54c560078933174c528fc163
Dmytro Moshkivskyi <dmosh@ukr.net>
2024-07-05 17:43:45 +0300

    js_task_parse_list_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const salaryArray = [...document.querySelectorAll('li[data-salary]')];
+
+salaryArray.sort((a, b) => {
+  const el1 = parseInt(a.getAttribute('data-salary').replace('$', ''));
+  const el2 = parseInt(b.getAttribute('data-salary').replace('$', ''));
+
+  return el2 - el1;
+});
+
+salaryArray.forEach((element) => {
+  document.querySelector('ul').appendChild(element);
+});
