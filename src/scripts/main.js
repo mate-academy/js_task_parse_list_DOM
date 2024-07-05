b52a29dff9a1c235b86880332edac85b6b1ed40c
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-07-06 00:26:37 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const convertSalary = (salary) =>
+  +salary.replaceAll('$', '').replaceAll(',', '');
+
+function getEmployees(list) {
+  return [...list.children].map((el) => {
+    const fullName = el.textContent.trim();
+
+    return { ...el.dataset, name: fullName };
+  });
+}
+
+function sortList(list) {
+  const newList = getEmployees(list)
+    .sort(
+      (person1, person2) =>
+        convertSalary(person2.salary) - convertSalary(person1.salary),
+    )
+    .map((person) => {
+      const element = document.createElement('li');
+
+      element.setAttribute('data-position', `${person.position}`);
+      element.setAttribute('data-salary', `${person.salary}`);
+      element.setAttribute('data-age', `${person.age}`);
+      element.textContent = `${person.name}`;
+
+      return element;
+    });
+
+  list.replaceChildren(...newList);
+}
+
+const listItem = document.querySelector('ul');
+
+sortList(listItem);
+getEmployees(listItem);
