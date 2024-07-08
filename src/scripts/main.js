8baf294f6746143d83a0acce99f2bfe3573c7e98
Yehor Gordiienko <egordienko319@gmail.com>
2024-07-08 21:15:24 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+const allPersons = [...ul.children];
+
+function sortList() {
+  allPersons.sort((first, second) => {
+    const getNormalized = (item) => {
+      return +item.dataset.salary.slice(1).split(',').join('');
+    };
+    return getNormalized(second) - getNormalized(first);
+  });
+
+  for (const key of allPersons) {
+    ul.append(key);
+  }
+}
+
+const getEmployees = (list) =>
+  [...ul.children].map((employee) => ({
+    name: employee.innerText,
+    position: employee.dataset.position,
+    age: employee.dataset.age,
+    salary: employee.dataset.salary,
+  }));
