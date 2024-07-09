883e0de4ae0d51c43c89fdfe7fcd7c4bc969d107
Oleksandra Kuziura <radioday2014@gmail.com>
2024-07-09 15:42:38 +0300

    add function sortList and getEmployees

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+const arrayOfEmployees = [...document.querySelectorAll('li')];
+
+//function for sorting list of employees and chenching order in html document
+function sortList(list) {
+  const newList = arrayOfEmployees.sort(sorting);
+
+  for (let i = 0; i < newList.length; i++) {
+    document.body.append(newList[i]);
+  }
+}
+
+//helping function to sort list by salary
+function sorting(a, b) {
+  const valueA = +a.getAttribute('data-salary').slice(1,-1).replace(',','');
+  const valueB = +b.getAttribute('data-salary').slice(1,-1).replace(',','');
+
+  return valueB - valueA;
+}
+
+//function to make array of employees object with information about them
+function getEmployees(array) {
+  const employeesList = [];
+
+  for (let i = 0; i < array.length; i++ ) {
+    const employee = {};
+    employee.name = arrayOfEmployees[i].innerText;
+    employee.position = arrayOfEmployees[i].getAttribute('data-position');
+    employee.salary = arrayOfEmployees[i].getAttribute('data-salary');
+    employee.age = arrayOfEmployees[i].getAttribute('data-age');
+    employeesList.push(employee);
+  }
+
+  return employeesList;
+}
+
+//invoke fubctions
+sortList(arrayOfEmployees);
+getEmployees(arrayOfEmployees);
