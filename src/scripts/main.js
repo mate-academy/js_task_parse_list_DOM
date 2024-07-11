ce01fa9002672b948849246f35e814ae7e2f98c1
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2024-07-11 14:16:27 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write code here
+
+const listaUl = document.querySelector('ul');
+const listaLi = listaUl.querySelectorAll('li');
+
+const posortowanaLista = [...listaLi].sort((a, b) => {
+  const salaryA = Number(a.getAttribute('data-salary').replace(/[$,]/g, ''));
+  const salaryB = Number(b.getAttribute('data-salary').replace(/[$,]/g, ''));
+
+  return salaryB - salaryA;
+});
+
+posortowanaLista.forEach((li) => listaUl.appendChild(li));
