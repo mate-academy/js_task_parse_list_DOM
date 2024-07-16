579eb7ae9a32487170e581d3ec1a130568a73eea
mrsvolodya <vovamursky@gmail.com>
2024-07-16 18:53:35 +0300

    my-solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const listArray = [...document.querySelectorAll('li')];
+const ul = document.querySelector('ul');
+
+const helper = (value) => {
+  const changeValue = value.getAttribute('data-salary');
+
+  if (typeof changeValue === 'string') {
+    return +changeValue.replaceAll('$', '').split(',').join('');
+  }
+};
+
+const sortList = (list) => {
+  const ulWrap = document.createElement('ul');
+  const sortedList = [...list].sort((a, b) => helper(b) - helper(a));
+
+  sortedList.forEach((item) => ulWrap.appendChild(item));
+  ul.replaceWith(ulWrap);
+};
+
+const getEployis = () => {
+  const sortedArray = [...document.querySelectorAll('li')];
+
+  return sortedArray;
+};
+
+sortList(listArray);
+getEployis();
