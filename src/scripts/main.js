b8706906c67699425890ff5ee25d09c51305406a
Bohdan Ivankov <bodanich600@gmail.com>
2024-07-16 20:10:01 +0100

    remove unnecessary map

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const listOfEmployees = document.querySelector('ul');
+const itemsArray = Array.from(listOfEmployees.children);
+
+function getSalary(element) {
+  const number = element.getAttribute('data-salary').replaceAll('$', '');
+
+  return parseFloat(number.replaceAll(',', ''));
+}
+
+function sortList() {
+  itemsArray.sort(
+    (employee1, employee2) => getSalary(employee2) - getSalary(employee1),
+  );
+
+  return itemsArray.map((item) => item);
+}
+
+const newItems = sortList();
+
+listOfEmployees.innerHTML = '';
+
+newItems.forEach((item) => listOfEmployees.append(item));
