0c2d05de07fb28f3eb167270a32b0bec44e584b2
Maksym <maksymka@icloud.com>
2024-07-16 20:05:54 -0400

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+function sortList(list) {
+  const coletion = [...list];
+
+  coletion.sort((a, b) => -(getIntegerNumber(a) - getIntegerNumber(b)));
+
+  coletion.forEach((el) => document.querySelector('ul').append(el));
+}
+
+function getEmployees(list) {
+  const employees = [];
+
+  list.forEach((el) => {
+    const employee = {};
+
+    employee.name = el.textContent;
+    employee.position = el.dataset.position;
+    employee.salary = el.dataset.salary;
+    employee.age = el.dataset.age;
+
+    employees.push(employee);
+  });
+}
+
+function getIntegerNumber(nodeElement) {
+  return Number(nodeElement.dataset.salary.replace(/[$,]/g, ''));
+}
+
+sortList(document.querySelectorAll('li'));
+
+getEmployees(document.querySelectorAll('li'));
