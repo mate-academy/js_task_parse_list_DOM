d13bfdd2088b1d0ebe391c2d7665a54a564d927b
Olya <olyavidzi97@gmail.com>
2024-07-17 14:49:00 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const salaryList = [...document.querySelectorAll('li[data-salary]')];
+
+salaryList.sort((a, b) => {
+  const item1 = parseInt(a.getAttribute('data-salary').replace('$', ''));
+  const item2 = parseInt(b.getAttribute('data-salary').replace('$', ''));
+
+  return item2 - item1;
+});
+
+salaryList.forEach((item) => {
+  document.querySelector('ul').appendChild(item);
+});
