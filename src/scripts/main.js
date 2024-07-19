1a3e69b2c3e3ea84b752e6a68d76277843dc271c
Vitalii Venher <vengervn@gmail.com>
2024-07-19 14:35:53 +0300

    added helper function wich converted string to digit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const employees = document.querySelector('ul');
+const arrayOfEmployees = [...employees.children];
+
+function stringToDigit(str) {
+  const digit = +str.replace(/\D/g, '');
+
+  // console.log(digit);
+  return digit;
+}
+
+// console.log(arrayOfEmployees[0].dataset.salary);
+// console.log(stringToDigit('$162,700'));
+// console.log(typeof x, x);
+// console.log(typeof NaN);
