1d9d4d3c6a6e2774940a2aaef092afce89951c4f
Vitalii Venher <vengervn@gmail.com>
2024-07-19 17:03:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,45 @@
 'use strict';
 
-const employees = document.querySelector('ul');
-const arrayOfEmployees = [...employees.children];
+const arrayOfEmployees = [...document.querySelectorAll('li')];
 
 function stringToDigit(str) {
   const digit = +str.replace(/\D/g, '');
 
-  // console.log(digit);
   return digit;
 }
 
-// console.log(arrayOfEmployees[0].dataset.salary);
-// console.log(stringToDigit('$162,700'));
-// console.log(typeof x, x);
-// console.log(typeof NaN);
+function sortList(list) {
+  const sortedList = document.querySelector('ul');
+  const sorted = list.sort((firstPerson, secondPerson) => {
+    return (
+      stringToDigit(secondPerson.dataset.salary) -
+      stringToDigit(firstPerson.dataset.salary)
+    );
+  });
+
+  sortedList.innerHTML = '';
+  sortedList.append(...sorted);
+
+  return sortedList;
+}
+
+function getEmployees(list) {
+  const arrayOfEmployeesOdj = list.reduce((acc, el) => {
+    const obj = [
+      ...acc,
+      {
+        name: el.innerText,
+        position: el.dataset.position,
+        salsry: el.dataset.salary,
+        age: el.dataset.age,
+      },
+    ];
+
+    return obj;
+  }, []);
+
+  return arrayOfEmployeesOdj;
+}
+
+sortList(arrayOfEmployees);
+getEmployees(arrayOfEmployees);
