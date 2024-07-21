075270888c99d02bd94516a52cd59276f8023d8a
Nadinnnka <tymoshyk02@gmail.com>
2024-07-21 14:02:32 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
+/* eslint-disable no-shadow */
 'use strict';
 
-// write code here
+const list = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  const salaryList = list
+    .map((el) => ({
+      element: el,
+      salary: parseInt(
+        el.getAttribute('data-salary').replace(',', '').replace('$', ''),
+      ),
+    }))
+    .sort((a, b) => b.salary - a.salary);
+
+  const ul = document.querySelector('ul');
+
+  ul.innerHTML = '';
+
+  salaryList.forEach(({ element }) => ul.appendChild(element));
+}
+
+function getEmployees(list) {
+  return list.map((li) => ({
+    name: li.textContent.trim(),
+    position: li.getAttribute('data-position'),
+    salary: parseInt(
+      li.getAttribute('data-salary').replace(',', '').replace('$', ''),
+    ),
+    age: parseInt(li.getAttribute('data-age')),
+  }));
+}
+
+sortList(list);
+getEmployees(list);
