cfc54b50a4d5800b0a16f03be0ee86c157234b9e
svyatoslav <svyatoslavik9@gmail.com>
2024-07-23 13:11:25 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,39 @@
-'use strict'
+'use strict';
 
 const convertToNumber = (data) => {
-  return +data.replace(',', '').slice(1)
-}
+  return +data.replace(',', '').slice(1);
+};
 
 const getEmployees = (list) => {
   const result = [];
 
   for (const item of list) {
-    const salary = item.getAttribute('data-salary')
-    const position = item.getAttribute('data-position')
-    const age = item.getAttribute('data-age')
-    const name = item.textContent.trim();
+    const salary = item.getAttribute('data-salary');
+    const position = item.getAttribute('data-position');
+    const age = item.getAttribute('data-age');
 
-    result.push({ name, position, salary: convertToNumber(salary), age });
+    result.push({
+      name: item.textContent.trim(),
+      position,
+      salary: convertToNumber(salary),
+      age,
+    });
   }
 
   return result;
-}
+};
 
 const ListItem = (text) => {
   const li = document.createElement('li');
+
   li.textContent = text;
 
   return li;
-}
+};
 
 const sortList = () => {
-  const list = document.querySelector('ul')
-  const items = [...list.children]
+  const list = document.querySelector('ul');
+  const items = [...list.children];
   const employees = getEmployees(items);
   const sortedEmployees = employees.sort((a, b) => b.salary - a.salary);
 
@@ -37,12 +42,12 @@ const sortList = () => {
   for (const employeer of sortedEmployees) {
     const li = ListItem(employeer.name);
 
-    li.setAttribute('data-salary', `$${employeer.salary.toLocaleString()}`)
+    li.setAttribute('data-salary', `$${employeer.salary.toLocaleString()}`);
     li.setAttribute('data-position', employeer.position);
     li.setAttribute('data-age', employeer.age);
 
     list.append(li);
   }
-}
+};
 
-sortList()
+sortList();
