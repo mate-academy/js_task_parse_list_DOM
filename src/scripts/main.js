e584f3516540f3f00468afcf0aa39a9e89638530
svyatoslav <svyatoslavik9@gmail.com>
2024-07-23 13:11:11 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
-'use strict';
+'use strict'
 
-// write code here
+const convertToNumber = (data) => {
+  return +data.replace(',', '').slice(1)
+}
+
+const getEmployees = (list) => {
+  const result = [];
+
+  for (const item of list) {
+    const salary = item.getAttribute('data-salary')
+    const position = item.getAttribute('data-position')
+    const age = item.getAttribute('data-age')
+    const name = item.textContent.trim();
+
+    result.push({ name, position, salary: convertToNumber(salary), age });
+  }
+
+  return result;
+}
+
+const ListItem = (text) => {
+  const li = document.createElement('li');
+  li.textContent = text;
+
+  return li;
+}
+
+const sortList = () => {
+  const list = document.querySelector('ul')
+  const items = [...list.children]
+  const employees = getEmployees(items);
+  const sortedEmployees = employees.sort((a, b) => b.salary - a.salary);
+
+  list.innerHTML = '';
+
+  for (const employeer of sortedEmployees) {
+    const li = ListItem(employeer.name);
+
+    li.setAttribute('data-salary', `$${employeer.salary.toLocaleString()}`)
+    li.setAttribute('data-position', employeer.position);
+    li.setAttribute('data-age', employeer.age);
+
+    list.append(li);
+  }
+}
+
+sortList()
