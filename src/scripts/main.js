a0b55f59a17ad27c7be246b8e040966755563527
Vadym Babisov <vadim.babisov@gmail.com>
2024-07-25 15:49:24 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+function sortList(list) {
+  const itemsArray = Array.from(list.children);
+
+  itemsArray.sort(
+    (a, b) =>
+      b.dataset.salary.slice(1).split(',').join('') -
+      a.dataset.salary.slice(1).split(',').join(''),
+  );
+
+  itemsArray.forEach((item) => list.appendChild(item));
+}
+
+const employeeList = document.getElementById('employee-list');
+
+sortList(employeeList);
