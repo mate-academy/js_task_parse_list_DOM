1498370469aa4959e99305c18e115453d2c6af35
Iryna Stefaniuk <99888510+Iryna1802@users.noreply.github.com>
2024-07-27 16:56:45 +0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -33,34 +33,5 @@ function getEmployees(array) {
   return result;
 }
 
-// const pageList = document.querySelector('ul');
-
-// function normalize(stringSalary) {
-//   return +stringSalary.split(',').join('').slice(1);
-// }
-
-// function sortList(array) {
-//   const copyEmployeesChilds = [...array.children];
-
-//   copyEmployeesChilds.sort((a, b) => {
-//     return normalize(b.dataset.salary) - normalize(a.dataset.salary);
-//   });
-
-//   copyEmployeesChilds.forEach((employee) => array.append(employee));
-// }
-
-// function getEmployees(list) {
-//   return [...list.children].reduce((array, child) => {
-//     array.push({
-//       name: child.innerText,
-//       position: child.dataset.position,
-//       age: child.dataset.age,
-//       salary: child.dataset.salary,
-//     });
-
-//     return array;
-//   }, []);
-// }
-
 sortList(list);
 getEmployees(list);
