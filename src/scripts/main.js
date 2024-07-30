bd2f10f641329a13a6bdde46b675e38d8cd56d0d
Sergiy Dolgov <info@devmax.pro>
2024-07-30 12:02:06 -0400

    v 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelectorAll('li');
+
+function sortList(unsortedList) {
+  const arrayList = getEmployees(unsortedList);
+
+  arrayList.sort((a, b) => {
+    const salaryA = salaryToNumber(a.dataset.salary);
+    const salaryB = salaryToNumber(b.dataset.salary);
+
+    if (salaryA < salaryB) {
+      return 1;
+    }
+
+    if (salaryA > salaryB) {
+      return -1;
+    }
+
+    return 0;
+  });
+
+  const ul = document.querySelector('ul');
+
+  ul.innerHTML = '';
+
+  for (const element of arrayList) {
+    const li = document.createElement('li');
+
+    li.innerHTML = element.innerText;
+    li.setAttribute('data-position', element.dataset.position);
+    li.setAttribute('data-salary', element.dataset.salary);
+    li.setAttribute('data-age', element.dataset.age);
+
+    ul.appendChild(li);
+  }
+}
+
+function salaryToNumber(str) {
+  return +str.replaceAll('$', '').replaceAll(',', '');
+}
+
+function getEmployees(list) {
+  return [...list];
+}
+
+sortList(employeesList);
