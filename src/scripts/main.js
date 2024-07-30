bd47a4be0a8e7c2f37c9e0c51a0283de03cd2b5f
Bronex25 <oleksandr.plaksin25@gmail.com>
2024-07-30 17:12:07 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const mainList = document.querySelector('ul');
+
+function toSort(input) {
+  const sortedList = Array.from(input).sort((itm1, itm2) => {
+    const person1 = itm1.dataset.salary.replace(/[$,]/g, '');
+    const person2 = itm2.dataset.salary.replace(/[$,]/g, '');
+
+    return Number(person2) - Number(person1);
+  });
+
+  sortedList.forEach((elem) => mainList.appendChild(elem));
+}
+
+function getEmployees(input) {
+  return Array.from(input).map((elem) => {
+    return {
+      name: elem.innerText,
+      position: elem.dataset.position,
+      salary: elem.dataset.salary,
+      age: elem.dataset.age,
+    };
+  });
+}
+
+toSort(list);
+getEmployees(list);
