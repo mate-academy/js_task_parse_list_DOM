0824006c525be976f52926adb79c63d15dede730
Maria <maria.pozniak2013@gmail.com>
2024-08-06 11:34:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const employeesElements = document.querySelectorAll('li');
+
+const employeesObj = [];
+
+employeesElements.forEach((element) => {
+  const obj = {
+    name: element.textContent.trim(),
+    position: element.getAttribute('data-position'),
+    salary: parseFloat(
+      element.getAttribute('data-salary').replace(/[$,]/g, ''),
+    ),
+    age: element.getAttribute('data-age'),
+  };
+
+  employeesObj.push(obj);
+});
+
+const employeesSorted = employeesObj.sort((a, b) => b.salary - a.salary);
+
+const ul = document.querySelector('ul');
+
+ul.innerHTML = '';
+
+employeesSorted.forEach((employee) => {
+  const li = document.createElement('li');
+
+  li.textContent = employee.name;
+  li.setAttribute('data-position', employee.position);
+  li.setAttribute('data-salary', '$' + employee.salary.toLocaleString('en-US'));
+  li.setAttribute('data-age', employee.age);
+
+  ul.appendChild(li);
+});
