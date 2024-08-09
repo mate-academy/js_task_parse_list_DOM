20cc9c329161aa3665c996f7c9858766e4c50575
Ivan Vaniuk <ivanvanuk77@gmail.com>
2024-08-09 16:57:54 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+function parseSalary(salaryString) {
+  return parseFloat(salaryString.replace(/[\$,]/g, ''));
+}
+
+function sortList(list) {
+  const items = Array.from(list.children);
+
+  items.sort((a, b) => {
+    const salaryA = parseSalary(a.getAttribute('data-salary'));
+    const salaryB = parseSalary(b.getAttribute('data-salary'));
+
+    return salaryB - salaryA;
+  });
+
+  items.forEach(elem => list.appendChild(elem));
+}
+
+function getEmployees(list) {
+  return Array.from(list.children).map(item => {
+    return {
+      name: item.textContent.trim(),
+      position: item.getAttribute('data-position'),
+      salary: parseSalary(item.getAttribute('data-salary')),
+      age: item.getAttribute('data-age')
+    };
+  });
+}
+
+const employeeList = document.querySelector('ul');
+
+sortList(employeeList);
+
+const employees = getEmployees(employeeList);
+
+// console.log(employees);
