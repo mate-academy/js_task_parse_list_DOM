835ea5e8847383b55149916bb86311f1323a588d
iryna lysko <goiralysko@gmail.com>
2024-08-12 16:09:20 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+function sortList(list) {
+  const sortedEmployees = [...list.children];
+
+  sortedEmployees.sort((el1, el2) => {
+    const salary1 = el1.dataset.salary.replace(/[$,]/g, '');
+    const salary2 = el2.dataset.salary.replace(/[$,]/g, '');
+
+    return salary2 - salary1;
+  });
+
+  sortedEmployees.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  const employees = [];
+
+  for (const person of list.children) {
+    const personName = person.textContent.trim();
+    const { age, position, salary } = person.dataset;
+    const cleanSalary = parseFloat(salary.replace(/[$,]/g, ''));
+    const numberAge = parseInt(age);
+
+    employees.push({
+      salary: cleanSalary,
+      age: numberAge,
+      position,
+      name: personName,
+    });
+  }
+
+  return employees;
+}
+
+const people = document.querySelector('ul');
+
+sortList(people);
+
+getEmployees(people);
