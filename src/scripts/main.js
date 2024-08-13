fc3a1d3d047329e3d51d38c775000b0de5238b68
Oleksandr Robenko <oleksandr.robenko@gmail.com>
2024-08-13 23:06:30 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const items = list.querySelectorAll('li');
+const employeesArr = Array.from(items);
+
+function sortList(arr) {
+  arr.sort(
+    (a, b) =>
+      +b.getAttribute('data-salary').replace(/[$,]/g, '') -
+      +a.getAttribute('data-salary').replace(/[$,]/g, ''),
+  );
+
+  list.innerHTML = '';
+  arr.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(arr) {
+  return arr.map((employee) => ({
+    name: employee.innerText,
+    position: employee.getAttribute('data-position'),
+    salary: employee.getAttribute('data-salary'),
+    age: employee.getAttribute('data-age'),
+  }));
+}
+
+sortList(employeesArr);
+getEmployees(employeesArr);
