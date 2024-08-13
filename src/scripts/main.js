039dea7113fec75bcee005ebc3764e71d2f6e43a
vanvalera <115824927+vanvalera@users.noreply.github.com>
2024-08-13 19:40:50 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const listEmployes = [...document.getElementsByTagName('li')];
+
+function sortList() {
+  const salaryArr = listEmployes.map((el) => {
+    const salary = el.dataset.salary;
+    const numericSalary = Number(salary.replace(/[\$,]/g, ''));
+
+    return { element: el, salary: numericSalary };
+  });
+
+  salaryArr.sort((fEl, sEl) => sEl.salary - fEl.salary);
+
+  const parentLi = listEmployes[0].parentNode;
+
+  salaryArr.forEach(({ element }) => {
+    parentLi.appendChild(element);
+  });
+}
+
+function getEmployees() {
+  const objectOfEmployees = listEmployes.map((el) => {
+    const name = el.textContent.trim();
+    const position = el.dataset.position;
+    const salary = el.dataset.salary;
+    const age = el.dataset.age;
+
+    return {
+      name,
+      position,
+      salary,
+      age,
+    };
+  });
+
+  return objectOfEmployees;
+}
+
+sortList();
+console.log(getEmployees());
