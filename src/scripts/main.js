3cb7d4740aef294b7303e45e79d346bb08035df5
Rostyslav <rostislavkpi@gmail.com>
2024-08-14 18:11:15 +0300

    Fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,11 @@
 const employeesList = document.querySelector('ul');
 const employees = [...document.querySelectorAll('li')];
 
-const salaryToNum = (salary) => Number(salary.replace(',', '').slice(1));
+const parseSalary = (salary) => parseInt(salary.replace(',', '').slice(1));
 
 const sortList = (list) => {
   list.sort(
-    (a, b) => salaryToNum(b.dataset.salary) - salaryToNum(a.dataset.salary),
+    (a, b) => parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary),
   );
 
   employeesList.append(...list);
