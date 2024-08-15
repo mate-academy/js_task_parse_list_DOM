a7f91e097eed406ae03144ccfff5e5ae9c031aaa
Vlad Zahorovskyi <zagorovskii12@gmail.com>
2024-08-15 20:39:28 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const listElements = document.getElementsByTagName('li');
+
+function sortList(list) {
+  const itemsArray = Array.from(list);
+
+  itemsArray.sort((a, b) => {
+    return parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary);
+  });
+
+  itemsArray.forEach((item) => list[0].parentElement.appendChild(item));
+}
+
+function parseSalary(salary) {
+  return parseFloat(salary.replace('$', '').replace(',', ''));
+}
+
+function getEmployees(list) {
+  const listOfEmployees = [];
+
+  for (let i = 0; i < list.length; i++) {
+    listOfEmployees.push({
+      name: list[i].textContent.trim(),
+      position: list[i].dataset.position,
+      salary: parseSalary(list[i].dataset.salary),
+      age: parseInt(list[i].dataset.age),
+    });
+  }
+
+  return listOfEmployees;
+}
+
+getEmployees(listElements);
+sortList(listElements);
