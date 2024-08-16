091b2a7b52e1f3fcd37cfce888c2fc6b9fa9c5c9
Zyrak Nur <noorzirak7@gmail.com>
2024-08-17 01:39:42 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+
+function convertToNumber(str) {
+  return +str.replace('$', '').replaceAll(',', '');
+}
+
+function sortList(list) {
+  const elements = [...list.children];
+
+  elements.sort((a, b) => {
+    const salaryA = convertToNumber(a.dataset.salary);
+    const salaryB = convertToNumber(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  return elements;
+}
+
+const sortedEmployees = sortList(employeesList);
+
+employeesList.innerHTML = '';
+
+sortedEmployees.forEach((employee) => employeesList.append(employee));
+
+function getEmployees(list) {
+  const elements = [...list.children];
+  const result = [];
+
+  elements.forEach((el) => {
+    result.push({
+      name: el.innerText,
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    });
+  });
+
+  return result;
+}
+
+getEmployees(employeesList);
