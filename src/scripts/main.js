13cfd0ee80cfccead7758e751762f4d868f6f2de
dmitruz <dmitruz970@gmail.com>
2024-08-20 21:48:07 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
 // write code here
+
+function convertToNumber(str) {
+  return +str.replace(/\D/g, '');
+}
+
+function sortList(list) {
+  const employees = [...list.children];
+
+  employees.sort((employeeA, employeeB) => {
+    const salaryA = convertToNumber(employeeA.dataset.salary);
+    const salaryB = convertToNumber(employeeB.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  return employees;
+}
+
+function getEmployees(list) {
+  const employees = [...list.children];
+  const result = [];
+
+  employees.forEach((employee) => {
+    result.push({
+      name: employee.innerText,
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    });
+  });
+
+  return result;
+}
+
+const listOfEmployees = document.querySelector('ul');
+const sortedListOfEmployees = sortList(listOfEmployees);
+
+listOfEmployees.innerHTML = '';
+sortedListOfEmployees.forEach((element) => listOfEmployees.append(element));
+getEmployees(listOfEmployees);
