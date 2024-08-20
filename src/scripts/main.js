411b69e65e34317d647e6f27475d9d0610ccc288
Stas <staskapica93@gmail.com>
2024-08-20 21:22:01 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write code here
+const arrayOfLi = [...document.querySelectorAll('li')];
+
+function getSortedListBySalary() {
+  const sortedList = arrayOfLi.sort((selector1, selector2) => {
+    const salaryOne = +selector1.dataset.salary.replaceAll(/[$,]/g, '');
+    // remove all $ and ',' and + make a number
+    const salaryTwo = +selector2.dataset.salary.replaceAll(/[$,]/g, '');
+
+    if (salaryOne === salaryTwo) {
+      return 0;
+    }
+
+    return salaryTwo > salaryOne ? 1 : -1;
+  });
+
+  const ulTeg = document.getElementsByTagName('ul')[0];
+
+  while (ulTeg.firstChild) {
+    ulTeg.removeChild(ulTeg.firstChild);
+  }
+
+  sortedList.forEach((li) => {
+    ulTeg.appendChild(li);
+  });
+}
+getSortedListBySalary(arrayOfLi);
+
+function getArrayOfemployees() {
+  const arrayOfemployees = arrayOfLi.map((employee) => {
+    const { position, salary, age } = employee.dataset;
+    const nameOfEmployee = employee.innerText;
+
+    return {
+      name: nameOfEmployee,
+      position,
+      salary,
+      age,
+    };
+  });
+
+  return arrayOfemployees;
+}
+
+getArrayOfemployees(arrayOfLi);
