a76b280307a55ecc8f1f739e62c7eaa887467260
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-08-26 00:22:55 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const people = document.querySelector('ul');
+
+sortList(people);
+getEmployees(people);
+
+function sortList(list) {
+  const sortedList = [...list.children].sort((person1, person2) => {
+    return (
+      person2.dataset.salary.replace(/[$,]/g, '') -
+      person1.dataset.salary.replace(/[$,]/g, '')
+    );
+  });
+
+  list.innerHTML = '';
+
+  sortedList.forEach((val) => {
+    list.append(val);
+  });
+}
+
+function getEmployees(list) {
+  return [...list.children].map((val) => {
+    const position = val.dataset.position;
+    const salary = val.dataset.salary;
+    const age = val.dataset.age;
+    const personName = val.textContent.trim();
+
+    return {
+      name: personName,
+      position,
+      salary,
+      age,
+    };
+  });
+}
