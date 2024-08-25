41a15e984dedca976b9df21eac4ec25809a0a347
Diana Darienko <diana.darienko21@gmail.com>
2024-08-25 18:44:37 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const listFromDocument = document.querySelector('ul');
+
+function convert(salary) {
+  return parseInt(salary.replace('$', '').replace(',', ''));
+}
+
+function sortList(list) {
+  const listItems = Array.from(list.children).sort(
+    (a, b) => convert(b.dataset.salary) - convert(a.dataset.salary),
+  );
+
+  list.innerHTML = '';
+  listItems.forEach((element) => list.appendChild(element));
+}
+
+function getEmployees(list) {
+  const employees = [];
+
+  const listItems = Array.from(list.children);
+
+  for (const item of listItems) {
+    employees.push({
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    });
+  }
+
+  return employees;
+}
+
+sortList(listFromDocument);
+getEmployees(listFromDocument);
