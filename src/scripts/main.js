fcf4eb627f3643e2ceb6af42a5e41968b7e6c0cd
Tetyana Chuhai <chugaytatyana@gmail.&git config --global user.email chugaytatyana@gmail.>
2024-08-28 15:25:40 +0300

    add solutiion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+
+function sortList(list) {
+  const sortedEmployeers = Array.from(list.children).sort(
+    (el1, el2) =>
+      getNumericalValue(el2.dataset.salary) -
+      getNumericalValue(el1.dataset.salary),
+  );
+
+  list.innetHTML = '';
+  sortedEmployeers.forEach((el) => list.appendChild(el));
+}
+
+function getNumericalValue(value) {
+  const nomalizedValue = value.replaceAll(',', '');
+
+  return parseInt(nomalizedValue.slice(1));
+}
+
+function getEmployees(list) {
+  return Array.from(list.children).map((el) => ({
+    name: el.textContent.trim(),
+    position: el.dataset.position,
+    salary: el.dataset.salary,
+    age: el.dataset.age,
+  }));
+}
+
+sortList(employeesList);
+getEmployees(employeesList);
