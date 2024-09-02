27ca72c149107c77ffa0be9f3e0e98c725da7373
Alex <tokyopac@gmail.com>
2024-09-02 23:47:03 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const listItems = Array.from(document.querySelectorAll('li'));
+const employeeList = document.querySelector('ul');
+
+function convertCurrencyToNumber(currencyString) {
+  const numericString = currencyString.replace(/[$,]/g, '').trim();
+
+  return parseInt(numericString, 10);
+}
+
+function sortList(list) {
+  return list.sort((itemA, itemB) => {
+    const salaryA = convertCurrencyToNumber(itemA.dataset.salary);
+    const salaryB = convertCurrencyToNumber(itemB.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+}
+
+const sortedItems = sortList(listItems);
+
+employeeList.append(...sortedItems);
+
+function getEmployees(list) {
+  return list.map((item) => {
+    const { position, salary, age } = item.dataset;
+
+    return {
+      name: item.textContent.trim(),
+      position,
+      salary,
+      age,
+    };
+  });
+}
+
+getEmployees(sortedItems);
