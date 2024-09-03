2345ace9f0731896b03c6e41371e9168872906f3
Anna Agerone <olesnevych12@gmail.com>
2024-09-03 11:32:26 -0500

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const listEmployes = [...document.getElementsByTagName('li')];
+
+function sortList() {
+  const salaryArr = listEmployes.map((el) => {
+    const salary = el.dataset.salary;
+    const numericSalary = Number(salary.replace(/[$,]/g, ''));
+
+    return { element: el, salary: numericSalary };
+  });
+
+  salaryArr.sort((firstEm, secondEm) => secondEm.salary - firstEm.salary);
+
+  const parentLi = listEmployes[0].parentNode;
+
+  salaryArr.forEach(({ element }) => {
+    parentLi.appendChild(element);
+  });
+}
+
+function getEmployees() {
+  const objectOfEmployees = listEmployes.map((el) => {
+    const nameEmp = el.textContent.trim();
+    const position = el.dataset.position;
+    const salary = el.dataset.salary;
+    const age = el.dataset.age;
+
+    return {
+      name: nameEmp,
+      position,
+      salary,
+      age,
+    };
+  });
+
+  return objectOfEmployees;
+}
+
+sortList();
+getEmployees();
