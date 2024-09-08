743740260751d32417575a21b4d4dab9a092a057
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2024-09-08 20:04:38 +0300

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const arr = Array.from(document.querySelectorAll('li')).sort(
+  (a, b) =>
+    parseInt(b.dataset.salary.slice(1).split(',').join('')) -
+    parseInt(a.dataset.salary.slice(1).split(',').join('')),
+);
+
+list.innerHTML = '';
+
+arr.forEach((item) => list.appendChild(item));
