6df3f6d3ded65f92c62e9c46dbed497fa7b09c1d
Vika <rudaviktoria0@gmail.com>
2024-09-08 18:15:34 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
 // write code here
+const allListEmployee = document.querySelectorAll('li');
+const salaryEpmloyee = document.querySelectorAll('[data-salary]');
+
+function sortList(listProperty) {
+  let newList = '';
+
+  for (let i = 0; i < listProperty.length; i++) {
+    if ([i] < [i + 1]) {
+      newList += [i];
+      newList = +newList;
+    }
+  }
+}
+
+function getEmployees(listPropertyTwo) {
+  const newArr = [];
+
+  for (let i = 0; i < listPropertyTwo.length; i++) {
+    const employee = {
+      name: listPropertyTwo[i].textConstent.trim(),
+      position: listPropertyTwo[i].getAttribute('data-position'),
+      salary: listPropertyTwo[i].getAttribute('data-salary'),
+      age: listPropertyTwo[i].getAttribute('data-age'),
+    };
+
+    newArr.push(employee);
+  }
+
+  return newArr;
+}
+
+sortList(salaryEpmloyee);
+getEmployees(allListEmployee);
