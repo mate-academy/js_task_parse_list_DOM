674aa90f18af3f133759c400e075414dd69ad72e
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-09 00:56:46 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
 // write code here
+
+const listOfNames = document.querySelector('ul');
+
+function formatSalary(input) {
+  return input.replace(/[^0-9.]/g, '');
+}
+
+function sortList(list) {
+  const listItemsArray = [...list.querySelectorAll('li')];
+  const listItemsArraySorted = listItemsArray.sort((a, b) => {
+    return (
+      Number(formatSalary(b.dataset.salary)) -
+      Number(formatSalary(a.dataset.salary))
+    );
+  });
+
+  listItemsArraySorted.forEach((item) => {
+    list.appendChild(item);
+  });
+}
+
+sortList(listOfNames);
+
+function getEmployeeList(list) {
+  const employeeList = [];
+
+  list.querySelectorAll('li').forEach((item) => {
+    employeeList.push({
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: formatSalary(item.dataset.salary),
+      age: item.dataset.age,
+    });
+  });
+
+  return employeeList;
+}
+
+getEmployeeList(listOfNames);
