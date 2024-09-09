9cf17408e48fd6cdba3df476f48defdd0ec5fd40
Burchak Dmitry <burchak.dmitry@icloud.com>
2024-09-09 18:20:43 +0300

    task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const items = [...document.querySelectorAll('[data-salary]')];
+const ul = document.querySelector('ul');
+
+function sortList(list) {
+  return list.sort(
+    (firstItem, secondItem) =>
+      getIterableItems(firstItem) - getIterableItems(secondItem),
+  );
+}
+
+function getIterableItems(item) {
+  const result = item
+    .getAttribute('data-salary')
+    .split('')
+    .filter((el) => Number.isInteger(+el))
+    .join('');
+
+  return parseInt(result);
+}
+
+const newList = sortList(items).reverse();
+
+newList.forEach((item) => ul.appendChild(item));
