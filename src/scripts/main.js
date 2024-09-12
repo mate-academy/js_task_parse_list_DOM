c256c3e8d0da81aeb8e511d9e8405d04efc3c333
Heorhii Lytvynenko <104064704+Grifion@users.noreply.github.com>
2024-09-12 02:43:55 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+const employees = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  const sorted = list.sort(
+    (a, b) => toInt(b.dataset.salary) - toInt(a.dataset.salary),
+  );
+
+  ul.append(...sorted);
+
+  function toInt(str) {
+    return parseInt(str.replace(/[$,]/g, ''));
+  }
+}
+
+function getEmployees(list) {
+  const employeesList = list.map((value) => ({
+    name: value.innerText,
+    position: value.dataset.position,
+    salary: value.dataset.salary,
+    age: value.dataset.age,
+  }));
+
+  return employeesList;
+}
+
+sortList(employees);
+getEmployees(employees);
