3049978c92f725b5a57ec76d3026decc2c911c85
dima <dima98doroshenko@gmail.com>
2024-09-14 12:16:50 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const usersArray = [...document.querySelectorAll('[data-salary]')];
+const usersList = document.querySelector('ul');
+
+function stringToInt(string) {
+  return parseInt(string);
+}
+
+function sortListOfSalary(usersArrayForSort) {
+  return usersArrayForSort.sort((a, b) => {
+    const salaryA = stringToInt(
+      a.dataset.salary.replace('$', '').replace(',', ''),
+    );
+    const salaryB = stringToInt(
+      b.dataset.salary.replace('$', '').replace(',', ''),
+    );
+
+    return salaryB - salaryA;
+  });
+}
+
+sortListOfSalary(usersArray);
+
+function createEmployeesArray(sortsArray) {
+  usersList.innerHTML = '';
+
+  sortsArray.forEach((user) => {
+    const li = document.createElement('li');
+
+    li.textContent = user.textContent;
+    li.dataset.position = user.dataset.position;
+    li.dataset.age = user.dataset.age;
+
+    usersList.appendChild(li);
+  });
+}
+
+createEmployeesArray(sortListOfSalary(usersArray));
