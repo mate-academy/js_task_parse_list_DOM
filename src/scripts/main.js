f8a196373b88baaf92fb7502d9035e181e49ac1f
Anastasiia Tatarchenko <neveykina.nastya@gmail.com>
2024-09-18 13:47:42 +0200

    task_done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const list = [...document.querySelectorAll('li')];
+const ul = document.querySelector('ul');
+
+function parseSalary(data) {
+  return +data.dataset.salary.slice(1).replace(/,/g, '');
+}
+
+function sortList(param) {
+  param.sort((a, b) => {
+    return parseSalary(b) - parseSalary(a);
+  });
+
+  param.forEach((li) => {
+    ul.append(li);
+  });
+}
+
+function getEmployees(param) {
+  return param.map((employee) => {
+    return {
+      name: employee.textContent.trim(''),
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    };
+  });
+}
+
+sortList(list);
+getEmployees(list);
+
