88fd1e7cb342bb101b15954092204ff07f5f70cb
Ruslana Berkunova <rberknova@gmail.com>
2024-09-18 14:33:00 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+function getSalaryFromData(item) {
+  let salary = item.getAttribute('data-salary');
+
+  salary = salary.replace(/[$,]/g, '');
+
+  return parseInt(salary, 10);
+}
+
+function sortList(list) {
+  const items = Array.from(list.querySelectorAll('li'));
+
+  const sortedItems = items.sort((a, b) => {
+    return getSalaryFromData(b) - getSalaryFromData(a);
+  });
+
+  list.innerHTML = '';
+  sortedItems.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  const items = Array.from(list.querySelectorAll('li'));
+
+  return items.map((item) => ({
+    position: item.getAttribute('data-position'),
+    salary: getSalaryFromData(item),
+    age: parseInt(item.getAttribute('data-age'), 10),
+  }));
+}
+
+const employeeList = document.querySelector('ul');
+
+sortList(employeeList);
+getEmployees(employeeList);
