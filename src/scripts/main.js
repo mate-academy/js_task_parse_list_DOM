5c8b2969492e6f14872876d52596d536804b9787
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2024-09-20 16:01:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const items = [...document.querySelectorAll('li')];
+
+const convertToNumber = (str) => {
+  return Number(str.split('$').join('').split(',').join(''));
+};
+
+function sortList(list) {
+  const listOfPeople = document.querySelector('ul');
+
+  list
+    .sort(
+      (item1, item2) =>
+        convertToNumber(item2.dataset.salary) -
+        convertToNumber(item1.dataset.salary),
+    )
+    .forEach((person) => listOfPeople.appendChild(person));
+}
+
+function getEmployees(list) {
+  return list.map((item) => {
+    return {
+      name: item.innerText,
+      age: +item.dataset.age,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+    };
+  });
+}
+
+sortList(items);
+getEmployees(items);
