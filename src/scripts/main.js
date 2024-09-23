5bfd47ed1bc2bfc1fa848ecc2d151a74de53789a
Tetiana Skuratovska <tanya.skuratovskaya@gmail.com>
2024-09-23 14:14:43 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const employeeList = document.querySelector('ul');
+const listItems = Array.from(employeeList.querySelectorAll('li'));
+const sortedItems = listItems.sort((a, b) => {
+  const salaryA = +a.dataset.salary.replace(/[$,]/g, '');
+  const salaryB = +b.dataset.salary.replace(/[$,]/g, '');
+
+  return salaryB - salaryA;
+});
+
+employeeList.innerHTML = '';
+
+sortedItems.forEach((item) => {
+  employeeList.appendChild(item);
+});
