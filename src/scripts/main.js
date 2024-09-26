aab04b5da94448a28679f57b22ebd6923b0bb0f4
Stas Borychevskyi <sborichevskiy74@gmail.com>
2024-09-26 15:17:01 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+function sortList() {
+  const employeeList = document.querySelector('ul');
+  const employees = Array.from(employeeList.children).map((li) => {
+    return {
+      name: li.textContent,
+      position: li.dataset.position,
+      salary: parseFloat(li.dataset.salary.replace(/[$,]/g, '')),
+      age: parseInt(li.dataset.age, 10),
+    };
+  });
+
+  employees.sort((a, b) => b.salary - a.salary);
+
+  employeeList.innerHTML = '';
+
+  employees.forEach((employee) => {
+    const li = document.createElement('li');
+
+    li.dataset.position = employee.position;
+    li.dataset.salary = `$${employee.salary.toLocaleString()}`;
+    li.dataset.age = employee.age;
+    li.textContent = employee.name;
+    employeeList.appendChild(li);
+  });
+}
+
+function getArrayOfEmployees() {
+  return document.querySelector('ul');
+}
+
+sortList();
+getArrayOfEmployees();
