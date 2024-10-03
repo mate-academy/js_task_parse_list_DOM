c01b616707579db08c42bc3f02423637ee79d5f6
dmytrochernega <dimadneprfdb@gmail.coma>
2024-10-03 20:55:58 +0300

    created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const lists = document.querySelector('ul');
+const listElement = Array.from(lists.querySelectorAll('li'));
+const sortedEl = listElement.sort((a, b) => {
+    const salaryA = +a.dataset.salary.replace(/[$,]/g, '');
+    const salaryB = +b.dataset.salary.replace(/[$,]/g, '');
+
+    return salaryB - salaryA;
+})
+
+ lists.innerHTML = '';
+
+sortedEl.forEach((el) => {
+    lists.appendChild(el);
+ });
