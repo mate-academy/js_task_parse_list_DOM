610c5b17d481af30c97e946954f9982eb8ac0f26
an-marryKyslenko <olih.a@icloud.com>
2024-10-04 14:49:36 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const employeesCollection = list.querySelectorAll('li');
+
+function getEmploees() {
+  return [...employeesCollection].map((employee) => ({
+    name: employee.innerText,
+    age: employee.dataset.age,
+    position: employee.dataset.position,
+    salary: employee.dataset.salary,
+  }));
+}
+
+function sortBySalary(employees) {
+  list.innerHTML = '';
+
+  employees
+    .sort((a, b) => {
+      const aSalary = a.salary.slice(1).split(',').join('');
+      const bSalary = b.salary.slice(1).split(',').join('');
+
+      return +bSalary - +aSalary;
+    })
+    .map(({ name: employeeName, age, position, salary }) => {
+      const employeeElement = document.createElement('li');
+
+      employeeElement.innerHTML = `
+      <li
+        data-position=${position}
+        data-salary=${salary}
+        data-age=${age}
+      >
+        ${employeeName}
+      </li>
+    `;
+      list.appendChild(employeeElement);
+    });
+}
+
+const sortedEmployees = getEmploees();
+
+sortBySalary(sortedEmployees);
