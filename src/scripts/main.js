e633cbf2b8a7c201119f18e79d0320f7b1249345
Ivan Paiareli <ipayareli@gmail.com>
2024-10-06 12:21:08 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const employees = document.querySelectorAll('li');
+const listOfEmployees = document.querySelector('ul');
+
+function toNum(str) {
+  const num = str.slice(1).split(',').join('');
+
+  return +num;
+}
+
+function getEmployees(workersArr) {
+  return [...workersArr].sort(
+    (a, b) => toNum(b.dataset.salary) - toNum(a.dataset.salary),
+  );
+}
+
+const sortedEmployees = getEmployees(employees);
+
+sortedEmployees.forEach((employee) => listOfEmployees.append(employee));
