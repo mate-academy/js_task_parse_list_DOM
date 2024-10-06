03400b32a34781e6c26f53ff0e3284296ccbe3d4
Nikita Serdyukov <nikita.serdyukov.20033002@gmail.com>
2024-10-06 17:45:24 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const persons = document.querySelectorAll('li');
+const listPersons = document.querySelector('ul');
+
+function stringToNum(str) {
+  const num = str.slice(1).split(',').join('');
+
+  return Number(num);
+}
+
+function getEmployees(list) {
+  return [...list].sort((a, b) => {
+    return stringToNum(b.dataset.salary) - stringToNum(a.dataset.salary);
+  });
+}
+
+const sortList = getEmployees(persons);
+
+for (let el = 0; el < sortList.length; el++) {
+  listPersons.append(sortList[el]);
+}
