7eb7f47e22b654d62820b67c6cf1240f77d3b65f
Andrii Chornyi <ixxxx@i.ua>
2024-10-08 19:11:12 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+
+const listArray = Array.from(listItems).map((el) => {
+  return {
+    name: el.textContent.trim(),
+    position: el.getAttribute('data-position'),
+    salary: el.getAttribute('data-salary'),
+    age: el.getAttribute('data-age'),
+  };
+});
+
+listArray.sort((a, b) => {
+  const salaryA = parseInt(a.salary.replace(/[$,]/g, ''));
+  const salaryB = parseInt(b.salary.replace(/[$,]/g, ''));
+
+  return salaryB - salaryA;
+});
+
+const getEmployees = (sortList) => {
+  const arr = [];
+
+  for (const ch of sortList) {
+    arr.push(ch.name);
+  }
+
+  return arr;
+};
+
+const employees = getEmployees(listArray);
+console.log(employees);
