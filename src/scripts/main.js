2883b28fbba313418fec7676f3cfc3a6bab6005a
Igor <4430704@gmail.com>
2024-10-08 12:17:13 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const employeesList = [...document.querySelectorAll('li')];
+const employeesListContainer = document.querySelector('ul');
+
+const numberFormat = (el) => Number(el.substring(1).replaceAll(',', ''));
+
+const sortList = (list) => {
+  list.sort(
+    (a, b) => numberFormat(b.dataset.salary) - numberFormat(a.dataset.salary),
+  );
+
+  return list;
+};
+
+const getEmployeesObjects = function (list) {
+  const employees = [];
+
+  for (const el of list) {
+    const employee = {
+      name: el.textContent.replaceAll('\n', '').trim(),
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    };
+
+    employees.push(employee);
+  }
+
+  return employees;
+};
+
+const sortedList = sortList(employeesList);
+
+getEmployeesObjects(sortedList);
+
+employeesListContainer.append(...sortedList);
