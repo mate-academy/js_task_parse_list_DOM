3b7f1931b487f2db60d7435ba4365ed77650602b
Your_Name Your_Surname <your@email>
2024-10-08 13:03:30 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const lists = [...document.querySelectorAll('li')];
+
+function getNumber(str) {
+  const toNumber = str.replace(/[$ m,]/g, '');
+
+  return Number(toNumber);
+}
+
+function sortList(list) {
+  const value = document.querySelector('ul');
+
+  list
+    .sort((a, b) => getNumber(b.dataset.salary) - getNumber(a.dataset.salary))
+    .forEach((el) => value.appendChild(el));
+}
+
+function getEmployees(list) {
+  return list.map((item) => {
+    return {
+      name: item.innerText,
+      age: +item.dataset.age,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+    };
+  });
+}
+
+sortList(lists);
+getEmployees(lists);
