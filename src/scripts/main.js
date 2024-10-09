785436b409dfcf9d69f8e3f5cd5c90f925186ec3
Taras Niklevych <niklevychtaras@gmail.com>
2024-10-09 14:40:34 +0300

    add solution to task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const listItems = [...document.querySelectorAll('li')];
+const employeesList = document.querySelector('ul');
+
+function toNum(string) {
+  const dolarString = string;
+  const numberString = dolarString.replace('$', '').replace(',', '');
+
+  return +numberString;
+}
+
+function sortBySalary(items, list) {
+  items.sort((a, b) => toNum(b.dataset.salary) - toNum(a.dataset.salary));
+
+  list.inerHTML = '';
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(items) {
+  return items.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+}
+
+sortBySalary(listItems, employeesList);
+
+getEmployees(listItems);
