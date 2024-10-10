fa6c8b416e980462aff53331b1187c9e3f50864e
Andrii Chornyi <ixxxx@i.ua>
2024-10-10 13:43:43 +0300

    SOLution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,40 +1,27 @@
 'use strict';
 
-const listItems = document.querySelectorAll('li');
+let listItems = document.querySelectorAll('li');
 
-const sortList = () => {
-  const arr = Array.from(listItems);
+const sortList = Array.from(listItems).sort((a, b) => {
+  const salaryA = parseInt(a.getAttribute('data-salary').replace(/[$,]/g, ''));
+  const salaryB = parseInt(b.getAttribute('data-salary').replace(/[$,]/g, ''));
 
-  return arr.sort((a, b) => {
-    const salaryA = parseInt(
-      a.getAttribute('data-salary').replace(/[$,]/g, ''),
-    );
-    const salaryB = parseInt(
-      b.getAttribute('data-salary').replace(/[$,]/g, ''),
-    );
+  return salaryB - salaryA;
+});
 
-    return salaryB - salaryA;
-  });
-};
 
-const getEmployees = (sortedArr) => {
-  const arr = [];
+const getEmployees = (list) => {
 
-  const listArray = sortedArr.map((el) => {
+  return list.map((el) => {
+
     return {
       name: el.textContent.trim(),
       position: el.getAttribute('data-position'),
       salary: el.getAttribute('data-salary'),
       age: el.getAttribute('data-age'),
     };
-  });
+  })
+}
 
-  for (const ch of listArray) {
-    arr.push(ch.name);
-  }
-
-  return arr;
-};
-
-sortList();
+listItems();
 getEmployees();
