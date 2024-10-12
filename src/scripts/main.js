12d9cf66e06947027fd4251b3e22f171572ff920
BeginerAlex <aleksander9677@gmail.com>
2024-10-12 09:57:59 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const listItems = [...document.querySelectorAll('li')];
+const employeesList = document.querySelector('ul');
+
+function convertToNumber(string) {
+  const salaryString = string;
+  const salaryNumber = salaryString.replace('$', '').replace(',', '');
+
+  return +salaryNumber;
+}
+
+function sortBySalary(items, list) {
+  items.sort(
+    (a, b) =>
+      convertToNumber(b.dataset.salary) - convertToNumber(a.dataset.salary),
+  );
+
+  list.innerHTML = '';
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(items) {
+  return items.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+}
+
+sortBySalary(listItems, employeesList);
+
+getEmployees(listItems);
