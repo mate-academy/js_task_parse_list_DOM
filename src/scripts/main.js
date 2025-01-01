a0a5b81b99b4441da182a2247261736149c7eb9b
Саша <martinecsasa5@gmail.com>
2025-01-01 12:16:53 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const array = [...document.querySelectorAll('li')];
+
+const toNum = (el) => {
+  return parseFloat(el.replace(/[$,]/g, ''));
+};
+
+const sortList = (list) => {
+  list.sort((a1, a2) => toNum(a2.dataset.salary) - toNum(a1.dataset.salary));
+
+  list.forEach((element) => {
+    document.querySelector('ul').insertAdjacentElement('beforeend', element);
+  });
+};
+
+const getEmployees = (list) => {
+  const result = [];
+
+  list.forEach((element) => {
+    result[result.length] = {
+      name: element.textContent,
+      position: element.dataset.position,
+      salary: element.dataset.salary,
+      age: element.dataset.age,
+    };
+  });
+
+  return result;
+};
+
+sortList(array);
+getEmployees(array);
