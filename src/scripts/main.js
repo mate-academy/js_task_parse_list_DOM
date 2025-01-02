d48d367dcf533b31b35f64265267c52d5a18d331
Blervin <blervin121212@gmail.com>
2025-01-02 22:30:23 +0100

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const employees = document.querySelector('ul');
+const num = (string) => Number(string.replace(/[$,]/g, ''));
+
+function sortList(list) {
+  const sortedPeopleList = [...list.children].sort(
+    (previousPerson, currentPerson) =>
+      num(currentPerson.dataset.salary) - num(previousPerson.dataset.salary),
+  );
+
+  return list.append(...sortedPeopleList);
+}
+
+function getEmployees(list) {
+  return [...list.children].map((person) => ({
+    name: person.innerText,
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+}
+
+sortList(employees);
+getEmployees(employees);
