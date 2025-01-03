c95d5874594ddbd9797a6ecbdf694b9e00775f61
Nikolay <diedaemon@gmail.com>
2025-01-03 23:47:51 +0100

    Task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+const convertToString = (string) => {
+  return string.slice(1).split(',').join('');
+};
+
+function sortList(list) {
+  const sortedList = [...list.children].sort(
+    (a, b) =>
+      convertToString(a.dataset.salary) - convertToString(b.dataset.salary),
+  );
+
+  return list.append(...sortedList);
+}
+
+function getEmployees(list) {
+  return [...list.children].map((person) => ({
+    name: person.innerHTML.trim(),
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  }));
+}
+
+sortList(employeesList);
+getEmployees(employeesList);
