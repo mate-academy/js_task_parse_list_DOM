650d1afae70edbb4243fc6b466d114023f72816d
Volodymyr Makhonin <zavozi@gmail.com>
2025-01-12 15:57:26 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+
+function parseSalary(salary) {
+  return parseInt(salary.replace(/[^0-9]/g, ''), 10);
+}
+
+function sortList(list) {
+  const employees = Array.from(list.children);
+
+  employees.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  employees.forEach((employee) => list.appendChild(employee));
+}
+
+function getEmployees(list) {
+  return Array.from(list.children).map((employee) => ({
+    name: employee.textContent.trim(),
+    position: employee.dataset.position,
+    salary: parseSalary(employee.dataset.salary),
+    age: parseInt(employee.dataset.age, 10),
+  }));
+}
+
+sortList(employeesList);
+
+getEmployees(employeesList);
