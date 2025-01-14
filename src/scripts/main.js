7c34801d3216ef80cb458190208714c2a1ba8046
Dmytro <dmytrokovaliak@gmail.com>
2025-01-14 04:07:43 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+function replaceCharOuter(string) {
+  let stringOuter = string;
+
+  function replaceCharInner(char) {
+    stringOuter = stringOuter.replaceAll(char, '');
+  }
+
+  return {
+    deleteChar(char) {
+      return replaceCharInner(char);
+    },
+    value() {
+      return stringOuter;
+    },
+  };
+}
+
+const parentElement = document.querySelector('ul');
+const elements = [...document.querySelectorAll('li')];
+
+elements.forEach((element) => {
+  const charsToDelete = [',', '$'];
+  const replaceChar = replaceCharOuter(element.dataset.salary);
+
+  charsToDelete.forEach((char) => replaceChar.deleteChar(char));
+
+  element.dataset.salary = replaceChar.value();
+});
+
+elements.sort(
+  (a, b) => parseInt(b.dataset.salary) - parseInt(a.dataset.salary),
+);
+
+parentElement.innerHTML = '';
+
+elements.forEach((element) => {
+  parentElement.appendChild(element);
+});
