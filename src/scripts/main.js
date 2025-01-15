f0d75195312f7db36bdd4809167e013fdb1bb8a1
Anton Tur <ant.tur85@gmail.com>
2025-01-15 18:27:23 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const parentElement = document.querySelector('ul');
+const list = document.querySelectorAll('li');
+
+function transformSalaryToNumber(string) {
+  return +string.slice(1).split(',').join('');
+}
+
+function sortList(data) {
+  const arrayList = Array.from(data);
+
+  arrayList
+    .sort((a, b) => {
+      return -(
+        transformSalaryToNumber(a.dataset.salary) -
+        transformSalaryToNumber(b.dataset.salary)
+      );
+    })
+    .map((item) => {
+      parentElement.appendChild(item);
+    });
+}
+
+function getEmployees(data) {
+  return Array.from(data).map((item) => ({
+    name: item.innerText,
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+}
+
+sortList(list);
+getEmployees(list);
