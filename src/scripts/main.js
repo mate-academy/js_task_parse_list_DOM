98f2e5b09b575be71a092d0efb66f735064586fd
Yurii Filiushkin <yurii.filiushkin@gmail.com>
2025-01-16 14:56:36 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const items = Array.from(list.children);
+
+getEmployees(list, sortList(items));
+
+function sortList(someList) {
+  return someList.sort((a, b) => {
+    return (
+      stringToNumber(b.getAttribute('data-salary')) -
+      stringToNumber(a.getAttribute('data-salary'))
+    );
+  });
+}
+
+function stringToNumber(string) {
+  return Number(string.replace('$', '').replaceAll(',', ''));
+}
+
+function getEmployees(node, itemsOfNode) {
+  node.innerHTML = '';
+
+  itemsOfNode.forEach((item) => {
+    list.appendChild(item);
+  });
+}
