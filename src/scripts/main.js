3a5e159394109cfdadd709b10bce8d1754ceab56
Oleh Mysko <olegmysko1996@gmail.com>
2025-01-17 18:52:04 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,50 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function convertSalaryToNumber(salaryString) {
+  return parseInt(salaryString.replace('$', '').replace(/,/g, ''), 10);
+}
+
+function sortList() {
+  const listItems = Array.from(list.children);
+
+  listItems.sort((a, b) => {
+    const salaryA = convertSalaryToNumber(a.getAttribute('data-salary'));
+    const salaryB = convertSalaryToNumber(b.getAttribute('data-salary'));
+
+    return salaryB - salaryA;
+  });
+
+  listItems.forEach((item) => {
+    list.appendChild(item);
+  });
+}
+
+function getEmployees() {
+  const employees = [];
+
+  const listItems = Array.from(list.children);
+
+  listItems.forEach((item) => {
+    const first = item.textContent.trim();
+    const position = item.getAttribute('data-position');
+    const salary = convertSalaryToNumber(item.getAttribute('data-salary'));
+    const age = parseInt(item.getAttribute('data-age'), 10);
+
+    const employee = {
+      first,
+      position,
+      salary,
+      age,
+    };
+
+    employees.push(employee);
+  });
+
+  return employees;
+}
+
+getEmployees(list);
+
+sortList(list);
