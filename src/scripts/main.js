b58b746f4c73e4659f6ed584f11013fbca47347b
Olha Lishenko <lishenko.olha@gmail.com>
2025-01-29 20:35:08 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('li')];
+const employeesList = document.querySelector('ul');
+
+const sortList = () => {
+  employees.sort((a, b) => {
+    const salaryA = parseInt(
+      a.getAttribute('data-salary').replace(/\$|,/g, ''),
+    );
+    const salaryB = parseInt(
+      b.getAttribute('data-salary').replace(/\$|,/g, ''),
+    );
+
+    return salaryB - salaryA;
+  });
+};
+
+sortList(employees);
+
+employeesList.innerHTML = '';
+
+employees.forEach((employee) => employeesList.appendChild(employee));
+
+const getEmployees = () => {
+  return employees.map((employee) => ({
+    position: employee.getAttribute('data-position'),
+    salary: employee.getAttribute('data-salary'),
+    age: employee.getAttribute('data-age'),
+    name: employee.textContent.replace(/\s/g, ''),
+  }));
+};
+
+getEmployees(employees);
