4ca377d9aae045004e873ccab1cbb439b32c169e
Nadiia Bulmak <nadgaval@gmail.com>
2025-02-03 17:06:25 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+
+const ul = document.querySelector('ul');
+const list = [...ul.children];
+
+function sortList(employeeList) {
+  employeeList.forEach((item) => {
+    item.dataset.salary = parseInt(
+      item.dataset.salary.replace('$', '').replace(',', ''),
+      10,
+    );
+  });
+
+  employeeList.sort((a, b) => b.dataset.salary - a.dataset.salary);
+}
+
+function getEmployees(employeeList) {
+  ul.innerHTML = '';
+  employeeList.forEach((item) => ul.appendChild(item));
+}
+
+sortList(list);
+getEmployees(list);
