ddfca40fc79209b9cfe91cb5591c3985fb232d8b
VadimKudrenko <48049362+VadimKudrenko@users.noreply.github.com>
2025-02-07 14:42:52 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write code here
+function sortList() {
+  const list = [...document.querySelectorAll('ul > li')];
+
+  list.sort((item, toCompare) => {
+    const getFirstSalary = getSalary(item);
+    const getSalaryToCompare = getSalary(toCompare);
+
+    return getSalaryToCompare - getFirstSalary;
+  });
+
+  document.querySelector('ul').innerHTML = '';
+
+  list.forEach((item) => {
+    document.querySelector('ul').appendChild(item);
+  });
+}
+
+function getSalary(item) {
+  return item.getAttribute('data-salary').slice(1).replace(/,/g, '.');
+}
+
+function getEmployees() {
+  const list = [...document.querySelectorAll('ul > li')];
+  const employees = list.map((item) => {
+    const employeeName = item.innerText;
+    const position = item.getAttribute('data-position');
+    const salary = item.getAttribute('data-salary');
+    const age = item.getAttribute('data-age');
+
+    return {
+      employeeName,
+      position,
+      salary,
+      age,
+    };
+  });
+
+  return employees;
+}
+
+sortList();
+getEmployees();
