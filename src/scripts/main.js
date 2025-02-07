d26139d86ca645314757e13523316ab798eb61f4
Egor Mamtsev <egormamcev@MacBook-Air.local>
2025-02-07 11:20:40 -0600

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const allEmployes = [...document.querySelectorAll('[data-salary]')];
+const papa = document.querySelector('ul');
+
+allEmployes.sort(
+  (a, b) =>
+    b.getAttribute('data-salary').replace(/[$,]/g, '') -
+    a.getAttribute('data-salary').replace(/[$,]/g, ''),
+);
+
+allEmployes.forEach((item) => papa.appendChild(item));
+
+function getEmployees(collection) {
+  const result = [];
+
+  collection.forEach((item) => {
+    const employ = {
+      name: item.textContent,
+      position: item.getAttribute('data-position'),
+      salary: item.getAttribute('data-salary'),
+      age: item.getAttribute('data-age'),
+    };
+
+    result.push(employ);
+  });
+
+  return result;
+}
+getEmployees(allEmployes);
