cba259b020ed3551ad91002d01e9de80becf5b66
David Moskalenko <dvdmsk21@gmail.com>
2025-02-14 22:38:12 +0200

    Update README and implement employee sorting functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
 // write code here
+const employees = document.querySelectorAll('li');
+
+sortEmployees();
+getAtributesEmployee();
+
+function sortEmployees() {
+  const sortedEmployees = Array.from(employees).sort((a, b) => {
+    const salaryA = parseFloat(
+      a.getAttribute('data-salary').replace(/[$,]/g, ''),
+    );
+    const salaryB = parseFloat(
+      b.getAttribute('data-salary').replace(/[$,]/g, ''),
+    );
+
+    return salaryB - salaryA;
+  });
+
+  const ul = document.querySelector('ul');
+
+  ul.innerHTML = '';
+
+  sortedEmployees.forEach((employee) => {
+    ul.appendChild(employee);
+  });
+}
+
+function getAtributesEmployee() {
+  const objEmployees = [];
+
+  for (let i = 0; i < employees.length; i++) {
+    objEmployees.push({
+      name: employees[i].textContent.trim(),
+      position: employees[i].dataset.position,
+      salary: employees[i].dataset.salary,
+      age: employees[i].dataset.age,
+    });
+  }
+
+  return objEmployees;
+}
