99c599856a7d00541a7dcfd44aa9f84bc8ff6bb8
Vasyl <priymak17ukrvaha@gmail.com>
2025-02-18 15:59:20 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const nativeList = document.querySelector('ul');
+
+function getEmployees(list) {
+  return Array.from(list).map((item) => ({
+    element: item,
+    salary: formatSalary(item.dataset.salary),
+  }));
+}
+
+function sortList(employees) {
+  return employees.sort((a, b) => b.salary - a.salary);
+}
+
+function formatSalary(salary) {
+  return Number(salary.replace(/[$,]/g, ''));
+}
+
+const sortedEmployees = sortList(getEmployees(nativeList.children));
+
+nativeList.innerHTML = '';
+sortedEmployees.forEach((employee) => nativeList.appendChild(employee.element));
