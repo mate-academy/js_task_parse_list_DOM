f150fd9206ab91352fb6d1e5da95d08666e32f6e
Nazar Shpylovyi <shpiloviy.nazar@ukr.net>
2025-02-24 16:19:26 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
-'use strict';
+function convertSalaryToNumber(salaryString) {
+  return Number(salaryString.replace(/[^0-9.-]+/g, ''));
+}
 
-// write code here
+function sortList(list) {
+  const items = Array.from(list.children);
+
+  items.sort((a, b) => {
+    const salaryA = convertSalaryToNumber(a.getAttribute('data-salary'));
+    const salaryB = convertSalaryToNumber(b.getAttribute('data-salary'));
+
+    return salaryB - salaryA;
+  });
+
+  list.innerHTML = '';
+
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  const items = Array.from(list.children);
+
+  return items.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.getAttribute('data-position'),
+    salary: convertSalaryToNumber(item.getAttribute('data-salary')),
+    age: Number(item.getAttribute('data-age')),
+  }));
+}
+
+const employeeList = document.querySelector('ul');
+
+sortList(employeeList);
+
+const employeesArray = getEmployees(employeeList);
+
+// eslint-disable-next-line no-console
+console.log('Sorted List:', employeeList);
+
+// eslint-disable-next-line no-console
+console.log('Employees Array:', employeesArray);
