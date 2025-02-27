0cd4384ceebbb466ecadd1daa01aff3a91539aaf
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-02-27 18:14:40 +0100

    taskParseList

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
-'use strict';
+function parseSalary(salaryString) {
+  return parseInt(salaryString, 10);
+}
 
-// write code here
+function sortList(list) {
+  const sortedList = Array.from(list).sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  sortedList.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  const employees = Array.from(list).map((item) => {
+    return {
+      name: item.dataset.name,
+      position: item.dataset.position,
+      salary: parseSalary(item.dataset.salary),
+      age: item.dataset.age,
+    };
+  });
+
+  return employees;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const employeeList = document.querySelectorAll('.employee');
+
+  sortList(employeeList);
+
+  const employees = getEmployees(employeeList);
+
+  alert(JSON.stringify(employees, null, 2));
+});
