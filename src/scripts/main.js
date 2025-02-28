ce7a571ca35cdbe4d81764dad0984e6cc6418860
SofiiaPelenska <soniapelenskaoe2002@gmail.com>
2025-02-28 22:37:12 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const ulList = document.querySelector('ul');
+const employee = Array.from(ulList.querySelectorAll('li'));
+
+function sort() {
+  employee.sort((a, b) => {
+    return (
+      parseInt(b.dataset.salary.replace(/[$,]/g, '')) -
+      parseInt(a.dataset.salary.replace(/[$,]/g, ''))
+    );
+  });
+
+  ulList.innerHTML = '';
+  employee.forEach((item) => ulList.appendChild(item));
+}
+
+function getEmployees() {
+  return employee.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: parseInt(item.dataset.salary.replace(/[$,]/g, '')),
+    age: parseInt(item.dataset.age),
+  }));
+}
+
+sort();
+getEmployees();
