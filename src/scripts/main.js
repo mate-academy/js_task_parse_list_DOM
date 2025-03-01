4dd41ae27494f3771fb113289f20da977b54c802
Maryna Kravchuk <marinalinkova02@gmail.com>
2025-03-01 21:56:40 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function getSalary(item) {
+  return Number(item.dataset.salary.split('$').join('').split(',').join(''));
+}
+
+function sortList() {
+  const items = [...list.children];
+
+  items.sort((a, b) => getSalary(b) - getSalary(a));
+
+  list.append(...items);
+}
+
+function getEmployees() {
+  return [...list.children].map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: getSalary(item),
+    age: Number(item.dataset.age),
+  }));
+}
+
+sortList(list);
+getEmployees(list);
