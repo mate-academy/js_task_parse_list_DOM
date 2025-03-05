c165f6254abd31511ed23b13c60f305b01500bfd
Mariia Koval <m.kovalmaria@gmail.com>
2025-03-05 16:03:53 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const parseSalary = (salaryStr) => {
+  return parseInt(salaryStr.replace(/[$,]/g, ''), 10);
+};
+
+const sortList = (list) => {
+  const items = Array.from(list.children);
+
+  items.sort((el1, el2) => {
+    const salary1 = parseSalary(el1.getAttribute('data-salary'));
+    const salary2 = parseSalary(el2.getAttribute('data-salary'));
+
+    return salary2 - salary1;
+  });
+
+  list.innerHTML = '';
+  items.forEach((item) => list.appendChild(item));
+};
+
+const ul = document.querySelector('ul');
+
+sortList(ul);
