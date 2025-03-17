13b2e970c750d0a53419605f16e51703952875e9
Ostap Danyliv <danyliv.ostap@gmail.com>
2025-03-17 12:43:57 -0500

    sorting impl

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const employees = Array.from(list);
+
+const sortedEmployees = employees.sort((a, b) => {
+  const salaryA = parseInt(a.getAttribute('data-salary').replace(/[$,]/g, ''));
+  const salaryB = parseInt(b.getAttribute('data-salary').replace(/[$,]/g, ''));
+
+  return salaryB - salaryA;
+});
+
+const ul = document.querySelector('ul');
+
+ul.innerHTML = '';
+
+sortedEmployees.forEach((employee) => ul.appendChild(employee));
