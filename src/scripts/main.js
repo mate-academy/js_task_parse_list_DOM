a27d168193fe5b28c77733e38b1ad278619ff304
Viktor Shchehelskyi <victorshegelsky@gmail.com>
2025-03-22 16:45:19 +0200

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+function sortList(list) {
+  const items = [...list.children];
+
+  items.sort((a, b) => {
+    return (
+      Number(b.getAttribute('data-salary').replace(',', '').replace('$', '')) -
+      Number(a.getAttribute('data-salary').replace(',', '').replace('$', ''))
+    );
+  });
+
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  const items = [...list.children];
+  const arr = [];
+
+  for (const li of items) {
+    arr.push({
+      name: li.textContent.trim(),
+      position: li.dataset.position,
+      salary: li.dataset.salary,
+      age: li.dataset.age,
+    });
+  }
+
+  return arr;
+}
+
+getEmployees(document.querySelector('ul'));
+sortList(document.querySelector('ul'));
