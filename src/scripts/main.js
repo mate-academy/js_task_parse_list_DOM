3a73ad1059a7cede8fb3d84766944331d6d127cb
Yurii Chain <yuriichaiun@gmail.com>
2025-03-26 10:51:37 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const listEmployees = [...document.querySelectorAll('li')];
+
+function getEmployees(list) {
+  const employees = [];
+
+  list.forEach((elem) => {
+    const employee = {
+      name: elem.innerHTML.trim(),
+      position: elem.getAttribute(['data-position']),
+      salary: elem.getAttribute(['data-salary']),
+      age: elem.getAttribute(['data-age']),
+    };
+
+    employees.push(employee);
+  });
+
+  return employees;
+}
+
+function sortList(list) {
+  const listSort = list.sort(
+    (a, b) =>
+      convertSalary(b.getAttribute(['data-salary'])) -
+      convertSalary(a.getAttribute(['data-salary'])),
+  );
+
+  for (const li of listSort) {
+    document.querySelector('ul').append(li);
+  }
+}
+
+function convertSalary(salary) {
+  return +salary.slice(1).split(',').join('');
+}
+
+getEmployees(listEmployees);
+sortList(listEmployees);
