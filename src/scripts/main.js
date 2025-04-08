02e157b5eccf7aa5cbced89053cfd74e4b986906
AVyhaa <artemvyhovanec1@gmail.com>
2025-04-08 10:14:06 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+function sortList(employees) {
+  const sortedEmployeesList = Object.values(employees).sort(
+    (firstEmployee, secondEmployee) => {
+      return parseNumber(firstEmployee) - parseNumber(secondEmployee);
+    },
+  );
+
+  return sortedEmployeesList;
+}
+
+function parseNumber(employee) {
+  let salary = employee.dataset.salary;
+
+  salary = salary.replaceAll('$', '');
+  salary = salary.replaceAll(',', '');
+
+  return parseInt(salary);
+}
+
+function changeEmployeesOrder(employees) {
+  const ulElement = document.querySelector('ul');
+
+  for (const nodeEmployee of employees) {
+    ulElement.prepend(nodeEmployee);
+  }
+}
+
+const employeesElements = document.querySelectorAll('ul li');
+const sortedEmployees = sortList(employeesElements);
+
+changeEmployeesOrder(sortedEmployees);
