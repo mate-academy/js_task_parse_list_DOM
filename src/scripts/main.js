453d580f090fbce6b2b0d4d5d52d674371306b4b
Yuliia Babych <jl.babych29@gmail.com>
2025-04-09 21:47:54 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const listDoc = document.querySelector('ul');
+
+function getSalary(li) {
+  const salaryNumber = li.dataset.salary
+    .split('$')
+    .join('')
+    .split(',')
+    .join('');
+
+  return Number(salaryNumber);
+}
+
+function sortList(list) {
+  const arr = Array.from(list.children);
+
+  arr.sort((a, b) => getSalary(b) - getSalary(a));
+
+  arr.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  const listArr = Array.from(list.children);
+
+  return listArr.map((li) => ({
+    name: li.textContent.trim(),
+    position: li.dataset.position,
+    salary: getSalary(li),
+    age: li.dataset.age,
+  }));
+}
+
+sortList(listDoc);
+getEmployees(listDoc);
