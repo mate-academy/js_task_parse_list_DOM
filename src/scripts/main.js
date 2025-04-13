9d3b04b25c21b63166eef84d22d823801c07cc0e
Mykyta Landar <mykytalandar@gmail.com>
2025-04-13 03:22:34 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write code here
+function parseSalary(salaryStr) {
+  let salary = '';
+
+  for (const ch of salaryStr) {
+    if (ch === '$' || ch === ',') {
+      continue;
+    }
+    salary += ch;
+  }
+
+  return Number(salary);
+}
+
+function sortList(list) {
+  const items = [...list.children];
+
+  items.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  for (const item of items) {
+    list.appendChild(item);
+  }
+}
+
+function getEmployees(list) {
+  const items = [...list.children];
+
+  const employee = items.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: parseSalary(item.dataset.salary),
+    age: Number(item.dataset.age),
+  }));
+
+  return employee;
+}
+
+const ul = document.querySelector('ul');
+
+sortList(ul);
+
+getEmployees(ul);
