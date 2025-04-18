aee45780627cdff7486736f8c2eb07f454819cd2
Roman Shevchyshyn <romanshevchyshyn1998@gmail.com>
2025-04-18 11:31:09 +0200

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+function parseSalary(salary) {
+  return +salary.replace(/[^0-9.-]+/g, '');
+}
+
+const listElement = document.querySelector('ul');
+const employees = [...listElement.querySelectorAll('[data-salary]')];
+
+function sortList(listItems) {
+  listItems.sort((a, b) => {
+    return parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary);
+  });
+
+  listElement.innerHTML = '';
+  listItems.forEach((item) => listElement.appendChild(item));
+}
+
+function getEmployees(listItems) {
+  return listItems.map((el) => ({
+    name: el.innerText,
+    position: el.dataset.position,
+    salary: el.dataset.salary,
+    age: el.dataset.age,
+  }));
+}
+
+sortList(employees);
+getEmployees(employees);
