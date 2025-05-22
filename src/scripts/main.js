6f6fa2d0464513aea4c19af347be6bcccf43203a
Taras <taras.holotiuk@gmail.com>
2025-05-22 15:50:13 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+let list = Array.from(document.getElementsByTagName('li'));
+
+list = list.sort((a, b) => {
+  const aSalary = parseInt(
+    a.getAttribute('data-salary').replace(/[$,]/g, ''),
+    10,
+  );
+  const bSalary = parseInt(
+    b.getAttribute('data-salary').replace(/[$,]/g, ''),
+    10,
+  );
+
+  return bSalary - aSalary;
+});
+
+document.querySelector('ul').innerHTML = '';
+
+list.forEach((item) => {
+  document.querySelector('ul').appendChild(item);
+});
