42e91be86ccc2b29a22a7240aec5482f60adb3a7
Taras <luchakt2@gmail.com>
2025-05-28 15:30:18 +0300

    finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+window.addEventListener('DOMContentLoaded', () => {
+  const ul = document.querySelector('ul');
+
+  sortList(ul);
+  getEmployees(ul);
+});
+
+function sortList(list) {
+  const selector = Array.from(list.querySelectorAll('li'));
+
+  const sortSalary = selector.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  list.innerHTML = '';
+  sortSalary.forEach((item) => list.append(item));
+}
+
+function parseSalary(salaryStr) {
+  return Number(salaryStr.replace(/[$,]/g, ''));
+}
+
+function getEmployees(list) {
+  const selector = Array.from(list.querySelectorAll('li'));
+
+  return selector.map((li) => ({
+    name: li.textContent.trim(),
+  }));
+}
