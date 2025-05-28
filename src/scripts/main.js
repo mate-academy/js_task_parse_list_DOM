c313ccea1c6d9f3b2c3925ee4228c046c82f8f6e
Kostiantyn Pavlenko <kostianpv@gmail.com>
2025-05-28 22:29:58 +0300

    solved task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
+/* eslint-disable prettier/prettier */
 'use strict';
 
-// write code here
+const employeesDOMList = document.querySelectorAll('ul > li');
+
+function getEmployees() {
+  const employeesList = [];
+
+  employeesDOMList.forEach(emp => {
+    const entries = Object.entries(emp.dataset);
+    const employee = {};
+
+    entries.push(['name', emp.textContent.trim()]);
+
+    entries.forEach(entry => {
+      const [key, value] = entry;
+
+      employee[key] = value;
+    });
+
+    employeesList.push(employee);
+  });
+
+  return employeesList;
+}
+
+function salaryToNumber(salary) {
+  return salary.slice(1, salary.length).split(',').join('');
+}
+
+function sortEmployeeBySalary(employeesList) {
+  const copyEmployees = employeesList.map(emp => {
+    return { ...emp };
+  });
+
+  copyEmployees.sort((a, b) => {
+    const salaryA = salaryToNumber(a['salary']);
+    const salaryB = salaryToNumber(b['salary']);
+
+    return Number(salaryA) - Number(salaryB);
+  });
+
+  return copyEmployees;
+}
+
+const employees = getEmployees();
+
+sortEmployeeBySalary(employees);
