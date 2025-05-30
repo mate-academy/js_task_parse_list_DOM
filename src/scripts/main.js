3f85494c8c885de81c40927f14f2f889ac031c17
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-05-30 10:32:17 +0300

    main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('li')];
+
+function sortList(list) {
+  list.sort((a, b) => {
+    const aSalary = parseSalary(a.dataset.salary);
+    const bSalary = parseSalary(b.dataset.salary);
+
+    return bSalary - aSalary;
+  });
+
+  list.forEach((element) => {
+    document.querySelector('ul').appendChild(element);
+  });
+
+  return list;
+}
+
+function getEmployees(list) {
+  return list.map((element) => {
+    return {
+      name: element.dataset.name,
+      position: element.dataset.position,
+      salary: parseSalary(element.dataset.salary),
+      age: +element.dataset.age,
+    };
+  });
+}
+
+function parseSalary(salary) {
+  return +salary.replaceAll(',', '').replaceAll('$', '');
+}
+
+const sorted = sortList(employees);
+
+getEmployees(sorted);
