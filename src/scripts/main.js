0d35a8ccaafe555e356c3aee271d78963d2f747a
Yura <yurii.st11@gmail.com>
2025-06-08 14:54:51 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+function normalizeSalary(str) {
+  return Number(str.replace(/[^\d]/g, ''));
+}
+
+const list = document.querySelector('ul');
+const employees = Array.from(list.children);
+
+function sortList(arr) {
+  const sorted = arr.sort(
+    (a, b) =>
+      normalizeSalary(b.dataset.salary) - normalizeSalary(a.dataset.salary),
+  );
+
+  list.innerHTML = '';
+
+  return sorted.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(arr) {
+  return arr.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: normalizeSalary(item.dataset.salary),
+    age: +item.dataset.age,
+  }));
+}
+
+sortList(employees);
+getEmployees(employees);
