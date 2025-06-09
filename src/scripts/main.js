b202c138c5e70cf52dfb379f1c4bb87577499b28
Oleksii Myronenko <oleksiimrnko@gmail.com>
2025-06-09 23:24:57 +0300

    add test solutiion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const list = document.querySelector('ul');
 
-// write code here
+  function parseSalary(salaryStr) {
+    return parseFloat(salaryStr.replace(/[$,]/g, ''));
+  }
+
+  function sortList(targetList) {
+    const items = Array.from(targetList.children);
+
+    items.sort((a, b) => {
+      const salaryA = parseSalary(a.dataset.salary);
+      const salaryB = parseSalary(b.dataset.salary);
+
+      return salaryB - salaryA;
+    });
+
+    targetList.innerHTML = '';
+    items.forEach((item) => targetList.appendChild(item));
+  }
+
+  function getEmployees(targetList) {
+    return Array.from(targetList.children).map((li) => ({
+      name: li.textContent.trim(),
+      position: li.dataset.position,
+      salary: li.dataset.salary,
+      age: parseInt(li.dataset.age, 10),
+    }));
+  }
+
+  sortList(list);
+
+  getEmployees(list);
+});
