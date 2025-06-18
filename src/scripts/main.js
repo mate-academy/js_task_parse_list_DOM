58591b87dfa5f0dc60a055394063850d2edab031
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-06-19 00:03:48 +0300

    res

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+const list = [...ul.getElementsByTagName('li')];
+
+sortList(list);
+getEmployees(list);
+
+function sortList(listParam) {
+  const res = listParam.sort((a, b) => {
+    const el1 = parseInt(a.dataset.salary.replace(/[$,]/g, ''), 10);
+    const el2 = parseInt(b.dataset.salary.replace(/[$,]/g, ''), 10);
+
+    return el2 - el1;
+  });
+
+  res.forEach((element) => {
+    ul.appendChild(element);
+  });
+}
+
+function getEmployees(listParam) {
+  const res = [];
+
+  for (const el of listParam) {
+    res.push({
+      name: el.innerText,
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    });
+  }
+
+  return res;
+}
