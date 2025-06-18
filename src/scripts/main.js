693a29230bf61be3a1af61ba403073c58c1b63b3
Savtenko Volodymyr <volodimer.sovtenko2018@gmail.com>
2025-06-18 18:11:20 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const lists = document.body.querySelector('ul');
+
+function sortList() {
+  const li = lists.querySelectorAll('li');
+  const arr = [...li];
+
+  arr.sort((a, b) => +b.dataset.salary - +a.dataset.salary);
+  arr.forEach((element) => lists.appendChild(element));
+}
