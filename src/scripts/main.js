6458611acabd63a4637aed27840afb6a0ace4c07
dima91020 <vereskodima76@gmail.com>
2025-06-28 21:24:16 +0300

    add main.js file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 // write code here
+const list = document.querySelector('ul');
+
+function sortList(listElements) {
+  return [...listElements.children].sort((a, b) => {
+    const salaryToNumber = (worker) =>
+      Number(worker.dataset.salary.replace(/[$,]/g, ''));
+
+    return salaryToNumber(b) - salaryToNumber(a);
+  });
+}
+
+function getEmployees(listElements) {
+  return [...listElements.children].map((worker) => {
+    return {
+      name: worker.innerText,
+      position: worker.dataset.position,
+      salary: worker.dataset.salary,
+      age: worker.dataset.age,
+    };
+  });
+}
+
+const sortedList = sortList(list);
+
+while (list.firstChild) {
+  list.firstChild.remove();
+}
+
+sortedList.forEach((item) => list.append(item));
+getEmployees(list);
