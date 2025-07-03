5d332f70a1eb3b1d9d22ba5abef939e55ea544b9
Bernavschi Natalia <natb@list.ru>
2025-07-03 06:17:20 +0300

    add task solution for js_task_parse_list_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function parseSalary(money) {
+  return Number(money.replace(/[$,]/g, ''));
+}
+
+function sortList() {
+  const arrayList = Array.from(list.children);
+
+  arrayList.sort(
+    (a, b) => parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary),
+  );
+
+  arrayList.forEach((value) => list.appendChild(value));
+}
+
+sortList();
+
+function getEmployees() {
+  return Array.from(list.children).map((item) => ({
+    name: item.textContent.trim(),
+    salary: parseSalary(item.dataset.salary),
+  }));
+}
+
+getEmployees();
