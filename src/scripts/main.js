35eae94f55b8efcae85ee19508bc6a589eb24f46
Pavlo Zhabort <zhabortp@gmail.com>
2025-07-09 14:05:18 +0300

    Finished parsing the list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
 // write code here
+
+
+function parseSalary(salaryString) {
+    return Number(salaryString.replace(/[^0-9.-]+/g, ""));
+}
+
+function sortList(list) {
+    const items = Array.from(list.children);
+
+    items.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+    return salaryB - salaryA; 
+  });
+
+  items.forEach(item => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  return Array.from(list.children).map(item => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      age: Number(item.dataset.age),
+      salary: parseSalary(item.dataset.salary),
+    };
+  });
+}
+
+
+const list = document.querySelector('ul');
+sortList(list);
+const employees = getEmployees(list);
+console.log(employees); 
