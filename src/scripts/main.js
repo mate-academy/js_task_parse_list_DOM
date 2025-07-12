a41e8fcbe4b2eac882167b306ff3886c2bb2fa58
Н  е андрей <oleksandrlib4@gmail.com>
2025-07-12 10:51:36 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+function parseSalary(salaryStr) {
+  return parseInt(salaryStr.replace(/[^0-9]/g, ''), 10);
+}
+
+function sortList(employeeList) {
+  const items = Array.from(employeeList.children);
+
+  items.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  items.forEach((item) => employeeList.appendChild(item));
+}
+
+const list = document.querySelector('ul');
+
+sortList(list);
