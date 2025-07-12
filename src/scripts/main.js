92403ce810a1271a9e44c8fae34dbdb763b24738
Svyatoslav Kozelchuk-Panulin <svyatoslavkozelchuk@gmail.com>
2025-07-13 01:28:29 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+const sortList = (list) => {
+  const listItems = [...list.querySelectorAll('li')];
+
+  const getSalary = (item) => {
+    return Number(item.dataset.salary.replace(/[\$,]/g, ''));
+  };
+
+  listItems.sort((a, b) => getSalary(b) - getSalary(a));
+
+  listItems.forEach((item) => list.appendChild(item));
+};
+
+sortList(list);
+
+const getEmployees = (list) => {
+  const employees = [];
+  const li = [...list.querySelectorAll('li')];
+
+  li.forEach((item) => {
+    const employee = {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: Number(item.dataset.salary.replace(/[\$,]/g, '')),
+      age: Number(item.dataset.age),
+    };
+
+    employees.push(employee);
+  });
+
+  return employees;
+};
+
+getEmployees(list);
