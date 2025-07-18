094b0f8cb6991c5b1598c083c68785b67e03a4bc
Denys Herych <fojjof2@gmail.com>
2025-07-18 16:07:55 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const nodes = document.querySelectorAll('[data-position]');
+const list = [...nodes];
+
+function sortList(employeeList) {
+  const container = document.getElementsByTagName('ul');
+
+  employeeList.sort((a, b) => {
+    const bEl = formatData(a.dataset.salary);
+    const aEl = formatData(b.dataset.salary);
+
+    return aEl - bEl;
+  });
+
+  container[0].innerHtml = '';
+  list.forEach((el) => container[0].appendChild(el));
+}
+
+function formatData(data) {
+  return Number(data.replace(/\$|,/g, ''));
+}
+
+function getEmployees(employeeList) {
+  const employeesArr = [];
+
+  list.forEach((el) => {
+    employeesArr.push({
+      name: el.textContent.trim(),
+      position: el.dataset.position,
+      salary: formatData(el.dataset.salary),
+      age: el.dataset.age,
+    });
+  });
+}
+sortList(list);
+getEmployees(list);
