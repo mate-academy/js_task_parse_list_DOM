2f9c6f906035b88517740a4c436ea7a4d2d55f38
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-07-23 14:47:36 +0300

    sort list salary

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+function getSortEmployes() {
+  const listEmployes = document.querySelectorAll('li');
+
+  return [...listEmployes].map((li) => {
+    const fullName = li.textContent.trim();
+    const position = li.dataset.position;
+    const age = li.dataset.age;
+    const salary = parseInt(li.dataset.salary.replace(/\$|,/g, ''));
+
+    return {
+      name: fullName,
+      position,
+      salary,
+      age,
+    };
+  });
+}
+
+function getSalarySort(workers) {
+  return workers.slice().sort((a, b) => b.salary - a.salary);
+}
+
+const ul = document.querySelector('ul');
+const employees = getSortEmployes();
+const sortedEmployees = getSalarySort(employees);
+
+ul.innerHTML = '';
+
+sortedEmployees.forEach((emp) => {
+  const li = document.createElement('li');
+
+  li.dataset.salary = `$${emp.salary}`;
+  li.dataset.position = emp.position;
+  li.dataset.age = emp.age;
+  li.dataset.fullName = emp.name;
+
+  li.textContent = `${emp.name} - $${emp.salary}`;
+
+  ul.appendChild(li);
+});
