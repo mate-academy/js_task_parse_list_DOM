19d92c104584106585b56044240dfbc2fd3a2b40
Ivan Denys <denisivan06007@gmail.com>
2025-07-30 12:15:58 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+getEmployees(list);
+sortList(list);
+
+function getEmployees(list2) {
+  const items = Array.from(list2.querySelectorAll('li'));
+  const employees = [];
+
+  for (const el of items) {
+    const salary = Number(el.dataset.salary.slice(1).replace(/,/g, ''));
+
+    employees.push({
+      name: el.textContent,
+      position: el.dataset.position,
+      salary: salary,
+      age: Number(el.dataset.age),
+    });
+  }
+
+  return employees;
+}
+
+function sortList(list2) {
+  const items = Array.from(list2.querySelectorAll('li'));
+
+  items.sort((a, b) => {
+    const salaryA = Number(a.dataset.salary.slice(1).replace(/,/g, ''));
+    const salaryB = Number(b.dataset.salary.slice(1).replace(/,/g, ''));
+
+    return salaryB - salaryA;
+  });
+
+  items.forEach((item) => list2.appendChild(item));
+}
