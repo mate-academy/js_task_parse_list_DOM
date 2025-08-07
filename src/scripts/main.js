a91c963d2a9c821cd6b46f1bcac362b863372c0a
tvk777 <klimenko.tv7@gmail.com>
2025-08-07 16:29:04 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
+/* eslint no-console: ["error", { allow: ["warn", "log"] }] */
 
-// write code here
+const formatSalary = (str) => +str.replaceAll('$', '').replaceAll(',', '');
+
+const sortList = (items) =>
+  items.sort(
+    (a, b) => formatSalary(b.dataset.salary) - formatSalary(a.dataset.salary),
+  );
+
+const getEmployees = (items) =>
+  items.map((item) => {
+    const { position, salary, age } = item.dataset;
+    const itemName = item.innerHTML.trim();
+
+    return {
+      name: itemName,
+      position,
+      salary,
+      age,
+    };
+  });
+
+const list = document.querySelector('ul');
+const listItems = [...document.querySelectorAll('li')];
+
+sortList(listItems);
+
+list.innerHTML = '';
+listItems.forEach((li) => list.appendChild(li));
+
+getEmployees(listItems);
