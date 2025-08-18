47e63982c1bc3674ad30cd8b78501ba16691cec5
Tetiana Susol <tetyanamyeskalo@gmail.com>
2025-08-18 19:33:46 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+function getSalary(li) {
+  return Number(li.dataset.salary);
+}
+
+function sortList(ul) {
+  const items = Array.from(ul.querySelectorAll('li'));
+
+  items.sort((a, b) => getSalary(b) - getSalary(a));
+
+  ul.innerHTML = '';
+  items.forEach((item) => ul.appendChild(item));
+}
+
+function getEmployees(ul) {
+  return Array.from(ul.querySelectorAll('li')).map((li) => ({
+    name: li.dataset.name,
+    position: li.dataset.position,
+    salary: Number(li.dataset.salary),
+    age: Number(li.dataset.age),
+  }));
+}
+
+const employeesList = document.getElementById('employees');
+
+sortList(employeesList);
+
+const employees = getEmployees(employeesList);
+
+// eslint-disable-next-line no-console
+console.log(employees);
