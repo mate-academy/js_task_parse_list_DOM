8d5b35a40ba4188c6b35775aa522405667c4f1fb
Mykola <nyr3mvneov@gmail.com>
2025-08-18 13:25:58 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function salaryToNumber(salaryString) {
+  return Number(salaryString.replace(/[$,]/g, ''));
+}
+
+const employees = Array.from(list.querySelectorAll('li'));
+
+employees.sort(function (a, b) {
+  const firstSalary = salaryToNumber(a.dataset.salary);
+  const SecondSalary = salaryToNumber(b.dataset.salary);
+
+  return SecondSalary - firstSalary;
+});
+
+list.innerHTML = '';
+
+employees.forEach(function (employee) {
+  list.appendChild(employee);
+});
