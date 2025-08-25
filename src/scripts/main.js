8b67aff80a184287468044fecde84efdfea4a2c8
anastasiiadns <nastyadenisenko17@gmail.com>
2025-08-25 16:38:31 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+function getEmployees(list) {
+  return Array.from(list.children).map((element) => {
+    return {
+      name: element.textContent.trim(),
+      position: element.dataset.position,
+      salary: Number(element.dataset.salary.replace(/[^0-9]/g, '')),
+      age: Number(element.dataset.age),
+    };
+  });
+}
+
+function sortList(list) {
+  const employees = getEmployees(list);
+
+  employees.sort((a, b) => b.salary - a.salary);
+
+  employees.forEach((emp) => {
+    const li = Array.from(list.children).find(
+      (el) => el.textContent.trim() === emp.name,
+    );
+
+    if (li) {
+      list.appendChild(li);
+    }
+  });
+}
+
+const employeeList = document.querySelector('ul');
+
+sortList(employeeList);
