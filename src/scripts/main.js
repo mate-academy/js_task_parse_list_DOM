a9a48532f4230f6eb21e58c6571c6fae1e9dbc8f
anastasiiadns <nastyadenisenko17@gmail.com>
2025-08-25 16:53:06 +0300

    task fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,5 @@
 'use strict';
 
-function getEmployees(list) {
-  return Array.from(list.children).map((element) => {
-    return {
-      name: element.textContent.trim(),
-      position: element.dataset.position,
-      salary: Number(element.dataset.salary.replace(/[^0-9]/g, '')),
-      age: Number(element.dataset.age),
-    };
-  });
-}
-
 function sortList(list) {
   const employees = getEmployees(list);
 
@@ -25,8 +14,27 @@ function sortList(list) {
       list.appendChild(li);
     }
   });
+
+  return employees;
+}
+
+function getEmployees(list) {
+  return Array.from(list.children).map((element) => {
+    return {
+      name: element.textContent.trim(),
+      position: element.dataset.position,
+      salary: Number(element.dataset.salary.replace(/[^0-9]/g, '')),
+      age: Number(element.dataset.age),
+    };
+  });
 }
 
 const employeeList = document.querySelector('ul');
 
 sortList(employeeList);
+
+const employeeArray = getEmployees(employeeList);
+
+window.getEmployees = getEmployees;
+window.sortList = sortList;
+window.employees = employeeArray;
