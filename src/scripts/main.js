6b0605610273f98c1440aa1aed72da5154e27b9a
Vadym Kirilkov <tav0r0tuk@gmail.com>
2025-08-26 20:05:22 +0200

    to check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
+// const employeesArray = employees.querySelectorAll('li');
 
-// write code here
+const list = document.querySelector('ul');
+
+function salaryToNumber(salary) {
+  return Number(salary.slice(1).replace(',', ''));
+}
+
+function sortList(listElement) {
+  const items = Array.from(listElement.querySelectorAll('li'));
+
+  const sorted = items.sort((a, b) => {
+    return salaryToNumber(b.dataset.salary) - salaryToNumber(a.dataset.salary);
+  });
+
+  sorted.forEach((item) => listElement.appendChild(item));
+}
+
+sortList(list);
+
+function getEmployees(listElement) {
+  const array = listElement.querySelectorAll('li');
+  const employeeArray = [];
+
+  array.forEach((employee) => {
+    const employeeObj = {
+      name: employee.textContent.trim(),
+      position: employee.dataset.position,
+      salary: salaryToNumber(employee.dataset.salary),
+      age: parseInt(employee.dataset.age),
+    };
+
+    employeeArray.push(employeeObj);
+  });
+
+  return employeeArray;
+}
+
+getEmployees(list);
