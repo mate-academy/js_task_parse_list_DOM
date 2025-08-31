2e3eda4abafb5ab94d45a3776511bb92e33363cd
Victor <viktorstupin9@gmail.comgit config --global user.email viktorstupin9@mail.com>
2025-08-31 11:47:52 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
+function parseSalary(salaryStr) {
+  return Number(salaryStr.replace(/[^\d.]/g, ''));
+}
+
+function sortList(list) {
+  const items = Array.from(list.children);
+
+  items.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+  items.forEach((item) => list.appendChild(item));
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const list = document.querySelector('ul[data-list]');
+
+  if (!list) {
+    return;
+  }
+  sortList(list);
+});
+
 // write code here
