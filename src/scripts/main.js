1c5f6e0102b035a98d51530613b351b44eef6279
Artem Arseniev <144947394+Aarseniev@users.noreply.github.com>
2025-09-02 19:16:53 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
 // write code here
+
+const regEx = /[$,]/g;
+
+function sortList(list = document.querySelectorAll('li')) {
+  const parsedEmployeeList = Array.from(list);
+
+  const sortedEmployeeList = parsedEmployeeList.sort((a, b) => {
+    return (
+      Number(b.dataset.salary.replace(regEx, '')) -
+      Number(a.dataset.salary.replace(regEx, ''))
+    );
+  });
+
+  const parentLiElement = list[0].parentElement;
+
+  parentLiElement.innerHTML = '';
+
+  sortedEmployeeList.forEach((li) => parentLiElement.append(li));
+}
+
+function getEmployees(list = document.querySelectorAll('li')) {
+  const parsedEmployeeList = Array.from(list);
+  const dataList = [];
+
+  parsedEmployeeList.forEach(
+    (li) =>
+      dataList.push({
+        name: li.textContent.trim(),
+        position: li.dataset.position,
+        salary: Number(li.dataset.salary.replace(regEx, '')),
+        age: Number(li.dataset.age),
+      }),
+    // eslint-disable-next-line function-paren-newline
+  );
+
+  return dataList;
+}
+
+sortList();
+getEmployees();
