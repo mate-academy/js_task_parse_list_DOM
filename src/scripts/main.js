e35be60a6925f689801e82fc25443a9fdd761b58
DropPredator <ionchiccc@gmail.com>
2025-09-02 12:47:54 +0300

    ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,19 +5,21 @@ function strToNum(string) {
 }
 
 function sortList(list) {
-  const listCopy = [...list];
+  const ul = document.querySelector('ul');
 
-  listCopy.sort((elem1, elem2) => {
+  list.sort((elem1, elem2) => {
     return strToNum(elem2.dataset.salary) - strToNum(elem1.dataset.salary);
   });
 
-  for (let i = 0; i < list.length; i++) {
-    list[i].replaceWith(listCopy[i].cloneNode(true));
-  }
+  ul.innerHTML = '';
+
+  list.forEach((elem) => {
+    ul.appendChild(elem);
+  });
 }
 
 function getEmployees() {
-  return document.querySelector('ul').children;
+  return Array.from(document.getElementsByTagName('li'));
 }
 
 sortList([...getEmployees()]);
