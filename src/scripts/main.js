0c2f80e3fc80dd620ffe3bbab4d9e53935b91fc2
DropPredator <ionchiccc@gmail.com>
2025-09-02 11:52:44 +0300

    completed task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const arr = document.querySelector('ul').children;
+const arrCopy = [...arr];
+
+function getSorted(list) {
+  function strToNum(string) {
+    return Number(string.replaceAll(/\D/g, ''));
+  }
+
+  const listCopy = [...list];
+
+  return listCopy.sort((elem1, elem2) => {
+    return strToNum(elem2.dataset.salary) - strToNum(elem1.dataset.salary);
+  });
+}
+
+function getEmployees(listToReplace, sortedList) {
+  for (let i = 0; i < listToReplace.length; i++) {
+    listToReplace[i].replaceWith(sortedList[i].cloneNode(true));
+  }
+}
+
+document.createElement('div');
+
+getEmployees(arr, getSorted(arrCopy));
