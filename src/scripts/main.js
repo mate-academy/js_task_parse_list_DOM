c069d5dd53f89e8acc881a9d5ae0f86e718da23a
Vladyslav Kostiuk <K4nezoor@gmail.com>
2025-09-11 19:51:59 +0200

    moved functions outside of eventListener

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,39 +1,42 @@
 'use strict';
 
+function parseSalary(salaryString) {
+  const cleanSalary = String(salaryString).replace(/[$,]/g, '');
+
+  return Number(cleanSalary);
+}
+
+function sortList(list) {
+  const array = Array.from(list.querySelectorAll('li'));
+
+  array.sort((a, b) => {
+    const listItemA = parseSalary(a.dataset.salary);
+    const listItemB = parseSalary(b.dataset.salary);
+
+    return listItemB - listItemA;
+  });
+
+  array.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  return Array.from(list.querySelectorAll('li')).map((emlp) => {
+    const fullName = emlp.textContent.trim();
+
+    return {
+      name: fullName,
+      position: emlp.dataset.position,
+      salary: parseSalary(emlp.dataset.salary),
+      age: Number(emlp.dataset.age),
+    };
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const listOfEmployees = document.querySelector('ul');
 
-  function parseSalary(salaryString) {
-    const cleanSalary = String(salaryString).replace(/[$,]/g, '');
-
-    return Number(cleanSalary);
-  }
-
-  function sortList(list) {
-    const array = Array.from(list.querySelectorAll('li'));
-
-    array.sort((a, b) => {
-      const listItemA = parseSalary(a.dataset.salary);
-      const listItemB = parseSalary(b.dataset.salary);
-
-      return listItemB - listItemA;
-    });
-
-    array.forEach((item) => list.appendChild(item));
-  }
-
-  function getEmployees(list) {
-    return Array.from(list.querySelectorAll('li')).map((emlp) => {
-      const fullName = emlp.textContent.trim();
-
-      return {
-        name: fullName,
-        position: emlp.dataset.position,
-        salary: parseSalary(emlp.dataset.salary),
-        age: Number(emlp.dataset.age),
-      };
-    });
-  }
   sortList(listOfEmployees);
-  getEmployees(listOfEmployees);
+
+  // eslint-disable-next-line no-unused-vars
+  const employees = getEmployees(listOfEmployees);
 });
