1b819cbdb72ad46e69d3c919917b9556f1079acd
Aleksander Chaika <alekschayka97@gmail.com>
2025-09-29 17:47:23 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function sortList(listElement) {
+  const sorted = Array.from(listElement.children).sort(
+    (a, b) => getSalary(b) - getSalary(a),
+  );
+
+  list.innerHTML = '';
+  list.append(...sorted);
+
+  return sorted;
+}
+
+function getSalary(el) {
+  const salaryStr = el.dataset.salary;
+  const cleaned = salaryStr.replace(/\$|,/g, '');
+
+  return Number(cleaned);
+}
+
+function getEmployees(listElement) {
+  const employees = [];
+
+  for (const element of listElement.children) {
+    const obj = {
+      name: element.textContent.trim(),
+      position: element.dataset.position,
+      salary: getSalary(element),
+      age: Number(element.dataset.age),
+    };
+
+    employees.push(obj);
+  }
+
+  return employees;
+}
+
+sortList(list);
+getEmployees(list);
