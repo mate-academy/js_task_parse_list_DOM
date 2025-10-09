023d694e4b1733d4af11af0198aba21ff92f7782
Andriy <a.tupko98@gmail.com>
2025-10-09 20:27:23 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write code here
+const salary = document.querySelectorAll('li.data-salary');
+const allSalary = Array.from(salary).sort((a, b) => a - b);
+
+const li = document.querySelectorAll('li');
+const liName = li.textContent.trim();
+
+const people = [];
+
+for (const i of li) {
+  const person = {
+    name: liName,
+    position: i.dataset.position,
+    salary: i.dataset.salary,
+    age: i.dataset.age,
+  };
+
+  people.push(person);
+}
