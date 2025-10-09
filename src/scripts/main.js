ac1c2f6290e5c21f4ad18cb675893034583011ea
Dima <dima2003strik@gmail.com>
2025-10-09 03:56:10 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+// Отримуємо всі елементи li
+const employees = [...document.querySelectorAll('li')];
+const ul = document.querySelector('ul');
+
+// Функція сортує список за зарплатою (у порядку спадання)
+const sortEmployees = (employees) => {
+  employees.sort((empA, empB) => {
+    const salaryA = Number(empA.getAttribute('data-salary').replace(/[^\d.-]/g, ''));
+    const salaryB = Number(empB.getAttribute('data-salary').replace(/[^\d.-]/g, ''));
+    return salaryB - salaryA; // спадання
+  });
+
+  return employees;
+};
+
+// Функція повертає масив об’єктів співробітників
+const getObjects = (employees) => {
+  return employees.map(emp => ({
+    name: emp.innerText.trim(),
+    position: emp.getAttribute('data-position'),
+    salary: emp.getAttribute('data-salary'),
+    age: emp.getAttribute('data-age'),
+  }));
+};
+
+const sortedEmployees = sortEmployees(employees);
+
+ul.innerHTML = '';
+for (const li of sortedEmployees) {
+  ul.appendChild(li);
+}
+
+const employeesObjects = getObjects(sortedEmployees);
+
