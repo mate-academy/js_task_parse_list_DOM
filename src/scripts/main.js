2ffcccea5d5ef3b520b770122556dadeff35891c
Roma Ivashchenko <rrrrgvvggh@gmail.com>
2025-10-10 18:10:59 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const allList = document.querySelector('ul');
+
+function sortList(list) {
+  function getSalary(li) {
+    return +li.dataset.salary;
+  }
+
+  const items = Array.from(list.children);
+  const itemsArray = Array.from(items);
+  const newSalary = itemsArray.sort((a, b) => getSalary(b) - getSalary(a));
+
+  newSalary.forEach((li) => list.appendChild(li));
+}
+
+function getEmployees(list) {
+  const items = Array.from(list.children);
+
+  const newEmployees = items.map((li) => ({
+    name: li.dataset.name,
+    position: li.dataset.position,
+    salary: +li.dataset.salary,
+    age: +li.dataset.age,
+  }));
+
+  return newEmployees;
+}
+
+sortList(allList);
+
+getEmployees(allList);
