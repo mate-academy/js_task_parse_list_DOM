b2ed5bbf777692c469c2abaf7bb9650dab94de0f
Roma Ivashchenko <rrrrgvvggh@gmail.com>
2025-10-10 19:09:38 +0300

    add task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,10 @@ const allList = document.querySelector('ul');
 
 function sortList(list) {
   function getSalary(li) {
-    return +li.dataset.salary;
+    return Number(li.dataset.salary.replace(/[$,]/g, '').trim());
   }
 
-  const items = Array.from(list.children);
-  const itemsArray = Array.from(items);
+  const itemsArray = Array.from(list.children);
   const newSalary = itemsArray.sort((a, b) => getSalary(b) - getSalary(a));
 
   newSalary.forEach((li) => list.appendChild(li));
@@ -18,9 +17,9 @@ function getEmployees(list) {
   const items = Array.from(list.children);
 
   const newEmployees = items.map((li) => ({
-    name: li.dataset.name,
+    name: li.textContent.trim(),
     position: li.dataset.position,
-    salary: +li.dataset.salary,
+    salary: Number(li.dataset.salary.replace(/,/g, '').trim()),
     age: +li.dataset.age,
   }));
 
