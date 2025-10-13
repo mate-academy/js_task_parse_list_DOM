4721158bcead1492a6f3bae31c5b5988cf256cc7
Tausvokse <parhom137@gmail.com>
2025-10-13 17:55:24 +0300

    Fixed all critical issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,35 @@
 'use strict';
 
-const employeesList = document.querySelectorAll('[data-salary]');
+const listElement = document.querySelector('#employees');
 
 function convertSalary(salaryString) {
-  return parseFloat(salaryString.replace('$', '').replace(',', ''));
+  return parseFloat(salaryString.replace(/[$,]/g, ''));
 }
 
 function sortList(list) {
-  const sortedArray = Array.from(list).sort((a, b) => {
+  const items = list.querySelectorAll('[data-salary]');
+  const sortedArray = Array.from(items).sort((a, b) => {
     const salaryA = convertSalary(a.dataset.salary);
     const salaryB = convertSalary(b.dataset.salary);
 
     return salaryB - salaryA;
   });
 
-  const parents = list[0].parentNode;
-
-  sortedArray.forEach((element) => parents.appendChild(element));
+  sortedArray.forEach((element) => list.appendChild(element));
 }
 
 function getEmployees(list) {
-  return Array.from(list).map((employee) => ({
-    name: employee.textContent.trim(),
+  const items = list.querySelectorAll('[data-salary]');
+
+  return Array.from(items).map((employee) => ({
+    name: employee.querySelector('.name').textContent.trim(),
     position: employee.dataset.position,
     salary: convertSalary(employee.dataset.salary),
     age: parseInt(employee.dataset.age, 10),
   }));
 }
 
-sortList(employeesList);
-getEmployees(employeesList);
+sortList(listElement);
+
+// eslint-disable-next-line no-unused-vars
+const employees = getEmployees(listElement);
