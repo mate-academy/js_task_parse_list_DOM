03ae1114a3995dd71c9c17dd35df17091a077e08
yahohulia <yaroslavhohulia@gmail.com>
2025-10-23 20:11:52 +0300

    added js solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write code here
+const listOfEmployees = document.querySelectorAll('li');
+const parents = document.querySelector('ul');
+
+function sortList([...list]) {
+  return list.sort(
+    (curr, next) =>
+      toNumber(next.dataset.salary) - toNumber(curr.dataset.salary),
+  );
+}
+
+function toNumber(notNumber) {
+  const number = notNumber.replace(/[^ 0-9.-]/g, '');
+
+  return +number;
+}
+
+function getEmployees([...list]) {
+  const listObj = [];
+
+  for (const item of list) {
+    const names = item.innerText.trim();
+    const salary = item.dataset.salary;
+    const age = +item.dataset.age;
+    const position = item.dataset.position;
+
+    listObj.push({
+      names,
+      position,
+      salary,
+      age,
+    });
+  }
+
+  return listObj;
+}
+
+const sortedList = sortList(listOfEmployees);
+
+sortedList.forEach((item) => parents.appendChild(item));
+
+getEmployees(listOfEmployees);
