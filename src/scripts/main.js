22172f38bd3b646b02ce37a4656eccac7363ea32
Anastasia <ahinata1717@gmail.com>
2025-11-01 18:18:22 +0200

    Implemented sorting function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const elements = document.querySelector('ul');
+
+function salaryToNumber(str) {
+  return Number(str.replace(/[$,]/g, ''));
+}
+
+function sortList(list) {
+  const items = [...list.querySelectorAll('li')];
+
+  items.sort(
+    (a, b) =>
+      salaryToNumber(b.dataset.salary) - salaryToNumber(a.dataset.salary),
+  );
+
+  list.innerHTML = '';
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  const itemsArray = [...list.querySelectorAll('li')];
+
+  return itemsArray.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: salaryToNumber(item.dataset.salary),
+    age: Number(item.dataset.age),
+  }));
+}
+
+sortList(elements);
+getEmployees(elements);
