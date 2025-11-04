91c58228419d3d0142cf4f24ffe94d3030d4fc81
Paulina <paulina.zwolenik@gmail.com>
2025-11-04 19:10:59 +0100

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const employeeList = document.querySelector('ul');
+
+function sortList(list) {
+  const items = list.querySelectorAll('li');
+  const itemsArray = [...items];
+
+  itemsArray.sort((a, b) => {
+    const salaryStringA = a.dataset.salary;
+    const salaryStringB = b.dataset.salary;
+    const salaryA = +salaryStringA.replace(/\$/g, '').replace(/,/g, '');
+    const salaryB = +salaryStringB.replace(/\$/g, '').replace(/,/g, '');
+
+    return salaryB - salaryA;
+  });
+
+  itemsArray.forEach((item) => {
+    list.appendChild(item);
+  });
+}
+
+function getEmployees(list) {
+  const items = list.querySelectorAll('li');
+  const itemsArray = [...items];
+  const employees = itemsArray.map((item) => {
+    return {
+      name: item.textContent.trim(),
+
+      position: item.dataset.position,
+      salary: +item.dataset.salary.replace(/\$/g, '').replace(/,/g, ''),
+      age: +item.dataset.age,
+    };
+  });
+
+  return employees;
+}
+
+sortList(employeeList);
+
+getEmployees(employeeList);
