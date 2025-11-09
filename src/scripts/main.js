6c2196a2730aa48fba0527317002cd91e496484f
Andrew17-2006 <andriy.veretelnyk@gmail.com>
2025-11-09 19:29:22 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+function sortList(ulElement) {
+  const listItems = Array.from(ulElement.querySelectorAll('li'));
+
+  listItems.sort((a, b) => {
+    const salaryA = Number(a.dataset.salary.replace(/[$,]/g, ''));
+    const salaryB = Number(b.dataset.salary.replace(/[$,]/g, ''));
+
+    return salaryB - salaryA;
+  });
+
+  ulElement.innerHTML = '';
+  listItems.forEach((li) => ulElement.appendChild(li));
+}
+
+function getEmployees(ulElement) {
+  const items = ulElement.querySelectorAll('li');
+
+  return Array.from(items).map((li) => ({
+    position: li.dataset.position,
+    salary: Number(li.dataset.salary.replace(/[$,]/g, '')),
+    age: Number(li.dataset.age),
+  }));
+}
+
+const list = document.querySelector('ul');
+
+sortList(list);
+// eslint-disable-next-line no-console
+console.log(getEmployees(list));
