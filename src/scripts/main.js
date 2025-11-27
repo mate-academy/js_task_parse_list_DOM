1fb389dd7bc7bd336000530af2ad509477d5ba60
Tymofii <zrozik@MacBook-Air-Tymofii.local>
2025-11-27 20:58:46 +0200

    commit fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,54 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function parseSalary(str) {
+  const digits = str.replace(/[^\d]/g, '');
+
+  return Number(digits);
+}
+
+function sortList(emList) {
+  if (!emList) {
+    return;
+  }
+
+  const items = Array.from(list.children);
+
+  items.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(emList) {
+  if (!emList) {
+    return [];
+  }
+
+  const employees = [];
+
+  for (const item of list.children) {
+    const nameTextEl = item.querySelector('.name');
+    const nameText = nameTextEl ? nameTextEl.textContent.trim() : '';
+    const position = item.dataset.position;
+    const salary = parseSalary(item.dataset.salary);
+    const age = Number(item.dataset.age);
+
+    employees.push({
+      nameText,
+      position,
+      salary,
+      age,
+    });
+  }
+
+  return employees;
+}
+
+sortList(list);
+getEmployees(list);
