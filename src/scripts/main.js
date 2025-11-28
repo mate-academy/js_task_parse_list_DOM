f19a0561cd7b31518387e3cebbfdf9ce3ebfec5c
Maksym Halushko <gallikillag@gmail.com>
2025-11-28 19:39:45 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+function salaryToNum(value) {
+  return Number(value.replace('$', '').replace(/,/g, ''));
+}
+
+function sortList(list) {
+  const items = Array.from(list.children);
+  const sorted = items.sort((a, b) => {
+    const sortedA = salaryToNum(a.dataset.salary);
+    const sortedB = salaryToNum(b.dataset.salary);
+
+    return sortedB - sortedA;
+  });
+
+  sorted.forEach((item) => list.appendChild(item));
+}
+
+const parse = document.querySelector('ul');
+
+sortList(parse);
+
+function getEmployees(person) {
+  const employees = Array.from(person.children);
+  const res = employees.map((a) => ({
+    name: a.textContent.trim(),
+    position: a.dataset.position,
+    salary: a.dataset.salary,
+    age: a.dataset.age,
+  }));
+
+  return res;
+}
+
+getEmployees(parse);
