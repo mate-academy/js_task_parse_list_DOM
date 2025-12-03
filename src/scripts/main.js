f3887bdb5d39e85582464ba5b97b007c870e38e4
Alex <1inboxna@gmail.com>
2025-12-03 21:52:00 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const employees = Array.from(document.getElementsByTagName('li'));
+
+employees.sort((a, b) => {
+  const salaryA = Number(a.dataset.salary.replace(/\D/g, ''));
+  const salaryB = Number(b.dataset.salary.replace(/\D/g, ''));
+
+  return salaryB - salaryA;
+});
+
+const list = document.querySelector('ul');
+
+list.innerHTML = '';
+employees.forEach((el) => list.appendChild(el));
