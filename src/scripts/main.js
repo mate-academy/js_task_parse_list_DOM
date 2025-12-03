0e745fc4911f501561a303f06cba10854297c506
Ivan <igrytsaenko@gmail.com>
2025-12-03 17:30:29 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+const foundList = document.querySelector('ul');
+
+function parseSalary(str) {
+  const replaced = str.replace(/[^\d.]/g, '');
+
+  return Number(replaced) || 0;
+}
+
+function sortList(list) {
+  const items = Array.from(list.children);
+
+  items.sort(
+    (a, b) => parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary),
+  );
+
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  const result = [];
+
+  Array.from(list.children).forEach((li) => {
+    const personName = li.textContent.trim();
+    const position = li.dataset.position.trim();
+    const salary = parseSalary(li.dataset.salary);
+    const age = parseInt(li.dataset.age, 10);
+
+    result.push({
+      name: personName,
+      position,
+      salary,
+      age,
+    });
+  });
+
+  return result;
+}
+
+sortList(foundList);
+getEmployees(foundList);
