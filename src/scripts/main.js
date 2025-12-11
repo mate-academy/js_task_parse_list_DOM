19d39cbe360071147bf03bd4c1245673aa49ba10
Yaroslav Ivasenko <yarik.dock@gmail.com>
2025-12-11 19:52:08 +0200

    created solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const items = [...document.querySelectorAll(`li`)];
+const roster = document.querySelector('ul');
+const workers = [];
+
+function salaryToNumber(li) {
+  return Number(li.dataset.salary.replaceAll(',', '').replaceAll('$', ''));
+}
+
+function sortList(list) {
+  const sortedArray = list
+    .slice()
+    .sort((a, b) => salaryToNumber(b) - salaryToNumber(a));
+
+  roster.innerHTML = '';
+
+  sortedArray.forEach((li) => roster.appendChild(li));
+}
+
+function getEmployees(list) {
+  for (const item of list) {
+    workers.push({
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    });
+  }
+}
+
+sortList(items);
+getEmployees(items);
