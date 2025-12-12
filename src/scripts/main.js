69eadb33c0b6cf7e8c86d9394ed6c3dfe3b7a533
Natalia Yakovleva <yakovlevane29@gmail.com>
2025-12-12 15:16:17 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const allEmployees = document.querySelectorAll('ul li');
+
+function conversionSalaryInNum(str) {
+  return Number(str.slice(1).replace(/,/g, ''));
+}
+
+function getEmployees(list) {
+  return Array.from(list).map((item) => {
+    return {
+      name: item.textContent.trim(),
+      ...item.dataset,
+    };
+  });
+}
+
+function sortList(list) {
+  const arr = Array.from(list);
+
+  arr.sort((a, b) => {
+    const firstSalary = conversionSalaryInNum(a.dataset.salary);
+    const secondSalary = conversionSalaryInNum(b.dataset.salary);
+
+    return secondSalary - firstSalary;
+  });
+
+  const parentEl = arr[0].parentElement;
+
+  arr.forEach((item) => parentEl.append(item));
+}
+
+sortList(allEmployees);
+
+getEmployees(allEmployees);
