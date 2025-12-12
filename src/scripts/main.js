6bb33313c69b67a72db61a405449aefdfe451101
Natalia Yakovleva <yakovlevane29@gmail.com>
2025-12-12 15:54:21 +0200

    changed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,17 +6,12 @@ function conversionSalaryInNum(str) {
   return Number(str.slice(1).replace(/,/g, ''));
 }
 
-function getEmployees(list) {
-  return Array.from(list).map((item) => {
-    return {
-      name: item.textContent.trim(),
-      ...item.dataset,
-    };
-  });
-}
-
 function sortList(list) {
   const arr = Array.from(list);
+
+  if (arr.length === 0) {
+    return;
+  }
 
   arr.sort((a, b) => {
     const firstSalary = conversionSalaryInNum(a.dataset.salary);
@@ -30,6 +25,15 @@ function sortList(list) {
   arr.forEach((item) => listOfEmployees.append(item));
 
   return arr;
+}
+
+function getEmployees(list) {
+  return Array.from(list).map((item) => {
+    return {
+      name: item.textContent.trim(),
+      ...item.dataset,
+    };
+  });
 }
 
 const sortedLi = sortList(allEmployees);
