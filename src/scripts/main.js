08ca95a57b552385064cf7fa42ab041cc31904bf
Evg3n22 <evgen.gulevaty@gmail.com>
2025-12-15 14:40:02 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write code here
+const ulElement = document.querySelector('ul');
+const employeesArray = [...document.querySelectorAll('ul li')];
+
+function sortList(list) {
+  list.map((employee) => {
+    employee.dataset.salary = employee.dataset.salary
+      .slice(1)
+      .replace(/,/g, '.');
+  });
+
+  return list.sort((a, b) => b.dataset.salary - a.dataset.salary);
+}
+
+function getEmployees(list) {
+  list.forEach((employee) => {
+    ulElement.appendChild(employee);
+  });
+
+  return list;
+}
+
+sortList(employeesArray);
+getEmployees(employeesArray);
