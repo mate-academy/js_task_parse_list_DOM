1e16520b2ea3803c9da44c903938b9eebada1411
artembaranovsky1 <baranovskyiartem2006@gmail.com>
2025-12-16 11:57:56 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const elements = document.querySelectorAll('li[data-salary]');
+
+const elementsArray = [...elements];
+
+const sorted = elementsArray.sort((a, b) => {
+  const salaryA = Number(a.dataset.salary.replace(/[$,]/g, ''));
+  const salaryB = Number(b.dataset.salary.replace(/[$,]/g, ''));
+
+  return salaryB - salaryA;
+});
+
+const ul = document.querySelector('ul');
+
+ul.innerHTML = '';
+
+for (const el of sorted) {
+  ul.append(el);
+}
