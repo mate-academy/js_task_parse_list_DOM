c2244d536264d11f2bcdad11d4ba296115732522
Vlad Zhovtani <zovtanyy@gmail.com>
2025-12-17 19:25:47 +0200

    edited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,40 +1,32 @@
 'use strict';
 
-// 1. Отримуємо дані з DOM (ОДИН раз)
-function getEmployees() {
-  const items = document.querySelectorAll('li');
+function parseSalary(salaryStr) {
+  return Number(salaryStr.replace('$', '').replaceAll(',', ''));
+}
 
-  return Array.from(items).map((item) => ({
+function sortList(list) {
+  const items = Array.from(list.querySelectorAll('li'));
+
+  items.sort(
+    (a, b) => parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary),
+  );
+
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  const items = Array.from(list.querySelectorAll('li'));
+
+  return items.map((item) => ({
     name: item.textContent.trim(),
     position: item.dataset.position,
-    salary: Number(item.dataset.salary.replace('$', '').replaceAll(',', '')),
+    salary: parseSalary(item.dataset.salary),
     age: Number(item.dataset.age),
   }));
 }
 
-// 2. Рендеримо DOM зі списку employees
-function renderEmployees(employeeses) {
-  const ul = document.querySelector('ul');
+// usage
+const ourList = document.querySelector('ul');
 
-  ul.innerHTML = '';
-
-  employeeses.forEach((employee) => {
-    const li = document.createElement('li');
-
-    li.textContent = employee.name;
-    li.dataset.position = employee.position;
-    li.dataset.salary = `$${employee.salary.toLocaleString('en-US')}`;
-    li.dataset.age = employee.age;
-
-    ul.appendChild(li);
-  });
-}
-
-// 3. Логіка
-const employees = getEmployees();
-
-// сортуємо ДАНІ
-employees.sort((a, b) => b.salary - a.salary);
-
-// рендеримо DOM
-renderEmployees(employees);
+sortList(ourList);
+getEmployees(ourList);
