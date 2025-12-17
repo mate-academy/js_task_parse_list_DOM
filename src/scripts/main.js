e6720bc4d10aa1b2b137cec195677b23f1bed6b0
Vlad Zhovtani <zovtanyy@gmail.com>
2025-12-17 19:22:07 +0200

    edited readme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,6 @@
 'use strict';
 
-// 1. Функція №1 — сортує DOM-список за salary (descending)
-function sortListBySalary() {
-  const ul = document.querySelector('ul');
-  const items = Array.from(ul.querySelectorAll('li'));
-
-  items.sort((a, b) => {
-    const salaryA = Number(
-      a.dataset.salary.replace('$', '').replaceAll(',', ''),
-    );
-    const salaryB = Number(
-      b.dataset.salary.replace('$', '').replaceAll(',', ''),
-    );
-
-    return salaryB - salaryA;
-  });
-
-  // переставляємо li в DOM
-  items.forEach((item) => ul.appendChild(item));
-}
-
-// 2. Функція №2 — повертає масив обʼєктів employees
+// 1. Отримуємо дані з DOM (ОДИН раз)
 function getEmployees() {
   const items = document.querySelectorAll('li');
 
@@ -32,6 +12,29 @@ function getEmployees() {
   }));
 }
 
-// виклики
-sortListBySalary();
-getEmployees();
+// 2. Рендеримо DOM зі списку employees
+function renderEmployees(employeeses) {
+  const ul = document.querySelector('ul');
+
+  ul.innerHTML = '';
+
+  employeeses.forEach((employee) => {
+    const li = document.createElement('li');
+
+    li.textContent = employee.name;
+    li.dataset.position = employee.position;
+    li.dataset.salary = `$${employee.salary.toLocaleString('en-US')}`;
+    li.dataset.age = employee.age;
+
+    ul.appendChild(li);
+  });
+}
+
+// 3. Логіка
+const employees = getEmployees();
+
+// сортуємо ДАНІ
+employees.sort((a, b) => b.salary - a.salary);
+
+// рендеримо DOM
+renderEmployees(employees);
