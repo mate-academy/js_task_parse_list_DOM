a5a7d1fe4bcd57fbbf99fca619043588f5013275
Evgenu <bogdanovevgenij87@gmail.com>
2025-12-24 13:20:24 +0200

    fix v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,31 @@
 'use strict';
 
 const ul = document.querySelector('ul');
-const items = [...ul.querySelectorAll('li')];
 
-function sortList () {
+function sortList(list) {
+  const items = [...list.querySelectorAll('li')];
 
   const getSalary = (li) => {
     return Number(li.dataset.salary.replace(/[$,]/g, ''));
   };
 
-  items.sort((a, b) => getSalary(a) + getSalary(b));
+  items.sort((a, b) => getSalary(b) - getSalary(a));
+
+  list.replaceChildren(...items);
 }
 
-function getEmployees () {
-  ul.replaceChildren(...items);
+function getEmployees(list) {
+  const items = [...list.querySelectorAll('li')];
+
+  return items.map((li) => {
+    return {
+      position: li.dataset.position,
+      salary: Number(li.dataset.salary.replace(/[$,]/g, '')),
+      name: li.textContent.trim(),
+      age: Number(li.dataset.age),
+    };
+  });
 }
+
+sortList(ul);
+getEmployees(ul);
