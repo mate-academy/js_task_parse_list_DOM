6de4d5bf63755bf4ce942e6613798b99f756eef3
Evgenu <bogdanovevgenij87@gmail.com>
2025-12-24 12:33:15 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const ul = document.querySelectorAll('ul');
+const items = [...ul.querySelectorAll('li')];
+
+const getSalary = (li) => {
+  Number(li.dataset.salary.replace(/[$,]/g, ''));
+};
+
+items.sort((a, b) => getSalary(a) - getSalary(b));
+
+ul.replaceChildren(...items);
