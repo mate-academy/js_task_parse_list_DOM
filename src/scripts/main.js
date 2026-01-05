2a6da2a1f5a72e1bdc7bed75216e0d4979097c4a
Nazarii Lesniak <naziksay@gmail.com>
2026-01-05 13:16:25 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+function getEmployees(list) {
+  const listEmployee = [...list.querySelectorAll('li')];
+
+  return listEmployee.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: Number(item.dataset.age),
+    };
+  });
+}
+
+function sortList(list) {
+  const listEmployee = [...list.querySelectorAll('li')];
+
+  listEmployee.sort((first, second) => {
+    const salaryA = Number(first.dataset.salary.replace(/[^0-9.-]+/g, ''));
+    const salaryB = Number(second.dataset.salary.replace(/[^0-9.-]+/g, ''));
+
+    return salaryB - salaryA;
+  });
+
+  listEmployee.forEach((item) => list.append(item));
+}
+
+const employee = document.querySelector('ul');
+
+sortList(employee);
+
+getEmployees(employee);
