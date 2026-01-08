db5355cd999f9b530efbb543c846c7d86767de7b
DevT <dev.tymofiichuk@gmail.com>
2026-01-08 15:40:54 +0200

    Parse list task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+// Forced by task requirements
+
+const getFloat = (str) => parseFloat(str.replace(/\D/g, ''));
+
+function sortList(list) {
+  const items = [...list.children];
+
+  items.sort((a, b) => {
+    const aSalary = getFloat(a.dataset.salary);
+    const bSalary = getFloat(b.dataset.salary);
+
+    return bSalary - aSalary;
+  });
+
+  list.innerHTML = '';
+  list.append(...items);
+}
+
+function getEmployees(list) {
+  return [...list.children].map((li) => ({
+    name: li.dataset.name,
+    position: li.dataset.position,
+    salary: li.dataset.salary,
+    age: li.dataset.age,
+  }));
+}
+
+const employeesList = document.querySelector('ul');
+
+sortList(employeesList);
+getEmployees(employeesList);
