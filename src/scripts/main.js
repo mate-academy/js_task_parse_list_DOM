3ff4c217bf7c9d0c9617f83f40028cc5217284bb
MacBook Pro <macbookpro@Andrew.local>
2026-01-09 19:35:03 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function getSalary(element) {
+  const salary = element.dataset.salary.replace(/[$,]/g, '');
+
+  return Number(salary);
+}
+
+function sortList(targetList) {
+  const items = [...targetList.children];
+
+  items.sort((a, b) => {
+    return getSalary(b) - getSalary(a);
+  });
+
+  targetList.innerHTML = '';
+  items.forEach((item) => targetList.append(item));
+}
+
+function getEmployeeNames(targetList) {
+  return [...targetList.children].map((item) => ({
+    name: item.textContent.trim(),
+    salary: getSalary(item),
+  }));
+}
+
+sortList(list);
+
+const employees = getEmployeeNames(list);
+
+window.employees = employees;
