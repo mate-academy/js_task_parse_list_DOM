16bab5ee441b7ec586785c5b1a4c49fcd82b391c
Illia Safonov <safonov.illia@gmail.com>
2026-01-10 16:30:38 +0100

    solution_parse_list_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+const parseSalary = (salaryStr) => {
+  return Number(salaryStr.replace(/[^\d]/g, ''));
+};
+
+function sortList() {
+  const items = Array.from(list.children);
+
+  items.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  items.forEach((item) => list.append(item));
+}
+
+function getEmployees() {
+  return Array.from(list.children).map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: parseSalary(item.dataset.salary),
+    age: Number(item.dataset.age),
+  }));
+}
+
+sortList(list);
+
+getEmployees(list);
