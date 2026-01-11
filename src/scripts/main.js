7364e8d4ee703845d1e602ffb48388095854a2fd
Yaroslav Kravchuk <Yaroslav.kravchyk@gmail.com>
2026-01-11 18:21:18 +0200

    Solution1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('#list');
+
+function parseSalary(dataSalary) {
+  return Number(dataSalary);
+}
+
+function sortList(listElement) {
+  const items = Array.from(listElement.children);
+
+  items.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  list.innerHTML = '';
+
+  items.forEach((item) => {
+    list.appendChild(item);
+  });
+}
+
+function getEmployees(listElement) {
+  return Array.from(listElement.children).map((item) => {
+    const employeeName = item.querySelector('.name').innerText.trim();
+    const position = item.querySelector('.position').innerText.trim();
+    const salary = Number(item.dataset.salaryValue);
+    const age = Number(item.dataset.age);
+
+    return {
+      employeeName,
+      position,
+      salary,
+      age,
+    };
+  });
+}
+
+sortList(list);
+getEmployees(list);
