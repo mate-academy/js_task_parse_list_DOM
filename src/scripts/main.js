25ea87fbe1a4daf708ed6d8d85e4c513d09cf283
Oleksandr Ratseburzhynskyi <ratseburzhynskyi@gmail.com>
2026-01-19 19:09:39 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('ul li')];
+
+const normSalary = (salary) => {
+  return Number(salary.replace(/[$,]/g, ''));
+};
+
+const sortList = (list) => {
+  list.sort((a, b) => {
+    return normSalary(b.dataset.salary) - normSalary(a.dataset.salary);
+  });
+
+  const ul = document.querySelector('ul');
+
+  for (const li of list) {
+    ul.appendChild(li);
+  }
+};
+
+const getEmployees = (list) => {
+  return list.map((li) => ({
+    name: li.textContent.trim(),
+    position: li.dataset.position,
+    salary: normSalary(li.dataset.salary),
+    age: +li.dataset.age,
+  }));
+};
+
+sortList(employees);
+getEmployees(employees);
