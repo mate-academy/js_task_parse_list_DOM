0076716d1930fb0f78cb3e3225c3c3f9f6d5dca8
Andriy <andriit35@gmail.com>
2026-01-22 12:38:30 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
 // write code here
+const listOfEmployeesFromHtml = document.querySelector('ul');
+const employeesFromHtml = Array.from(document.querySelectorAll('li'));
+
+function parceSalary(salary) {
+  return parseFloat(salary.replace(/[$,]/g, ''));
+}
+
+function sortList(listElement, items) {
+  items.sort((a, b) => {
+    return parceSalary(b.dataset.salary) - parceSalary(a.dataset.salary);
+  });
+
+  listElement.innerHTML = '';
+
+  items.forEach((li) => {
+    listElement.appendChild(li);
+  });
+}
+
+function getEmployees(items) {
+  return items.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: parceSalary(item.dataset.salary),
+      age: Number(item.dataset.age),
+    };
+  });
+}
+
+sortList(listOfEmployeesFromHtml, employeesFromHtml);
+
+const employeesArray = getEmployees(employeesFromHtml);
+
+// eslint-disable-next-line no-console
+console.log(employeesArray);
