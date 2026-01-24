c3b64f162921ab2d009b8f286a29a53ca13e207d
Oleksii <olexijukraina@gmail.com>
2026-01-24 15:08:10 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('ul li')];
+const ul = document.querySelector('ul');
+
+const normSalary = (salary) => {
+  return Number(salary.replace(/\D/g, ''));
+};
+
+const sortList = (list) => {
+  list.sort((a, b) => {
+    return normSalary(b.dataset.salary) - normSalary(a.dataset.salary);
+  });
+
+  list.forEach((li) => ul.appendChild(li));
+};
+
+const getEmployees = (list) => {
+  return list.map((li) => ({
+    name: li.textContent.trim(),
+    position: li.dataset.position,
+    salary: normSalary(li.dataset.salary),
+    age: +li.dataset.age,
+  }));
+};
+
+sortList(employees);
+getEmployees(employees);
