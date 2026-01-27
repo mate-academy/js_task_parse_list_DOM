be5dfad53089efd00a0ebfb9805964d41260c25a
vladahvala <80634246+vladahvala@users.noreply.github.com>
2026-01-27 14:55:22 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+// write code herefunction getEmployees(list) {
+function getEmployees(list) {
+  return list.map((li) => ({
+    name: li.textContent.trim(),
+    salary: Number(li.dataset.salary.replace(/[^\d]/g, '')),
+    element: li,
+  }));
+}
+
+function sortList(list) {
+  list.sort((a, b) => {
+    return (
+      Number(b.dataset.salary.replace(/[^\d]/g, '')) -
+      Number(a.dataset.salary.replace(/[^\d]/g, ''))
+    );
+  });
+}
+
+const people = [...document.querySelectorAll('li[data-salary]')];
+
+sortList(people);
+
+const employees = getEmployees(people);
+
+const ul = document.querySelector('ul');
+
+employees.forEach((person) => {
+  ul.append(person.element);
+});
