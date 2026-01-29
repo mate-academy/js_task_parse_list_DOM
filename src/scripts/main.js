a90b0f46517ff5783d163631d7a3240d1ee532f4
DenisNosov22 <dennosov2210@gmail.com>
2026-01-29 15:14:38 +0200

    task parse list DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+function toNumber(value) {
+  return Number(value.replace('$', '').replace(',', ''));
+}
+
+function sortList(ulElement) {
+  const items = Array.from(ulElement.children);
+
+  items.sort((a, b) => {
+    const salaryA = toNumber(a.dataset.salary);
+    const salaryB = toNumber(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  items.forEach((item) => ulElement.append(item));
+}
+
+function getEmployees(ulElement) {
+  return Array.from(ulElement.children).map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: toNumber(item.dataset.salary),
+    age: toNumber(item.dataset.age),
+  }));
+}
+
+const list = document.querySelector('ul');
+
+sortList(list);
+getEmployees(list);
