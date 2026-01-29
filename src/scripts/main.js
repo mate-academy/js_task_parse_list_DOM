0621d0d6da1b87c9adc6194f9a5c70fa3634486a
Inesa Shtepa <khinessa@gmail.com>
2026-01-29 15:11:14 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+function salaryToNumber(salaryStr) {
+  return Number(String(salaryStr).replace(/[$,]/g, ''));
+}
+
+function sortList(l) {
+  const items = Array.from(list.querySelectorAll('li'));
+
+  items.sort((a, b) => {
+    const salaryA = salaryToNumber(a.dataset.salary);
+    const salaryB = salaryToNumber(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  list.innerHTML = '';
+  list.append(...items);
+}
+
+function getEmployees(l) {
+  const items = Array.from(list.querySelectorAll('li'));
+
+  return items.map((li) => ({
+    name: li.textContent.trim(),
+    position: li.dataset.position,
+    salary: salaryToNumber(li.dataset.salary),
+    age: Number(li.dataset.age),
+  }));
+}
+
+const list = document.querySelector('ul');
+
+sortList(list);
+
+getEmployees(list);
