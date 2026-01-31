90e2663fa73a2749746f88a09f32bd29581c15d7
Anastasiia Tkachenko <nastya240509@gmail.com>
2026-01-31 02:02:12 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const ulElement = document.querySelector('ul');
+// const listItem = document.querySelectorAll('ul li');
+
+function parseSalary(salaryString) {
+  return Number(salaryString.replace(/[$,]/g, ''));
+}
+
+function sortList(list) {
+  const itemsArray = Array.from(list.children);
+
+  itemsArray.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  itemsArray.forEach((item) => list.appendChild(item));
+}
+
+function getEmployess(list) {
+  const employeesArray = Array.from(list.children).map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: parseSalary(item.dataset.salary),
+      age: Number(item.dataset.age),
+    };
+  });
+
+  return employeesArray;
+}
+
+sortList(ulElement);
+
+getEmployess(ulElement);
