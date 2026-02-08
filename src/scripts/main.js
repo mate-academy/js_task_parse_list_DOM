15a1f1b218ffbf2e239330dba93f204bdcd75897
Mykola Fatkullin <fatkullinmykola@gmail.com>
2026-02-08 21:26:51 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+class Employee {
+  constructor(employeeName, position, salary, age) {
+    this.name = employeeName;
+    this.position = position;
+    this.salary = salary;
+    this.age = age;
+  }
+}
+
+const ul = document.querySelector('ul');
+const li = ul.children;
+
+getEmployees(li);
+sortList(li);
+
+function sortList(list) {
+  [...list]
+    .sort(
+      (a, b) =>
+        convertSalary(b.dataset.salary) - convertSalary(a.dataset.salary),
+    )
+    .forEach((element) => ul.appendChild(element));
+}
+
+function getEmployees(list) {
+  const arrayOfEmployees = [...list].map((element) => {
+    return new Employee(
+      element.textContent.trim(),
+      element.dataset.position,
+      element.dataset.salary,
+      element.dataset.age,
+    );
+  });
+
+  return arrayOfEmployees;
+}
+
+function convertSalary(salary) {
+  return Number.parseInt(salary.replace(/\D/g, ''));
+}
