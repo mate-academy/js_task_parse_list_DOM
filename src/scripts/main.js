5dab567f1e4097cfce8309dd9c01b55cc698a1bf
Oleksandr Shyshenok <shyshenok81@gmail.com>
2026-02-20 17:51:32 +0200

    add first task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+const parseSalary = (el) => Number(el.dataset.salary) || 0;
+
+function sortList(listElement) {
+  // 1. Перетворюємо NodeList у масив
+  const items = [...listElement.children];
+
+  // 2. Сортуємо в порядку спадання (b - a)
+  items.sort((a, b) => parseSalary(b) - parseSalary(a));
+
+  // 3. Оновлюємо DOM
+  items.forEach((item) => listElement.appendChild(item));
+}
+
+function getEmployees(listElement) {
+  return [...listElement.children].map((item) => {
+    const employeeName = item.textContent.split('\n')[0].trim() || '';
+    const position = item.dataset.position || '';
+    const salary = parseSalary(item);
+    const age = Number(item.dataset.age) || null;
+
+    return {
+      employeeName,
+      position,
+      salary,
+      age,
+    };
+  });
+}
+
+sortList(list);
+
+getEmployees(list);
