3149b93de122c7d5ad802b3604c16405948acf74
Ola <aleksandra-chomicz@outlook.com>
2026-03-04 17:30:05 +0100

    Implement employee sorting and data extraction functions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const listOfEmployees = document.querySelector('ul');
+const employeesArr = Array.from(listOfEmployees.children);
+
+console.log(employeesArr);
+
+function sortList(array) {
+    array.sort((a, b) => {
+        const firstSalary = a.dataset.salary.slice(1).split(',').join('');
+        const secondSalary = b.dataset.salary.slice(1).split(',').join('');
+
+        return Number(secondSalary) - Number(firstSalary)});
+
+    array.forEach(person => {
+        listOfEmployees.appendChild(person);
+});
+}
+
+function getEmployees(array) {
+    return array.map(person => {
+        return {
+            name: person.outerText,
+            position: person.dataset.position,
+            salary: person.dataset.salary,
+            age: person.dataset.age,
+        }
+    });
+}
+
+
+
+sortList(employeesArr);
+
+console.log(getEmployees(employeesArr));
+
+
