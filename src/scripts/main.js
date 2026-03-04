d553ce16dc96196ea9bd1c48e96e2a9bc29393e7
Mykyta Brenhach <nikassafronovs@gmail.com>
2026-03-04 11:33:19 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+function getSalaryNumber(salary) {
+  return Number(salary.replace(/\$/g, '').replace(/,/g, ''));
+}
+
+function sortList({ list }) {
+  const items = Array.from(list.children);
+
+  const sortedItems = items.sort((a, b) => {
+    const salaryA = getSalaryNumber(a.dataset.salary);
+    const salaryB = getSalaryNumber(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  sortedItems.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees({ list }) {
+  return Array.from(list.children).map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: getSalaryNumber(item.dataset.salary),
+    age: Number(item.dataset.age),
+  }));
+}
+
+const employeeList = document.querySelector('ul');
+
+sortList({ list: employeeList });
+getEmployees({ list: employeeList });
