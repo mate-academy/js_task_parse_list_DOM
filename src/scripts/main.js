ec54a7247c1ea3e4e41f14f10463be156e8f2948
Taras Demianiuk <demtaras1@gamil.com>
2026-03-09 14:31:03 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const ul = document.getElementsByTagName('ul')[0];
+
+const items = Array.from(ul.getElementsByTagName('li'));
+
+function normalizeSalary(salary) {
+  return parseInt(salary.slice(1).split(',').join(''));
+}
+
+function sortList(list) {
+  list.sort((a, b) => {
+    return (
+      normalizeSalary(b.dataset.salary) - normalizeSalary(a.dataset.salary)
+    );
+  });
+
+  return list;
+}
+
+function getEmployees(list) {
+  return list.map((employee) => ({
+    name: employee.dataset.name,
+    position: employee.dataset.position,
+    salary: normalizeSalary(employee.dataset.salary),
+    age: parseInt(employee.dataset.age),
+  }));
+}
+
+const sortedList = sortList(items);
+
+ul.innerHTML = '';
+
+sortedList.forEach((item) => ul.appendChild(item));
+
+getEmployees(sortedList);
