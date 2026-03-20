e80642b8d33b96dc9d506d6fa9479d04d53f9e03
Valentyna Bahyrly <valentynabahyrly@gmail.com>
2026-03-20 13:44:18 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('li')];
+
+const arrEmployees = [];
+
+for (const employee of employees) {
+  const fullName = employee.textContent.trim();
+  const { position, salary, age } = employee.dataset;
+
+  const obj = {
+    fullName,
+    position,
+    salary: Number(salary.replace(/[$\s,]/g, '')),
+    age: Number(age),
+  };
+
+  arrEmployees.push(obj);
+}
+
+// --- sorted saslary ---
+
+function sortList(list) {
+  return list.sort((a, b) => b.salary - a.salary);
+}
+
+sortList(arrEmployees);
+
+// --- get employees ---
+
+function getEmployees(list) {
+  return list.map((item) => ({
+    fullName: item.name,
+    position: item.position,
+    salary: item.salary,
+    age: item.age,
+  }));
+}
+
+getEmployees(arrEmployees);
