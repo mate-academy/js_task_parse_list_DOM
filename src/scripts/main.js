78e19db454359d9fd47e8ff2858fa4a64e564c3c
Anastasiia Riadnova <a.ryadnova01@gmail.com>
2026-03-24 21:36:47 +0100

    add task solution v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+function parseSalary(str) {
+  return Number(str);
+}
+
+function sortList(list) {
+  const items = [...list.querySelectorAll('li')];
+
+  items.sort(
+    (a, b) => parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary),
+  );
+
+  list.append(...items);
+}
+
+function getEmployees(list) {
+  const items = Array.from(list.querySelectorAll('li'));
+
+  return items.map((li) => ({
+    name: li.querySelector('.name').textContent.trim(),
+    position: li.querySelector('.position').textContent.trim(),
+    salary: parseSalary(li.dataset.salary),
+    age: Number(li.dataset.age),
+  }));
+}
+
+const employees = document.getElementById('ul');
+
+sortList(employees);
+
+getEmployees(employees);
