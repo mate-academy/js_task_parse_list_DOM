56ab0c2b3d1e129f9cd6cfb88921477314c4a322
Sidach Ruslan <flashangel4600@gmail.com>
2026-04-02 10:07:49 +0300

    add fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,19 +4,25 @@ const elementUl = document.querySelector('ul');
 
 /**
  *
+ * @param {String} str
+ * @returns
+ */
+function Salary2Number(str) {
+  return Number(str.replaceAll(',', '').replaceAll('$', ''));
+}
+
+/**
+ *
  * @param {HTMLElement} list
  */
 function sortList(list) {
   const items = [...list.children];
 
   items.sort((a, b) => {
-    let selaryA = a.dataset.salary;
-    let selaryB = b.dataset.salary;
+    const salaryA = Salary2Number(a.dataset.salary);
+    const salaryB = Salary2Number(b.dataset.salary);
 
-    selaryA = selaryA.replaceAll(',', '').replaceAll('$', '');
-    selaryB = selaryB.replaceAll(',', '').replaceAll('$', '');
-
-    return Number(selaryB) - Number(selaryA);
+    return Number(salaryB) - Number(salaryA);
   });
 
   items.forEach((item) => list.append(item));
@@ -32,7 +38,7 @@ function getEmployees(list) {
     return {
       name: item.textContent,
       position: item.dataset.position,
-      selary: item.dataset.selary,
+      salary: item.dataset.salary,
       age: item.dataset.age,
     };
   });
