bb0965e5e8f49117824f3764c02934a8bbfd6e77
Sidach Ruslan <flashangel4600@gmail.com>
2026-04-02 09:56:47 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write code here
+const elementUl = document.querySelector('ul');
+
+/**
+ *
+ * @param {HTMLElement} list
+ */
+function sortList(list) {
+  const items = [...list.children];
+
+  items.sort((a, b) => {
+    let selaryA = a.dataset.salary;
+    let selaryB = b.dataset.salary;
+
+    selaryA = selaryA.replaceAll(',', '').replaceAll('$', '');
+    selaryB = selaryB.replaceAll(',', '').replaceAll('$', '');
+
+    return Number(selaryB) - Number(selaryA);
+  });
+
+  items.forEach((item) => list.append(item));
+}
+
+/**
+ * @param {HTMLElement} list
+ */
+function getEmployees(list) {
+  const items = [...list.children];
+
+  const employees = items.map((item) => {
+    return {
+      name: item.textContent,
+      position: item.dataset.position,
+      selary: item.dataset.selary,
+      age: item.dataset.age,
+    };
+  });
+
+  return employees;
+}
+
+sortList(elementUl);
+getEmployees(elementUl);
