85ad7d35c28b8093608e2ec99111438cbb65e64c
yevkrasnenkov <krasnenkov.evgen@gmail.com>
2026-04-08 19:09:08 -0400

    sorter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const emploees = Array.from(document.querySelectorAll('li'));
+
+emploees.sort((e1, e2) => parseInt(e2.dataset.salary) - parseInt(e1.dataset.salary));
+
+emploees
+    .forEach(li => document.querySelector('ul').appendChild(li));
+
+function parseInt(salary) {
+    const toNum = Number(salary.replace(/[$,]/g, ''));
+    return toNum;
+}
