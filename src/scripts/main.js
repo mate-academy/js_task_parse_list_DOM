a8e677779e7db47e73fce180c731e9bfeaa5be2b
yevkrasnenkov <krasnenkov.evgen@gmail.com>
2026-04-08 19:12:48 -0400

    ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,15 @@
 'use strict';
 
+function parseNum(salary) {
+  const toNum = Number(salary.replace(/[$,]/g, ''));
+
+  return toNum;
+}
+
 const emploees = Array.from(document.querySelectorAll('li'));
 
-emploees.sort((e1, e2) => parseInt(e2.dataset.salary) - parseInt(e1.dataset.salary));
+emploees.sort(
+  (e1, e2) => parseNum(e2.dataset.salary) - parseNum(e1.dataset.salary),
+);
 
-emploees
-    .forEach(li => document.querySelector('ul').appendChild(li));
-
-function parseInt(salary) {
-    const toNum = Number(salary.replace(/[$,]/g, ''));
-    return toNum;
-}
+emploees.forEach((li) => document.querySelector('ul').appendChild(li));
