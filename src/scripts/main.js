7b39366cf1e4930c08b47ccbbeb5ef422e422ed6
Maksym <maxim.golya@gmail.com>
2026-04-08 19:38:18 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const parseSalary = (salaryStr) => Number(salaryStr.replace(/[^\d]/g, ''));
+
+const sortList = (listElement) => {
+  const items = Array.from(list.querySelectorAll('li'));
+
+  items.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  list.append(...items);
+
+  listElement.innerHTML = '';
+  listElement.append(...items);
+};
+
+const getEmployees = (listElement) => {
+  const items = [...listElement.querySelectorAll('li')];
+
+  return Array.from(items).map((li) => ({
+    name: li.textContent.trim(),
+    position: li.dataset.position,
+    salary: parseSalary(li.dataset.salary),
+    age: Number(li.dataset.age),
+  }));
+};
+
+const employeeList = document.querySelector('ul');
+
+if (employeeList) {
+  sortList(employeeList);
+
+  const employees = getEmployees(employeeList);
+
+  // eslint-disable-next-line no-console
+  console.log('Sorted Employees Array:', employees);
+}
