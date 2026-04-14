bb2611b6f68fe6b1cfa5fc80d114245632ce17c0
OlehYuriev <urievoleg94@gmail.com>
2026-04-14 12:31:42 +0300

    fix container

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,36 @@
 'use strict';
 
-const users = document.querySelectorAll('[data-salary]');
-const listContainer = document.querySelector('ul');
+'use strict';
+
+const ul = document.querySelector('ul');
 
 function convertSalaryToNumber(value) {
   return Number(value.replace(/[$,]/g, ''));
 }
 
 function sortList(list) {
-  const sortedItems = [...list].sort(
+  const items = [...list.querySelectorAll('[data-salary]')];
+
+  const sortedItems = items.sort(
     (a, b) =>
       convertSalaryToNumber(b.dataset.salary) -
       convertSalaryToNumber(a.dataset.salary),
   );
 
-  listContainer.append(...sortedItems);
+  list.append(...sortedItems);
 
   return sortedItems;
 }
 
-const sortUsers = sortList(users);
-
 function getEmployees(list) {
-  return list.map((item) => {
-    const firstName = item.textContent.trim().split(' ');
-
-    return {
-      name: firstName[0],
-      position: item.dataset.position,
-      salary: convertSalaryToNumber(item.dataset.salary),
-      age: item.dataset.age,
-    };
-  });
+  return [...list.querySelectorAll('[data-salary]')].map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: convertSalaryToNumber(item.dataset.salary),
+    age: item.dataset.age,
+  }));
 }
 
-getEmployees(sortUsers);
+sortList(ul);
+
+getEmployees(ul);
