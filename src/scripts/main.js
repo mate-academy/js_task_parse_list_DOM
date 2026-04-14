ff72dbf64e9b5f1f3398134ae04b5f911d49f812
OlehYuriev <urievoleg94@gmail.com>
2026-04-14 11:54:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const users = document.querySelectorAll('[data-salary]');
+const listContainer = document.querySelector('ul');
+
+function sortList(list) {
+  return [...list].sort(
+    (a, b) =>
+      Number(b.dataset.salary.replace(/[$,]/g, '')) -
+      Number(a.dataset.salary.replace(/[$,]/g, '')),
+  );
+}
+
+const sortUsers = sortList(users);
+
+listContainer.prepend(...sortUsers);
+
+function getEmployees(list) {
+  return list.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    };
+  });
+}
+
+getEmployees(sortUsers);
