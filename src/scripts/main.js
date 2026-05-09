4f241c69b58fa70c1ac351b39a1b18a4cf97ab10
nex1994 <bilicki1994@gmail.com>
2026-05-09 10:45:17 +0200

    init

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+function parseSalary(salaryStr) {
+  return Number(salaryStr.replace(/[$,]/g, ''));
+}
+
+function sortList(employeeList) {
+  const items = [...employeeList.children];
+
+  items.sort(
+    (a, b) => parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary),
+  );
+
+  items.forEach((item) => employeeList.append(item));
+}
+
+function getEmployees(employeeList) {
+  return [...employeeList.children].map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: parseSalary(item.dataset.salary),
+    age: Number(item.dataset.age),
+  }));
+}
+
+const list = document.querySelector('ul');
+
+sortList(list);
+
+window.employees = getEmployees(list);
