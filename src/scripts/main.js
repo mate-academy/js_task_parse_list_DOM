1baf82b604a15437405d5b903ddb7b9b573fef71
Vita Martsekhovska <marcehovskavita@gmail.com>
2026-05-20 19:21:20 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+function getSalaryNumber(employee) {
+  const salaryString = employee.dataset.salary.slice(1).replaceAll(',', '');
+
+  return +salaryString;
+}
+
+function sortList(list) {
+  const employeesArray = [...list.children];
+
+  const newemployeesArray = employeesArray.sort((a, b) => {
+    const childA = getSalaryNumber(a);
+    const childB = getSalaryNumber(b);
+
+    return childB - childA;
+  });
+
+  list.append(...newemployeesArray);
+}
+
+function getEmployees(list) {
+  const workerArray = [...list.children];
+
+  return workerArray.map((worker) => {
+    return {
+      name: worker.textContent.trim(),
+      position: worker.dataset.position,
+      salary: getSalaryNumber(worker),
+      age: +worker.dataset.age,
+    };
+  });
+}
+
+const mainList = document.querySelector('ul');
+
+sortList(mainList);
+getEmployees(mainList);
