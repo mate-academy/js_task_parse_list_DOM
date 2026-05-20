199fb4bf8138382afacef77b31d0aca3eb4e4148
kateryna Habchak <kvizovapidtrimka@gmail.com>
2026-05-20 15:06:47 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
+const list = document.querySelector('ul');
 
-// write code here
+  function getSalary(item) {
+  return Number(
+    item.dataset.salary
+      .replace(/[S,]/g, '')
+  );
+}
+
+  function sortList(list) {
+    const items = [...list.children];
+
+    items.sort((a, b) => {
+      return getSalary(b) - getSalary(a)
+    });
+
+    list.append(...items);
+
+  }
+
+  function getEmployees(list) {
+    const items = [...list.children];
+
+    return items.map(item => ({
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: getSalary(item),
+      age: Number(item.dataset.age)
+
+    }))
+
+    sortList(list);
+
+    const employees = getEmployees(list);
+
+    console.log(getEmployees);
+
+
+  }
+
