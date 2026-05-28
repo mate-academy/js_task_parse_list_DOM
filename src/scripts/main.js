4ece26e7d8bea289344b54c83fb5cee115b1dad6
sqw <bogdanfust@gmail.com>
2026-05-28 20:26:56 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listChildren = document.querySelectorAll('li');
+const ArrayListChildren = [...listChildren];
+
+ArrayListChildren.sort((a, b) => {
+  const cleanA = a.dataset.salary.replace(/[$,]/g, '');
+  const cleanB = b.dataset.salary.replace(/[$,]/g, '');
+
+  const salaryA = Number(cleanA);
+  const salaryB = Number(cleanB);
+
+  return salaryB - salaryA;
+});
+
+list.innerHTML = '';
+ArrayListChildren.forEach((li) => list.appendChild(li));
