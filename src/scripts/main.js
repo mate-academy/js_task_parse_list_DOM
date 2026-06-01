9c97b021c9f70c15d5523af42a25ab75c6819557
Mykhailo Oliinyk <mykhailo.oliinyk9@gmail.com>
2026-06-01 14:36:36 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
 // write code here
+const listNode = document.querySelector('ul');
+
+function sortList(list) {
+  const items = [...list.querySelectorAll('li')];
+
+  items.sort((a, b) => {
+    const aSalary = a.dataset.salary.replace(/[$,]/g, '');
+    const bSalary = b.dataset.salary.replace(/[$,]/g, '');
+
+    return bSalary - aSalary;
+  });
+
+  list.append(...items);
+}
+
+function getEmployees(list) {
+  const items = [...list.querySelectorAll('li')];
+
+  const persons = items.map((item) => {
+    const person = { name: item.textContent.trim() };
+
+    for (const key of Object.keys(item.dataset)) {
+      person[key] = item.dataset[key];
+    }
+
+    return person;
+  });
+
+  return persons;
+}
+
+sortList(listNode);
+getEmployees(listNode);
