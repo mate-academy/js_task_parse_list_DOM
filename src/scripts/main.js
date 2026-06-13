bdfe31a86d1edcdf81af8b1de36ccb24d6870d99
Denys Boiko <big4ever6996@gmail.com>
2026-06-13 21:27:26 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const listElements = document.querySelector('ul');
+
+function parseSalary(salaryString) {
+  return Number(salaryString.slice(1).split(',').join(''));
+}
+
+function sortList(listElement) {
+  const employeesArray = [...listElement.children];
+
+  employeesArray.sort((a, b) => {
+    const cleanSalaryA = parseSalary(a.dataset.salary);
+    const cleanSalaryB = parseSalary(b.dataset.salary);
+
+    return cleanSalaryB - cleanSalaryA;
+  });
+
+  listElement.append(...employeesArray);
+}
+
+function getEmployees(list) {
+  const itemsArray = [...list.children];
+  const employeesArray = itemsArray.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    };
+  });
+
+  return employeesArray;
+}
+
+sortList(listElements);
+
+getEmployees(listElements);
