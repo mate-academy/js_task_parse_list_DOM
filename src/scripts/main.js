0a670b9a36f9fe9dd5524862a7b8a3e60bb8056f
Tendeeer <leifisisidj747@gmail.com>
2026-06-14 17:35:03 +0300

    parce list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+function parseSalary(value) {
+  return parseInt(value.replace(/[$,]/g, ''), 10);
+}
+
+function sortList(list) {
+  const sortedList = Array.from(list.querySelectorAll('li'));
+
+  sortedList.sort((a, b) => {
+    const aValue = parseSalary(a.dataset.salary);
+    const bValue = parseSalary(b.dataset.salary);
+
+    return bValue - aValue;
+  });
+
+  sortedList.forEach((item) => {
+    list.appendChild(item);
+  });
+}
+
+function getEmployees(list) {
+  const employees = Array.from(list.querySelectorAll('li')).map((item) => {
+    return {
+      name: item.dataset.name,
+      position: item.dataset.position,
+      salary: parseSalary(item.dataset.salary),
+      age: parseInt(item.dataset.age, 10),
+    };
+  });
+
+  return employees;
+}
+
+const employeeList = document.querySelector('ul');
+
+sortList(employeeList);
+getEmployees(employeeList);
