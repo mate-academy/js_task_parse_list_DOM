79ef067f5fa3b7f9e4eec676f91d49a0eebcaa2d
Anna Pryimak <sapfira03@gmail.com>
2026-06-21 20:38:51 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const listEmployees = document.querySelector('ul');
+
+function sortList(list) {
+  const listElements = list.querySelectorAll('li');
+  const elementsArray = [...listElements];
+  const sorted = [...elementsArray].sort((a, b) => {
+    const salaryA = Number(a.dataset.salary.replace(/\D/g, ''));
+    const salaryB = Number(b.dataset.salary.replace(/\D/g, ''));
+
+    return salaryB - salaryA;
+  });
+
+  sorted.forEach((element) => {
+    list.appendChild(element);
+  });
+}
+
+function getEmployees(list) {
+  const listElements = list.querySelectorAll('li');
+  const elementsArray = [...listElements];
+  const employees = elementsArray.map((person) => ({
+    name: person.dataset.name,
+    position: person.dataset.position,
+    salary: Number(person.dataset.salary),
+    age: Number(person.dataset.age),
+  }));
+
+  return employees;
+}
+
+sortList(listEmployees);
+getEmployees(listEmployees);
