67615cc641845eb87f8926050a89b6e4f530c1f6
Vitalii <vtysiachnyk@gmail.com>
2026-06-21 12:18:53 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const employeeListElement = document.querySelector('ul');
+
+function sortList(list) {
+  const items = [...list.querySelectorAll('li')];
+
+  items.sort(
+    (empl1, empl2) =>
+      parseSalary(empl2.dataset.salary) - parseSalary(empl1.dataset.salary),
+  );
+
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(employeeList) {
+  const items = [...employeeList.querySelectorAll('li')];
+
+  return items.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: parseSalary(item.dataset.salary),
+    age: Number(item.dataset.age),
+  }));
+}
+
+function parseSalary(salaryStr) {
+  if (!salaryStr) {
+    return 0;
+  }
+
+  const normalizedSalary = Number(salaryStr.slice(1).split(',').join(''));
+
+  return Number.isNaN(normalizedSalary) ? 0 : normalizedSalary;
+}
+
+sortList(employeeListElement);
+getEmployees(employeeListElement);
