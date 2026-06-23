b89d23e7327f29cb4ea792d4d053324b69aff9fa
Olexander <parfenov4589@gmail.com>
2026-06-23 15:02:46 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+const sortList = (employees) => {
+  const items = [...employees.children];
+
+  items.sort((a, b) => {
+    const salaryA = Number(a.getAttribute('data-salary').replace(/[$,]/g, ''));
+    const salaryB = Number(b.getAttribute('data-salary').replace(/[$,]/g, ''));
+
+    return salaryB - salaryA;
+  });
+
+  employees.append(...items);
+};
+
+const getEmployees = (employees) => {
+  return [...employees.children].map((item) => ({
+    name: item.textContent.trim(),
+    position: item.getAttribute('data-position'),
+    salary: Number(item.getAttribute('data-salary').replace(/[$,]/g, '')),
+    age: Number(item.getAttribute('data-age')),
+  }));
+};
+
+sortList(list);
+getEmployees(list);
