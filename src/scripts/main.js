2829f45b1de1d0ad7dea83b09877c7a2edd3edac
Svitlana Slavets <slavets.sa@gmail.com>
2026-06-28 21:48:27 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function getSalary(element) {
+  return Number(element.dataset.salary.replace('$', '').replace(',', ''));
+}
+
+function sortList(employeeList) {
+  const sortedList = [...employeeList.employees].sort((a, b) => {
+    return getSalary(b) - getSalary(a);
+  });
+
+  sortedList.forEach((item) => employeeList.append(item));
+}
+
+function getEmployees(employeeList) {
+  return [...employeeList.employees].map((employee) => {
+    return {
+      name: employee.textContent.trim(),
+      position: employee.dataset.position,
+      salary: getSalary(employee),
+      age: Number(employee.dataset.age),
+    };
+  });
+}
+
+sortList(list);
+
+const employees = getEmployees(list);
+
+// eslint-disable-next-line no-console
+console.log(employees);
