84aca42d6f69420c97951a4d42c728b37e06650d
Sofiia Hryshko <sofiahryshko@gmail.com>
2026-07-03 18:44:55 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+function getSalary(item) {
+  return Number(item.dataset.salary.replace('$', '').replace(',', ''));
+}
+
+function sortList(list) {
+  const items = [...list.children];
+
+  items.sort((a, b) => {
+    return getSalary(b) - getSalary(a);
+  });
+
+  items.forEach((item) => list.append(item));
+
+  return list;
+}
+
+function getEmployees(list) {
+  const items = [...list.children];
+
+  return items.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: getSalary(item),
+    age: Number(item.dataset.age),
+  }));
+}
+
+const employeesList = document.querySelector('ul');
+
+sortList(employeesList);
+
+getEmployees(employeesList);
