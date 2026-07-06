996bddc0f3af15f9c99b1de506e0f1cc475d7add
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-06 18:00:05 +0300

    try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,9 @@
 'use strict';
 
+const res = [...document.querySelectorAll('li')].map((item) => item.textContent.trim());
+const salaries = [];
+
+for (const item of document.querySelectorAll('li')) {
+  salaries.push(+item.dataset.salary.slice(1).replaceAll(',', ''));
+}
+console.log(salaries);
