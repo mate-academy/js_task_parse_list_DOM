631bb92bb4e6fc3da0116c89c6129c113c2d68a6
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-06 18:46:42 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,39 @@
 'use strict';
 
-const res = [...document.querySelectorAll('li')].map((item) => item.textContent.trim());
-const salaries = [];
+const list = document.querySelector('ul');
+const sortList = function (l) {
+  const children = [...l.children];
 
-for (const item of document.querySelectorAll('li')) {
-  salaries.push(+item.dataset.salary.slice(1).replaceAll(',', ''));
-}
-console.log(salaries);
+  children.sort((a, b) => {
+    const salaryA = +a.dataset.salary.replace(/[^0-9]/g, '');
+    const salaryB = +b.dataset.salary.replace(/[^0-9]/g, '');
+
+    return salaryB - salaryA;
+  });
+
+  children.forEach((item) => list.appendChild(item));
+};
+const getEmployees = function (l) {
+  const children = [...l.children];
+  const res = [];
+  const obj = {
+    name: 0,
+    position: 0,
+    salary: 0,
+    age: 0,
+  };
+
+  for (const item of children) {
+    obj.name = item.textContent.trim();
+    obj.position = item.dataset.position;
+    obj.salary = item.dataset.salary;
+    obj.age = item.dataset.age;
+
+    res.push(obj);
+  }
+
+  return res;
+};
+
+getEmployees(list);
+sortList(list);
