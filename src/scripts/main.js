fc334d36d50b92320488d33e49d77a60a10f4cc5
pparys93 <pparys93@gmail.com>
2026-07-07 23:24:11 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+function getSalary(item) {
+  return parseInt(item.dataset.salary, 10);
+}
+
+function getEmployees(list) {
+  const items = Array.from(list.children);
+  const employees = [];
+
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+
+    employees.push({
+      name: item.dataset.name,
+      position: item.dataset.position,
+      salary: getSalary(item),
+      age: parseInt(item.dataset.age, 10),
+    });
+  }
+
+  return employees;
+}
+
+function sortList(list) {
+  const items = Array.from(list.children);
+
+  items.sort((a, b) => {
+    const salaryA = getSalary(a);
+    const salaryB = getSalary(b);
+
+    return salaryB - salaryA;
+  });
+
+  items.forEach((item) => list.appendChild(item));
+}
+
+const employeeList = document.querySelector('ul');
+
+sortList(employeeList);
+getEmployees(employeeList);
