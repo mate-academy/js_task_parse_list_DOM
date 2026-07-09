17bfbf5af3328a137cadbb9a32974007aca30fdf
ParamooreXO <taraskull123@gmail.com>
2026-07-09 13:09:01 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const employees = [];
+
+document.querySelectorAll('[data-salary]').forEach((listItem) => {
+  employees.push({
+    name: listItem.textContent.trim(),
+    position: listItem.dataset.position,
+    salary: listItem.dataset.salary,
+    age: listItem.dataset.age,
+  });
+});
+
+employees.sort((a, b) => {
+  return b.salary.replace(/[^0-9]/g, '') - a.salary.replace(/[^0-9]/g, '');
+});
+
+document.querySelectorAll('li[data-salary]').forEach((listItem, index) => {
+  listItem.dataset.position = `${employees[index].position}`;
+  listItem.dataset.salary = `$${employees[index].salary}`;
+  listItem.dataset.age = `${employees[index].age}`;
+  listItem.textContent = `${employees[index].name}`;
+});
