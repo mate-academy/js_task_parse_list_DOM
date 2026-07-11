2c2c8fb2760edab77f8cc55f379ca1e329541c55
Mariana <zh.yu.st22@gmail.com>
2026-07-11 21:30:34 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write code here
+const people = [...document.querySelectorAll('[data-salary]')];
+const emp = [];
+const list = document.querySelector('ul');
+
+people.forEach((person) => {
+  emp.push({
+    name: person.dataset.name,
+    salary: Number(person.dataset.salary.replace('$', '').replace(/,/g, '')),
+  });
+});
+
+people.sort((a, b) => {
+  return (
+    Number(b.dataset.salary.replace('$', '').replace(/,/g, '')) -
+    Number(a.dataset.salary.replace('$', '').replace(/,/g, ''))
+  );
+});
+
+people.forEach((person) => {
+  list.append(person);
+});
