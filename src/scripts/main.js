3e7c0ebb472f715c5f1e1baae435873e91a0a523
Nik200309 <nikitapidlypnij@gmail.com>
2026-07-15 12:11:58 +0300

    First commit attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
 // write code here
+
+function parseSalary(salaryString) {
+  return Number(salaryString.replace(/[$,]/g, ''));
+}
+
+function sortList(employeeList) {
+  const items = Array.from(list.children);
+
+  items.sort((a, b) => {
+    return parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary);
+  });
+
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(employeeList) {
+  const items = Array.from(list.children);
+
+  return items.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: parseSalary(item.dataset.salary),
+      age: Number(item.dataset.age),
+    };
+  });
+}
+
+const list = document.querySelector('ul');
+
+sortList(list);
+getEmployees(list);
