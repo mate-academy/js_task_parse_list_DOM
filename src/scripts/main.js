463a0500cc1401d632ba2e937cd6a4b662da2393
Nataliia <nata.kovbasnyukk@gmail.com>
2026-07-15 18:54:54 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const employeesList = document.querySelector('ul');
+
+function parseSalary(item) {
+  const cleaned = item.dataset.salary.replace(/[$,]/g, '');
+
+  return Number(cleaned);
+}
+
+function sortList(list) {
+  const items = Array.from(list.children);
+
+  items.sort((a, b) => parseSalary(b) - parseSalary(a));
+
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  const items = Array.from(list.children);
+
+  return items.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: parseSalary(item),
+      age: Number(item.dataset.age),
+    };
+  });
+}
+
+sortList(employeesList);
+
+getEmployees(employeesList);
