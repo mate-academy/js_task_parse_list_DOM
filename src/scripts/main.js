463b3667757ef76dcf0fbd7c6751044a4168bcfc
Vira Shmyhelska <vira.zykhor@gmail.com>
2026-07-15 13:45:31 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const people = [...document.querySelectorAll('li')];
+
+const getSalary = (el) => Number(el.dataset.salary.replace(/[$,]/g, ''));
+
+function sortList(items) {
+  items.sort((a, b) => getSalary(b) - getSalary(a));
+
+  const ul = items[0].parentElement;
+
+  ul.replaceChildren(...items);
+}
+
+function getEmployees(items) {
+  return items.map((item) => ({
+    name: item.dataset.name,
+    position: item.dataset.position,
+    salary: getSalary(item),
+    age: Number(item.dataset.age),
+  }));
+}
+
+sortList(people);
+
+getEmployees(people);
