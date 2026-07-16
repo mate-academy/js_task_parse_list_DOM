4855b59922e11df23b920e1f4302fd60333b5273
Oleksandr Stepaniuk <o.step4niuk@gmail.com>
2026-07-16 22:54:55 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const employeeList = document.querySelector('ul');
+
+function getSalary(employeeElement) {
+  const salary = employeeElement.getAttribute('data-salary');
+
+  return Number(salary.replaceAll('$', '').replaceAll(',', ''));
+}
+
+function sortList(listElement) {
+  const employeeItems = [...listElement.querySelectorAll('li')];
+
+  employeeItems.sort((firstEmployee, secondEmployee) => {
+    return getSalary(secondEmployee) - getSalary(firstEmployee);
+  });
+
+  employeeItems.forEach((employeeElement) => {
+    listElement.append(employeeElement);
+  });
+}
+
+function getEmployees(listElement) {
+  const employeeItems = [...listElement.querySelectorAll('li')];
+
+  return employeeItems.map((employeeElement) => ({
+    name: employeeElement.textContent.trim(),
+    position: employeeElement.getAttribute('data-position'),
+    salary: getSalary(employeeElement),
+    age: Number(employeeElement.getAttribute('data-age')),
+  }));
+}
+
+sortList(employeeList);
+getEmployees(employeeList);
