ebc4ea6999d584f4d2f22d56befefd339a017736
Ol. <holmate@dontsp.am>
2026-07-22 14:20:32 +0300

    Solution added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+function strToNum(str) {
+  return Number(str.replace(/[$,]/g, ''));
+}
+
+function sortList(arr) {
+  return [...arr].sort(
+    (a, b) => strToNum(b.dataset.salary) - strToNum(a.dataset.salary),
+  );
+}
+
+function getEmployees(arr) {
+  return arr.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: strToNum(item.dataset.salary),
+    age: +item.dataset.age,
+  }));
+}
+
+const list = document.querySelector('ul');
+const employees = Array.from(list.querySelectorAll('li'));
+
+sortList(employees);
+
+const sortedEmployees = sortList(employees);
+
+sortedEmployees.forEach((item) => list.appendChild(item));
+
+getEmployees(employees);
