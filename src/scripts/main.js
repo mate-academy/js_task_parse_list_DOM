167b0dd966a2be179e1c9a05d75569efe8bf8b2c
Alexander Okhotnychenko <shashaohota@gmail.com>
2026-07-29 15:50:33 +0300

    solution: parse list DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write code here
+function sortList(list) {
+  const itemArray = getItemsArray(list);
+
+  itemArray.sort((a, b) => {
+    const salaryA = parseNumber(a?.dataset?.salary);
+    const salaryB = parseNumber(b?.dataset?.salary);
+
+    return salaryB - salaryA;
+  });
+
+  const checklist = document.querySelector(list);
+
+  itemArray.forEach((item) => {
+    checklist.appendChild(item);
+  });
+}
+
+function getEmployees(list) {
+  const itemArray = getItemsArray(list);
+
+  const employees = itemArray.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: parseNumber(item?.dataset?.salary),
+      age: item.dataset.age,
+    };
+  });
+
+  return employees;
+}
+
+const getItemsArray = (selector) => {
+  const checklist = document.querySelector(selector);
+  const listItem = checklist.querySelectorAll('li');
+
+  return Array.from(listItem);
+};
+
+const parseNumber = (item) => parseInt(item.replace(/[^\d]/g, ''));
+
+sortList('ul');
+getEmployees('ul');
