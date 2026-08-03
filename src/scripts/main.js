02e73a2a359ce4c8e1777c3e489bab5c4dfa1dce
Marichka <maria_volynchuk@ukr.net>
2026-08-04 01:31:39 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const employeeList = [...document.querySelectorAll('li')];
+
+function converter(salary) {
+  const cleanedSalary = salary.replaceAll('$', '');
+
+  return Number(cleanedSalary.replaceAll(',', ''));
+}
+
+function sortList(list) {
+  const sortedList = list.sort(
+    (a, b) => converter(b.dataset.salary) - converter(a.dataset.salary),
+  );
+
+  const ul = document.querySelector('ul');
+
+  for (const employee of sortedList) {
+    ul.append(employee);
+  }
+
+  return sortedList;
+}
+
+function getEmployees(list) {
+  const result = list.map((x) => ({
+    name: x.textContent.trim(),
+    ...x.dataset,
+  }));
+
+  return result;
+}
+
+const sorted = sortList(employeeList);
+
+getEmployees(sorted);
