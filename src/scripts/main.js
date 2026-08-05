a855440d640d9b2dc3189faae54e2ad9c9a3b5ee
PrinceValue <princeossoa@gmail.com>
2026-08-05 17:03:09 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function parseSalary(salaryString) {
+  return Number(salaryString.replace('$', '').replace(',', ''));
+}
+
+function sortList(listElement) {
+  const items = [...listElement.children];
+
+  items.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  items.forEach((n) => listElement.appendChild(n));
+}
+
+function getEmployees(emplist) {
+  return [...emplist.children].map((n) => {
+    return {
+      name: n.textContent.trim(),
+      position: n.dataset.position,
+      salary: parseSalary(n.dataset.salary),
+      age: Number(n.dataset.age),
+    };
+  });
+}
+
+sortList(list);
+getEmployees(list);
