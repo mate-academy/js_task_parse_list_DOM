8cf9756a9ca2d1210014adc1df77be246de79ba9
Roman Yasinskiy <4yk.roman@gmail.com>
2026-08-11 20:36:15 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,42 +1,47 @@
 'use strict';
 
+const listBlock = document.querySelector('.js-list');
+const list = getEmployees(listBlock.querySelectorAll('li'));
+
 // write code here
-function parseList(listArr) {
-  return listArr.map((item) => {
+function getEmployees(listArr) {
+  return [...listArr].map((item) => {
     return {
-      name: item.innerText,
-      position: item.dataset.position,
-      salary: item.dataset.salary,
-      age: item.dataset.age,
+      user: item.innerHTML,
+      name: item.dataset.position,
+      salary: convertToNumber(item.dataset.salary),
+      age: +item.dataset.age,
     };
   });
 }
 
-function sortBySalary(arr) {
-  return arr.sort((a, b) => {
-    return (
-      b.salary.replace(/,/g, '').slice(1) - a.salary.replace(/,/g, '').slice(1)
-    );
-  });
-}
-
-const listBlock = document.querySelector('.js-list');
-const list = parseList([...listBlock.querySelectorAll('li')]);
-const sortedList = sortBySalary(list);
-
-function creatNewListItem(data) {
+function createNewListItem(data) {
   const elem = document.createElement('li');
 
   elem.innerText = data.name || '';
   elem.dataset.position = data.position || '';
-  elem.dataset.salary = data.salary || '';
+  elem.dataset.salary = '$' + data.salary || '';
   elem.dataset.age = data.age || '';
 
   listBlock.appendChild(elem);
 }
 
-listBlock.innerHTML = '';
+function convertToNumber(string) {
+  return string.replace(/,/g, '').slice(1);
+}
 
-sortedList.forEach((empl) => {
-  creatNewListItem(empl);
-});
+function sortList(List) {
+  const sortedList = list.sort((a, b) => {
+    return convertToNumber(b.salary) - convertToNumber(a.salary);
+  });
+
+  listBlock.innerHTML = '';
+
+  sortedList.forEach((empl) => {
+    createNewListItem(empl);
+  });
+}
+
+
+
+sortList(listBlock.querySelectorAll('li'));
