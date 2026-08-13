963b375a44e08a794fe50c1dd88bdb981548d1ea
Jakub Kumorek <jkumorek01@gmail.com>
2026-08-13 12:15:08 +0200

    First Task Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const employees = document.querySelector('ul');
+
+function toNumber(string) {
+  return Number(string.replaceAll('$', '').replaceAll(',', ''));
+}
+
+function sortList(list) {
+  const array = [...list.children];
+
+  const sorted = array.sort((a, b) => {
+    return toNumber(b.dataset.salary) - toNumber(a.dataset.salary);
+  });
+
+  list.append(...sorted);
+
+  return sorted;
+}
+
+function getEmployees(people) {
+  return people.map((person) => {
+    const employee = {
+      name: person.textContent.trim(),
+      position: person.dataset.position,
+      salary: toNumber(person.dataset.salary),
+      age: Number(person.dataset.age),
+    };
+
+    return employee;
+  });
+}
+
+getEmployees(sortList(employees));
