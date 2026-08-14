08bdb1352e66cc4c3b763cecdbc81b2bb65dd85f
Olya <fialkaolya08@gmail.com>
2026-08-14 15:18:54 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+function toNumber(item) {
+  return +item.replaceAll('$', '').replaceAll(',', '');
+}
+
+function sortList(list) {
+  const masOfList = [...list.children];
+
+  masOfList.sort(
+    (a, b) => toNumber(b.dataset.salary) - toNumber(a.dataset.salary),
+  );
+
+  masOfList.forEach((li) => list.append(li));
+}
+
+function getEmployees(list) {
+  const masOfElementLi = [...list.children];
+  const masObjectsElement = masOfElementLi.map((element) => {
+    return {
+      name: element.textContent.trim(),
+      position: element.dataset.position,
+      salary: element.dataset.salary,
+      age: +element.dataset.age,
+    };
+  });
+
+  return masObjectsElement;
+}
+
+const employeesList = document.querySelector('ul');
+
+sortList(employeesList);
+getEmployees(employeesList);
