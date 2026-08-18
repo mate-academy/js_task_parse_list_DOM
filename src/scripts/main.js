b92c3f10c23fea8005d826a5114909accc811fe3
Yaroslav <your@mail.com>
2026-08-18 21:33:22 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function getSalary(item) {
+  return Number(item.dataset.salary.replace('$', '').replace(',', ''));
+}
+
+function sortList(ul) {
+  const items = [...ul.querySelectorAll('li')];
+
+  items.sort((a, b) => {
+    return getSalary(b) - getSalary(a);
+  });
+
+  ul.append(...items);
+}
+
+function getEmployees(ul) {
+  const items = [...ul.querySelectorAll('li')];
+
+  return items.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: getSalary(item),
+      age: Number(item.dataset.age),
+    };
+  });
+}
+
+sortList(list);
+getEmployees(list);
