cd7ea95246ddb2e5d08b031a297a29118e326504
Vitalii <vitaliy.doronin4@gmail.com>
2026-08-19 14:12:52 +0300

    Add a solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const listContainer = document.querySelector('ul');
+const liList = document.querySelectorAll('li');
+
+function sortList(list) {
+  const sorted = [...list].sort(
+    (a, b) =>
+      b.dataset.salary.slice(1).replaceAll(',', '') -
+      a.dataset.salary.slice(1).replaceAll(',', ''),
+  );
+
+  listContainer.innerHTML = '';
+
+  sorted.forEach((element) => {
+    listContainer.appendChild(element);
+  });
+}
+
+function getEmployees(list) {
+  const resArr = [];
+
+  list.forEach((item) => {
+    resArr.push({
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    });
+  });
+
+  return resArr;
+}
+
+sortList(liList);
+getEmployees(liList);
