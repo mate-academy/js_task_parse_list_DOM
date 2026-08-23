779c8dbd7965c490e6b8d442166c50f531e76e27
AchekcSs_ <achekcss@gmail.com>
2026-08-23 14:38:07 +0300

    feat: add employee list sorting and parsing logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+const employeeListElement = document.querySelector('ul');
+
+function sortList(list) {
+  const employees = [...list.children];
+
+  employees.sort(
+    ({ dataset: { salary: salary1 } }, { dataset: { salary: salary2 } }) => {
+      return convertToNumber(salary2) - convertToNumber(salary1);
+    },
+  );
+
+  employees.forEach((employee) => {
+    list.append(employee);
+  });
+}
+
+function convertToNumber(string) {
+  return string.slice(1).split(',').join('');
+}
+
+function getEmployees(list) {
+  const employees = [...list.children];
+  const result = [];
+
+  employees.forEach((employee) => {
+    result.push({
+      name: employee.textContent.trim(),
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    });
+  });
+
+  return result;
+}
+
+sortList(employeeListElement);
+getEmployees(employeeListElement);
