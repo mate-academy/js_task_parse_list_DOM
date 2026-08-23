6fa24756e93bc1f8e219af51c1dd9f54b8795c0c
Sumar Illya <illasumar002@gmail.com>
2026-08-23 13:22:09 +0300

    Tasks complited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('ul li');
+
+const employees = Array.from(listItems).map((li) => {
+  return {
+    name: li.textContent.trim(),
+    ...li.dataset,
+  };
+});
+
+const sortedBySalary = [...employees].sort((a, b) => {
+  return (
+    Number(b.salary.replace('$', '').replace(',', '')) -
+    Number(a.salary.replace('$', '').replace(',', ''))
+  );
+});
+
+const list = document.querySelector('ul');
+
+list.innerHTML = sortedBySalary
+  .map((employee) => {
+    return `
+  <li
+        data-position="${employee.position}"
+        data-salary="${employee.salary}"
+        data-age="${employee.age}"
+      >
+        ${employee.name}
+      </li>
+  `;
+  })
+  .join('');
