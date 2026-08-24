ee4d34f27ebbac7985466a6cd45fdfa8c66d1ba8
Artem Mayboroda <artmay77777@gmail.com>
2026-08-24 23:20:36 +0300

    Sort and parse employee list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function getSalary(salary) {
+  const cleanSalary = salary.replaceAll('$', '').replaceAll(',', '');
+
+  return Number(cleanSalary);
+}
+
+function sortList(employeesList) {
+  const listItems = [...employeesList.children].sort((itemA, itemB) => {
+    return getSalary(itemB.dataset.salary) - getSalary(itemA.dataset.salary);
+  });
+
+  employeesList.append(...listItems);
+}
+
+function getEmployees(employeesList) {
+  return [...employeesList.children].map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+}
+
+sortList(list);
+getEmployees(list);
