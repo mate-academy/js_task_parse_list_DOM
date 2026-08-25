dd1e6d0d77b605b2ec0d4049ea619deaab065fd7
Anastasi Bielikova <anastasiabelikova39@gmail.com>
2026-08-25 22:42:19 +0300

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const listElement = document.querySelector('ul');
+
+const getSalary = (li) => {
+  const salaryStr = li.dataset.salary;
+  const cleanSalary = salaryStr.replace(/\D/g, '');
+
+  return Number(cleanSalary);
+};
+
+const sortList = (list) => {
+  const items = Array.from(list.children);
+
+  items.sort((a, b) => getSalary(b) - getSalary(a));
+
+  list.append(...items);
+};
+
+const getEmployees = (list) => {
+  const result = [];
+
+  for (const li of list.children) {
+    result.push({
+      name: li.textContent.trim(),
+      position: li.dataset.position,
+      salary: getSalary(li),
+      age: Number(li.dataset.age),
+    });
+  }
+
+  return result;
+};
+
+sortList(listElement);
+
+getEmployees(listElement);
