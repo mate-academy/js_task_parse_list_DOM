5b54bb5f86a04170f9d7039cf11353ed33da9c95
Alex Sinchuk <120649140+alexsinchuk@users.noreply.github.com>
2026-08-27 12:05:14 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write code here
+const listEmployees = document.querySelector('ul');
+
+function parseSalary(salary) {
+  return Number(salary.replace(/[$,]/g, ''));
+}
+
+function sortList(list) {
+  const items = [...list.querySelectorAll('li')];
+
+  items.sort((a, b) => {
+    if (parseSalary(a.dataset.salary) === parseSalary(b.dataset.salary)) {
+      return 0;
+    }
+
+    if (parseSalary(a.dataset.salary) > parseSalary(b.dataset.salary)) {
+      return -1;
+    }
+
+    return 1;
+  });
+
+  items.forEach((item) => {
+    list.appendChild(item);
+  });
+}
+
+function getEmployees(list) {
+  const items = [...list.querySelectorAll('li')];
+
+  const employees = items.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: parseSalary(item.dataset.salary),
+      age: Number(item.dataset.age),
+    };
+  });
+
+  return employees;
+}
+
+sortList(listEmployees);
+
+getEmployees(listEmployees);
