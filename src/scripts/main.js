ea726c9369c696c4aa80cb8bff517dc1b302688a
Your Name <you@example.com>
2026-08-30 12:57:07 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const List = document.querySelector('ul');
+const list = Array.from(document.querySelectorAll('li'));
+const salariesMap = new Map();
+
+list.forEach((item) => {
+  const value = +item.getAttribute('data-salary').replace(/[$,]/g, '');
+
+  salariesMap.set(item, value);
+});
+
+const sortedSalaries = Array.from(salariesMap.entries());
+
+sortedSalaries.sort((a, b) => b[1] - a[1]);
+
+for (let i = 0; i < sortedSalaries.length; i++) {
+  List.appendChild(sortedSalaries[i][0]);
+}
