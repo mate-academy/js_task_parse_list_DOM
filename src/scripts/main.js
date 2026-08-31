b2f3dbcfbed30b59110fec3603f44cf10acfca82
Ruslan Sydorenko <letovletov111@gmail.com>
2026-08-31 18:02:38 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
+/* eslint-disable function-paren-newline */
 'use strict';
 
-// write code here
+// eslint-disable-next-line max-len
+const cleanSalary = (str) =>
+  Number(str.replaceAll('$', '').replaceAll(',', ''));
+
+function sortList(list) {
+  const workers = Array.from(list.querySelectorAll('li'));
+
+  workers.sort((worker1, worker2) => {
+    const salary1 = cleanSalary(worker1.dataset.salary);
+    const salary2 = cleanSalary(worker2.dataset.salary);
+
+    return salary2 - salary1;
+  });
+
+  list.append(...workers);
+}
+
+function getEmployees(list) {
+  const workers = Array.from(list.querySelectorAll('li'));
+
+  return workers.map((worker) => {
+    return {
+      name: worker.dataset.name,
+      position: worker.dataset.position,
+      salary: cleanSalary(worker.dataset.salary),
+      age: Number(worker.dataset.age),
+    };
+  });
+}
+
+const foundList = document.querySelector('ul');
+
+sortList(foundList);
+getEmployees(foundList);
