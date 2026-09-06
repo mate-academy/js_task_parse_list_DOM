8864c01b3c28c4183b6d444adffeebbd57412936
Kristina Kovalchuk <kristina36247@gmail.com>
2026-09-06 19:09:34 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function sortList(listElement) {
+  const sortedList = Array.from(listElement.children).sort((a, b) => {
+    const salaryA = Number((a.dataset.salary || '').replace(/,/g, ''));
+    const salaryB = Number((b.dataset.salary || '').replace(/,/g, ''));
+
+    return salaryB - salaryA;
+  });
+
+  sortedList.forEach((li) => {
+    listElement.append(li);
+  });
+}
+
+function getEmployees(listElement) {
+  return Array.from(list.children).map((li) => {
+    return {
+      name: li.textContent.trim(),
+      salary: Number((li.dataset.salary || '').replace(/,/g, '')),
+    };
+  });
+}
+
+sortList(list);
+
+getEmployees(list);
