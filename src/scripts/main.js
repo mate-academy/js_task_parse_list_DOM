5c3674c457d06a90049d5267610743c0bdc955f1
unknown <marian.fedun3423@gmail.com>
2026-09-08 15:20:46 +0200

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
+function sortList() {
+const list = document.querySelector('ul');
+const workers = [...document.querySelectorAll('li[data-salary]')];
+
+workers.sort((a, b) => {
+  const salaryA = Number(a.dataset.salary.slice(1).split(',').join(''));
+  const salaryB = Number(b.dataset.salary.slice(1).split(',').join(''));
+
+  return salaryB - salaryA;
+});
+
+workers.forEach((worker) => {
+  list.append(worker);
+});
+}
 
 // write code here
