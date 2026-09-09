4f5b1c552a29882ccd8348a90f086b9e8b07e5d3
Inna Haichenia <gajchenja2424@ukr.net>
2026-09-09 10:24:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+function getSalaryNumber(salaryStr) {
+  const cleanString = salaryStr.replace('$', '').replaceAll(',', '');
+
+  return Number(cleanString);
+}
+
+function sortList(listElement) {
+  const items = [...listElement.children];
+
+  items.sort((a, b) => {
+    const salaryA = getSalaryNumber(a.dataset.salary);
+    const salaryB = getSalaryNumber(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  listElement.append(...items);
+}
+
+function getEmployees(listElement) {
+  const items = [...listElement.children];
+
+  return items.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: Number(item.dataset.age),
+    };
+  });
+}
+
+const list = document.querySelector('ul');
+
+if (list) {
+  sortList(list);
+  getEmployees(list);
+}
