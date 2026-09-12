5ca846d3c4b158f8d730a87ddb0f5398060f3628
Max <143881777+ZloyHoma@users.noreply.github.com>
2026-09-12 15:27:28 +0300

    *

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
 // write code here
+const outerList = document.querySelectorAll('li');
+
+function sortList(list) {
+  const sortedList = [...list];
+
+  sortedList.sort((a, b) => {
+    const salaryA = transformSalary(a.dataset.salary);
+    const salaryB = transformSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  const ul = document.querySelector('ul');
+
+  sortedList.forEach((li) => ul.appendChild(li));
+}
+
+function transformSalary(salary) {
+  const value = salary.slice(1).replaceAll(',', '');
+
+  return Number(value);
+}
+
+function getEmployees(list) {
+  const employees = [...list].map((li) => {
+    return {
+      ...li.dataset,
+      name: li.textContent.trim(),
+    };
+  });
+
+  return employees;
+}
+
+sortList(outerList);
+getEmployees(outerList);
