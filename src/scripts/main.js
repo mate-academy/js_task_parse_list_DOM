8ec8f98aecbea44967c399078f51a3f4c9d322a0
Veronika Nychyporuk <veronikanychyporuk@gmail.com>
2026-09-16 00:04:51 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+function sortEmployees(list) {
+  const listElement = document.querySelector('ul');
+  const newListElement = document.createElement('ul');
+  const employees = [...list];
+
+  employees.sort(
+    (employee1, employee2) =>
+      Number(employee2.dataset.salary.replaceAll('$', '').replaceAll(',', '')) -
+      Number(employee1.dataset.salary.replaceAll('$', '').replaceAll(',', '')),
+  );
+
+  for (const employee of employees) {
+    newListElement.append(employee);
+  }
+
+  listElement.replaceWith(newListElement);
+}
+
+function getEmployees(list) {
+  const employees = [];
+
+  for (const employee of list) {
+    employees.push({
+      name: employee.textContent.trim(),
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    });
+  }
+
+  return employees;
+}
+
+const employeeElementsList = document.querySelectorAll('li');
+
+sortEmployees(employeeElementsList);
+getEmployees(employeeElementsList);
